ffecea4ac2df84287992aef199f215a458c5233a
Masoudas <Masoudas@hotmai.com>
2020-01-08 10:22:55 +0100

    Create PathFactory

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/PathFactory.java
@@ -0,0 +1,19 @@
+package fr.fresnel.fourPolar.io;
+
+import java.io.File;
+import java.nio.file.Paths;
+
+/**
+ * This class generates the paths we need to store or read data, using the path
+ * to the root folder.
+ */
+public class PathFactory {
+    /**
+     * Returns the path to the 0_Params folder.
+     * @param rootFolder
+     * @return
+     */
+    public static File getFolder_0_Params(File rootFolder) {
+        return new File(rootFolder, Paths.get("4Polar", "0_Params").toString());
+    }
+}
