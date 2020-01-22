9c2fcfeae0a799d76fcfbcece116448033c9fcec
Masoudas <Masoudas@hotmai.com>
2020-01-22 10:35:10 +0100

    Add single rejected image

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imageSet/acquisition/RejectedCapturedImage.java
@@ -0,0 +1,38 @@
+package fr.fresnel.fourPolar.core.imageSet.acquisition;
+
+import java.io.File;
+
+/**
+ * A class for keeping a captured image that is not compatible with our format.
+ */
+public class RejectedCapturedImage {
+    private File _image;
+    private String _reason;
+
+    /**
+     * Set the file that has been rejected together with the reason.
+     * @param image
+     * @param reason
+     */
+    public void set(File image, String reason) {
+        this._image = image;
+        this._reason = reason;
+    }
+
+    /**
+     * Returns the file that was rejected.
+     * @return
+     */
+    public File getFile(){
+        return this._image;
+    }
+
+    /**
+     * Returns the reason why this image was rejected as a string.
+     * @return
+     */
+    public String getReason(){
+        return this._reason;
+    }
+    
+}
