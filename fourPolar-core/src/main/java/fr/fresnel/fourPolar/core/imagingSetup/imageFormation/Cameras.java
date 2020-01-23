4935ab59d0e8df416b0761a8a8f0115c2e89d02b
Masoudas <Masoudas@hotmai.com>
2020-01-23 14:08:05 +0100

    use static array so as not regenerate labels

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/Cameras.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/Cameras.java
@@ -3,45 +3,33 @@ package fr.fresnel.fourPolar.core.imagingSetup.imageFormation;
 public enum Cameras {
     One, Two, Four;
 
-    public static String[] getLabels(Cameras camera) {
-       String[] labels = null;
+    // One camera case
+    private static String[] _one = {"Pol0_45_90_135"};
 
-        if (camera == One)
-        {
-            labels = new String[1];
-            labels[0] = "Pol0_45_90_135";
+    // Two camera case
+    private static String[] _two = {"Pol0_90", "Pol45_135"};
+
+    // Four camera case
+    private static String[] _four = {"Pol0", "Pol45", "Pol90", "Pol135"};
+
+    public static String[] getLabels(Cameras camera) {
+        switch (camera) {
+        case One:
+            return _one;
+
+        case Two:
+            return _two;
+
+        case Four:
+            return _four;
+        
+        default:
+            return null;
         }
-        else if (camera == Two)
-        {
-            labels = new String[2];
-            labels[0] = "Pol0_90";
-            labels[1] = "Pol45_135";
-        }
-        else if (camera == Four)
-        {
-            labels = new String[4];
-            labels[0] = "Pol0";
-            labels[1] = "Pol45";
-            labels[2] = "Pol90";
-            labels[3] ="Pol135";
-        }
-            
-        return labels;
     }
 
     public static int getNImages(Cameras camera) {
-        if (camera == One)
-        {
-            return 1;
-        }
-        else if (camera == Two)
-        {
-            return 2;
-        }
-        else 
-        {
-            return 4;
-        }
+        return getLabels(camera).length;
     }
 
 }
