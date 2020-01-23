0daff9f06d354b790b196b1662dfe20d680bb723
Masoudas <Masoudas@hotmai.com>
2020-01-23 12:57:42 +0100

    Define interface

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/ICapturedImageReader.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.io.image;
+
+import java.io.IOException;
+
+import fr.fresnel.fourPolar.core.image.captured.ICapturedImage;
+import fr.fresnel.fourPolar.core.imageSet.acquisition.ICapturedImageFileSet;
+
+/**
+ * An interface for reading the captured image.
+ */
+
+public interface ICapturedImageReader {
+    /**
+     * Read an image of the file set labeled by fileLabel;
+     * @param fileSet
+     * @param fileLabel : The file label of the image to be read. Should be from {@link Cameras} 
+     * @return
+     * @throws IllegalArgumentException : thrown in case the given fileLabel is not in the file set.
+     * @throws IOException : In case of file IO issues.
+     */
+    public ICapturedImage read(ICapturedImageFileSet fileSet, String fileLabel) throws IllegalArgumentException, IOException;
+    
+}
