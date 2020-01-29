7f02f021a64a88e40339dacd1f8878d730734bf6
Masoudas <Masoudas@hotmail.com>
2020-01-29 17:50:28 +0100

    Model XYZ direction of dipoles

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/dipole/DipoleDirection.java
@@ -0,0 +1,8 @@
+package fr.fresnel.fourPolar.core.physics.dipole;
+
+/**
+ * This enumeration indicates the directions of the dipole in the XYZ space.
+ */
+public enum DipoleDirection {
+    XX, YY, ZZ, XY,
+}
