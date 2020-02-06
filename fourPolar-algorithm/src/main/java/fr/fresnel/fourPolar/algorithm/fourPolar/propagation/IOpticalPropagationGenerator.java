f0445cdc6e8ca9454eb375b9fad03f07aff1cb87
Masoudas <Masoudas@hotmail.com>
2020-02-06 13:47:52 +0100

    add body

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/fourPolar/propagation/IOpticalPropagationGenerator.java
@@ -0,0 +1,24 @@
+package fr.fresnel.fourPolar.algorithm.fourPolar.propagation;
+
+import fr.fresnel.fourPolar.algorithm.fourPolar.propagation.exceptions.fourPolar.propagation.CannotComputePropagationFactor;
+import fr.fresnel.fourPolar.algorithm.fourPolar.propagation.exceptions.fourPolar.propagation.OpticalPropagationNotInvertible;
+import fr.fresnel.fourPolar.core.physics.channel.IPropagationChannel;
+import fr.fresnel.fourPolar.core.physics.propagation.IOpticalPropagation;
+
+/**
+ * The interface for generating the optical propagation of a propagation
+ * channel.
+ */
+public interface IOpticalPropagationGenerator {
+    /**
+     * Generate the optical propagation for the given propagation channel.
+     * 
+     * @param channel
+     * @return
+     * @throws CannotComputePropagationFactor
+     * @throws OpticalPropagationNotInvertible
+     */
+    public IOpticalPropagation generate(IPropagationChannel channel)
+            throws CannotComputePropagationFactor, OpticalPropagationNotInvertible;
+
+}
---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/fourPolar/propagation/OpticalPropagationGenerator.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.algorithm.fourPolar.propagation;
+
+import fr.fresnel.fourPolar.algorithm.fourPolar.propagation.exceptions.fourPolar.propagation.CannotComputePropagationFactor;
+import fr.fresnel.fourPolar.algorithm.fourPolar.propagation.exceptions.fourPolar.propagation.OpticalPropagationNotInvertible;
+import fr.fresnel.fourPolar.core.physics.channel.IPropagationChannel;
+import fr.fresnel.fourPolar.core.physics.propagation.IOpticalPropagation;
+
+/**
+ * This class generates the optical propagation for the given propagation
+ * channel.
+ */
+public class OpticalPropagationGenerator implements IOpticalPropagationGenerator {
+
+    @Override
+    public IOpticalPropagation generate(IPropagationChannel channel)
+            throws CannotComputePropagationFactor, OpticalPropagationNotInvertible {
+        // TODO Auto-generated method stub
+        return null;
+    }
+    
+    
+
+}
