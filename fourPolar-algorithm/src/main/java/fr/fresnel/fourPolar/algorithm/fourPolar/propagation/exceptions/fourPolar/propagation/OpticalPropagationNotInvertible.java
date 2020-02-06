8402ea65d51e4289679336becb92342684127d24
Masoudas <Masoudas@hotmail.com>
2020-02-06 13:01:18 +0100

    exception for calculation of optical prop

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/fourPolar/propagation/exceptions/fourPolar/propagation/OpticalPropagationNotInvertible.java
@@ -0,0 +1,14 @@
+package fr.fresnel.fourPolar.algorithm.fourPolar.propagation.exceptions.fourPolar.propagation;
+
+/**
+ * Thrown when a calculated optical propagation does not have inverse.
+ */
+public class OpticalPropagationNotInvertible extends Exception{
+
+    /**
+     *
+     */
+    private static final long serialVersionUID = 1542352352543218989L;
+
+    
+}
