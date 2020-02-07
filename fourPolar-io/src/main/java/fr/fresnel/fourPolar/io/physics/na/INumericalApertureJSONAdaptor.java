8364afabc7bd9a1cd2f6cfc2a763b384ad234c7d
Masoudas <Masoudas@hotmail.com>
2020-02-07 16:38:37 +0100

    raise null exeption in io

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/physics/na/INumericalApertureJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/physics/na/INumericalApertureJSONAdaptor.java
@@ -1,31 +1,36 @@
 package fr.fresnel.fourPolar.io.physics.na;
 
+import java.io.IOException;
+
 import com.fasterxml.jackson.annotation.JsonProperty;
 import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 
+import fr.fresnel.fourPolar.core.exceptions.physics.na.NumericalApertureOutOfRange;
 import fr.fresnel.fourPolar.core.physics.na.INumericalAperture;
 import fr.fresnel.fourPolar.core.physics.na.NumericalAperture;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
- * This class is used as an adaptor of {@link INumericalAperture} adaptor to yaml.
+ * This class is used as an adaptor of {@link INumericalAperture} adaptor to
+ * yaml.
  */
-@JsonPropertyOrder({"pol0", "pol45", "pol90", "pol135"})
+@JsonPropertyOrder({ "pol0", "pol45", "pol90", "pol135" })
 public class INumericalApertureJSONAdaptor {
     @JsonProperty("pol0")
-    private double _pol0;
+    private Double _pol0;
 
     @JsonProperty("pol45")
-    private double _pol45;
+    private Double _pol45;
 
     @JsonProperty("pol90")
-    private double _pol90;
+    private Double _pol90;
 
     @JsonProperty("pol135")
-    private double _pol135;
+    private Double _pol135;
 
     /**
      * Allows the interface to be written to JSON as an instance of this class.
+     * 
      * @param na
      */
     public void toJSON(INumericalAperture na) {
@@ -37,10 +42,12 @@ public class INumericalApertureJSONAdaptor {
 
     /**
      * Returns the interface from the JSON.
+     * 
      * @return
+     * @throws IOException
      */
-    public INumericalAperture fromJSON() {
-        return new NumericalAperture(this._pol0, this._pol45, this._pol90, this._pol135);
+    public INumericalAperture fromJSON() throws IOException {
+        return new NumericalAperture(_getPol0(), _getPol45(), _getPol90(), _getPol135());
     }
 
     private void _setPol0(INumericalAperture na) {
@@ -54,9 +61,43 @@ public class INumericalApertureJSONAdaptor {
     private void _setPol90(INumericalAperture na) {
         _pol90 = na.getNA(Polarization.pol90);
     }
-    
+
     private void _setPol135(INumericalAperture na) {
         _pol135 = na.getNA(Polarization.pol135);
+    }
+
+    private double _getPol0() throws IOException {
+        if (_pol0 == null) {
+            throw new IOException(_getNotExistsMessage(Polarization.pol0));
+        }
+        return _pol0;
+    }
+
+    private double _getPol45() throws IOException {
+        if (_pol45 == null) {
+            throw new IOException(_getNotExistsMessage(Polarization.pol45));
+        }
+
+        return _pol45;
+    }
+
+    private double _getPol90() throws IOException {
+        if (_pol90 == null) {
+            throw new IOException(_getNotExistsMessage(Polarization.pol90));
+        }
+
+        return _pol90;
+    }
+
+    private double _getPol135() throws IOException {
+        if (_pol135 == null) {
+            throw new IOException(_getNotExistsMessage(Polarization.pol90));
+        }
+        return _pol135;
+    }
+
+    private String _getNotExistsMessage(Polarization pol) {
+        return "Numerical aperture for " + pol + " is not found on the disk";
     }
 
 }
