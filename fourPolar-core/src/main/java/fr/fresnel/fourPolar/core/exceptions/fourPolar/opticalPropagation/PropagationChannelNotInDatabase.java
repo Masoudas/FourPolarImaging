3455d3fc2a2f5cb68aba8c8617b38354ac8e33c8
Masoudas <Masoudas@hotmail.com>
2020-02-12 10:38:45 +0100

    put inside OpticalPropagation module

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/exceptions/fourPolar/opticalPropagation/PropagationChannelNotInDatabase.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.core.exceptions.fourPolar.opticalPropagation;
+
+/**
+ * The exception that is thrown in case there's no {@link OpticalPropagation} is
+ * not in the database of optical propagations.
+ */
+public class PropagationChannelNotInDatabase extends Exception {
+    private static final long serialVersionUID = 4234549789843L;
+    private final String _message;
+
+    public PropagationChannelNotInDatabase() {
+        _message = "No optical propagation is defined for the given propagation channel and numerical aperture in the database.";
+    }
+
+    public PropagationChannelNotInDatabase(String message) {
+        _message = message;
+    }
+
+    @Override
+    public String getMessage() {
+        return _message;
+    }
+}
