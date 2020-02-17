48a7a63b02a270d0fbe6426b665c1383a969ef50
Masoudas <Masoudas@hotmail.com>
2020-02-17 16:17:30 +0100

    implement pixel interface and class

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/IPixel.java
@@ -0,0 +1,20 @@
+package fr.fresnel.fourPolar.core.image.generic;
+
+/**
+ * An interface that models the pixels of an image.
+ * The generic type takes the pixel types.
+ */
+public interface IPixel<T> {
+    /**
+     * Returns the pixel coordinates in the image as a long array,
+     * as [row, column, ...]
+     */
+    public long[] localize();
+
+    /**
+     * Returns the value associated with the pixel.
+     */
+    public T value();
+
+    
+}
---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/Pixel.java
@@ -0,0 +1,30 @@
+package fr.fresnel.fourPolar.core.image.generic;
+
+/**
+ * Represents the pixel entity, and implements the {@link IPixel}.
+ */
+public class Pixel<T> implements IPixel<T> {
+    T _value;
+    long[] _location;
+
+    /**
+     * Represents the pixel entity, and implements the {@link IPixel}.
+     * @param location
+     * @param value
+     */
+    public Pixel(long[] location, T value) {
+        this._location = location;
+        this._value = value;
+    }
+
+    @Override
+    public long[] localize() {
+        return _location;
+    }
+
+    @Override
+    public T value() {
+        return _value;
+    }
+
+}
