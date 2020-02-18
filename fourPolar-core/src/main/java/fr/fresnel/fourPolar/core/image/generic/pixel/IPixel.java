4531cd9a6e32289266c2605083a98d7146e0ddcb
Masoudas <Masoudas@hotmail.com>
2020-02-18 09:26:13 +0100

    add documentation

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/IPixel.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/IPixel.java
@@ -3,12 +3,14 @@ package fr.fresnel.fourPolar.core.image.generic.pixel;
 /**
  * An interface that models the pixels of an image.
  * The generic type takes the pixel types.
+ *
+ * @param <T> is the pixel type.
  */
 public interface IPixel<T> {
     /**
-     * Returns the value associated with the pixel.
+     * Returns the value associated with the pixel. This value would be the actual
+     * underlying pixel, hence it can be modified directly.
      */
     public T value();
-
     
 }
