9c1334930c7366f827a53212f91fa99826c8ac2e
Masoudas <Masoudas@hotmail.com>
2020-02-19 11:40:50 +0100

    sketch and implement pixel model

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/IPixel.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/IPixel.java
@@ -1,16 +1,17 @@
 package fr.fresnel.fourPolar.core.image.generic.pixel;
 
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
 /**
- * An interface that models the pixels of an image.
- * The generic type takes the pixel types.
- *
- * @param <T> is the pixel type.
+ * An interface that models the pixels of an image. The generic type takes the
+ * pixel types.
+ * 
+ * @param <T> is a {@code PixelType} 
  */
-public interface IPixel<T> {
+public interface IPixel<T extends PixelType> {
     /**
      * Returns the value associated with the pixel. This value would be the actual
      * underlying pixel, hence it can be modified directly.
      */
     public T value();
-    
 }
--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/Pixel.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/pixel/Pixel.java
@@ -1,9 +1,11 @@
 package fr.fresnel.fourPolar.core.image.generic.pixel;
 
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
 /**
  * Represents the pixel entity, and implements the {@link IPixel}.
  */
-public class Pixel<T> implements IPixel<T> {
+public class Pixel<T extends PixelType> implements IPixel<T> {
     T _value;
 
     /**
