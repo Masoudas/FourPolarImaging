f0fd707c1fb745e18c6d55b9b78d092158919d1a
Masoudas <Masoudas@hotmail.com>
2020-02-19 11:40:02 +0100

    sketch our image

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/Image.java
@@ -0,0 +1,28 @@
+package fr.fresnel.fourPolar.core.image.generic;
+
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
+public interface Image<T extends PixelType> {
+    /**
+     * Returns the dimensions of the underlying image as [columns, rows, ...]
+     * 
+     * @return
+     */
+    public long[] getDims();
+
+    /**
+     * Returns the interface for iterating over the pixels of an image in an ordered
+     * fashion.
+     * 
+     * @return
+     */
+    public IPixelCursor getCursor();
+
+    /**
+     * Returns the interface for randomly accessing the pixels of an image.
+     * 
+     * @return
+     */
+    public IPixelRandomAccess getRandomAccess();
+
+}
