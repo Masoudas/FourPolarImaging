106c44a1730f99fcbf58dcdf87d3c3797e8f27dc
Masoudas <Masoudas@hotmail.com>
2020-02-19 17:20:07 +0100

    get Type from converter

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImgLib2Image.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImgLib2Image.java
@@ -15,8 +15,6 @@ import net.imglib2.Interval;
 import net.imglib2.RandomAccess;
 import net.imglib2.img.Img;
 import net.imglib2.img.ImgFactory;
-import net.imglib2.img.array.ArrayImgFactory;
-import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.type.NativeType;
 
 /**
@@ -30,21 +28,20 @@ class ImgLib2Image<U extends PixelType, V extends NativeType<V>> implements Imag
 
     private final TypeConverter<V> _tConverter;
 
-    private final U _pixelType;
+    private final Type _pixelType;
 
     /**
      * This constructor is works as a wrapper from ImgLib2 type to our type.
      * 
      * @param img       is the ImgLib2 interface.
      * @param type      is the data type of ImgLib2.
-     * @param pixelType is the pixel type associated with the {@code Image}.
      * @throws ConverterNotFound is thrown in case conversion to our data types is
      *                           not supported.
      */
-    public ImgLib2Image(final Img<V> img, V type, U pixelType) throws ConverterNotFound {
+    public ImgLib2Image(final Img<V> img, V type) throws ConverterNotFound {
         this._img = img;
         this._tConverter = TypeConverterFactory.create(type);
-        this._pixelType = pixelType;
+        this._pixelType = _tConverter.getPixel(type).getType();
     }
 
     @Override
@@ -132,7 +129,7 @@ class ImgLib2Image<U extends PixelType, V extends NativeType<V>> implements Imag
 
     @Override
     public Type getPixelType() {
-        return _pixelType.getType();
+        return _pixelType;
     }
 
 }
