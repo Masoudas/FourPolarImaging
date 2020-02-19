5ac41ffacb535d6f5fb4209fe46d96ca2c309b82
Masoudas <Masoudas@hotmail.com>
2020-02-19 16:52:41 +0100

    add factory interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/ImageFactory.java
@@ -0,0 +1,11 @@
+package fr.fresnel.fourPolar.core.image.generic;
+
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
+/**
+ * Interface for factories of Image implementations.
+ */
+public interface ImageFactory<T extends PixelType> {
+    public Image<T> create();
+    
+}
