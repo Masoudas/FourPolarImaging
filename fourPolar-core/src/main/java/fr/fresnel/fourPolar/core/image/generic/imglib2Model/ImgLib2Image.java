132ba05bb976290c22aba92266790b5f367dfe52
Masoudas <Masoudas@hotmail.com>
2020-02-19 11:41:41 +0100

    implement our image using imglib2

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImgLib2Image.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImgLib2Image.java
@@ -2,9 +2,13 @@ package fr.fresnel.fourPolar.core.image.generic.imglib2Model;
 
 import java.util.Iterator;
 
-import fr.fresnel.fourPolar.core.image.generic.IPixelIterator;
+import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.types.ConverterNotFound;
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.imglib2Model.types.TypeConverter;
+import fr.fresnel.fourPolar.core.image.generic.imglib2Model.types.TypeConverterFactory;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 import net.imglib2.Cursor;
 import net.imglib2.Interval;
 import net.imglib2.RandomAccess;
@@ -14,32 +18,34 @@ import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.img.cell.CellImgFactory;
 import net.imglib2.type.NativeType;
 
-
-class ImgLib2Image<U, V extends NativeType<V>> implements Image<U>, Img<V> {
+class ImgLib2Image<U extends PixelType, V extends NativeType<V>> implements Image<U>, Img<V> {
     /**
-     * This is the threshold for the number of pixels, after which we opt
-     * for a cell image rather than an array image.
-     * This is roughly 256MB for an image of type float.
+     * This is the threshold for the number of pixels, after which we opt for a cell
+     * image rather than an array image. This is roughly 256MB for an image of type
+     * float.
      */
-    private static long _staticImageThr = 256 * 256 * 1024; 
+    private static long _staticImageThr = 256 * 256 * 1024;
 
     private final Img<V> _img;
 
-    public ImgLib2Image(final Img<V> img) {
-        _img = img;
+    private final TypeConverter<V> _tConverter;
+
+
+    public ImgLib2Image(final Img<V> img, V v) throws ConverterNotFound {
+        this._img = img;
+        this._tConverter = TypeConverterFactory.create(v);
     }
 
-    public ImgLib2Image(final long[] dim, V v) {
+    public ImgLib2Image(final long[] dim, V v) throws ConverterNotFound {
+        this._tConverter = TypeConverterFactory.create(v);
         long nPixels = _getNPixels(dim);
 
-        if (nPixels < _staticImageThr){
+        if (nPixels < _staticImageThr) {
             _img = new ArrayImgFactory<V>(v).create(dim);
-        }
-        else{
+        } else {
             _img = new CellImgFactory<V>(v).create(dim);
         }
     }
-
 
     @Override
     public RandomAccess<V> randomAccess() {
@@ -115,14 +121,7 @@ class ImgLib2Image<U, V extends NativeType<V>> implements Image<U>, Img<V> {
     }
 
     @Override
-    public IPixelIterator getIterator() {
-        // TODO Auto-generated method stub
-        return null;
-    }
-
-    @Override
     public IPixelRandomAccess getRandomAccess() {
-        // TODO Auto-generated method stub
         return null;
     }
 
@@ -133,6 +132,11 @@ class ImgLib2Image<U, V extends NativeType<V>> implements Image<U>, Img<V> {
         }
 
         return nPixels;
+    }
+
+    @Override
+    public IPixelCursor getCursor() {
+        return new ImgLib2PixelCursor<V>(this._img.cursor(), this.numDimensions(), this._tConverter);
     }
 
 }
