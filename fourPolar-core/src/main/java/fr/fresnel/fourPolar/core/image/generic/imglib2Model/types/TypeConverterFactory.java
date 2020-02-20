d29af3845bea744a7f2e517c3f576768404209b5
Masoudas <Masoudas@hotmail.com>
2020-02-20 13:50:35 +0100

    Add a method to return converter based on our type

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/types/TypeConverterFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/types/TypeConverterFactory.java
@@ -1,6 +1,8 @@
 package fr.fresnel.fourPolar.core.image.generic.imglib2Model.types;
 
 import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.types.ConverterNotFound;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.float32;
 import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
@@ -35,6 +37,37 @@ public class TypeConverterFactory {
             throw new ConverterNotFound();
         }
 
+        return converter;
+    }
+
+    /**
+     * Returns the suitable converter based on the given {@code PixelType}.
+     * 
+     * @param <T> is a native type of ImgLib2.
+     * @param pixelType is the {@ PixelType} for which we seek to find a converter.
+     * @return an instance of {@code TypeConverter} 
+     * @throws ConverterNotFound in case the given type has no suitable converter.
+     */
+    public static <T extends NativeType<T>> TypeConverter<T> getConverter(PixelType pixelType) throws ConverterNotFound {
+        TypeConverter<T> converter = null;
+
+        switch (pixelType.getType()) {
+            case FLOAT_32:
+                converter = (TypeConverter<T>) new FloatTypeConverter();    
+                break;
+
+            case UINT_16:
+                converter = (TypeConverter<T>) new UnsignedShortTypeConverter();
+                break;
+
+            case RGB_16:
+                converter = (TypeConverter<T>) new ARGBConverter();
+                break;                
+        
+            default:
+                throw new ConverterNotFound();
+        }
+        
         return converter;
     }
 }
