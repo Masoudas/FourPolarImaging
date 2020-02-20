b9127838259a26e4203d3ce6ddb2d5d6ab44d4bd
Masoudas <Masoudas@hotmail.com>
2020-02-20 09:29:57 +0100

    Document the class methods

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImageToImgLib2Converter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imglib2Model/ImageToImgLib2Converter.java
@@ -1,20 +1,27 @@
 package fr.fresnel.fourPolar.core.image.generic.imglib2Model;
 
 import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.float32;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.uint16;
+
 import net.imglib2.img.Img;
-import net.imglib2.type.NativeType;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 
 /**
- * ImageToImgLib2Converter
+ * Using this class, we can convert an {@code Image} interface to a {@code Img} interface.
  */
 public class ImageToImgLib2Converter {
+    /**
+     * This method transforms {@code Image} of type uint16 to {@code Img} of type
+     * UnsignedShortType.
+     * 
+     * @param image is the {@code Image} instance. 
+     * @param pixelType is the pixel type associated with the image.
+     * @return
+     */
     public static Img<UnsignedShortType> getImg(Image<uint16> image, uint16 pixelType) {
         Img<UnsignedShortType> img = null;
 
@@ -25,6 +32,14 @@ public class ImageToImgLib2Converter {
         return img;
     }
 
+    /**
+     * This method transforms {@code Image} of type float32 to {@code Img} of type
+     * FloatType.
+     * 
+     * @param image is the {@code Image} instance. 
+     * @param pixelType is the pixel type associated with the image.
+     * @return
+     */
     public static Img<FloatType> getImg(Image<float32> image, float32 pixelType) {
         Img<FloatType> img = null;
 
@@ -35,6 +50,14 @@ public class ImageToImgLib2Converter {
         return img;
     }
 
+    /**
+     * This method transforms {@code Image} of type RGB16 to {@code Img} of type
+     * ARGBType.
+     * 
+     * @param image is the {@code Image} instance. 
+     * @param pixelType is the pixel type associated with the image.
+     * @return
+     */
     public static Img<ARGBType> getImg(Image<RGB16> image, RGB16 pixelType) {
         Img<ARGBType> img = null;
 
@@ -44,38 +67,4 @@ public class ImageToImgLib2Converter {
 
         return img;
     }
-
-    // public static <U extends NativeType<U>, V extends PixelType> Img<U> getImg(Image<V> image) {
-    //     Img<U> img = null;
-
-    //     if (image instanceof ImgLib2Image) {
-    //         img = _getByCast(image);
-    //     }
-
-    //     return img;
-
-    // }
-
-    // private static <U extends NativeType<U>, V extends PixelType> Img<U>
-    // _getByCast(Image<V> image) {
-    // Img<U> img;
-    // switch (image.getPixelType()) {
-    // case UINT_16:
-    // img = (Img<UnsignedShortType>)image;
-    // break;
-
-    // case FLOAT_32:
-
-    // break;
-
-    // case RGB_16:
-
-    // break;
-
-    // default:
-    // break;
-    // }
-
-    // }
-
 }
