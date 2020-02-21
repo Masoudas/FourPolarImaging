d50fdde63e098f8c6a1d9a9a3507c2eb2c2a6fb6
Masoudas <Masoudas@hotmail.com>
2020-02-21 17:03:18 +0100

    add test method for ImgLib2Image

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2ImageTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2ImageTest.java
@@ -1,65 +1,42 @@
 package fr.fresnel.fourPolar.core.image.generic.imgLib2Model;
 
-import static org.junit.jupiter.api.Assertions.assertArrayEquals;
-import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
-import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.types.ConverterNotFound;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import net.imglib2.img.Img;
-import net.imglib2.img.array.ArrayImg;
-import net.imglib2.img.cell.CellImg;
+import net.imglib2.img.array.ArrayImgFactory;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
 
 public class ImgLib2ImageTest {
     @Test
-    public void getDimensions_UINT16Image_CreatesImageOfSameDimension() {
-        long[] dimensions = new long[] { 1, 1, 1, 1, 1 };
-        Image<UINT16> image = new ImgLib2ImageFactory<UINT16>().create(dimensions, new UINT16());
-        
-        assertArrayEquals(image.getDimensions(), dimensions);
+    public void imgInterfaceMethods_UnsignedShortImage_AllMethodsShouldReturnTheSameResult() throws ConverterNotFound {
+        long[] dimensions = new long[] { 1, 1, 1, 1 };
+        UnsignedShortType type = new UnsignedShortType();
+        Img<UnsignedShortType> img = new ArrayImgFactory<UnsignedShortType>(type).create(dimensions);
+
+        ImgLib2Image<UINT16, UnsignedShortType> image = new ImgLib2Image<UINT16, UnsignedShortType>(img, type);
+
+
+        assertTrue(// img.cursor().hashCode() == image.cursor().hashCode() && 
+            img.dimension(0) == image.dimension(0) && 
+            img.factory() == image.factory() &&
+            img.firstElement() == image.firstElement() &&
+            img.iterationOrder() == image.iterationOrder() &&
+            img.iterator() == image.iterator() &&
+            img.localizingCursor() == image.localizingCursor() &&
+            img.max(1) == image.max(1) &&
+            img.min(1) == image.min(1) &&
+            img.numDimensions() == image.numDimensions() &&
+            img.randomAccess() == image.randomAccess() &&
+            img.realMax(1) == image.realMax(1) &&
+            img.realMin(1) == img.realMin(1) &&
+            img.size() == image.size() &&
+            img.spliterator() == image.spliterator() &&
+            img.toString().equals(image.toString()));
     }
-
-    @Test
-    public void getDimensions_Float32Image_CreatesImageOfSameDimension() {
-        long[] dimensions = new long[] { 1, 1, 1, 1, 1 };
-        Image<Float32> image = new ImgLib2ImageFactory<Float32>().create(dimensions, new Float32());
-
-        assertArrayEquals(image.getDimensions(), dimensions);
-    }
-
-    @Test
-    public void getDimensions_RGBImage_CreatesImageOfSameDimension() {
-        long[] dimensions = new long[] { 1, 1, 1, 1, 1 };
-        Image<RGB16> image = new ImgLib2ImageFactory<RGB16>().create(dimensions, new RGB16());
-
-        assertArrayEquals(image.getDimensions(), dimensions);
-    }
-
-    @Test
-    public void checkImageType_SmallDimension_CreatesAnArrayImage() {
-        long[] dimensions = new long[] { 1, 1, 1, 1, 1 };
-        Image<UINT16> image = new ImgLib2ImageFactory<UINT16>().create(dimensions, new UINT16());
-
-        assertTrue(image.toString().contains("ArrayImg"));
-    }
-
-    @Test
-    public void checkImageType_LargeDimension_CreatesACellImage() {
-        long[] dimensions = new long[] { 250, 250, 1024, 2 };
-        Image<UINT16> image = new ImgLib2ImageFactory<UINT16>().create(dimensions, new UINT16());
-        
-        assertTrue(image.toString().contains("CellImg"));
-    }
-
-    @Test
-    public void () {
-        
-    }
-    
 }
+
