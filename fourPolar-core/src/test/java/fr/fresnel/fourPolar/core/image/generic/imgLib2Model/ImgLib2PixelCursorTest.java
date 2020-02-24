3276571b6c7f4af5fe45bb479ba67cfe46033074
Masoudas <Masoudas@hotmail.com>
2020-02-24 16:47:45 +0100

    add test for cursor

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2PixelCursorTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2PixelCursorTest.java
@@ -11,6 +11,8 @@ import org.junit.jupiter.api.Test;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 
 public class ImgLib2PixelCursorTest {
@@ -28,11 +30,11 @@ public class ImgLib2PixelCursorTest {
             list.add(cursor.localize());
         }
 
-        assertArrayEquals(new long[]{0, 0}, list.get(0));
-        assertArrayEquals(new long[]{1, 0}, list.get(1));
-        assertArrayEquals(new long[]{0, 1}, list.get(2));
-        assertArrayEquals(new long[]{1, 1}, list.get(3));
-        
+        assertArrayEquals(new long[] { 0, 0 }, list.get(0));
+        assertArrayEquals(new long[] { 1, 0 }, list.get(1));
+        assertArrayEquals(new long[] { 0, 1 }, list.get(2));
+        assertArrayEquals(new long[] { 1, 1 }, list.get(3));
+
     }
 
     @Test
@@ -40,25 +42,75 @@ public class ImgLib2PixelCursorTest {
         long[] dimensions = new long[] { 2, 2 };
         Image<UINT16> image = new ImgLib2ImageFactory().create(dimensions, new UINT16());
         IPixelCursor<UINT16> cursor = image.getCursor();
- 
+
         int value = 1;
         while (cursor.hasNext()) {
-            value++;
             cursor.next();
             cursor.setPixel(new Pixel<UINT16>(new UINT16(value)));
+            value++;
         }
 
         List<UINT16> pixels = new ArrayList<>();
+
+        cursor.reset();
+
+        while (cursor.hasNext()) {
+            pixels.add(cursor.next().value());
+        }
+
+        assertTrue(pixels.get(0).get() == 1 && pixels.get(1).get() == 2 && pixels.get(2).get() == 3
+                && pixels.get(3).get() == 4);
+    }
+
+    @Test
+    public void setPixel_Float32Image_SetsPixelsToDefinedValues() {
+        long[] dimensions = new long[] { 2, 2 };
+        Image<Float32> image = new ImgLib2ImageFactory().create(dimensions, new Float32());
+        IPixelCursor<Float32> cursor = image.getCursor();
+
+        float value = 1.1f;
+        while (cursor.hasNext()) {
+            cursor.next();
+            cursor.setPixel(new Pixel<Float32>(new Float32(value)));
+            value++;
+        }
+
+        List<Float32> pixels = new ArrayList<>();
+
+        cursor.reset();
+
+        while (cursor.hasNext()) {
+            pixels.add(cursor.next().value());
+        }
+
+        assertTrue(pixels.get(0).get() == 1.1f && pixels.get(1).get() == 2.1f && pixels.get(2).get() == 3.1f
+                && pixels.get(3).get() == 4.1f);
+    }
+
+    @Test
+    public void setPixel_RGB16Image_SetsPixelsToDefinedValues() {
+        long[] dimensions = new long[] { 2, 2 };
+        Image<RGB16> image = new ImgLib2ImageFactory().create(dimensions, new RGB16());
+        IPixelCursor<RGB16> cursor = image.getCursor();
+ 
+        int value = 0;
+        while (cursor.hasNext()) {
+            cursor.next();
+            cursor.setPixel(new Pixel<RGB16>(new RGB16(++value, 0, 0)));
+        }
+
+        List<RGB16> pixels = new ArrayList<>();
         
+        cursor.reset();
         while (cursor.hasNext()) {
             pixels.add(cursor.next().value());
         }
         
         assertTrue(
-            pixels.get(0).get() == 1 &&
-            pixels.get(1).get() == 2 &&
-            pixels.get(2).get() == 3 && 
-            pixels.get(3).get() == 4);
+            pixels.get(0).getR() == 1 && pixels.get(0).getG() == 0 && pixels.get(0).getB() == 0 &&
+            pixels.get(1).getR() == 2 && pixels.get(1).getG() == 0 && pixels.get(1).getB() == 0 &&
+            pixels.get(2).getR() == 3 && pixels.get(2).getG() == 0 && pixels.get(2).getB() == 0 &&
+            pixels.get(3).getR() == 4 && pixels.get(3).getG() == 0 && pixels.get(3).getB() == 0 );
     }
-    
+
 }
