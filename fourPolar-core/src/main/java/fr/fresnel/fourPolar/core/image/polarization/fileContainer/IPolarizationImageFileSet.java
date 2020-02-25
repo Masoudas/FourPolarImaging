d549f3b3249c2be47bcddc918c3784d4f10c3385
Masoudas <Masoudas@hotmail.com>
2020-02-25 12:12:57 +0100

    add file set interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/fileContainer/IPolarizationImageFileSet.java
@@ -0,0 +1,26 @@
+package fr.fresnel.fourPolar.core.image.polarization.fileContainer;
+
+import java.io.File;
+
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+
+/**
+ * An interface for accessing the files corresponding to set of polarization
+ * images.
+ */
+public interface IPolarizationImageFileSet {
+    /**
+     * Returns the file corresponding to the given polarization.
+     * 
+     * @param pol
+     * @return
+     */
+    public File getFile(Polarization pol);
+
+    /**
+     * Returns the name extract as defined by the {@link ICapturedImageFileSet}.
+     * 
+     * @return
+     */
+    public String getNameExtract();
+}
