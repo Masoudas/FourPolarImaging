79943b634abf5df1c2481e1cf470ff15b2527441
Masoudas <Masoudas@hotmail.com>
2020-02-25 16:09:06 +0100

    create test body7

---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/polarization/fileContainer/PolarizationFileSetTest.java
@@ -0,0 +1,14 @@
+package fr.fresnel.fourPolar.core.image.polarization.fileContainer;
+
+import org.junit.jupiter.api.Test;
+
+public class PolarizationFileSetTest {
+    String _root = "/root";
+    @Test
+    public void getFile_OneCameraCase_CreatesFilesInCorrectPath(){
+
+
+    }
+
+    
+}
