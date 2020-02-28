900beba8f8841fa8e6d29b35fbedfd1433b79924
Masoudas <Masoudas@hotmail.com>
2020-02-28 10:15:09 +0100

    design orientation image file set

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/orientation/fileContainer/IOrientationImageFileSet.java
@@ -0,0 +1,29 @@
+package fr.fresnel.fourPolar.core.image.orientation.fileContainer;
+
+import java.io.File;
+
+import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
+
+/**
+ * An interface for accessing the files that correspond to a given
+ * {@link IOrientationImage}.
+ */
+public interface IOrientationImageFileSet {
+    /**
+     * Return the disk file that corresponds to this angle image.
+     * 
+     * @param angle
+     * @return
+     */
+    public File getFile(OrientationAngle angle);
+
+    /**
+     * Returns the set name designated to this set of angle images that form the
+     * orientation image. This set name is exactly the same as the set name assigned
+     * by {@link ICapturedImageFileSet}.
+     * 
+     * @return
+     */
+    public String getSetName();
+
+}
