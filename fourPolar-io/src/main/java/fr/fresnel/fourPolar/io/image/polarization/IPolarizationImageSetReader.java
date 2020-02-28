855981f669ad24f8f130835a34c3e2ec4dbff121
Masoudas <Masoudas@hotmail.com>
2020-02-28 18:02:06 +0100

    Read using captured fileset

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/IPolarizationImageSetReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/IPolarizationImageSetReader.java
@@ -1,24 +1,28 @@
 package fr.fresnel.fourPolar.io.image.polarization;
 
+import java.io.File;
 import java.io.IOException;
 
 import fr.fresnel.fourPolar.core.image.captured.fileSet.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
 
 /**
- * An interface for reading a {@link IPolarizationImageSet}.
+ * Interface for reading the {@link IPolarizationImageSet} that corresponds to
+ * the given {@link ICapturedImageFileSet}
  */
 public interface IPolarizationImageSetReader {
     /**
      * Reads the given {@link IPolarizationImageSet}.
-     * @param fileSet is the set of image files associated with the polarization images. 
+     * 
+     * @param fileSet is the set of image files associated with the polarization
+     *                images.
      * @return the polarization image set.
      */
-    public IPolarizationImageSet read(ICapturedImageFileSet fileSet) throws IOException;
+    public IPolarizationImageSet read(File rootFolder, ICapturedImageFileSet fileSet) throws IOException;
 
     /**
      * Close all resources associated with this reader.
      */
     public void close() throws IOException;
-    
+
 }
--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/PolarizationImageSetReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/PolarizationImageSetReader.java
@@ -3,33 +3,40 @@ package fr.fresnel.fourPolar.io.image.polarization;
 import java.io.File;
 import java.io.IOException;
 
+import fr.fresnel.fourPolar.core.image.captured.fileSet.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImage;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
 import fr.fresnel.fourPolar.core.image.polarization.PolarizationImage;
 import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageSet;
-import fr.fresnel.fourPolar.core.image.polarization.fileContainer.IPolarizationImageFileSet;
+import fr.fresnel.fourPolar.io.image.polarization.fileSet.IPolarizationImageFileSet;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+import fr.fresnel.fourPolar.io.exceptions.image.generic.NoReaderFoundForImage;
 import fr.fresnel.fourPolar.io.image.generic.ImageReader;
+import fr.fresnel.fourPolar.io.image.generic.tiff.TiffImageReaderFactory;
+import fr.fresnel.fourPolar.io.image.polarization.fileSet.TiffPolarizationImageFileSet;
 
 /**
- * A concrete implementation of the {@link IPolarizationImageSetReader}, which can be used
- * to read an instance of {@link PolarizationImageSet} from disk.
+ * A concrete implementation of the {@link IPolarizationImageSetReader}, which
+ * can be used to read an instance of {@link PolarizationImageSet} from disk.
+ * In this implementation, it is assumed that the images are tiff.
  */
 public class PolarizationImageSetReader implements IPolarizationImageSetReader {
     final private ImageReader<UINT16> _reader;
 
-    public PolarizationImageSetReader(ImageReader<UINT16> reader) {
-        this._reader = reader;
+    public PolarizationImageSetReader(ImageFactory factory) throws NoReaderFoundForImage {
+        this._reader = TiffImageReaderFactory.getReader(factory, new UINT16());
     }
 
     @Override
-    public IPolarizationImageSet read(IPolarizationImageFileSet fileSet) throws IOException {
-        IPolarizationImage pol0 = _readPolarizationImage(Polarization.pol0, fileSet);
-        IPolarizationImage pol45 = _readPolarizationImage(Polarization.pol45, fileSet);
-        IPolarizationImage pol90 = _readPolarizationImage(Polarization.pol90, fileSet);
-        IPolarizationImage pol135 = _readPolarizationImage(Polarization.pol90, fileSet);
+    public IPolarizationImageSet read(File rootFolder, ICapturedImageFileSet fileSet) throws IOException {
+        IPolarizationImageFileSet polFileSet = new TiffPolarizationImageFileSet(rootFolder, fileSet);
+        IPolarizationImage pol0 = _readPolarizationImage(Polarization.pol0, polFileSet);
+        IPolarizationImage pol45 = _readPolarizationImage(Polarization.pol45, polFileSet);
+        IPolarizationImage pol90 = _readPolarizationImage(Polarization.pol90, polFileSet);
+        IPolarizationImage pol135 = _readPolarizationImage(Polarization.pol90, polFileSet);
         return new PolarizationImageSet(fileSet, pol0, pol45, pol90, pol135);
     }
 
