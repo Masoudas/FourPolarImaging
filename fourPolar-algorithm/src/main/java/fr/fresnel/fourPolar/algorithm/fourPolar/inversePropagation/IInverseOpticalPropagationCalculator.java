17a827e5698086d34ae4b3b11307047fbf7b1cac
Masoudas <Masoudas@hotmail.com>
2020-03-02 16:30:12 +0100

    Return Inverse propagation as a class

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/fourPolar/inversePropagation/IInverseOpticalPropagationCalculator.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/fourPolar/inversePropagation/IInverseOpticalPropagationCalculator.java
@@ -1,19 +1,22 @@
 package fr.fresnel.fourPolar.algorithm.fourPolar.inversePropagation;
 
-import fr.fresnel.fourPolar.core.physics.dipole.DipoleSquaredComponent;
-import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+import fr.fresnel.fourPolar.algorithm.exceptions.fourPolar.propagation.OpticalPropagationNotInvertible;
+import fr.fresnel.fourPolar.core.exceptions.physics.propagation.PropagationFactorNotFound;
+import fr.fresnel.fourPolar.core.physics.propagation.IInverseOpticalPropagation;
+import fr.fresnel.fourPolar.core.physics.propagation.IOpticalPropagation;
 
 /**
  * An interface for calculating the inverse propagation factors.
  */
 public interface IInverseOpticalPropagationCalculator {
     /**
-     * Returns the inverse propagation factor from the given {@link Polarization} to the given
-     * {@link DipoleSquaredComponent}
+     * Returns the inverse propagation factor from the given {@link Polarization} to
+     * the given {@link DipoleSquaredComponent}
      * 
      * @param polarization
      * @param component
      */
-    public double getInverseFactor(Polarization polarization, DipoleSquaredComponent component);
-    
+    public IInverseOpticalPropagation getInverse(IOpticalPropagation opticalPropagation)
+            throws PropagationFactorNotFound, OpticalPropagationNotInvertible;
+
 }
