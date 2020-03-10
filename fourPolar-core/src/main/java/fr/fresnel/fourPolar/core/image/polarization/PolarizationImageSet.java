dc187ea2944c5079330aa2e020d65f013da3c1b3
Masoudas <Masoudas@hotmail.com>
2020-03-10 15:45:52 +0100

    Add constructor based on image

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/PolarizationImageSet.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/PolarizationImageSet.java
@@ -1,7 +1,12 @@
 package fr.fresnel.fourPolar.core.image.polarization;
 
+import java.util.Arrays;
+
+import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
 import fr.fresnel.fourPolar.core.fourPolar.IPolarizationsIntensityIterator;
 import fr.fresnel.fourPolar.core.image.captured.fileSet.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
@@ -15,13 +20,26 @@ public class PolarizationImageSet implements IPolarizationImageSet{
     final private IPolarizationImage _pol135;
     final private ICapturedImageFileSet _fileSet;
 
-    public PolarizationImageSet(
-        ICapturedImageFileSet fileSet, IPolarizationImage pol0,
-        IPolarizationImage pol45, IPolarizationImage pol90, IPolarizationImage pol135) {
-        this._pol0 = pol0;
-        this._pol45 = pol45;
-        this._pol90 = pol90;
-        this._pol135 = pol135;
+    /**
+     * 
+     * @param fileSet
+     * @param pol0
+     * @param pol45
+     * @param pol90
+     * @param pol135
+     * @throws CannotFormPolarizationImageSet
+     */
+    public PolarizationImageSet(ICapturedImageFileSet fileSet, Image<UINT16> pol0, Image<UINT16> pol45,
+            Image<UINT16> pol90, Image<UINT16> pol135) throws CannotFormPolarizationImageSet {
+        if (this._checkDimensions(pol0, pol45, pol90, pol135)){
+            throw new CannotFormPolarizationImageSet(
+                "Cannot form the polarization image set  because the given images don't have the same dimension.");
+        }    
+
+        this._pol0 = new PolarizationImage(Polarization.pol0, pol0);
+        this._pol45 = new PolarizationImage(Polarization.pol45, pol45);
+        this._pol90 = new PolarizationImage(Polarization.pol90, pol90);
+        this._pol135 = new PolarizationImage(Polarization.pol135, pol135);
         this._fileSet = fileSet;
     }
 
@@ -63,6 +81,14 @@ public class PolarizationImageSet implements IPolarizationImageSet{
     @Override
     public ICapturedImageFileSet getFileSet() {
         return this._fileSet;
+    }
+
+    private boolean _checkDimensions(
+        Image<UINT16> pol0, Image<UINT16> pol45, Image<UINT16> pol90, 
+        Image<UINT16> pol135) {
+        return Arrays.equals(pol0.getDimensions(), pol45.getDimensions()) &&
+            Arrays.equals(pol0.getDimensions(), pol90.getDimensions()) &&
+            Arrays.equals(pol0.getDimensions(), pol135.getDimensions());
     }
 
 }
