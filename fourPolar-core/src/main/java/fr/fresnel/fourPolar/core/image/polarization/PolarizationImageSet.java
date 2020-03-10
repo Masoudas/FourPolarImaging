5dba65b8d0cb34e7da62a9c8c265697466ca3cbd
Masoudas <Masoudas@hotmail.com>
2020-03-10 16:07:40 +0100

    Check for duplicate image

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/PolarizationImageSet.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/PolarizationImageSet.java
@@ -10,10 +10,11 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
- * A concrete implementation of the {@link IPolarizationsIntensitySet}. In this implementation,
- * we assume that the underlying intensity set is represented by an intensity image.
+ * A concrete implementation of the {@link IPolarizationsIntensitySet}. In this
+ * implementation, we assume that the underlying intensity set is represented by
+ * an intensity image.
  */
-public class PolarizationImageSet implements IPolarizationImageSet{
+public class PolarizationImageSet implements IPolarizationImageSet {
     final private IPolarizationImage _pol0;
     final private IPolarizationImage _pol45;
     final private IPolarizationImage _pol90;
@@ -31,10 +32,14 @@ public class PolarizationImageSet implements IPolarizationImageSet{
      */
     public PolarizationImageSet(ICapturedImageFileSet fileSet, Image<UINT16> pol0, Image<UINT16> pol45,
             Image<UINT16> pol90, Image<UINT16> pol135) throws CannotFormPolarizationImageSet {
-        if (this._checkDimensions(pol0, pol45, pol90, pol135)){
+        if (!this._hasDuplicateImage(pol0, pol45, pol90, pol135)) {
             throw new CannotFormPolarizationImageSet(
-                "Cannot form the polarization image set  because the given images don't have the same dimension.");
-        }    
+                    "Cannot form the polarization image set due to duplicate image for polarizations.");
+        }
+        if (!this._hasEqualDimensions(pol0, pol45, pol90, pol135)) {
+            throw new CannotFormPolarizationImageSet(
+                    "Cannot form the polarization image set because the given images don't have the same dimension.");
+        }
 
         this._pol0 = new PolarizationImage(Polarization.pol0, pol0);
         this._pol45 = new PolarizationImage(Polarization.pol45, pol45);
@@ -45,32 +50,31 @@ public class PolarizationImageSet implements IPolarizationImageSet{
 
     @Override
     public IPolarizationsIntensityIterator getCursor() {
-        return new PolarizationsIntensityIterator(
-            _pol0.getImage().getCursor(), _pol45.getImage().getCursor(),
-            _pol90.getImage().getCursor(), _pol135.getImage().getCursor());
+        return new PolarizationsIntensityIterator(_pol0.getImage().getCursor(), _pol45.getImage().getCursor(),
+                _pol90.getImage().getCursor(), _pol135.getImage().getCursor());
     }
 
     @Override
     public IPolarizationImage getPolarizationImage(Polarization pol) {
         IPolarizationImage image = null;
-        
+
         switch (pol) {
             case pol0:
-                image = _pol0;        
+                image = _pol0;
                 break;
 
             case pol45:
-                image = _pol45;        
-                break;                
+                image = _pol45;
+                break;
 
             case pol90:
-                image = _pol90;        
-                break;                 
+                image = _pol90;
+                break;
 
             case pol135:
-                image = _pol135;        
-                break;                                 
-        
+                image = _pol135;
+                break;
+
             default:
                 break;
         }
@@ -83,12 +87,22 @@ public class PolarizationImageSet implements IPolarizationImageSet{
         return this._fileSet;
     }
 
-    private boolean _checkDimensions(
-        Image<UINT16> pol0, Image<UINT16> pol45, Image<UINT16> pol90, 
-        Image<UINT16> pol135) {
-        return Arrays.equals(pol0.getDimensions(), pol45.getDimensions()) &&
-            Arrays.equals(pol0.getDimensions(), pol90.getDimensions()) &&
-            Arrays.equals(pol0.getDimensions(), pol135.getDimensions());
+    /**
+     * Checks that all images have the same dimension.
+     */
+    private boolean _hasEqualDimensions(Image<UINT16> pol0, Image<UINT16> pol45, Image<UINT16> pol90,
+            Image<UINT16> pol135) {
+        return Arrays.equals(pol0.getDimensions(), pol45.getDimensions())
+                && Arrays.equals(pol0.getDimensions(), pol90.getDimensions())
+                && Arrays.equals(pol0.getDimensions(), pol135.getDimensions());
+    }
+
+    /**
+     * checks for duplicate image reference.
+     */
+    private boolean _hasDuplicateImage(Image<UINT16> pol0, Image<UINT16> pol45, Image<UINT16> pol90,
+            Image<UINT16> pol135) {
+        return pol0 == pol45 || pol0 == pol90 || pol0 == pol135 || pol45 == pol90 || pol45 == pol135 || pol90 == pol135;
     }
 
 }
