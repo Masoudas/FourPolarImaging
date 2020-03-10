162ed761c277d0b48b5a799e7f890b5206658244
Masoudas <Masoudas@hotmail.com>
2020-03-10 14:00:05 +0100

    add second matrix for test

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/fourPolar/inversePropagation/MatrixBasedInverseOpticalPropagationCalculatorTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/fourPolar/inversePropagation/MatrixBasedInverseOpticalPropagationCalculatorTest.java
@@ -106,6 +106,92 @@ public class MatrixBasedInverseOpticalPropagationCalculatorTest {
     }
 
     @Test
+    public void getInverseFactor_MatlabCodes_InverseEqualsPrecalculatedValuesTo3Digits()
+            throws PropagationFactorNotFound, OpticalPropagationNotInvertible {
+        IChannel channel = new Channel(520, 1, 0.7, 1, 0.7);
+        INumericalAperture na = new NumericalAperture(1.45, 1.015, 1.45, 1.015);
+        
+        IOpticalPropagation opticalPropagation = new OpticalPropagation(channel, na);
+
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XX, Polarization.pol0, 1.7262);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.YY, Polarization.pol0, 0.0120);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.ZZ, Polarization.pol0, 0.3482);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XY, Polarization.pol0, 0.0);
+                
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XX, Polarization.pol90, 0.0120);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.YY, Polarization.pol90, 1.726);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.ZZ, Polarization.pol90, 0.348);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XY, Polarization.pol90, 0.0);
+
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XX, Polarization.pol45, 1.636);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.YY, Polarization.pol45, 1.636);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.ZZ, Polarization.pol45, 1.559);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XY, Polarization.pol45, 2.970);
+
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XX, Polarization.pol135, 1.636);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.YY, Polarization.pol135, 1.636);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.ZZ, Polarization.pol135, 1.559);
+        opticalPropagation.setPropagationFactor(DipoleSquaredComponent.XY, Polarization.pol135, -2.970);
+
+        MatrixBasedInverseOpticalPropagationCalculator inverseCalculator = new MatrixBasedInverseOpticalPropagationCalculator();
+
+        // Matlab code matrix results.    
+        double ixx_0 = 0.788;
+        double ixx_45 = -0.110;
+        double ixx_90 = 0.204;
+        double ixx_135 = -0.1108;
+        double iyy_0 = 0.204;
+        double iyy_45 = -0.110;
+        double iyy_90 = 0.788;
+        double iyy_135 = -0.110;
+        double izz_0 = -1.042;
+        double izz_45 = 0.553;
+        double izz_90 = -1.042;
+        double izz_135 = 0.553; 
+        double ixy_0 = 0;
+        double ixy_45 = 0.168;
+        double ixy_90 = 0;
+        double ixy_135 = -0.168;
+
+        IInverseOpticalPropagation iPropagation = inverseCalculator.getInverse(opticalPropagation);
+
+        double acceptedError = 1e-3;
+        assertTrue( 
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol0, DipoleSquaredComponent.XX), ixx_0, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol45, DipoleSquaredComponent.XX), ixx_45, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol90, DipoleSquaredComponent.XX), ixx_90, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol135, DipoleSquaredComponent.XX), ixx_135, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol0, DipoleSquaredComponent.YY), iyy_0, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol45, DipoleSquaredComponent.YY), iyy_45, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol90, DipoleSquaredComponent.YY), iyy_90, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol135, DipoleSquaredComponent.YY), iyy_135, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol0, DipoleSquaredComponent.ZZ), izz_0, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol45, DipoleSquaredComponent.ZZ), izz_45, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol90, DipoleSquaredComponent.ZZ), izz_90, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol135, DipoleSquaredComponent.ZZ), izz_135, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol0, DipoleSquaredComponent.XY), ixy_0, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol45, DipoleSquaredComponent.XY), ixy_45, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol90, DipoleSquaredComponent.XY), ixy_90, acceptedError) &&
+            _checkPrecision(
+                iPropagation.getInverseFactor(Polarization.pol135, DipoleSquaredComponent.XY), ixy_135, acceptedError));        
+    }
+
+    @Test
     public void getInverseFactor_SingularPropagationMatrix_ThrowsOpticalPropagationNotInvertible()
             throws PropagationFactorNotFound, OpticalPropagationNotInvertible {
         IChannel channel = new Channel(520, 1, 0.7, 1, 0.7);
@@ -151,4 +237,9 @@ public class MatrixBasedInverseOpticalPropagationCalculatorTest {
         assertThrows(
             PropagationFactorNotFound.class, ()->{inverseCalculator.getInverse(opticalPropagation);});
     }
+
+    private static boolean _checkPrecision(double val1, double val2, double error) {
+        return Math.abs(val1 - val2) < error;
+    }
+
 }
