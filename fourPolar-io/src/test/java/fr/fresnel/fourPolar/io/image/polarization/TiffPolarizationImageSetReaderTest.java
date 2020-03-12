fb349ccf56fe49c446a79736e54d9434d6d54d61
Masoudas <Masoudas@hotmail.com>
2020-03-12 10:55:05 +0100

    Write directly before reading images

--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetReaderTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetReaderTest.java
@@ -11,11 +11,15 @@ import org.junit.jupiter.api.Test;
 import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
 import fr.fresnel.fourPolar.core.image.captured.fileSet.CapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.captured.fileSet.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.NoReaderFoundForImage;
+import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.UINT16ImgLib2TiffImageWriter;
+import fr.fresnel.fourPolar.io.image.polarization.fileSet.TiffPolarizationImageFileSet;
 
 public class TiffPolarizationImageSetReaderTest {
     final private static File _root = new File(TiffPolarizationImageSetReaderTest.class.getResource("").getPath(),
@@ -24,21 +28,37 @@ public class TiffPolarizationImageSetReaderTest {
     @Test
     public void read_PolarizationImageSetFromDisk_ReadImagesHaveSameDimensionAsDisk()
             throws NoReaderFoundForImage, IOException, CannotFormPolarizationImageSet {
-        
-
         File capturedImageFile = new File(_root, "testFile.tif");
-        ICapturedImageFileSet fileSet = new CapturedImageFileSet(1, capturedImageFile);
+        CapturedImageFileSet fileSet = new CapturedImageFileSet(1, capturedImageFile);
+        TiffPolarizationImageFileSet fSet = new TiffPolarizationImageFileSet(_root, fileSet);
+
+        long[] dim = { 2, 2 };
+
+        Image<UINT16> pol0 = new ImgLib2ImageFactory().create(dim, new UINT16());
+        Image<UINT16> pol45 = new ImgLib2ImageFactory().create(dim, new UINT16());
+        Image<UINT16> pol90 = new ImgLib2ImageFactory().create(dim, new UINT16());
+        Image<UINT16> pol135 = new ImgLib2ImageFactory().create(dim, new UINT16());
+
+        UINT16ImgLib2TiffImageWriter writer = new UINT16ImgLib2TiffImageWriter();
+        writer.write(fSet.getFile(Polarization.pol0), pol0);
+        writer.write(fSet.getFile(Polarization.pol45), pol45);
+        writer.write(fSet.getFile(Polarization.pol90), pol90);
+        writer.write(fSet.getFile(Polarization.pol135), pol135);
+
 
         ImageFactory factory = new ImgLib2ImageFactory();
         IPolarizationImageSetReader reader = new TiffPolarizationImageSetReader(factory);
         IPolarizationImageSet imageSet = reader.read(_root, fileSet);
 
-        long[] dim = { 2, 2 };
         assertTrue(       
-            Arrays.equals(imageSet.getPolarizationImage(Polarization.pol0).getImage().getDimensions(), dim) &&
-            Arrays.equals(imageSet.getPolarizationImage(Polarization.pol45).getImage().getDimensions(), dim) &&
-            Arrays.equals(imageSet.getPolarizationImage(Polarization.pol90).getImage().getDimensions(), dim) &&
-            Arrays.equals(imageSet.getPolarizationImage(Polarization.pol135).getImage().getDimensions(), dim));
+            Arrays.equals(
+                imageSet.getPolarizationImage(Polarization.pol0).getImage().getDimensions(), dim) &&
+            Arrays.equals(
+                imageSet.getPolarizationImage(Polarization.pol45).getImage().getDimensions(), dim) &&
+            Arrays.equals(
+                imageSet.getPolarizationImage(Polarization.pol90).getImage().getDimensions(), dim) &&
+            Arrays.equals(
+                imageSet.getPolarizationImage(Polarization.pol135).getImage().getDimensions(), dim));
     }
 
 }
