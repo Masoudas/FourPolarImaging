d37ccc335281b10911f6def81acd0a27c47106ca
Masoudas <Masoudas@hotmail.com>
2020-03-30 12:25:12 +0200

    Define angle stick iterator interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/IAngleStickIterator.java
@@ -0,0 +1,13 @@
+package fr.fresnel.fourPolar.core.visualization.figures.stickFigure;
+
+import java.util.Iterator;
+
+import fr.fresnel.fourPolar.core.util.DPoint;
+
+/**
+ * An interface for iterating over the pixels corresponding to an angle stick of
+ * a particular length and thickness.
+ */
+public interface IAngleStickIterator extends Iterator<DPoint> {
+    
+}
