dcc15b5cbfde0e99cf62e7f8b20102da995321f7
Masoudas <Masoudas@hotmail.com>
2020-03-30 12:30:40 +0200

    Define angle stick interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/IAngleStick.java
@@ -0,0 +1,52 @@
+package fr.fresnel.fourPolar.core.visualization.figures.stickFigure;
+
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.core.util.DPoint;
+
+/**
+ * This interface represents a stick, which is used to visualize a particular
+ * {@link OrientationAngle}. A stick has:
+ * <ul>
+ * <li>position: The central {@link Point} in the 2D plane (not the
+ * n-dimensional position)</li>
+ * <li>color: Representing the color of the stick (that corresponds to the value
+ * of a particular orientation angle</li>
+ * <li>length: This would be the x length of the stick. Note that the length
+ * counts for negative and positive values of the stick. Hence a stick of
+ * length 3 starts from -1 and ends in 1.</li>
+ * </ul>
+ * 
+ */
+public interface IAngleStick {
+    /**
+     * Returns a 2D vector, corresponding to the pixel position of the stick in the plane.
+     * 
+     */
+    public DPoint getPosition();
+
+    /**
+     * Returns the color associated with this stick.
+     */
+    public RGB16 getColor();
+
+    /**
+     * Returns the length of this stick.
+     */
+    public int getLength();
+
+    /**
+     * Returns the slope of the stick.
+     */
+    public float getSlope();
+
+    /**
+     * Returns the thickness of this stick.
+     */
+    public int getThickness();
+
+    /**
+     * Returns the {@link IAngleStickIterator} of this stick. 
+     */
+    public IAngleStickIterator getIterator();
+
+}
