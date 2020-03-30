dad209ab3f47bda512080c4daea22252eb192e39
Masoudas <Masoudas@hotmail.com>
2020-03-30 15:29:04 +0200

    Add constructor

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/DPoint.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/DPoint.java
@@ -4,6 +4,12 @@ package fr.fresnel.fourPolar.core.util;
  * A discrete point.
  */
 public class DPoint {
-    public int x = 0;
-    public int y = 0;
+    public int x;
+    public int y;
+
+    public DPoint(int x, int y){
+        this.x = x;
+        this.y = y;
+
+    }
 }
