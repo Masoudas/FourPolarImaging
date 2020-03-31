9a6dc6a34c8704b26b6007eda885e185b8f293cd
Masoudas <Masoudas@hotmail.com>
2020-03-31 13:56:55 +0200

    Add double precision point

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/Point.java
@@ -0,0 +1,15 @@
+package fr.fresnel.fourPolar.core.util;
+
+/**
+ * A double precision point.
+ */
+public class Point {
+    public double x;
+    public double y;
+
+    public Point(double x, double y){
+        this.x = x;
+        this.y = y;
+    }
+    
+}
