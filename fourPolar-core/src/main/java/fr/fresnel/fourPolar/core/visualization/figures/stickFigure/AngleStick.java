3229e875a00d6b833a0fb7a3c779b4945426c38d
Masoudas <Masoudas@hotmail.com>
2020-03-31 14:43:02 +0200

    Implement angle stick

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/AngleStick.java
@@ -0,0 +1,124 @@
+package fr.fresnel.fourPolar.core.visualization.figures.stickFigure;
+
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.core.util.DPoint;
+import fr.fresnel.fourPolar.core.util.Point;
+import ij.gui.Line;
+
+/**
+ * A concrete implementation of {@link IAngleStick}. Note that the sticks can
+ * have negative coordinates in this implementation.
+ */
+public class AngleStick implements IAngleStick {
+    private final DPoint _pose;
+    private final RGB16 _color;
+    private final int _len;
+    private final double _slopeAngle;
+    private final int _thickness;
+
+    /**
+     * Creates a stick representing the dipole based on the following parameters:
+     * 
+     * @param pose       is the position of the stick in the image coordinates.
+     * @param slopeAngle is the angle corresponding this stick. It should be given
+     *                   in radian.
+     * @param len        is the length of the stick in pixels.
+     * @param thickness  is the thickness of the stick in pixels.
+     * @param color      is the {@link RGB16} color of the pixel.
+     */
+    public AngleStick(DPoint pose, float slopeAngle, int len, int thickness, RGB16 color) {
+        this._pose = pose;
+        this._color = color;
+        this._len = len;
+        this._slopeAngle = slopeAngle;
+        this._thickness = thickness;
+    }
+
+    @Override
+    public DPoint getPosition() {
+        return _pose;
+    }
+
+    @Override
+    public RGB16 getColor() {
+        return _color;
+    }
+
+    @Override
+    public int getLength() {
+        return _len;
+    }
+
+    @Override
+    public double getSlopeAngle() {
+        return _slopeAngle;
+    }
+
+    @Override
+    public int getThickness() {
+        return _thickness;
+    }
+
+    @Override
+    public IAngleStickIterator getIterator() {
+        /**
+         * Define a negative line, one that starts from the end point to the position,
+         * and one that starts from position to the other line.
+         * 
+         * This would ensure that the stick always passes through the dipole!
+         */
+        Point[] endPoints = _getEndPoints();
+        Line negativeLine = new Line(endPoints[0].x, endPoints[0].y, this._pose.x, this._pose.y);
+        negativeLine.setStrokeWidth(this._thickness);
+        
+        Line positiveLine = new Line(this._pose.x, this._pose.y, endPoints[1].x, endPoints[1].y);
+        positiveLine.setStrokeWidth(this._thickness);
+
+        return new AngleStickIterator(negativeLine.iterator(), positiveLine.iterator());
+    }
+
+    /**
+     * Calculate the end points of the stick.
+     * 
+     * @return start_point and end_point.
+     */
+    private Point[] _getEndPoints() {
+        // if (new BigDecimal(Math.PI).compareTo(new BigDecimal(this._slopeAngle)) == 0)
+        // {
+        // int aytop = this._pose.y - (this._len - 1) / 2;
+
+        // return new DPoint[] { new DPoint(this._pose.x, aytop > 0 ? aytop : 0),
+        // new DPoint(this._pose.x, this._pose.y + (this._len) / 2) };
+        // }
+
+        // if (this._len == 1) {
+        // return new DPoint[] { new DPoint(this._pose.x, this._pose.y), new
+        // DPoint(this._pose.x, this._pose.y), };
+        // }
+
+        // /// Define start point
+        // // Set x to to zero if negative, or one pixel less than length.
+        // int astart = this._pose.x - (this._len - 1) / 2;
+        // int xstart = astart > 1 ? astart : 1;
+
+        // // Set y based on slope.
+        // int ystart = (int) Math.tan(this._slopeAngle) * (xstart - this._pose.x) +
+        // this._pose.y;
+
+        // /// Define end point.
+        // // Set x to plus half the end, and y according to slope.
+        // int xend = this._pose.x + this._len / 2;
+        // int yend = (int) Math.tan(this._slopeAngle) * (xend - this._pose.x) +
+        // this._pose.y;
+
+        // return new DPoint[] { new DPoint(xstart, ystart), new DPoint(xend, yend), };
+
+        double xStart = this._pose.x - Math.cos(this._slopeAngle) * ((this._len - 1) / 2);
+        double yStart = this._pose.y - Math.sin(this._slopeAngle) * ((this._len - 1) / 2);
+
+        double xEnd = this._pose.x + Math.cos(this._slopeAngle) * (this._len / 2);
+        double yEnd = this._pose.y + Math.sin(this._slopeAngle) * (this._len / 2);
+
+        return new Point[] { new Point(xStart, yStart), new Point(xEnd, yEnd) };
+    }
+}
