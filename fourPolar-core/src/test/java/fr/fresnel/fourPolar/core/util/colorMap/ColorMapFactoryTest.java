f923d67571b0e1cceb7488b47226004e4401aae5
Masoudas <Masoudas@hotmail.com>
2020-04-01 17:08:16 +0200

    Implement test method

---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/util/colorMap/ColorMapFactoryTest.java
@@ -0,0 +1,27 @@
+package fr.fresnel.fourPolar.core.util.colorMap;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.lang.reflect.Field;
+
+import org.junit.jupiter.api.Test;
+
+public class ColorMapFactoryTest {
+    @Test
+    public void create_AllColorMaps_CreatesColorMap() throws IllegalArgumentException, IllegalAccessException {
+        boolean equals = true;
+        Field[] allFields = ColorMapFactory.class.getDeclaredFields();
+
+        for (Field field : allFields) {
+            Object colorMap = field.get(new ColorMapFactory());
+            if (colorMap instanceof String){
+                equals &= ColorMapFactory.create((String)colorMap).getColorMap().equals((String)colorMap);
+            }
+            
+        }
+        
+        assertTrue(equals);
+    }
+
+    
+}
