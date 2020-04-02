17bbd99a1f2c922967e92a9233dfc1e1e627dc60
Masoudas <Masoudas@hotmail.com>
2020-04-02 16:13:01 +0200

    Create SoI Image

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/ISoIImage.java
@@ -0,0 +1,22 @@
+package fr.fresnel.fourPolar.core.image.polarization;
+
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+
+/**
+ * An interface that models the SoI (Sum of Intensity) images. This image is
+ * formed by adding the four intensities of dipole together. See {@link SoICalculator}.
+ */
+public interface ISoIImage {
+    /**
+     * Returns the image interface corresponding to this SoI Image.
+     */
+    public Image<UINT16> getImage();
+
+    /**
+     * Returns the captured image file set that corresponds to this SoI image
+     */
+    public ICapturedImageFileSet getFileSet();
+
+}
---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/SoIImage.java
@@ -0,0 +1,51 @@
+package fr.fresnel.fourPolar.core.image.polarization;
+
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+
+/**
+ * A concrete implementation of {@link ISoIImage}.
+ */
+public class SoIImage implements ISoIImage {
+    final private Image<UINT16> _image;
+    final private ICapturedImageFileSet _fileSet;
+
+    /**
+     * Create an empty SoI image, whose dimensions are equal to
+     * {@link IPolarizationsImageSet}, using the provided image factory.
+     * 
+     * @param polarizationImageSet is a polarization image set.
+     * @param factory              is the desired image factory.
+     */
+    public SoIImage(IPolarizationImageSet polarizationImageSet, ImageFactory factory) {
+        long[] dim = polarizationImageSet.getPolarizationImage(Polarization.pol0).getImage().getDimensions();
+        this._image = factory.create(dim, new UINT16());
+
+        this._fileSet = polarizationImageSet.getFileSet();
+    }
+
+    /**
+     * This constructor is used for an already generated SoI image.
+     * 
+     * @param fileSet
+     * @param image
+     */
+    public SoIImage(ICapturedImageFileSet fileSet, Image<UINT16> image) {
+        this._fileSet = fileSet;
+        this._image = image;
+    }
+
+    @Override
+    public Image<UINT16> getImage() {
+        return this._image;
+    }
+
+    @Override
+    public ICapturedImageFileSet getFileSet() {
+        return this._fileSet;
+    }
+
+}
