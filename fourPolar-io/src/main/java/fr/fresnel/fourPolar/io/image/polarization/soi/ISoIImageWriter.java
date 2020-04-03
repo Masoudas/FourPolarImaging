074070cfc415b5ed6abc38687bb02d65fa12410a
Masoudas <Masoudas@hotmail.com>
2020-04-03 15:09:51 +0200

    Implement writer

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/soi/ISoIImageWriter.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/soi/ISoIImageWriter.java
@@ -13,7 +13,7 @@ public interface ISoIImageWriter {
      * Writes the image to the destination determined by the {@ISoIImageFile}.
      * @param imageSet
      */
-    public void write(File rootFolder, ISoIImage soIImage) throws IOException;
+    public void write(File rootFolder, ISoIImage soiImage) throws IOException;
     
     /**
      * Close all resources associated with this writer.
---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/polarization/soi/TiffSoIImageWriter.java
@@ -0,0 +1,37 @@
+package fr.fresnel.fourPolar.io.image.polarization.soi;
+
+import java.io.File;
+import java.io.IOException;
+
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import fr.fresnel.fourPolar.core.image.polarization.soi.ISoIImage;
+import fr.fresnel.fourPolar.io.exceptions.image.generic.NoWriterFoundForImage;
+import fr.fresnel.fourPolar.io.image.generic.ImageWriter;
+import fr.fresnel.fourPolar.io.image.generic.tiff.TiffImageWriterFactory;
+import fr.fresnel.fourPolar.io.image.polarization.soi.file.ISoIImageFile;
+import fr.fresnel.fourPolar.io.image.polarization.soi.file.TiffSoIImageFile;
+
+/**
+ * A concrete implementation of {@link ISoIImageWriter} to write the SoI image
+ * as tiff file.
+ */
+public class TiffSoIImageWriter implements ISoIImageWriter {
+    final private ImageWriter<UINT16> _writer;
+
+    public TiffSoIImageWriter(ISoIImage soiImage) throws NoWriterFoundForImage {
+        this._writer = TiffImageWriterFactory.getWriter(soiImage.getImage(), new UINT16());
+    }
+
+    @Override
+    public void write(File rootFolder, ISoIImage soiImage) throws IOException {
+        ISoIImageFile file = new TiffSoIImageFile(rootFolder, soiImage.getFileSet());
+
+        this._writer.write(file.getFile(), soiImage.getImage());
+    }
+
+    @Override
+    public void close() throws IOException {
+        this._writer.close();
+    }
+
+}
