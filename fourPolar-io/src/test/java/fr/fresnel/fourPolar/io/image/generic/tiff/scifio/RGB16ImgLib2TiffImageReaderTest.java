0e7a6319c316089a34da5ffdf18693dec1ea9e7a
Masoudas <Masoudas@hotmail.com>
2020-04-06 15:46:01 +0200

    Implement tests for RGB16 io

---
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/RGB16ImgLib2TiffImageReaderTest.java
@@ -0,0 +1,38 @@
+package fr.fresnel.fourPolar.io.image.generic.tiff.scifio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.Test;
+
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+
+public class RGB16ImgLib2TiffImageReaderTest {
+
+    @Test
+    public void read_PreDefinedDiskRGBImg_ReadPixelsAreEqual() throws IOException {
+        File path = new File(RGB16ImgLib2TiffImageReaderTest.class.getResource("Readers").getFile(), "RGB16Image.tif");
+
+        ImgLib2ImageFactory factory = new ImgLib2ImageFactory();
+
+        Image<RGB16> img = new RGB16ImgLib2TiffImageReader(factory).read(path);
+
+        IPixelCursor<RGB16> cursor = img.getCursor();
+
+        boolean equals = true;
+        while (cursor.hasNext()) {
+            RGB16 rgb16 = cursor.next().value();
+            
+            equals &= rgb16.getR() == 255 && rgb16.getG() == 0 && rgb16.getB() == 255;
+        }
+
+        assertTrue(equals);
+        
+    }
+
+}
---
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/RGB16ImgLib2TiffImageWriterTest.java
@@ -0,0 +1,61 @@
+package fr.fresnel.fourPolar.io.image.generic.tiff.scifio;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.Test;
+
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+
+public class RGB16ImgLib2TiffImageWriterTest {
+    private static long[] _dim = {2, 2};
+    private static File _root;
+    private static ImgLib2ImageFactory _factory = new ImgLib2ImageFactory();
+
+    private RGB16 _pixel = new RGB16(255, 0, 255);
+
+    @BeforeAll
+    private static void setRoot() {
+        _root = new File(RGB16ImgLib2TiffImageWriterTest.class.getResource("Writers").getPath());
+    }
+
+    @Test
+    public void write_RGB16Image_DiskImageHasSameData() throws IOException {
+        File destination = new File(_root, "RGB16Image.tif");
+
+        Image<RGB16> image = _factory.create(_dim, new RGB16());
+
+        IPixelCursor<RGB16> cursor = image.getCursor();
+        while (cursor.hasNext()) {
+            cursor.next();
+            cursor.setPixel(new Pixel<RGB16>(_pixel));
+        }
+
+        RGB16ImgLib2TiffImageWriter writer = new RGB16ImgLib2TiffImageWriter();
+        
+        writer.write(destination, image);
+        writer.close();
+
+        
+        Image<RGB16> diskImage = new RGB16ImgLib2TiffImageReader(_factory).read(destination);
+
+        boolean equals = true;
+        IPixelCursor<RGB16> diskCursor = diskImage.getCursor();
+        while (diskCursor.hasNext()) {
+            RGB16 pixel = diskCursor.next().value();
+
+            equals &= pixel.getR() == _pixel.getR() && pixel.getG() == _pixel.getG() &&
+                pixel.getB() == _pixel.getB();
+        }
+
+        assertTrue(equals);
+    }
+
+}
