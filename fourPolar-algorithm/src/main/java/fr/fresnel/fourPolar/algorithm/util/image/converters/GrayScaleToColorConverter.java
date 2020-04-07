18b3e073658b52e1fb4c1f5066686745324b5417
Masoudas <Masoudas@hotmail.com>
2020-04-07 17:09:21 +0200

    Use own cursor

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
@@ -7,8 +7,10 @@ import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.Converter
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImageToImgLib2Converter;
+import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import net.imglib2.RandomAccess;
 import net.imglib2.RandomAccessibleInterval;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.Converters;
@@ -41,19 +43,23 @@ public class GrayScaleToColorConverter {
         
         // We use ImgLib2 classes, i.e, we convert to Img. Then we convert the Image.
         // Finaly, we create a new Image instance and fill it, and return it.
-        Img<UnsignedShortType> imgLib2Model = ImageToImgLib2Converter.getImg(grayImage, UINT16.zero());
-
-        final RandomAccessibleInterval<UnsignedShortType> rai = imgLib2Model;
         final ColorTable8 cTable8 = new ColorTable8();
         final Converter<UnsignedShortType, ARGBType> converter = new RealLUTConverter<UnsignedShortType>(0, 0xffff,
                 cTable8);
-        final RandomAccessibleInterval<ARGBType> convertedRAI = Converters.convert(rai, converter, new ARGBType());
 
-        final IPixelCursor<RGB16> cursor = colorImage.getCursor();
-        while (cursor.hasNext()) {
-            RGB16 pixel = cursor.next().value();
-            int color = convertedRAI.getAt(cursor.localize()).get();
-            pixel.set(ARGBType.red(color), ARGBType.green(color), ARGBType.blue(color));
+        final IPixelCursor<UINT16> grayCursor = grayImage.getCursor();
+        final IPixelCursor<RGB16> colorCursor = colorImage.getCursor();
+        
+        UnsignedShortType uShortType = new UnsignedShortType();
+        ARGBType argbType = new ARGBType();
+        while (grayCursor.hasNext()) {
+            IPixel<RGB16> pixel = colorCursor.next();
+            uShortType.set(grayCursor.next().value().get());
+            converter.convert(uShortType, argbType);
+
+            int color = argbType.get();
+            pixel.value().set(ARGBType.red(color), ARGBType.green(color), ARGBType.blue(color));
+            colorCursor.setPixel(pixel);
         }
     }
 }
