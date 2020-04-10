a150900c48241a60c51a4832320bcba335d922b4
Masoudas <Masoudas@hotmail.com>
2020-04-10 08:51:44 +0200

    Remove unused imports

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/fourPolar/converters/IntensityToOrientationConverterTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/fourPolar/converters/IntensityToOrientationConverterTest.java
@@ -3,16 +3,10 @@ package fr.fresnel.fourPolar.algorithm.fourPolar.converters;
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.io.BufferedOutputStream;
 import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileNotFoundException;
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-import java.io.OutputStream;
-import java.io.OutputStreamWriter;
 import java.math.BigDecimal;
 
 import org.junit.jupiter.api.BeforeAll;
@@ -20,7 +14,6 @@ import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.algorithm.exceptions.fourPolar.converters.ImpossibleOrientationVector;
 import fr.fresnel.fourPolar.algorithm.exceptions.fourPolar.propagation.OpticalPropagationNotInvertible;
-import fr.fresnel.fourPolar.core.exceptions.physics.dipole.OrientationAngleOutOfRange;
 import fr.fresnel.fourPolar.core.exceptions.physics.propagation.PropagationFactorNotFound;
 import fr.fresnel.fourPolar.core.physics.channel.Channel;
 import fr.fresnel.fourPolar.core.physics.dipole.DipoleSquaredComponent;
