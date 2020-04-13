03b279660c426d3c17a68d15bee50fac56c909fa
Masoudas <Masoudas@hotmail.com>
2020-04-13 17:21:13 +0200

    Add set method to intensity

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/polarization/IntensityVector.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/polarization/IntensityVector.java
@@ -5,17 +5,22 @@ package fr.fresnel.fourPolar.core.physics.polarization;
  * that if an intensity is negative, it will be set to zero.
  */
 public class IntensityVector {
-    final private double _pol0;
-    final private double _pol45;
-    final private double _pol90;
-    final private double _pol135;
+    private double _pol0;
+    private double _pol45;
+    private double _pol90;
+    private double _pol135;
 
     public IntensityVector(double pol0, double pol45, double pol90, double pol135) {
+        this.setIntensity(pol0, pol45, pol90, pol135);
+
+    }
+
+    public void setIntensity(double pol0, double pol45, double pol90, double pol135) {
         _pol0 = pol0 > 0 ? pol0 : 0;
         _pol45 = pol45 > 0 ? pol45 : 0;
         _pol90 = pol90 > 0 ? pol90 : 0;
         _pol135 = pol135 > 0 ? pol135 : 0;
-
+        
     }
 
     public double getIntensity(Polarization pol) {
