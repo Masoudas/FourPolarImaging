65237efafc67d49716a6cbc1f5423a934d728b37
Masoudas <Masoudas@hotmail.com>
2020-04-14 11:34:09 +0200

    Change method to normalize per plane

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
@@ -3,17 +3,21 @@ package fr.fresnel.fourPolar.algorithm.util.image.converters;
 import java.util.Arrays;
 import java.util.Objects;
 
+import fr.fresnel.fourPolar.algorithm.util.image.stats.ImageStatistics;
 import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.ConverterToImgLib2NotFound;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RealType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import net.imglib2.converter.Converter;
 import net.imglib2.converter.RealLUTConverter;
 import net.imglib2.display.ColorTable8;
 import net.imglib2.type.numeric.ARGBType;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
+import net.imglib2.type.numeric.real.DoubleType;
+import net.imagej.minmax.DefaultMinMaxMethod;
 
 /**
  * Using this class, we can convert GrayScale types (e.g, {@link UINT16}) to
@@ -22,11 +26,13 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 public class GrayScaleToColorConverter {
     /**
      * Convertes an {@link UINT16} to an {@link RGB16} image. Note that an 8 bit
-     * lookup table is used for the conversion.
+     * lookup table is used for the conversion, hence there are only 256 white
+     * pixels. Note that each image plane is scaled with respect to it's minimum and
+     * maximum (not the maximum of the entire image.)
      * 
      * @throws ConverterToImgLib2NotFound
      */
-    public static void convertImage(final Image<UINT16> grayImage, final Image<RGB16> colorImage)
+    public static <T extends RealType> void convertPlane(final Image<T> grayImage, final Image<RGB16> colorImage)
             throws ConverterToImgLib2NotFound {
         Objects.requireNonNull(grayImage, "grayImage cannot be null");
         Objects.requireNonNull(grayImage, "colorImage cannot be null");
@@ -35,24 +41,35 @@ public class GrayScaleToColorConverter {
             throw new ArrayIndexOutOfBoundsException(
                     "Cannot convert, because the two images don't have same dimension.");
         }
-        
+
         // We use ImgLib2 classes, i.e, we convert to Img. Then we convert the Image.
         // Finaly, we create a new Image instance and fill it, and return it.
         final ColorTable8 cTable8 = new ColorTable8();
-        final Converter<UnsignedShortType, ARGBType> converter = new RealLUTConverter<UnsignedShortType>(0, 0xffff,
-                cTable8);
-
-        final IPixelCursor<UINT16> grayCursor = grayImage.getCursor();
+        final IPixelCursor<T> grayCursor = grayImage.getCursor();
         final IPixelCursor<RGB16> colorCursor = colorImage.getCursor();
-        
-        UnsignedShortType uShortType = new UnsignedShortType();
-        ARGBType argbType = new ARGBType();
-        while (grayCursor.hasNext()) {
-            IPixel<RGB16> pixel = colorCursor.next();
-            uShortType.set(grayCursor.next().value().get());
-            converter.convert(uShortType, argbType);
 
-            int color = argbType.get();
+        final DoubleType doubleType = new DoubleType();
+        final ARGBType argbType = new ARGBType();
+
+        final double[][] minMax = ImageStatistics.getPlaneMinMax(grayImage);
+        final int planeSize = ImageStatistics.getPlaneSize(grayImage);
+
+        int planeNo = 1;
+        int planePixelCounter = 0;
+        while (grayCursor.hasNext()) {
+            if (++planePixelCounter > planeSize) {
+                planePixelCounter = 0;
+                planeNo++;
+            }
+
+            final Converter<DoubleType, ARGBType> converter = new RealLUTConverter<DoubleType>(
+                    minMax[0][planeNo - 1], minMax[1][planeNo - 1], cTable8);
+            final IPixel<RGB16> pixel = colorCursor.next();
+
+            doubleType.set(grayCursor.next().value().getRealValue());
+            converter.convert(doubleType, argbType);
+
+            final int color = argbType.get();
             pixel.value().set(ARGBType.red(color), ARGBType.green(color), ARGBType.blue(color));
             colorCursor.setPixel(pixel);
         }
--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverterTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverterTest.java
@@ -12,8 +12,7 @@ import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImageToImgLib2Conver
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
-import ij.ImagePlus;
-import ij.io.FileSaver;
+
 import io.scif.img.ImgOpener;
 import net.imglib2.img.display.imagej.ImageJFunctions;
 import net.imglib2.type.numeric.integer.UnsignedShortType;
@@ -21,19 +20,19 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 public class GrayScaleToColorConverterTest {
     @Test
     public void convertImage_() throws ConverterToImgLib2NotFound, InterruptedException {
-        File grayImageFile = new File("/home/masoud/Documents/SampleImages/UnsignedShort.tif");
+        File grayImageFile = new File(GrayScaleToColorConverterTest.class.getResource("Example1.tif").getFile());
         ImgLib2ImageFactory factory = new ImgLib2ImageFactory();
 
-        UnsignedShortType type =new UnsignedShortType();
-        Image<UINT16> grayImage = factory.create(
-            new ImgOpener().openImgs(grayImageFile.getAbsolutePath(), type).get(0), type);
+        UnsignedShortType type = new UnsignedShortType();
+        Image<UINT16> grayImage = factory.create(new ImgOpener().openImgs(grayImageFile.getAbsolutePath(), type).get(0),
+                type);
 
         Image<RGB16> colorImage = factory.create(grayImage.getDimensions(), RGB16.zero());
 
-        GrayScaleToColorConverter.convertImage(grayImage, colorImage);
-    
+        GrayScaleToColorConverter.convertPlane(grayImage, colorImage);
+
         ImageJFunctions.show(ImageToImgLib2Converter.getImg(colorImage, RGB16.zero()));
-        
+
         TimeUnit.SECONDS.sleep(20);
     }
 }
