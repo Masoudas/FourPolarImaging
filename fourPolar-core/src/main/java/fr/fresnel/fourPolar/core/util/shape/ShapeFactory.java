a92f3e6d2cc808f4f8c1d03712a3eee48964cab1
Masoudas <Masoudas@hotmail.com>
2020-04-16 16:44:41 +0200

    Add polygon and test methods

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
@@ -5,18 +5,12 @@ import java.util.HashMap;
 import java.util.Map;
 import java.util.Objects;
 
-import net.imglib2.realtransform.AffineTransform;
 import net.imglib2.realtransform.AffineTransform2D;
 import net.imglib2.realtransform.AffineTransform3D;
-import net.imglib2.roi.IterableRegion;
-import net.imglib2.roi.Masks;
 import net.imglib2.roi.RealMaskRealInterval;
-import net.imglib2.roi.Regions;
 import net.imglib2.roi.geom.GeomMasks;
 import net.imglib2.roi.geom.real.WritableBox;
-import net.imglib2.type.logic.BoolType;
-import net.imglib2.util.Intervals;
-import net.imglib2.view.Views;
+import net.imglib2.roi.geom.real.WritablePolygon2D;
 
 public class ShapeFactory {
     private final Map<ShapeType, Shape> _shapes;
@@ -31,26 +25,27 @@ public class ShapeFactory {
     }
 
     /**
-     * Generates a 2d box from min to max. Both coordinates can be
-     * multi-dimensional, but third dimension and above should be equal (min higher
-     * dimensions will be taken). The box is closed in the sense that it contains
-     * the boundary.
+     * Generates a box from min to max. The box is closed in the sense that it
+     * contains the boundary.
      * 
      * @param min is the minimum coordinate.
      * @param max is the maximum coordinate.
      */
-    public IShape close2DBox(long[] min, long[] max) {
+    public IShape closedBox(long[] min, long[] max) {
         Objects.requireNonNull(min, "min should not be null");
         Objects.requireNonNull(max, "max should not be null");
 
-        if (min.length != max.length || min.length < 2 || max.length < 2) {
-            throw new IllegalArgumentException("min and max should have the same dimension and greater than one.");
+        if (min.length != max.length) {
+            throw new IllegalArgumentException("min and max should have the same dimension");
         }
 
-        WritableBox box = GeomMasks.closedBox(new double[] { min[0], min[1] }, new double[] { max[0], max[1] });
+        double[] minCopy = Arrays.stream(min).asDoubleStream().toArray();
+        double[] maxCopy = Arrays.stream(max).asDoubleStream().toArray();
+
+        WritableBox box = GeomMasks.closedBox(minCopy, maxCopy);
 
         Shape shape = this._getUniqueShape(ShapeType.Close2DBox);
-        shape.setImgLib2Shape(2, min, box);
+        shape.setImgLib2Shape(min.length, min, box);
         return shape;
     }
 
@@ -68,7 +63,7 @@ public class ShapeFactory {
      * @param rotationAngle is the rotation of the rectangle in Radian.
      * @return
      */
-    public IShape close2DBox(long[] center, long xlen, long ylen, double rotationAngle) {
+    public IShape closed2DBox(long[] center, long xlen, long ylen, double rotationAngle) {
         Objects.requireNonNull(center, "center should not be null");
 
         if (center.length < 2) {
@@ -80,9 +75,8 @@ public class ShapeFactory {
         }
 
         long[] min = { -xlen / 2, -ylen / 2 };
-        long[] max = { xlen / 2 + 1, ylen / 2 + 1 };
 
-        WritableBox box = GeomMasks.closedBox(new double[] { min[0], min[1] }, new double[] { max[0], max[1] });
+        WritableBox box = GeomMasks.closedBox(new double[] { min[0], min[1] }, new double[] { xlen / 2, ylen / 2 });
         AffineTransform2D transform2d = _generate2DTransform(center, rotationAngle);
         RealMaskRealInterval rotatedTranslatedBox = box.transform(transform2d);
 
@@ -93,37 +87,12 @@ public class ShapeFactory {
     }
 
     /**
-     * Generates a 3d box from min to max. Both coordinates can be
-     * multi-dimensional, but fourth dimension and above should be equal (min higher
-     * dimensions will be taken). The box is closed in the sense that it contains
-     * the boundary.
-     * 
-     * @param min is the minimum coordinate.
-     * @param max is the maximum coordinate.
-     */
-
-    public IShape close3DBox(long[] min, long[] max) {
-        Objects.requireNonNull(min, "min should not be null");
-        Objects.requireNonNull(max, "max should not be null");
-
-        if (min.length != max.length || min.length < 3 || max.length < 3) {
-            throw new IllegalArgumentException("min and max should have the same dimension and greater than two.");
-        }
-
-        WritableBox box = GeomMasks.closedBox(new double[] { min[0], min[1], min[2] },
-                new double[] { max[0], max[1], max[2] });
-
-        Shape shape = this._getUniqueShape(ShapeType.Close3DBox);
-        shape.setImgLib2Shape(3, min, box);
-        return shape;
-
-    }
-
-    /**
      * Generate a 3d box placed at the center, with xlen/2, ylen/2, zlen/2 over each
      * side of the center. The box is rotated around the z-axis by z_rotation and
      * around x-axis by x_rotation. The box is closed in the sense that it contains
      * the boundary.
+     * <p>
+     * VERY IMPORTANT NOTE: The order of rotation would be x and then z.
      * 
      * @param center     is the center of the box.
      * @param xlen       is the len in the x-direction.
@@ -132,7 +101,7 @@ public class ShapeFactory {
      * @param z_rotation is the rotation around the z-axis.
      * @param x_rotation is the rotation around the x-axis.
      */
-    public IShape close3DBox(long[] center, long xlen, long ylen, long zlen, double z_rotation, double x_rotation) {
+    public IShape closed3DBox(long[] center, long xlen, long ylen, long zlen, double x_rotation, double z_rotation) {
         Objects.requireNonNull(center, "center should not be null");
 
         if (center.length < 3) {
@@ -144,15 +113,34 @@ public class ShapeFactory {
         }
 
         long[] min = { -xlen / 2, -ylen / 2, -zlen / 2 };
-        long[] max = { xlen / 2 + 1, ylen / 2 + 1, zlen / 2 + 1 };
 
         WritableBox box = GeomMasks.closedBox(new double[] { min[0], min[1], min[2] },
-                new double[] { max[0], max[1], max[2] });
-        AffineTransform3D transform3d = _generate3DTransform(center, z_rotation, x_rotation);
+                new double[] { xlen / 2, ylen / 2, zlen / 2 });
+
+        AffineTransform3D transform3d = _generate3DTransform(center, x_rotation, z_rotation);
         RealMaskRealInterval rotatedTranslatedBox = box.transform(transform3d);
 
         Shape shape = this._getUniqueShape(ShapeType.Close3DBox);
         shape.setImgLib2Shape(3, min, rotatedTranslatedBox);
+        return shape;
+    }
+
+    public IShape closedPolygon2D(long[] x, long[] y) {
+        Objects.requireNonNull(x, "x should not be null");
+        Objects.requireNonNull(y, "y should not be null");
+
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("x and y should have the same dimension.");
+        }
+
+        double[] xPoints =  Arrays.stream(x).asDoubleStream().toArray();
+        double[] yPoints = Arrays.stream(y).asDoubleStream().toArray();
+        
+        WritablePolygon2D polygon2D = GeomMasks.closedPolygon2D(xPoints, yPoints);
+
+        Shape shape = _getUniqueShape(ShapeType.Polygon2D);
+        shape.setImgLib2Shape(2, new long[]{x[0], y[0]}, polygon2D);
+
         return shape;
     }
 
@@ -163,22 +151,25 @@ public class ShapeFactory {
         final AffineTransform2D transform2d = new AffineTransform2D();
         transform2d.rotate(-rotationAngle);
 
-        final double[] rotatedTranslation = { translation[0], translation[1] };
+        double[] rotatedTranslation = { -translation[0], -translation[1] };
         transform2d.apply(rotatedTranslation, rotatedTranslation);
         transform2d.translate(rotatedTranslation);
 
         return transform2d;
     }
 
-    private AffineTransform3D _generate3DTransform(long[] translation, double z_rotation, double x_rotation) {
+    private AffineTransform3D _generate3DTransform(long[] translation, double x_rotation, double z_rotation) {
         final AffineTransform3D transform3d = new AffineTransform3D();
-        transform3d.rotate(0, -z_rotation);
-        transform3d.rotate(3, -x_rotation);
 
-        final double[] rotatedTranslation = { translation[0], translation[1], translation[2] };
+        // In the following order, x rotation is applied first, and then z rotation.
+        transform3d.rotate(2, -z_rotation);
+        transform3d.rotate(0, -x_rotation);
+        
+        double[] rotatedTranslation = { -translation[0], -translation[1], -translation[2] };
         transform3d.apply(rotatedTranslation, rotatedTranslation);
+        
         transform3d.translate(rotatedTranslation);
-
+        
         return transform3d;
 
     }
--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactoryTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactoryTest.java
@@ -1,22 +1,162 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.util.Arrays;
+
 import org.junit.jupiter.api.Test;
+
+import net.imglib2.roi.RealMaskRealInterval;
+import net.imglib2.roi.geom.GeomMasks;
+import net.imglib2.roi.geom.real.PointMask;
+import net.imglib2.roi.geom.real.Polygon2D;
+import net.imglib2.roi.geom.real.WritableBox;
 
 public class ShapeFactoryTest {
 
     @Test
-    public void close3DBox_zRotation90AtOrigin_ReturnsCorrectCoordinates() {
-        long[] center = {0,0,0};
+    public void closedBox_RectangularBox_ReturnsCorrectPoints() {
+        long[] min = { 1, 1, 1 };
+        long[] max = { 2, 2, 2 };
+        IShape shape = new ShapeFactory().closedBox(min, max);
 
-        IShape shape = new ShapeFactory().close3DBox(center, 1, 1, 1, Math.PI/2, 0);
-
+        WritableBox box = GeomMasks.closedBox(new double[] { 1, 1, 1 }, new double[] { 2, 2, 2 });
         IShapeIteraror iterator = shape.getIterator();
-        while (iterator.hasNext()) {
-            long[] coords = iterator.next();
 
-            System.out.println(coords[0] + " " + coords[1] + " " + coords[2]);
-            
-        }
+        assertTrue(_checkPointInsideMask(box, iterator));
+
     }
 
+    @Test
+    public void closed2DBox_Rotation90_ReturnsCorrectCoordinates() {
+        long[] center = { 0, 0 };
+
+        IShape shape = new ShapeFactory().closed2DBox(center, 4, 2, Math.PI / 2);
+        WritableBox box = GeomMasks.closedBox(new double[] { -1, -2 }, new double[] { 1, 2 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+    }
+
+    @Test
+    public void closed2DBox_WithShift_ReturnsCorrectCoordinates() {
+        long[] center = { 50, 50 };
+
+        IShape shape = new ShapeFactory().closed2DBox(center, 4, 2, 0);
+        WritableBox box = GeomMasks.closedBox(new double[] { 50 - 2, 50 - 1 }, new double[] { 50 + 2, 50 + 1 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+    }
+
+    @Test
+    public void closed2DBox_Rotation90WithShift_ReturnsCorrectCoordinates() {
+        long[] center = { 50, 50 };
+
+        IShape shape = new ShapeFactory().closed2DBox(center, 4, 2, Math.PI / 2);
+        WritableBox box = GeomMasks.closedBox(new double[] { 50 - 1, 50 - 2 }, new double[] { 50 + 1, 50 + 2 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+    }
+
+    @Test
+    public void closed3DBox_zRotation90_ReturnsCorrectCoordinates() {
+        long[] center = { 0, 0, 0 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 6, Math.PI / 2, 0);
+        WritableBox box = GeomMasks.closedBox(new double[] { -2, -3, -1 }, new double[] { 2, 3, 1 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+
+    }
+
+    @Test
+    public void closed3DBox_xRotation90_ReturnsCorrectCoordinates() {
+        long[] center = { 0, 0, 0 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 6, 0, Math.PI / 2);
+        WritableBox box = GeomMasks.closedBox(new double[] { -1, -2, -3 }, new double[] { 1, 2, 3 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+
+    }
+
+    @Test
+    public void closed3DBox_xRotation90zrotation90_ReturnsCorrectCoordinates() {
+        long[] center = { 0, 0, 0 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 6, Math.PI / 2, Math.PI / 2);
+        WritableBox box = GeomMasks.closedBox(new double[] { -3, -2, -1 }, new double[] { 3, 2, 1 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+
+    }
+
+    @Test
+    public void closed3DBox_50Shift_ReturnsCorrectCoordinates() {
+        long[] center = { 50, 50, 50 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 4, 0, 0);
+        WritableBox box = GeomMasks.closedBox(new double[] { 50 - 2, 50 - 1, 50 - 2 },
+                new double[] { 50 + 2, 50 + 1, 50 + 2 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+
+    }
+
+    @Test
+    public void closed3DBox_xRotation90With50Shift_ReturnsCorrectCoordinates() {
+        long[] center = { 50, 50, 50 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 6, Math.PI / 2, 0);
+        WritableBox box = GeomMasks.closedBox(new double[] { 50 - 2, 50 - 3, 50 - 1 },
+                new double[] { 50 + 2, 50 + 3, 50 + 1 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+    }
+
+    @Test
+    public void closed3DBox_zRotation90With50Shift_ReturnsCorrectCoordinates() {
+        long[] center = { 50, 50, 50 };
+
+        IShape shape = new ShapeFactory().closed3DBox(center, 4, 2, 6, 0, Math.PI / 2);
+        WritableBox box = GeomMasks.closedBox(new double[] { 50 - 1, 50 - 2, 50 - 3 },
+                new double[] { 50 + 1, 50 + 2, 50 + 3 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(box, iterator));
+    }
+
+    @Test
+    public void closedPolygon2d_TriangleShape_ReturnsCorrectCoordinates() {
+        long[] x = { 0, 2, 2 };
+        long[] y = { 0, 0, 2 };
+
+        IShape shape = new ShapeFactory().closedPolygon2D(x, y);
+        Polygon2D polygon2d = GeomMasks.closedPolygon2D(new double[] { 0, 2, 2 }, new double[] { 0, 0, 2 });
+
+        IShapeIteraror iterator = shape.getIterator();
+        assertTrue(_checkPointInsideMask(polygon2d, iterator));
+    }
+
+    public void polygon2D() {
+
+    }
+
+    private boolean _checkPointInsideMask(RealMaskRealInterval box, IShapeIteraror iterator) {
+        boolean equals = true;
+        while (iterator.hasNext()) {
+            double[] point = Arrays.stream(iterator.next()).asDoubleStream().toArray();
+            PointMask pointMask = GeomMasks.pointMask(point);
+            equals &= box.test(pointMask);
+
+        }
+        return equals;
+    }
 }
