e4515902f3a7b27f69feb7b9c41c3fdee5cd40b7
Masoudas <Masoudas@hotmail.com>
2020-04-16 17:01:33 +0200

    Change interface to painter of stick

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/stick/IAngleStickPainter.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.core.visualization.figures.stickFigure.stick;
+
+import fr.fresnel.fourPolar.core.util.shape.IShapeIteraror;
+import fr.fresnel.fourPolar.core.visualization.figures.stickFigure.IStickFigure;
+
+/**
+ * An interface for painting a stick in a {@link IStickFigure}.
+ */
+public interface IAngleStickPainter {
+    /**
+     * Draw the sticks in the specified region by shapeIterator, for positions where
+     * intensity >= soiThreshold.
+     * 
+     * @param shapeIteraror is the iterator of the region over which we wish to draw
+     *                      sticks.
+     * @param soiThreshold  is the resired threshold for sum of intensity. For
+     *                      positions below threshold, no stick is drawn.
+     */
+    public void draw(IShapeIteraror shapeIteraror, double soiThreshold);
+
+    public IStickFigure getStickFigure();
+
+}
--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/stick/StickType.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/stickFigure/stick/StickType.java
@@ -1,6 +1,7 @@
 package fr.fresnel.fourPolar.core.visualization.figures.stickFigure.stick;
 
 public enum StickType {
+    Empty,
     Rho2D,
     Delta2D,
     Eta2D,
