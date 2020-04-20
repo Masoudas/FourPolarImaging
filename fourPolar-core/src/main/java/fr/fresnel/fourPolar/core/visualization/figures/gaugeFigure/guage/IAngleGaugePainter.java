d1ca6379d8dfc898bac35d441c699aea14123eee
Masoudas <Masoudas@hotmail.com>
2020-04-20 14:15:05 +0200

    Ask for shape instead of iterator

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/guage/IAngleGaugePainter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/guage/IAngleGaugePainter.java
@@ -1,5 +1,7 @@
 package fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.guage;
 
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import fr.fresnel.fourPolar.core.util.shape.IShape;
 import fr.fresnel.fourPolar.core.util.shape.IShapeIteraror;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
 
@@ -10,13 +12,16 @@ public interface IAngleGaugePainter {
     /**
      * Draw the sticks in the specified region by shapeIterator, for positions where
      * intensity >= soiThreshold.
+     * <p>
+     * For the shape, note that the pixels start from 0 to image dimension - 1. If a
+     * pixel of the shape is out of image dimension, no sticks are drawn, and the
+     * process continues.
      * 
-     * @param shapeIteraror is the iterator of the region over which we wish to draw
-     *                      sticks.
-     * @param soiThreshold  is the resired threshold for sum of intensity. For
-     *                      positions below threshold, no stick is drawn.
+     * @param region       is the region over which we wish to draw sticks.
+     * @param soiThreshold is the resired threshold for sum of intensity. For
+     *                     positions below threshold, no stick is drawn.
      */
-    public void draw(IShapeIteraror shapeIteraror, double soiThreshold);
+    public void draw(IShape region, UINT16 soiThreshold);
 
     /**
      * Returns the stick figure filled with this painter.
