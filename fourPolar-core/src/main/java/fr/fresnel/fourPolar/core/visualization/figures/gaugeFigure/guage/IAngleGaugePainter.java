538cc8e88e24bd477e8e943db985388104c6b7c3
Masoudas <Masoudas@hotmail.com>
2020-04-29 07:51:36 +0200

    Change comments

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/guage/IAngleGaugePainter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/guage/IAngleGaugePainter.java
@@ -2,7 +2,6 @@ package fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.guage;
 
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.util.shape.IShape;
-import fr.fresnel.fourPolar.core.util.shape.IShapeIterator;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
 
 /**
@@ -16,17 +15,14 @@ public interface IAngleGaugePainter {
      * For the region, note that the pixels start from 0 to image dimension - 1 of
      * the orientation image (or equivalently the soi Image). If the space dimension
      * of the provided region is greater than that of soi Image, an exception is
-     * thrown. Otherwise, if a pixel of the region is out of image dimension, no
-     * sticks are drawn. If the region's space dimension is less than that of the
-     * orientation image, it's automatically scaled to all higher dimensions. For
-     * example, the same 2D box region would be used for z = 0, 1, ... .
+     * thrown. Further constrains maybe set by implementation classes.
      * 
      * @param region       is the region over which we wish to draw sticks.
      * @param soiThreshold is the resired threshold for sum of intensity. For
      *                     positions below threshold, no stick is drawn.
      * 
-     * @throws IllegalArgumentException in case the dimension of the space of region
-     *                                  is greater than SoIImage dimension.
+     * @throws IllegalArgumentException in case the provided region does not satisfy
+     *                                  the constrains demanded by implementations.
      */
     public void draw(IShape region, UINT16 soiThreshold) throws IllegalArgumentException;
 
