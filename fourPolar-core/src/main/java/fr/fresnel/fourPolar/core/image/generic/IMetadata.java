49bc4da468783abe0d7f2a67d9ffea77c006df94
Masoudas <Masoudas@hotmail.com>
2020-04-29 13:19:46 +0200

    Add axisorder

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/IMetadata.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/IMetadata.java
@@ -1,9 +1,15 @@
 package fr.fresnel.fourPolar.core.image.generic;
 
+import fr.fresnel.fourPolar.core.physics.axis.AxisOrder;
+
 /**
  * An interface for accessing tiff metadata.
  */
 public interface IMetadata {
+    /**
+     * Returns the order of axis associated with the image.
+     */
+    public AxisOrder axisOrder();
 
     
 }
