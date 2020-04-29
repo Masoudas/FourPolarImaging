d5d819ae40a6eb00a0a813fc89782357e1787af0
Masoudas <Masoudas@hotmail.com>
2020-04-29 13:39:45 +0200

    Move metadat to base folder

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/Metadata.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/Metadata.java
@@ -1,21 +1,30 @@
-package fr.fresnel.fourPolar.core.image.generic.imgLib2Model;
+package fr.fresnel.fourPolar.core.image.generic;
 
 import java.util.Objects;
 
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.physics.axis.AxisOrder;
 
-public class ImgLib2Metadata implements IMetadata {
+/**
+ * Implements the metadata info for the ImgLib2 image model. Note that if 
+ * additional metadata fields are added, they should have default values. Otherwise,
+ * unexpected results may happen when there's dependency on metadata.
+ */
+class ImgLib2Metadata implements IMetadata {
     private AxisOrder _axisOrder;
 
     public class ImgLib2MetadataBuilder {
-        private AxisOrder _axisOrder;
+        private AxisOrder _axisOrder = AxisOrder.NoOrder;
 
-        public ImgLib2MetadataBuilder(AxisOrder axisOrder){
+        public ImgLib2MetadataBuilder() {
+
+        }
+
+        public void axisOrder(AxisOrder axisOrder){
             this._axisOrder = Objects.requireNonNull(axisOrder, "axisOrder must not be null")
         }
 
-        public ImgLib2Metadata build(){
+        public ImgLib2Metadata build() {
             return new ImgLib2Metadata(this);
         }
     }
