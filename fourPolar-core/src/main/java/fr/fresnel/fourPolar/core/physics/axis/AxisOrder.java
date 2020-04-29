90891455c656be2c51ea0d6b3fc7b2e9562a1f6f
Masoudas <Masoudas@hotmail.com>
2020-04-29 13:18:50 +0200

    Add axis order static method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrder.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrder.java
@@ -1,15 +1,19 @@
 package fr.fresnel.fourPolar.core.physics.axis;
 
 public enum AxisOrder {
-    XY, 
-    XYC, 
-    XYCT, 
-    XYCZT, 
-    XYT, 
-    XYTC, 
-    XYZ, 
-    XYZC, 
-    XYZCT, 
-    XYZT, 
-    XYZTC
+    NoOrder, XY, XYC, XYCT, XYCZT, XYT, XYTC, XYZ, XYZC, XYZCT, XYZT, XYZTC;
+
+    /**
+     * Returns the number of axis corresponding to the given order.
+     * 
+     * @return -1 if no order is defined, otherwise the number of axis.
+     */
+    public static int getNumAxis(AxisOrder axisOrder) {
+        if (axisOrder == NoOrder) {
+            return -1;
+        } else {
+            return axisOrder.name().length();
+        }
+    }
+
 }
