4cd6ca1e85b9b62993613b81c58e59230277948a
Masoudas <Masoudas@hotmail.com>
2020-04-29 13:04:51 +0200

    Define axis order

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrder.java
@@ -0,0 +1,15 @@
+package fr.fresnel.fourPolar.core.physics.axis;
+
+public enum AxisOrder {
+    XY, 
+    XYC, 
+    XYCT, 
+    XYCZT, 
+    XYT, 
+    XYTC, 
+    XYZ, 
+    XYZC, 
+    XYZCT, 
+    XYZT, 
+    XYZTC
+}
