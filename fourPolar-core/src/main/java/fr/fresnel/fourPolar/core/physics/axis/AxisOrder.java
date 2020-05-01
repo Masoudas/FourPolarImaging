b0548802bb466a5fa09d794cc8e7e1722df6a7ce
Masoudas <Masoudas@hotmail.com>
2020-05-01 10:28:28 +0200

    Add method to append z-axis

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrder.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrder.java
@@ -1,7 +1,9 @@
 package fr.fresnel.fourPolar.core.physics.axis;
 
 /**
- * Defines the order of axis associated with a data set.
+ * Defines the order of axis associated with a data set. NO FURTHER ORDERS
+ * SHOULD BE ADDED WITHOUT CAREFUL CONSIDERATION BY CHECKING THE ENTIRE CODE
+ * STRUCTURE, WHERE AXIS ORDER CONTRIBUTES.
  */
 public enum AxisOrder {
     NoOrder, XY, XYC, XYCT, XYCZT, XYT, XYTC, XYZ, XYZC, XYZCT, XYZT, XYZTC;
@@ -9,9 +11,11 @@ public enum AxisOrder {
     /**
      * Returns the number of labeled axis corresponding to the given order.
      * <p>
-     * WARNING: This method should not be used to check how many dimensions an entity has,
-     * given that it returns -1 for NoOrder. For example for the {@link Image} interface, 
-     * we should directly work with the dimension vector of image.
+     * WARNING: This method should not be used to check how many dimensions an
+     * entity has, given that it returns -1 for NoOrder. For example for the
+     * {@link Image} interface, we should directly work with the dimension vector of
+     * image.
+     * 
      * @return -1 if no order is defined, otherwise the number of axis.
      */
     public static int getNumDefinedAxis(AxisOrder axisOrder) {
@@ -50,6 +54,33 @@ public enum AxisOrder {
             return 2;
         } else {
             return -1;
+        }
+
+    }
+
+    /**
+     * If z-axis already present, returns the originial order. Else, appends z-axis
+     * to first convenient location of the given order. The goal is to increase the
+     * number of axis by one.
+     * 
+     * Any change to the above decision should seriously be entertained (@see
+     * WholeSampleStick3DPainterBuilder).
+     */
+    public static AxisOrder append_zAxis(AxisOrder axisOrder) {
+        if (AxisOrder.getZAxis(axisOrder) > 0) {
+            return axisOrder;
+        }
+
+        if (axisOrder == XY) {
+            return XYZ;
+        } else if (axisOrder == XYT) {
+            return XYZT;
+        } else if (axisOrder == XYC) {
+            return XYZC;
+        } else if (axisOrder == XYTC || axisOrder == XYCT) {
+            return XYCZT;
+        } else {
+            return NoOrder;
         }
 
     }
---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/physics/axis/AxisOrderTest.java
@@ -0,0 +1,16 @@
+package fr.fresnel.fourPolar.core.physics.axis;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+public class AxisOrderTest {
+    @Test
+    public void appendZ_NoZPresent_appendsToEndOfOrder() {
+        
+        assertTrue(AxisOrder.append_zAxis(AxisOrder.XY) == AxisOrder.XYZ);
+        assertTrue(AxisOrder.append_zAxis(AxisOrder.XYCT) == AxisOrder.XYCZT);
+        
+    }
+    
+}
