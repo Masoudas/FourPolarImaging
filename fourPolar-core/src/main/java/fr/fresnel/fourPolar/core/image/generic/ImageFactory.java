08c773daa86780caa4c4c5d9394d4732b0eacb91
Masoudas <Masoudas@hotmail.com>
2020-05-01 13:11:47 +0200

    Throws exception if dim not equal to axis order

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/ImageFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/ImageFactory.java
@@ -8,13 +8,23 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 public interface ImageFactory {
     /**
      * Create an image, with the given dimensions and type. All {@link IMetadata}
-     * fields would be set to default.
+     * fields would be set to default with this method.
      * 
      * @return
      */
     public <T extends PixelType> Image<T> create(long[] dim, T pixelType);
 
+    /**
+     * Create an image, with the given dimensions, type and metadata.
+     * 
+     * @param dim       is the dimension of the image.
+     * @param pixelType is the pixel type of the image
+     * @param metadata  is the metadata of the image.
 
-    public <T extends PixelType> Image<T> create(long[] dim, T pixelType, IMetadata metadata);
+     * @throws IllegalArgumentException if there's inconsitency between the metadata
+     *                                  and dimension.
+     */
+    public <T extends PixelType> Image<T> create(long[] dim, T pixelType, IMetadata metadata)
+            throws IllegalArgumentException;
 
 }
