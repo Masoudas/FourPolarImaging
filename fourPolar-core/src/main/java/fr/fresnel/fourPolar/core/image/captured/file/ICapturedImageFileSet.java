b7625e7cc5181b6a4e03cfbbb4631a68b64f764f
Masoudas <Masoudas@hotmail.com>
2020-05-04 15:07:31 +0200

    Returns a file array for each polarization label

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/captured/file/ICapturedImageFileSet.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/captured/file/ICapturedImageFileSet.java
@@ -5,25 +5,32 @@ import java.io.File;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 
-
 /**
- * An interface for accessing the image files provided in the original constellation format.
+ * An interface for accessing the image files provided in the original
+ * constellation format.
  */
-public interface ICapturedImageFileSet  {    
+public interface ICapturedImageFileSet {
     /**
-     * Returns the file. The labels are the one given by {@link Cameras};
+     * For each polarization label, returns an array of files that correspond to the
+     * given label. If there's a file present for each channel, this array would
+     * correspond to the number of channels. Otherwise, if multiple channels are
+     * present in a single image, the size of the array is one. The labels are the
+     * one given by {@link Cameras};
+     * 
      * @return
-     */ 
-    public File getFile(String label);
+     */
+    public File[] getFile(String label);
 
     /**
-     * Returns the name extract (a name abbrevation) for the file set.
+     * Returns the set name (a name abbrevation) associated with the given fileSet.
+     * 
      * @return
      */
     public String getSetName();
 
     /**
      * Returns the number of cameras corresponding to this file set.
+     * 
      * @return
      */
     public Cameras getnCameras();
@@ -33,16 +40,18 @@ public interface ICapturedImageFileSet  {
      */
     public boolean hasLabel(String label);
 
-
     @Override
     public boolean equals(Object obj);
-
 
     @Override
     public int hashCode();
 
     /**
      * Returns the part of metadata that is common to all the files in this fileSet.
+     * Of special intereset are {@link IMetadata#axisOrder()} and
+     * {@link IMetadata#numChannels()}. Using this denominator of metadata we can
+     * determine, for example, if the underlying fileset is that of a multi-channel
+     * image.
      */
     public IMetadata getMetadataIntersecion();
 }
