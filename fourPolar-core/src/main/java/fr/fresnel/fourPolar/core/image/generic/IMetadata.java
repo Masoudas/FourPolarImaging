827ebc1175149819579d74c3ad56f3410106e3f6
Masoudas <Masoudas@hotmail.com>
2020-05-05 12:47:12 +0200

    Return dim and pixelType

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/IMetadata.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/IMetadata.java
@@ -1,6 +1,7 @@
 package fr.fresnel.fourPolar.core.image.generic;
 
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 
 /**
  * An interface for accessing tiff metadata.
@@ -16,6 +17,16 @@ public interface IMetadata {
      * defined, returns -1.
      */
     public int numChannels();
+
+    /**
+     * Get the pixel type associated with the image.
+     */
+    public PixelTypes getPixelType();
+
+    /**
+     * Get dimension of the associated image.
+     */
+    public long[] getDim();
 
     
 }
