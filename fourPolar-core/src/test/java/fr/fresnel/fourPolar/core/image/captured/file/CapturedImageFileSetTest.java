f8d6b82e9e0fb870146359b54230115406fd4d8b
Masoudas <Masoudas@hotmail.com>
2020-05-08 12:42:48 +0200

    Rectify test

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/captured/file/CapturedImageFileSetTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/captured/file/CapturedImageFileSetTest.java
@@ -15,27 +15,28 @@ public class CapturedImageFileSetTest {
     File root = new File("/root");
 
     /**
-     * This test should not confuse us about the fact only pol0 file is used
-     * for creating the set name!
+     * This test should not confuse us about the fact only pol0 file is used for
+     * creating the set name!
      */
     @Test
     public void getSetName_SameFileSet_ReturnsEqualSetName() {
-        File[] pol0 = { new File(root, "pol0.tiff") };
-        File[] pol45 = { new File(root, "pol45.tiff") };
-        File[] pol90 = { new File(root, "pol90.tiff") };
-        File[] pol135 = { new File(root, "pol135.tiff") };
+        int[] channel = { 1 };
 
-        CapturedImageFileSet fileSet = new CapturedImageFileSet(null, pol0, pol45, pol90, pol135);
+        CapturedImageFile[] pol0 = { new CapturedImageFile(channel, new File(root, "pol0.tiff")) };
+        CapturedImageFile[] pol45 = { new CapturedImageFile(channel, new File(root, "pol45.tiff")) };
+        CapturedImageFile[] pol90 = { new CapturedImageFile(channel, new File(root, "pol90.tiff")) };
+        CapturedImageFile[] pol135 = { new CapturedImageFile(channel, new File(root, "pol135.tiff")) };
 
-        File[] pol0_1 = { new File(root, "pol0.tiff") };
-        File[] pol45_1 = { new File(root, "pol45.tiff") };
-        File[] pol90_1 = { new File(root, "pol90.tiff") };
-        File[] pol135_1 = { new File(root, "pol135.tiff") };
+        CapturedImageFileSet fileSet = new CapturedImageFileSet(pol0, pol45, pol90, pol135);
 
-        CapturedImageFileSet fileSet_1 = new CapturedImageFileSet(null, pol0_1, pol45_1, pol90_1, pol135_1);
+        CapturedImageFile[] pol0_1 = { new CapturedImageFile(channel, new File(root, "pol0.tiff")) };
+        CapturedImageFile[] pol45_1 = { new CapturedImageFile(channel, new File(root, "pol45.tiff")) };
+        CapturedImageFile[] pol90_1 = { new CapturedImageFile(channel, new File(root, "pol90.tiff")) };
+        CapturedImageFile[] pol135_1 = { new CapturedImageFile(channel, new File(root, "pol135.tiff")) };
+
+        CapturedImageFileSet fileSet_1 = new CapturedImageFileSet(pol0_1, pol45_1, pol90_1, pol135_1);
 
         assertTrue(fileSet.getSetName().equals(fileSet_1.getSetName()));
     }
-
 
 }
