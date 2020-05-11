38df71a9e0cce106219089b604773b2c262e38d4
Masoudas <Masoudas@hotmail.com>
2020-05-11 16:54:08 +0200

    Refactor for simpler implementation

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imageSet/acquisition/sample/finders/namePattern/FourCameraChannelImageFinder.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imageSet/acquisition/sample/finders/namePattern/FourCameraChannelImageFinder.java
@@ -2,62 +2,45 @@ package fr.fresnel.fourPolar.io.imageSet.acquisition.sample.finders.namePattern;
 
 import java.io.File;
 import java.util.ArrayList;
+import java.util.Iterator;
 import java.util.List;
 
 import fr.fresnel.fourPolar.core.exceptions.imageSet.acquisition.IncompatibleCapturedImage;
 import fr.fresnel.fourPolar.core.imageSet.acquisition.RejectedCapturedImage;
 import fr.fresnel.fourPolar.core.imageSet.acquisition.sample.SampleImageSet;
-import fr.fresnel.fourPolar.io.exceptions.imageSet.acquisition.sample.finders.namePattern.NoImageFoundOnRoot;
 
 class FourCameraChannelImageFinder implements IChannelImageFinder {
-    private List<RejectedCapturedImage> _rejectedImages;
 
     @Override
-    public List<RejectedCapturedImage> find(SampleImageSetByNamePatternFinder sampleSetFinder,
-            SampleImageSet sampleImageSet, int channel, String channelLabel) throws NoImageFoundOnRoot {
+    public Iterator<File[]> find(SampleImageSetByNamePatternFinder sampleSetFinder, String channelLabel) {
         String[] polLabel = sampleSetFinder.getPolLabel();
 
-        File[] imagesPol0 = sampleSetFinder.getRootFolder().listFiles(new FilterCapturedImage(polLabel[0], channelLabel,
-                sampleImageSet.getCapturedImageChecker().getExtension()));
+        File[] imagesPol0 = sampleSetFinder.getRootFolder().listFiles(new FilterCapturedImage(polLabel[0], channelLabel);
 
-        if (imagesPol0.length == 0) {
-            throw new NoImageFoundOnRoot("No images found for channel " + channel);
-        }
 
         File[] polFiles = new File[4];
-        this._rejectedImages = new ArrayList<RejectedCapturedImage>();
+        MatchingFilesIterator itrCreator = new MatchingFilesIterator();
         for (File imagePol0 : imagesPol0) {
             polFiles[0] = imagePol0;
 
-            int polCtr = 0;
-            boolean polCandidateFound = true;
-            while (++polCtr < 4 && polCandidateFound) {
+            boolean allPolsFound = true;
+            for (int polCtr = 0; polCtr < 4 && allPolsFound; polCtr++){
                 FilterPolarizationFile filterFile = new FilterPolarizationFile(imagePol0, polLabel[0],
                         polLabel[polCtr]);
                 File[] candidates = sampleSetFinder.getRootFolder().listFiles(filterFile);
 
                 if (candidates.length != 1) {
-                    polCandidateFound = false;
-                    this._rejectedImages.add(
-                            new RejectedCapturedImage(polFiles[0], "Other polarization images could no be detected."));
+                    allPolsFound = false;
                 } else {
                     polFiles[polCtr] = candidates[0];
                 }
             }
 
-            if (polCtr == 4) {
-                try {
-                    sampleImageSet.addImage(channel, polFiles[0], polFiles[1], polFiles[2], polFiles[3]);
-                } catch (IncompatibleCapturedImage e) {
-                    this._rejectedImages.add(e.getRejectedImage());
-                }
+            if (allPolsFound) {
+                    itrCreator.add(new File[]{polFiles[0], polFiles[1], polFiles[2], polFiles[3]});
             }
         }
 
-        if (sampleImageSet.getChannelImages(channel).isEmpty()) {
-            throw new NoImageFoundOnRoot("No images found for channel " + channel);
-        }
-
-        return this._rejectedImages;
+        return itrCreator.iterator();
     }
 }
