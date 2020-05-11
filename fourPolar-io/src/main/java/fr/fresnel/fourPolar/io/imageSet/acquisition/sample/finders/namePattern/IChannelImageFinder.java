e1dc13a1374859391325e9113fdbf838d5aa003d
Masoudas <Masoudas@hotmail.com>
2020-05-11 16:54:53 +0200

    Opt for simpler interface

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imageSet/acquisition/sample/finders/namePattern/IChannelImageFinder.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imageSet/acquisition/sample/finders/namePattern/IChannelImageFinder.java
@@ -1,16 +1,14 @@
 package fr.fresnel.fourPolar.io.imageSet.acquisition.sample.finders.namePattern;
 
-import java.util.List;
-
-import fr.fresnel.fourPolar.core.imageSet.acquisition.RejectedCapturedImage;
-import fr.fresnel.fourPolar.core.imageSet.acquisition.sample.SampleImageSet;
-import fr.fresnel.fourPolar.io.exceptions.imageSet.acquisition.sample.finders.namePattern.NoImageFoundOnRoot;
+import java.io.File;
+import java.util.Iterator;
 
 /**
- * An interface for finding the channel images with respect to cameras.
+ * An interface for finding the channel images with respect to cameras. 
+ * The resulting iterator would be empty if no files are found.
  */
 interface IChannelImageFinder {
-    public List<RejectedCapturedImage> find(SampleImageSetByNamePatternFinder sampleSetFinder,
-            SampleImageSet sampleImageSet, int channel, String channelLabel) throws NoImageFoundOnRoot;
+    public Iterator<File[]> find(SampleImageSetByNamePatternFinder sampleSetFinder,
+            String channelLabel);
 
 }
