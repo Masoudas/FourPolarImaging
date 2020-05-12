335acf2c63bbbb83833d08906718989cb5424ea9
Masoudas <Masoudas@hotmail.com>
2020-05-12 08:23:11 +0200

    Read metadata as well

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/RGB16SCIFIOTiffImageReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/RGB16SCIFIOTiffImageReader.java
@@ -4,10 +4,14 @@ import java.io.File;
 import java.io.IOException;
 import java.util.Objects;
 
+import fr.fresnel.fourPolar.core.exceptions.image.generic.axis.UnsupportedAxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.io.exceptions.image.generic.metadata.MetadataParseError;
 import fr.fresnel.fourPolar.io.image.generic.ImageReader;
+import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.metadata.SCIFIOTiffMetadataConverter;
 import io.scif.FormatException;
 import io.scif.Reader;
 import io.scif.config.SCIFIOConfig;
@@ -47,23 +51,23 @@ public class RGB16SCIFIOTiffImageReader implements ImageReader<RGB16> {
     }
 
     @Override
-    public Image<RGB16> read(File path) throws IOException {
-        Objects.requireNonNull(path, "Read path should not be null");
-
-        if (!path.exists()) {
-            throw new IOException("The given Tiff file does not exist.");
-        }
+    public Image<RGB16> read(File path) throws IOException, MetadataParseError {
+        Objects.requireNonNull(path, "path should not be null");
+        this._checkExtension(path.getName());
+        this._checkFileExists(path);
 
         this._reader.setSource(path.getAbsolutePath(), this._config);
 
+        // Read metadata
+        final IMetadata metadata = _readMetadata();
+
         // Read the raw image, which holds R, G and B as channels.
-        Img<UnsignedByteType> rawImage = this._imgOpener.openImgs(
-            this._reader, this._readType, this._config).get(0);
+        Img<UnsignedByteType> rawImage = this._imgOpener.openImgs(this._reader, this._readType, this._config).get(0);
 
         // Merger RGB channes to form ARGB image.
         Img<ARGBType> img = createARGBImage(rawImage);
 
-        return this._imgFactory.create(img, this._argbType);
+        return this._imgFactory.create(img, this._argbType, metadata);
     }
 
     /**
@@ -87,6 +91,22 @@ public class RGB16SCIFIOTiffImageReader implements ImageReader<RGB16> {
     }
 
     /**
+     * Read metadata independently of the underlying image.
+     * 
+     * @throws MetadataParseError if there are problems parsing the metadata.
+     */
+    private IMetadata _readMetadata() throws IOException, MetadataParseError {
+        IMetadata metadata = null;
+        try {
+            metadata = SCIFIOTiffMetadataConverter.convertFrom(this._reader.getMetadata().get(0));
+        } catch (UnsupportedAxisOrder e) {
+            throw new MetadataParseError(MetadataParseError.UNDEFINED_AXIS_ORDER);
+        }
+
+        return metadata;
+    }
+
+    /**
      * Sets the configuration for how the image is opened.
      * 
      * @return
@@ -96,6 +116,27 @@ public class RGB16SCIFIOTiffImageReader implements ImageReader<RGB16> {
         SCIFIOConfig config = new SCIFIOConfig();
         config.imgOpenerSetImgModes(SCIFIOConfig.ImgMode.AUTO);
         return config;
+    }
+
+    /**
+     * Check file extension is tif or tiff.
+     * 
+     * @param fileName
+     * @throws IOException
+     */
+    private void _checkExtension(String fileName) throws IOException {
+        String extension = fileName.substring(fileName.lastIndexOf('.')).toLowerCase();
+
+        if (!extension.equals("tiff") && !extension.equals("tif")) {
+            throw new IOException("The given file to tiff reader is not tiff");
+        }
+
+    }
+
+    private void _checkFileExists(File path) throws IOException {
+        if (!path.exists()) {
+            throw new IOException("The given Tiff file does not exist.");
+        }
     }
 
 }
