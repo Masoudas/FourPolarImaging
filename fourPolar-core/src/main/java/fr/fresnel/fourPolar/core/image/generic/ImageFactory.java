f2fa453ed56ab833168d6adfe43177d39f231468
Masoudas <Masoudas@hotmail.com>
2020-05-12 09:46:33 +0200

    Create image only with metadata

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/ImageFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/ImageFactory.java
@@ -7,14 +7,6 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
  */
 public interface ImageFactory {
     /**
-     * Create an image, with the given dimensions and type. All {@link IMetadata}
-     * fields would be set to default with this method.
-     * 
-     * @return
-     */
-    public <T extends PixelType> Image<T> create(long[] dim, T pixelType);
-
-    /**
      * Create an image based on its metadata.
      * 
      * @param metadata is the metadata of the desired image.
