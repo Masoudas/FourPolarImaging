a798f315d9f74850f04b47c5c6d788828432123c
Masoudas <Masoudas@hotmail.com>
2020-05-12 12:10:07 +0200

    Add utils

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/SCIFIOUtils.java
@@ -0,0 +1,28 @@
+package fr.fresnel.fourPolar.io.image.generic.tiff.scifio;
+
+import java.io.File;
+import java.io.IOException;
+
+class SCIFIOUtils {
+    /**
+     * Check file extension is tif or tiff.
+     * 
+     * @param fileName
+     * @throws IOException
+     */
+    public static void checkExtension(String fileName) throws IOException {
+        String extension = fileName.substring(fileName.lastIndexOf('.') + 1).toLowerCase();
+
+        if (!extension.equals("tiff") && !extension.equals("tif")) {
+            throw new IOException("The given file is not tiff");
+        }
+
+    }
+
+    public static void checkFileExists(File path) throws IOException {
+        if (!path.exists()) {
+            throw new IOException("The given Tiff file does not exist.");
+        }
+    }
+
+}
