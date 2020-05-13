f761d8cb84aaac0e2c960767e26d09569a61bd92
Masoudas <Masoudas@hotmail.com>
2020-05-13 11:58:22 +0200

    Check channel range

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/soi/TiffSoIImageReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/soi/TiffSoIImageReader.java
@@ -9,6 +9,7 @@ import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.soi.ISoIImage;
 import fr.fresnel.fourPolar.core.image.soi.SoIImage;
+import fr.fresnel.fourPolar.core.physics.channel.Channel;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.NoReaderFoundForImage;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.metadata.MetadataParseError;
 import fr.fresnel.fourPolar.io.image.generic.ImageReader;
@@ -21,13 +22,16 @@ import fr.fresnel.fourPolar.io.image.soi.file.TiffSoIImageFile;
  */
 public class TiffSoIImageReader implements ISoIImageReader {
     final private ImageReader<UINT16> _reader;
+    final private int _numChannels;
 
-    public TiffSoIImageReader(ImageFactory factory) throws NoReaderFoundForImage {
+    public TiffSoIImageReader(ImageFactory factory, int numChannels) throws NoReaderFoundForImage {
         this._reader = TiffImageReaderFactory.getReader(factory, UINT16.zero());
+        this._numChannels = numChannels;
     }
 
     @Override
     public ISoIImage read(File root4PProject, ICapturedImageFileSet fileSet, int channel) throws IOException {
+        Channel.checkChannel(channel, _numChannels);
         ISoIImageFile file = new TiffSoIImageFile(root4PProject, fileSet, channel);
 
         Image<UINT16> soi = null;
