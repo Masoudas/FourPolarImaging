c2522b16ed897852db049f11f797d5382d03517c
Masoudas <Masoudas@hotmail.com>
2020-05-13 14:05:09 +0200

    Add documentation

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/IPolarizationImage.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/polarization/IPolarizationImage.java
@@ -5,19 +5,23 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
- * An interface for proper access to a polarization image.
+ * An interface for accessing the polarization image. A polarization image is
+ * the image corresponding to one of the four polarizations of a particular
+ * channel.
  */
 public interface IPolarizationImage {
     /**
      * Returns the polarization of this image.
+     * 
      * @return
      */
     public Polarization getPolarization();
 
     /**
      * Returns the {@link Image} interface of this polarization image
+     * 
      * @return
      */
     public Image<UINT16> getImage();
-    
+
 }
