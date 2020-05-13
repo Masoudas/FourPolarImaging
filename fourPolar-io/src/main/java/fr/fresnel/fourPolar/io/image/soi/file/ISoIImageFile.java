607f8617bcf17f5a5e4436d5c1ae2ffbf089d783
Masoudas <Masoudas@hotmail.com>
2020-05-13 10:57:45 +0200

    Get channel

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/soi/file/ISoIImageFile.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/soi/file/ISoIImageFile.java
@@ -6,5 +6,13 @@ import java.io.File;
  * An interface for accessing the file corresponding to an {@link SoIImage}.
  */
 public interface ISoIImageFile {
+    /**
+     * @return the image file path of this soi image.
+     */
     public File getFile();
+
+    /**
+     * @return channel number associated with this soi image.
+     */
+    public int channel();
 }
