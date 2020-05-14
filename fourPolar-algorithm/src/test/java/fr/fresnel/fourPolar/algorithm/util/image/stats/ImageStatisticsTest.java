88e415ed1b0448f28eb62776f0c74334530facc2
Masoudas <Masoudas@hotmail.com>
2020-05-14 11:15:43 +0200

    Use metadata to create image

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/stats/ImageStatisticsTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/stats/ImageStatisticsTest.java
@@ -4,9 +4,11 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
 
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 
@@ -14,31 +16,32 @@ public class ImageStatisticsTest {
 
     @Test
     public void getNPlanes_ReturnsCorrectNPlanes() {
-        Image<UINT16> image = new ImgLib2ImageFactory().create(new long[]{2, 2, 2, 2, 2, 2}, UINT16.zero());
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 2, 2, 2, 2 }).build();
+        Image<UINT16> image = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
 
         assertTrue(ImageStatistics.getNPlanes(image) == 16);
 
-        
     }
 
     @Test
     public void getMinMax_4PlaneImage_ReturnsCorrectMinMaxPerPlane() {
-        Image<UINT16> image = new ImgLib2ImageFactory().create(new long[]{50, 50, 2, 2}, UINT16.zero());
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 50, 50, 2, 2 }).build();
+        Image<UINT16> image = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
 
         IPixelRandomAccess<UINT16> ra = image.getRandomAccess();
 
-        long[] loc = {25, 25, 0, 0};
+        long[] loc = { 25, 25, 0, 0 };
         for (int i = 0; i < 2; i++) {
             loc[3] = i;
             for (int j = 0; j < 2; j++) {
-               loc[2] = j;
-               ra.setPosition(loc);
-               ra.setPixel(new Pixel<UINT16>(new UINT16(25 + i + j)));
+                loc[2] = j;
+                ra.setPosition(loc);
+                ra.setPixel(new Pixel<UINT16>(new UINT16(25 + i + j)));
             }
         }
 
         double[][] minMax = ImageStatistics.getPlaneMinMax(image);
-        
+
         boolean equals = true;
         for (int i = 0; i < minMax.length; i++) {
             equals &= minMax[0][i] == 0;
@@ -53,25 +56,25 @@ public class ImageStatisticsTest {
 
     @Test
     public void getMinMax_CornerMinMax_CatchesCornetMinMax() {
-        long[][] loc = {{0, 0}, {0, 49}, {49, 0}, {49, 49}};
+        long[][] loc = { { 0, 0 }, { 0, 49 }, { 49, 0 }, { 49, 49 } };
         boolean equals = true;
 
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 50, 50 }).build();
         for (int j = 0; j < 4; j++) {
-            Image<UINT16> image = new ImgLib2ImageFactory().create(new long[]{50, 50}, UINT16.zero());
+            Image<UINT16> image = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
 
             IPixelRandomAccess<UINT16> ra = image.getRandomAccess();
             ra.setPosition(loc[j]);
             ra.setPixel(new Pixel<UINT16>(new UINT16(25)));
-            
+
             double[][] minMax = ImageStatistics.getPlaneMinMax(image);
-            
+
             equals &= minMax[0][0] == 0;
             equals &= minMax[1][0] == 25;
-    
+
         }
 
         assertTrue(equals);
     }
-
 
 }
