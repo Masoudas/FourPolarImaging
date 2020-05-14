ec960a7d268eea627f1c1778f8811b234cb010e9
Masoudas <Masoudas@hotmail.com>
2020-05-14 10:54:56 +0200

    Change to only convert XYCZT

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/converters/GrayScaleToColorConverter.java
@@ -5,9 +5,13 @@ import java.util.Objects;
 
 import fr.fresnel.fourPolar.algorithm.util.image.stats.ImageStatistics;
 import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.ConverterToImgLib2NotFound;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RealType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
@@ -24,23 +28,39 @@ import net.imglib2.type.numeric.real.DoubleType;
  */
 public class GrayScaleToColorConverter {
     /**
-     * Convertes an {@link UINT16} to an {@link RGB16} image. Note that an 8 bit
-     * lookup table is used for the conversion, hence there are only 256 white
-     * pixels. Note that each image plane is scaled with respect to it's minimum and
-     * maximum (not the maximum of the entire image.). If a plane has no maximum,
-     * all pixels will be black.
+     * Convertes the given channel of {@link UINT16} image to an {@link RGB16}
+     * image. Note that an 8 bit lookup table is used for the conversion, hence
+     * there are only 256 white pixels. Note that each image plane is scaled with
+     * respect to it's minimum and maximum (not the maximum of the entire image.).
+     * If a plane has no maximum, all pixels will be black.
      * 
-     * @throws ConverterToImgLib2NotFound
+     * 
+     * @throws ConverterToImgLib2NotFound if the image model can't be converted to
+     *                                    ImgLib2 model.
+     * @throws IllegalArgumentException   if the @param grayImage is not XYCZT, or
+     *                                    channel is not in the image.
+     * 
      */
-    public static <T extends RealType> void useMaxEachPlane(final Image<T> grayImage, final Image<RGB16> colorImage)
+    public static <T extends RealType> Image<RGB16> useMaxEachPlane_ImageXYCZT(final Image<T> grayImage, int channel)
             throws ConverterToImgLib2NotFound {
         Objects.requireNonNull(grayImage, "grayImage cannot be null");
         Objects.requireNonNull(grayImage, "colorImage cannot be null");
 
-        if (!Arrays.equals(grayImage.getMetadata().getDim(), colorImage.getMetadata().getDim())) {
-            throw new ArrayIndexOutOfBoundsException(
-                    "Cannot convert, because the two images don't have same dimension.");
+        if (grayImage.getMetadata().axisOrder() != AxisOrder.XYCZT) {
+            throw new IllegalArgumentException("Cannot convert, because the grayImage is not XYCZT");
         }
+
+        if (grayImage.getMetadata().getDim()[2] < channel) {
+            throw new IllegalArgumentException(
+                    "Cannot convert, because gray image does not have the specified channel. ");
+        }
+
+        long[] grayImgDim = grayImage.getMetadata().getDim();
+
+        long[] colorImgDim = { grayImgDim[0], grayImgDim[1], grayImgDim[3], grayImgDim[4] };
+        IMetadata colorMetadata = new Metadata.MetadataBuilder(colorImgDim).axisOrder(AxisOrder.XYZT)
+                .bitPerPixel(PixelTypes.RGB_16).build();
+        Image<RGB16> colorImage = grayImage.getFactory().create(colorMetadata, RGB16.zero());
 
         // We use ImgLib2 classes, i.e, we convert to Img. Then we convert the Image.
         // Finaly, we create a new Image instance and fill it, and return it.
@@ -52,7 +72,7 @@ public class GrayScaleToColorConverter {
         final ARGBType argbType = new ARGBType();
 
         final double[][] minMax = _getPlaneMinMax(grayImage);
-        
+
         DPoint planeDim = ImageStatistics.getPlaneDim(grayImage);
         final long planeSize = planeDim.x * planeDim.y;
 
@@ -64,8 +84,8 @@ public class GrayScaleToColorConverter {
                 planeNo++;
             }
 
-            final Converter<DoubleType, ARGBType> converter = new RealLUTConverter<DoubleType>(
-                    minMax[0][planeNo - 1], minMax[1][planeNo - 1], cTable8);
+            final Converter<DoubleType, ARGBType> converter = new RealLUTConverter<DoubleType>(minMax[0][planeNo - 1],
+                    minMax[1][planeNo - 1], cTable8);
             final IPixel<RGB16> pixel = colorCursor.next();
 
             doubleType.set(grayCursor.next().value().getRealValue());
@@ -75,12 +95,14 @@ public class GrayScaleToColorConverter {
             pixel.value().set(ARGBType.red(color), ARGBType.green(color), ARGBType.blue(color));
             colorCursor.setPixel(pixel);
         }
+
+        return colorImage;
     }
 
     private static <T extends RealType> double[][] _getPlaneMinMax(final Image<T> grayImage) {
         final double[][] minMax = ImageStatistics.getPlaneMinMax(grayImage);
         for (int i = 0; i < minMax[0].length; i++) {
-            if (minMax[0][i] == minMax[1][i]){
+            if (minMax[0][i] == minMax[1][i]) {
                 minMax[1][i]++;
             }
         }
