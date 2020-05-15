932d979f6c597e4819ded3349b5dc4b41115824b
Masoudas <Masoudas@hotmail.com>
2020-05-15 09:17:53 +0200

    Use box shape instead of rectangle

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/FieldOfView.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/FieldOfView.java
@@ -3,15 +3,15 @@ package fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov;
 import java.util.Hashtable;
 
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
-import fr.fresnel.fourPolar.core.util.DRectangle;
+import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 
 /**
  * Models the field of view of the bead images of a particular channel.
  */
 public class FieldOfView implements IFieldOfView {
-    private Hashtable<Polarization, DRectangle> _fov = new Hashtable<Polarization, DRectangle>(4);
+    private Hashtable<Polarization, IBoxShape> _fov = new Hashtable<Polarization, IBoxShape>(4);
 
-    public FieldOfView(DRectangle pol0, DRectangle pol45, DRectangle pol90, DRectangle pol135){
+    public FieldOfView(IBoxShape pol0, IBoxShape pol45, IBoxShape pol90, IBoxShape pol135){
         _fov.put(Polarization.pol0, pol0);
         _fov.put(Polarization.pol45, pol45);
         _fov.put(Polarization.pol90, pol90);
@@ -19,7 +19,7 @@ public class FieldOfView implements IFieldOfView {
     }
 
     @Override
-    public DRectangle getFoV(Polarization pol) {
+    public IBoxShape getFoV(Polarization pol) {
         return _fov.get(pol);
     }
     
--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/IFieldOfView.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/IFieldOfView.java
@@ -1,12 +1,12 @@
 package fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov;
 
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
-import fr.fresnel.fourPolar.core.util.DRectangle;
+import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 
 /**
  * The interface for accessing the field of view (fov) information of the bead images.
  */
 public interface IFieldOfView {
-    public DRectangle getFoV(Polarization pol);
+    public IBoxShape getFoV(Polarization pol);
     
 }
