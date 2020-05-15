410dbe6500b7195c5d90f690e2a51b9e3ebecc5d
Masoudas <Masoudas@hotmail.com>
2020-05-15 12:18:57 +0200

    Implement

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/TwoCameraConstellation.java
@@ -0,0 +1,48 @@
+package fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov;
+
+/**
+ * Defines the placement of polarization in the bead (or sample) image for the
+ * two camera case.
+ */
+public class TwoCameraConstellation {
+    public enum Position {
+        Left, Right
+    };
+
+    final private Position _pol0;
+    final private Position _pol45;
+    final private Position _pol90;
+    final private Position _pol135;
+
+    /**
+     * 
+     * @throws IllegalArgumentException position for pol0 and pol90, and pol45 and
+     *                                  pol135 must be unequal.
+     */
+    public TwoCameraConstellation(Position pol0, Position pol45, Position pol90, Position pol135) {
+        if (pol0 == pol90 || pol45 == pol135) {
+            throw new IllegalArgumentException("position for pol0 and pol90, and pol45 and pol135 must be unequal.");
+        }
+
+        _pol0 = pol0;
+        _pol45 = pol45;
+        _pol90 = pol90;
+        _pol135 = pol135;
+    }
+
+    public Position pol0() {
+        return _pol0;
+    }
+
+    public Position pol45() {
+        return _pol45;
+    }
+
+    public Position pol90() {
+        return _pol90;
+    }
+
+    public Position pol135() {
+        return _pol135;
+    }
+}
