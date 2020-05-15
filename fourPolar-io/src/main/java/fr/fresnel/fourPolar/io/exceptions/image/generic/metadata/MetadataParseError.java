bb07ae0ecf51bd0bb5889ad454095320a62f27f9
Masoudas <Masoudas@hotmail.com>
2020-05-15 08:03:05 +0200

    Separete types of error

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataParseError.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataParseError.java
@@ -7,12 +7,19 @@ public class MetadataParseError extends Exception {
     private static final long serialVersionUID = 5374378579477003124L;
 
     /**
-     * This error message is printed in case the axis order defined in the metadata
-     * does not match any of those defined in {@link AxisOrder}.
+     * This error message is printed in case an axis in metadata does not correspond
+     * to x, y, z, time or channel.
+     */
+    public static final String UNDEFINED_AXIS = "Undefined axis label.";
+
+    /**
+     * This error message is printed in case all axis in the image are defined, but
+     * no proper axis order can be deduced from {@link AxisOrder} (including
+     * NoOrder).
      */
     public static final String UNDEFINED_AXIS_ORDER = "Axis order can't be set.";
 
-    public MetadataParseError(String message){
+    public MetadataParseError(String message) {
         super(message);
     }
 }
