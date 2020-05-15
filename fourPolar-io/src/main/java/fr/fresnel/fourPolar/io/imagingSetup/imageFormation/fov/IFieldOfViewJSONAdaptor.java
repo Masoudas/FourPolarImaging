deb2c05e8a76711ebeeaaee70e82e453ce4fd978
Masoudas <Masoudas@hotmail.com>
2020-05-15 09:29:57 +0200

    Use box writer

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imagingSetup/imageFormation/fov/IFieldOfViewJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/imagingSetup/imageFormation/fov/IFieldOfViewJSONAdaptor.java
@@ -6,6 +6,7 @@ import com.fasterxml.jackson.annotation.JsonPropertyOrder;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.FieldOfView;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.IFieldOfView;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+import fr.fresnel.fourPolar.io.util.shape.IBoxShape2DJSONAdaptor;
 
 /**
  * This class is used as an adaptor of {@link IFieldOfView} to JSON.
@@ -13,16 +14,16 @@ import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 @JsonPropertyOrder({ "pol0", "pol45", "pol90", "pol135" })
 public class IFieldOfViewJSONAdaptor {
     @JsonProperty("pol0")
-    private RectangleJSONAdaptor _pol0;
+    private IBoxShape2DJSONAdaptor _pol0;
 
     @JsonProperty("pol45")
-    private RectangleJSONAdaptor _pol45;
+    private IBoxShape2DJSONAdaptor _pol45;
 
     @JsonProperty("pol90")
-    private RectangleJSONAdaptor _pol90;
+    private IBoxShape2DJSONAdaptor _pol90;
 
     @JsonProperty("pol135")
-    private RectangleJSONAdaptor _pol135;
+    private IBoxShape2DJSONAdaptor _pol135;
 
     /**
      * Allows the interface to be written to JSON as an instance of this class.
@@ -47,22 +48,22 @@ public class IFieldOfViewJSONAdaptor {
     }
 
     private void _setPol0(IFieldOfView fov) {
-        _pol0 = new RectangleJSONAdaptor();
+        _pol0 = new IBoxShape2DJSONAdaptor();
         _pol0.toYaml(fov.getFoV(Polarization.pol0));
     }
 
     private void _setPol45(IFieldOfView fov) {
-        _pol45 = new RectangleJSONAdaptor();
+        _pol45 = new IBoxShape2DJSONAdaptor();
         _pol45.toYaml(fov.getFoV(Polarization.pol45));
     }
 
     private void _setPol90(IFieldOfView fov) {
-        _pol90 = new RectangleJSONAdaptor();
+        _pol90 = new IBoxShape2DJSONAdaptor();
         _pol90.toYaml(fov.getFoV(Polarization.pol90));
     }
 
     private void _setPol135(IFieldOfView fov) {
-        _pol135 = new RectangleJSONAdaptor();
+        _pol135 = new IBoxShape2DJSONAdaptor();
         _pol135.toYaml(fov.getFoV(Polarization.pol135));
     }
 
