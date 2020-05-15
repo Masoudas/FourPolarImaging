6289b7943a3d83317c48c4b6e28b4585606af6be
Masoudas <Masoudas@hotmail.com>
2020-05-15 10:35:58 +0200

    Check pixels start from one

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/FieldOfView.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/FieldOfView.java
@@ -1,5 +1,6 @@
 package fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov;
 
+import java.util.Arrays;
 import java.util.Hashtable;
 
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
@@ -11,7 +12,12 @@ import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 public class FieldOfView implements IFieldOfView {
     private Hashtable<Polarization, IBoxShape> _fov = new Hashtable<Polarization, IBoxShape>(4);
 
-    public FieldOfView(IBoxShape pol0, IBoxShape pol45, IBoxShape pol90, IBoxShape pol135){
+    public FieldOfView(IBoxShape pol0, IBoxShape pol45, IBoxShape pol90, IBoxShape pol135) {
+        _checkNonZero(pol0);
+        _checkNonZero(pol45);
+        _checkNonZero(pol90);
+        _checkNonZero(pol135);
+
         _fov.put(Polarization.pol0, pol0);
         _fov.put(Polarization.pol45, pol45);
         _fov.put(Polarization.pol90, pol90);
@@ -22,5 +28,14 @@ public class FieldOfView implements IFieldOfView {
     public IBoxShape getFoV(Polarization pol) {
         return _fov.get(pol);
     }
-    
+
+    /**
+     * Check to make sure that pixels don't start from zero.
+     */
+    public void _checkNonZero(IBoxShape pol) {
+        if (Arrays.stream(pol.min()).summaryStatistics().getMin() == 0) {
+            throw new IllegalArgumentException("Pixels must start from one, and not zero");
+        }
+
+    }
 }
--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/IFieldOfView.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/imagingSetup/imageFormation/fov/IFieldOfView.java
@@ -4,9 +4,11 @@ import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 
 /**
- * The interface for accessing the field of view (fov) information of the bead images.
+ * The interface for accessing the field of view (fov) information of the bead
+ * images. Note that for convenience, the pixels are supposed to start from 1 to
+ * dimension of the image.
  */
 public interface IFieldOfView {
     public IBoxShape getFoV(Polarization pol);
-    
+
 }
