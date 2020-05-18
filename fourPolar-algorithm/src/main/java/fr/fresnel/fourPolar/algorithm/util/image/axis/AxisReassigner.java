298e69bd3a72bf2704b5365607668bfdacd47096
Masoudas <Masoudas@hotmail.com>
2020-05-18 10:28:54 +0200

    Implement to XYCZT reassigner

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/AxisReassigner.java
@@ -0,0 +1,39 @@
+package fr.fresnel.fourPolar.algorithm.util.image.axis;
+
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
+/**
+ * Using this class, we can reassign the axis of an image. In other words, we
+ * change the {@link AxisOrder} of an image to a new order.
+ */
+public class AxisReassigner {
+    private AxisReassigner() {
+        throw new AssertionError();
+    }
+
+    /**
+     * Reassigns the axis of an image. Note that if an axis does not exist, it's
+     * appended to the new image with dimension one.
+     * 
+     * @throws IllegalArgumentException if the old or new axis don't start with XY.
+     */
+    public static <T extends PixelType> Image<T> reassignToXYCZT(Image<T> image, T t) {
+        return new ReassingerToXYCZT().reassign(image, t);
+    }
+
+
+    // /**
+    //  * Breaks down the channels of the given image and returns an array of images, where each
+    //  * element is a channel image. Note that axis order of the new image would be that of 
+    //  * @param <T>
+    //  * @param image
+    //  * @param t
+    //  * @return
+    //  */
+    // public static <T extends PixelType> Image<T>[] breakChannels(Image<T> image, T t) {
+        
+    // }
+
+}
---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/ReassingerToXYCZT.java
@@ -0,0 +1,76 @@
+package fr.fresnel.fourPolar.algorithm.util.image.axis;
+
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+
+class ReassingerToXYCZT {
+
+    /**
+     * Reassigns the axis of an image. Note that if an axis does not exist, it's
+     * appended to the new image with dimension one.
+     * 
+     * @throws IllegalArgumentException if the old or new axis don't start with XY.
+     */
+    public <T extends PixelType> Image<T> reassign(Image<T> image, T t) {
+        if (!image.getMetadata().axisOrder().name().substring(0, 2).equals("XY")) {
+            throw new IllegalArgumentException("Axis does not start with XY");
+        }
+
+        IMetadata metadata_xyczt = _createMetadata(image.getMetadata());
+        Image<T> reassignedImage = image.getFactory().create(metadata_xyczt, t);
+
+        AxisOrder oldAxisOrder = image.getMetadata().axisOrder();
+        IPixelRandomAccess<T> ra = reassignedImage.getRandomAccess();
+        for (IPixelCursor<T> cursor = image.getCursor(); cursor.hasNext();) {
+            IPixel<T> pixel = cursor.next();
+            long[] position_xyczt = _convertPositionToXYCZT(cursor.localize(), oldAxisOrder);
+
+            ra.setPosition(position_xyczt);
+            ra.setPixel(pixel);
+        }
+
+        return reassignedImage;
+    }
+
+    /**
+     * Create metdata of the reassigned image. If an axis does not exist, set it to
+     * one.
+     * 
+     * @param metadata
+     * @return
+     */
+    private IMetadata _createMetadata(IMetadata metadata) {
+        int z_axis = metadata.axisOrder().z_axis;
+        int c_axis = metadata.axisOrder().c_axis;
+        int t_axis = metadata.axisOrder().t_axis;
+
+        long[] dim = metadata.getDim();
+        long[] dim_xyczt = { dim[0], dim[1], 0, 0, 0 };
+
+        dim_xyczt[2] = c_axis > 0 ? dim[c_axis] : 1;
+        dim_xyczt[3] = z_axis > 0 ? dim[z_axis] : 1;
+        dim_xyczt[4] = t_axis > 0 ? dim[t_axis] : 1;
+
+        return new Metadata.MetadataBuilder(dim_xyczt).axisOrder(AxisOrder.XYCZT).bitPerPixel(metadata.bitPerPixel())
+                .build();
+    }
+
+    private long[] _convertPositionToXYCZT(long[] position, AxisOrder oldAxisOrder) {
+        long[] position_xyczt = { position[0], position[1], 0, 0, 0 };
+
+        if (oldAxisOrder.c_axis > 0)
+            position_xyczt[2] = position[oldAxisOrder.c_axis];
+        if (oldAxisOrder.z_axis > 0)
+            position_xyczt[3] = position[oldAxisOrder.z_axis];
+        if (oldAxisOrder.t_axis > 0)
+            position_xyczt[4] = position[oldAxisOrder.t_axis];
+
+        return position_xyczt;
+    }
+}
