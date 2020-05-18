5517f77d821609191f594c696449ac5453331c45
Masoudas <Masoudas@hotmail.com>
2020-05-18 07:42:51 +0200

    Move interval cursor test to cursor tests

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2ImageTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2ImageTest.java
@@ -52,40 +52,4 @@ public class ImgLib2ImageTest {
         assertTrue(counter == 1);
     }
 
-    @Test
-    public void getCursor_IntervalCursor_ShouldGetAndSetProperly() throws ConverterNotFound {
-        long[] dimensions = new long[] { 5, 1 };
-        UnsignedShortType type = new UnsignedShortType();
-        Img<UnsignedShortType> img = new ArrayImgFactory<UnsignedShortType>(type).create(dimensions);
-        TypeConverter<UINT16, UnsignedShortType> converter = TypeConverterFactory.getConverter(UINT16.zero(), type);
-        IMetadata metadata = new Metadata.MetadataBuilder(dimensions).bitPerPixel(PixelTypes.UINT_16)
-                .axisOrder(AxisOrder.XY).build();
-
-        int pixelNumber = 2;
-        int intervalLen = 2;
-        ImgLib2Image<UINT16, UnsignedShortType> image = new ImgLib2Image<UINT16, UnsignedShortType>(img, converter,
-                null, metadata);
-        IPixelCursor<UINT16> cursor = image.getCursor(new long[] { pixelNumber, 0 }, new long[] { intervalLen, 0 });
-
-        int counter = 0;
-        while (cursor.hasNext()) {
-            System.out.println(counter);
-            cursor.next();
-            cursor.setPixel(new Pixel<UINT16>(new UINT16(3)));
-        }
-
-        IPixelCursor<UINT16> wholeImageCursor = image.getCursor();
-        boolean equals = true;
-        counter = 0;
-        while (wholeImageCursor.hasNext()) {
-            if (counter++ > 2 && counter < 5)
-                equals = wholeImageCursor.next().value().get() == 3;
-            else
-                equals = wholeImageCursor.next().value().get() == 0;
-        }
-
-        assertTrue(equals);
-
-    }
-
 }
--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2PixelCursorTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2PixelCursorTest.java
@@ -8,15 +8,22 @@ import java.util.List;
 
 import org.junit.jupiter.api.Test;
 
+import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.types.ConverterNotFound;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.types.TypeConverter;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.types.TypeConverterFactory;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.integer.UnsignedShortType;
 
 public class ImgLib2PixelCursorTest {
     @Test
@@ -107,6 +114,42 @@ public class ImgLib2PixelCursorTest {
         }
 
         assertTrue(equals);
+    }
+
+    @Test
+    public void getCursor_IntervalCursor_ShouldGetAndSetProperly() throws ConverterNotFound {
+        long[] dimensions = new long[] { 5, 1 };
+        UnsignedShortType type = new UnsignedShortType();
+        Img<UnsignedShortType> img = new ArrayImgFactory<UnsignedShortType>(type).create(dimensions);
+        TypeConverter<UINT16, UnsignedShortType> converter = TypeConverterFactory.getConverter(UINT16.zero(), type);
+
+        IMetadata metadata = new Metadata.MetadataBuilder(dimensions).bitPerPixel(PixelTypes.UINT_16)
+                .axisOrder(AxisOrder.XY).build();
+
+        int pixelNumber = 3;
+        int intervalLen = 2;
+        ImgLib2Image<UINT16, UnsignedShortType> image = new ImgLib2Image<UINT16, UnsignedShortType>(img, converter,
+                null, metadata);
+        IPixelCursor<UINT16> cursor = image.getCursor(new long[] { pixelNumber, 0 }, new long[] { intervalLen, 1 });
+
+        while (cursor.hasNext()) {
+            cursor.next();
+            System.out.println(cursor.localize()[0]);
+            cursor.setPixel(new Pixel<UINT16>(new UINT16(3)));
+        }
+
+        IPixelCursor<UINT16> wholeImageCursor = image.getCursor();
+        boolean equals = true;
+        int counter = 0;
+        while (wholeImageCursor.hasNext() && counter++ < 5) {
+            if (counter > 3)
+                equals &= wholeImageCursor.next().value().get() == 3;
+            else
+                equals &= wholeImageCursor.next().value().get() == 0;
+        }
+
+        assertTrue(equals && counter == 5);
+
     }
 
 }
