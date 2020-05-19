85604162120def02b0806908654ada17d00057c8
Masoudas <Masoudas@hotmail.com>
2020-05-19 09:32:56 +0200

    Abstraction for polarization segmenter

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segment/PolarizationSegmenter.java
@@ -0,0 +1,26 @@
+package fr.fresnel.fourPolar.algorithm.preprocess.segment;
+
+import fr.fresnel.fourPolar.core.image.captured.ICapturedImage;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
+
+/**
+ * An abstract class for segmenting captured images using the fov.
+ */
+abstract class PolarizationSegmenter {
+    public abstract Image<UINT16>[] segment(ICapturedImage[] capturedImages, IBoxShape polFoV, int numChannels);
+
+    protected Image<UINT16> _createSegmentedImageFromInterval(Image<UINT16> image, long[] bottomCorner, long[] len) {
+        IPixelCursor<UINT16> segmentedImgCursor = image.getCursor(bottomCorner, len);
+        IMetadata segmentedImgMetadata = new Metadata.MetadataBuilder(len).axisOrder(image.getMetadata().axisOrder())
+                .bitPerPixel(PixelTypes.UINT_16).build();
+        Image<UINT16> segmentedImage = new SegmentedImage(segmentedImgCursor, image.getFactory(), segmentedImgMetadata);
+        return segmentedImage;
+    }
+
+}
