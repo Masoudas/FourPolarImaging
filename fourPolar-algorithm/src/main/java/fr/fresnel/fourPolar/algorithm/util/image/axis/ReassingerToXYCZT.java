f50dc0c331aa6f6db584b4bf79b9dc62d9e83a1b
Masoudas <Masoudas@hotmail.com>
2020-05-19 09:26:20 +0200

    Constructor accepts image instead of cursor

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/ReassingerToXYCZT.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/ReassingerToXYCZT.java
@@ -4,7 +4,6 @@ import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
@@ -20,29 +19,24 @@ class ReassingerToXYCZT {
      * not exist, it's appended to the new image with dimension one. Returns the
      * same image if it is XYCZT.
      * 
-     * @param cursor   is the cursor of the image.
-     * @param metadata is the metadata of the associated image.
-     * @param factory  is the factory for creating the image inteface.
-     * @param t        is the image pixel type.
+     * @param image is the image interface to be reassigned.
+     * @param t     is the image pixel type.
      * 
      * @throws IllegalArgumentException if the old or new axis don't start with XY,
      *                                  or if dimension of cursor and metadata don't
      *                                  match.
      */
-    public static <T extends PixelType> Image<T> reassign(IPixelCursor<T> cursor, IMetadata metadata,
-            ImageFactory factory, T t) {
-        if (!metadata.axisOrder().name().substring(0, 2).equals("XY")) {
+    public static <T extends PixelType> Image<T> reassign(Image<T> image, T t) {
+        if (!image.getMetadata().axisOrder().name().substring(0, 2).equals("XY")) {
             throw new IllegalArgumentException("Axis does not start with XY");
         }
 
-        _cursorHasSameDimAsMetadata(cursor, metadata);
+        IMetadata metadata_xyczt = _createMetadata(image.getMetadata());
+        Image<T> reassignedImage = image.getFactory().create(metadata_xyczt, t);
 
-        IMetadata metadata_xyczt = _createMetadata(metadata);
-        Image<T> reassignedImage = factory.create(metadata_xyczt, t);
-
-        AxisOrder oldAxisOrder = metadata.axisOrder();
+        AxisOrder oldAxisOrder = image.getMetadata().axisOrder();
         IPixelRandomAccess<T> ra = reassignedImage.getRandomAccess();
-        for (; cursor.hasNext();) {
+        for (IPixelCursor<T> cursor = image.getCursor(); cursor.hasNext();) {
             IPixel<T> pixel = cursor.next();
             long[] position_xyczt = _convertPositionToXYCZT(cursor.localize(), oldAxisOrder);
 
@@ -87,13 +81,5 @@ class ReassingerToXYCZT {
             position_xyczt[4] = position[oldAxisOrder.t_axis];
 
         return position_xyczt;
-    }
-
-    private static void _cursorHasSameDimAsMetadata(IPixelCursor<?> cursor, IMetadata metadata) {
-        cursor.next();
-        if (cursor.localize().length != metadata.getDim().length) {
-            throw new IllegalArgumentException("Dimension of cursor and metadata don't match");
-        }
-
     }
 }
