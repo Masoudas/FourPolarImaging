d5f8dd9837a84f79f027c9d53314080eadff94da
Masoudas <Masoudas@hotmail.com>
2020-05-19 09:34:59 +0200

    Rename interface

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segment/CameraImageSegmenter.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.algorithm.preprocess.segment;
+
+import fr.fresnel.fourPolar.core.image.captured.ICapturedImageSet;
+import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
+
+/**
+ * An interface for segmenting the captured images.
+ */
+interface CameraImageSegmenter {
+    /**
+     * Define the captured image set to be segmented.
+     */
+    public void setCapturedImageSet(ICapturedImageSet capturedImageSet);
+
+    /**
+     * Returns the polarization image set of the given channel.
+     * 
+     * @param channel
+     * @return
+     */
+    public IPolarizationImageSet segment(int channel);
+
+}
