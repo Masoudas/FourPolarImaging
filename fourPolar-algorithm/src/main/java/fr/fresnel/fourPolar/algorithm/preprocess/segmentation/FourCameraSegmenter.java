c6592c1017ce4a88e9989f75b8c1eeb266418f19
Masoudas <Masoudas@hotmail.com>
2020-05-21 07:02:53 +0200

    Define abstract class

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/FourCameraSegmenter.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/FourCameraSegmenter.java
@@ -1,43 +1,18 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.segmentation;
 
-import fr.fresnel.fourPolar.algorithm.util.image.axis.AxisReassigner;
-import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
 import fr.fresnel.fourPolar.core.image.captured.ICapturedImage;
 import fr.fresnel.fourPolar.core.image.captured.ICapturedImageSet;
-import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
-import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
-import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
-import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageSetBuilder;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.IFieldOfView;
-import fr.fresnel.fourPolar.core.physics.channel.ChannelUtils;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 
-public class FourCameraSegmenter implements ConstellationSegmenter {
-    /**
-     * Holds the captured images of the corresponding channels in ascending order.
-     */
-    private final IFieldOfView _fov;
-    private final int _numChannels;
-    private final ChannelPolarizationSegmenter _singleChannelSegmenter;
-    private final ChannelPolarizationSegmenter _multiChannelSegmenter;
-
-    private Image<UINT16>[] _pol0;
-    private Image<UINT16>[] _pol45;
-    private Image<UINT16>[] _pol90;
-    private Image<UINT16>[] _pol135;
-
-    private ICapturedImageFileSet _fileSet;
-
+public class FourCameraSegmenter extends ConstellationSegmenter {
     public FourCameraSegmenter(IFieldOfView fov, int numChannels) {
-        this._numChannels = numChannels;
-        this._fov = fov;
-        this._singleChannelSegmenter = new SingleChannelPolarizationSegmenter();
-        this._multiChannelSegmenter = new MultiChannelPolarizationSegmenter();
+        super(fov, numChannels);
     }
 
+    @Override
     public void setCapturedImageSet(ICapturedImageSet capturedImageSet) {
         ChannelPolarizationSegmenter segmenter = _selectSegmenter(capturedImageSet);
 
@@ -60,34 +35,6 @@ public class FourCameraSegmenter implements ConstellationSegmenter {
         this._pol135 = segmenter.segment(capturedImages_pol135, pol135FoV);
 
         this._fileSet = capturedImageSet.fileSet();
-    }
-
-    private ChannelPolarizationSegmenter _selectSegmenter(ICapturedImageSet capturedImageSet) {
-        if (capturedImageSet.hasMultiChannelImage()) {
-            return _multiChannelSegmenter;
-        } else {
-            return _singleChannelSegmenter;
-        }
-    }
-
-    @Override
-    public IPolarizationImageSet segment(int channel) {
-        ChannelUtils.checkChannel(channel, this._numChannels);
-
-        Image<UINT16> pol0 = AxisReassigner.reassignToXYCZT(this._pol0[channel - 1], UINT16.zero());
-        Image<UINT16> pol45 = AxisReassigner.reassignToXYCZT(this._pol45[channel - 1], UINT16.zero());
-        Image<UINT16> pol90 = AxisReassigner.reassignToXYCZT(this._pol90[channel - 1], UINT16.zero());
-        Image<UINT16> pol135 = AxisReassigner.reassignToXYCZT(this._pol135[channel - 1], UINT16.zero());
-
-        IPolarizationImageSet polImageSet = null;
-        try {
-            polImageSet = new PolarizationImageSetBuilder(this._numChannels).pol0(pol0).pol45(pol45).pol90(pol90)
-                    .pol135(pol135).channel(channel).fileSet(this._fileSet).build();
-        } catch (CannotFormPolarizationImageSet e) {
-            // Since all images have the same dimension, exception is never caught.
-        }
-
-        return polImageSet;
     }
 
 }
--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/OneCameraSegmenter.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/OneCameraSegmenter.java
@@ -1,47 +1,18 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.segmentation;
 
-import fr.fresnel.fourPolar.algorithm.util.image.axis.AxisReassigner;
-import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
 import fr.fresnel.fourPolar.core.image.captured.ICapturedImage;
 import fr.fresnel.fourPolar.core.image.captured.ICapturedImageSet;
-import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
-import fr.fresnel.fourPolar.core.image.generic.IMetadata;
-import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
-import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
-import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
-import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageSetBuilder;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.IFieldOfView;
-import fr.fresnel.fourPolar.core.physics.channel.ChannelUtils;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 
 /**
  * Segments the images of a one camera setup.
  */
-class OneCameraSegmenter implements ConstellationSegmenter {
-    /**
-     * Holds the captured images of the corresponding channels in ascending order.
-     */
-    private final IFieldOfView _fov;
-    private final int _numChannels;
-    private final ChannelPolarizationSegmenter _singleChannelSegmenter;
-    private final ChannelPolarizationSegmenter _multiChannelSegmenter;
-
-    private Image<UINT16>[] _pol0 = null;
-    private Image<UINT16>[] _pol45 = null;
-    private Image<UINT16>[] _pol90 = null;
-    private Image<UINT16>[] _pol135 = null;
-
-    private ICapturedImageFileSet _fileSet;
-
+class OneCameraSegmenter extends ConstellationSegmenter {
     public OneCameraSegmenter(IFieldOfView fov, int numChannels) {
-        _numChannels = numChannels;
-        _fov = fov;
-        this._singleChannelSegmenter = new SingleChannelPolarizationSegmenter();
-        this._multiChannelSegmenter = new MultiChannelPolarizationSegmenter();
+        super(fov, numChannels);
     }
 
     @Override
@@ -63,37 +34,6 @@ class OneCameraSegmenter implements ConstellationSegmenter {
         this._pol135 = segmenter.segment(capturedImages, pol135FoV);
 
         this._fileSet = capturedImageSet.fileSet();
-    }
-
-    private ChannelPolarizationSegmenter _selectSegmenter(ICapturedImageSet capturedImageSet) {
-        if (capturedImageSet.hasMultiChannelImage()) {
-            return _multiChannelSegmenter;
-        } else {
-            return _singleChannelSegmenter;
-        }
-    }
-
-    @Override
-    public IPolarizationImageSet segment(int channel) {
-        if (_pol0 == null) {
-            throw new IllegalAccessError("No captured image set has been provided.");
-        }
-        ChannelUtils.checkChannel(channel, this._numChannels);
-
-        Image<UINT16> pol0 = AxisReassigner.reassignToXYCZT(this._pol0[channel - 1], UINT16.zero());
-        Image<UINT16> pol45 = AxisReassigner.reassignToXYCZT(this._pol45[channel - 1], UINT16.zero());
-        Image<UINT16> pol90 = AxisReassigner.reassignToXYCZT(this._pol90[channel - 1], UINT16.zero());
-        Image<UINT16> pol135 = AxisReassigner.reassignToXYCZT(this._pol135[channel - 1], UINT16.zero());
-
-        IPolarizationImageSet polImageSet = null;
-        try {
-            polImageSet = new PolarizationImageSetBuilder(this._numChannels).pol0(pol0).pol45(pol45).pol90(pol90)
-                    .pol135(pol135).channel(channel).fileSet(this._fileSet).build();
-        } catch (CannotFormPolarizationImageSet e) {
-            // Since all images have the same dimension, exception is never caught.
-        }
-
-        return polImageSet;
     }
 
 }
