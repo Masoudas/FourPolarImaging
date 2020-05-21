648318727b6ef2b9d6f935c90d21138ffe849dc4
Masoudas <Masoudas@hotmail.com>
2020-05-21 14:37:44 +0200

    Make matric non-final

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/transform/AffineTransform2D.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/transform/AffineTransform2D.java
@@ -4,14 +4,22 @@ package fr.fresnel.fourPolar.core.util.transform;
  * An affine transform over a 2D space. Note that the underlying matrix is 2*3.
  */
 public class AffineTransform2D implements AffineTransform {
-    private final double[][] _matrix;
+    private double[][] _matrix;
 
-    public AffineTransform2D(){
+    public AffineTransform2D() {
         this._matrix = new double[2][3];
     }
 
     public void set(int row, int column, double value) {
         this._matrix[row][column] = value;
+    }
+
+    public void set(double[][] matrix) {
+        if (matrix.length != 2 || matrix[0].length != 3) {
+            throw new IllegalArgumentException("Matrix is not 2*3");
+        }
+
+        this._matrix = matrix;
     }
 
     @Override
