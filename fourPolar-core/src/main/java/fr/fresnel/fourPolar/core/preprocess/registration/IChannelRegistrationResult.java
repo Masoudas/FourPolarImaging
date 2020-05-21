5bf9ff8697ca0b73b006457433bdca253fdbc24e
Masoudas <Masoudas@hotmail.com>
2020-05-21 11:35:50 +0200

    Transform to channel result

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/preprocess/registration/IChannelRegistrationResult.java
@@ -0,0 +1,51 @@
+package fr.fresnel.fourPolar.core.preprocess.registration;
+
+import java.io.File;
+import java.util.Map;
+
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
+import fr.fresnel.fourPolar.core.util.transform.AffineTransform;
+
+/**
+ * Represents the results of a registration algorithm for a channel.
+ */
+public interface IChannelRegistrationResult {
+    /**
+     * Returns true if registration was successful. See also
+     * {@link IRegistrationResult#getDescription()}.
+     * 
+     * @return
+     */
+    public boolean registrationSuccessful(ChannelRegistrationOrder rule);
+
+    /**
+     * Returns the equivalent affine transform of this registration.
+     */
+    public AffineTransform getAffineTransform(ChannelRegistrationOrder rule);
+
+    /**
+     * The registration error of the algorithm.
+     * 
+     * @return
+     */
+    public double error(ChannelRegistrationOrder rule);
+
+    /**
+     * Returns a string representation of the result of the algorithm. (Example: For
+     * the bead descriptor based algorithm, it returns the number detected feature
+     * points as a string, or that no feature points were detected).
+     * 
+     * @return
+     */
+    public String getDescription(ChannelRegistrationOrder rule);
+
+    /**
+     * Returns any possible resulting images that can be used to subjectively
+     * qualify the result of registration. The string represents a discription of
+     * what the image contains (Example: For the descripto based algorithm, the
+     * description is "Color Image", which is simply the overlay of the two images).
+     */
+    public Map<String, Image<RGB16>> getResultFiles(ChannelRegistrationOrder rule);
+
+}
