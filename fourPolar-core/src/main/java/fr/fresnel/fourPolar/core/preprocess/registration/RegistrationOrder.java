1eaf7e0a8d8191242e20f132c394adeb15cf99ee
Masoudas <Masoudas@hotmail.com>
2020-05-22 08:17:49 +0200

    Rename enum

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/preprocess/registration/RegistrationOrder.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/preprocess/registration/RegistrationOrder.java
@@ -4,6 +4,6 @@ package fr.fresnel.fourPolar.core.preprocess.registration;
  * Determines the order for applying registration. The rule is that pol45, pol90
  * and pol135 are all registered to pol0.
  */
-public enum ChannelRegistrationOrder {
+public enum RegistrationOrder {
     Pol45_to_Pol0, Pol90_to_Pol0, Pol135_to_Pol0,
 }
