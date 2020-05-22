11116e9a84062ae7b8b0a7560d264b01eea0021f
Masoudas <Masoudas@hotmail.com>
2020-05-22 18:02:15 +0200

    Indiacate angles are in radians

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/orientation/IAngleImage.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/orientation/IAngleImage.java
@@ -8,7 +8,7 @@ import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
  * The interface for accessing an angle image. An angle image is the equivalent
  * image representation of an orientation angle (rho, delta or eta) of a sample
  * for a particular frequency, where each pixel represents the angle at that
- * location.
+ * location. Note that the underlying angles are in radians.
  */
 public interface IAngleImage {
     /**
