1a9f7515f3ed1c0988d35459420b8a5dc1f59d47
Masoudas <Masoudas@hotmail.com>
2020-05-27 14:06:49 +0200

    Remove to do comments

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/TwoCameraPolararizationImageSetCreatorTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/preprocess/segmentation/TwoCameraPolararizationImageSetCreatorTest.java
@@ -310,31 +310,26 @@ class TCISDummyFileSet implements ICapturedImageFileSet {
 
     @Override
     public ICapturedImageFile[] getFile(String label) {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public String getSetName() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public Cameras getnCameras() {
-        // TODO Auto-generated method stub
         return null;
     }
 
     @Override
     public boolean hasLabel(String label) {
-        // TODO Auto-generated method stub
         return false;
     }
 
     @Override
     public boolean deepEquals(ICapturedImageFileSet fileset) {
-        // TODO Auto-generated method stub
         return false;
     }
 
