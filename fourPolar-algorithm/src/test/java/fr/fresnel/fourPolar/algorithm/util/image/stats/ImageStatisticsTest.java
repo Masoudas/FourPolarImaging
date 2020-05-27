37f09b668c9d33ad230fb3905029224912ed7695
Masoudas <Masoudas@hotmail.com>
2020-05-27 12:09:54 +0200

    Move test to util tests

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/stats/ImageStatisticsTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/stats/ImageStatisticsTest.java
@@ -9,19 +9,11 @@ import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.metadata.MetadataUtil;
 import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 
 public class ImageStatisticsTest {
-
-    @Test
-    public void getNPlanes_ReturnsCorrectNPlanes() {
-        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 2, 2, 2, 2 }).build();
-        Image<UINT16> image = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
-
-        assertTrue(ImageStatistics.getNPlanes(image) == 16);
-
-    }
 
     @Test
     public void getMinMax_4PlaneImage_ReturnsCorrectMinMaxPerPlane() {
---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/Metadata/MetadataUtilTest.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.core.image.generic.Metadata;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.junit.jupiter.api.Test;
+
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.metadata.MetadataUtil;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+
+public class MetadataUtilTest {
+    @Test
+    public void getNPlanes_ReturnsCorrectNPlanes() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 2, 2, 2, 2 }).build();
+        Image<UINT16> image = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+
+        assertTrue(MetadataUtil.getNPlanes(image) == 16);
+
+    }
+}
