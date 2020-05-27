797d878bb3d4c45dfd8dae8f3b26f74405f3cb2c
Masoudas <Masoudas@hotmail.com>
2020-05-27 09:32:08 +0200

    Implement writer in degrees

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageWriter.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageWriter.java
@@ -3,13 +3,16 @@ package fr.fresnel.fourPolar.io.image.orientation;
 import java.io.File;
 import java.io.IOException;
 
+import fr.fresnel.fourPolar.algorithm.postprocess.orientation.OrientationAngleConverter;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
 import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
 import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.NoWriterFoundForImage;
 import fr.fresnel.fourPolar.io.image.generic.ImageWriter;
 import fr.fresnel.fourPolar.io.image.generic.tiff.TiffImageWriterFactory;
+import fr.fresnel.fourPolar.io.image.orientation.file.IOrientationImageFileSet;
 import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageFileSet;
+import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageInDegreeFileSet;
 
 /**
  * A concrete implementation of {@link IOrientationImageReader} to write the
@@ -26,27 +29,48 @@ public class TiffOrientationImageWriter implements IOrientationImageWriter {
      * @throws NoWriterFoundForImage
      */
     public TiffOrientationImageWriter(IOrientationImage image) throws NoWriterFoundForImage {
-        _writer = TiffImageWriterFactory.getWriter(
-            image.getAngleImage(OrientationAngle.rho).getImage(), Float32.zero());
+        _writer = TiffImageWriterFactory.getWriter(image.getAngleImage(OrientationAngle.rho).getImage(),
+                Float32.zero());
     }
 
     @Override
     public void write(File root4PProject, IOrientationImage orientationImage) throws IOException {
-        TiffOrientationImageFileSet oSet = new TiffOrientationImageFileSet(
-            root4PProject, orientationImage.getCapturedSet(), orientationImage.channel());
+        TiffOrientationImageFileSet oSet = new TiffOrientationImageFileSet(root4PProject,
+                orientationImage.getCapturedSet(), orientationImage.channel());
 
-        _writer.write(
-            oSet.getFile(OrientationAngle.rho), orientationImage.getAngleImage(OrientationAngle.rho).getImage());
-        _writer.write(
-            oSet.getFile(OrientationAngle.delta), orientationImage.getAngleImage(OrientationAngle.delta).getImage());
-        _writer.write(
-            oSet.getFile(OrientationAngle.eta), orientationImage.getAngleImage(OrientationAngle.eta).getImage());
+        _write(orientationImage, oSet);
 
     }
 
     @Override
     public void close() throws IOException {
         _writer.close();
+    }
+
+    @Override
+    public void writeInDegrees(File root4PProject, IOrientationImage orientationImage) throws IOException {
+        TiffOrientationImageInDegreeFileSet oSet = new TiffOrientationImageInDegreeFileSet(root4PProject,
+                orientationImage.getCapturedSet(), orientationImage.channel());
+        this._converAnglesToDegrees(orientationImage);
+        this._write(orientationImage, oSet);
+        this._converAnglesBackToRadian(orientationImage);
+    }
+
+    private void _write(IOrientationImage orientationImage, IOrientationImageFileSet oSet) throws IOException {
+        _writer.write(oSet.getFile(OrientationAngle.rho),
+                orientationImage.getAngleImage(OrientationAngle.rho).getImage());
+        _writer.write(oSet.getFile(OrientationAngle.delta),
+                orientationImage.getAngleImage(OrientationAngle.delta).getImage());
+        _writer.write(oSet.getFile(OrientationAngle.eta),
+                orientationImage.getAngleImage(OrientationAngle.eta).getImage());
+    }
+
+    private void _converAnglesBackToRadian(IOrientationImage orientationImage) {
+        OrientationAngleConverter.convertToRadian(orientationImage);
+    }
+
+    private void _converAnglesToDegrees(IOrientationImage orientationImage) {
+        OrientationAngleConverter.convertToDegree(orientationImage);
     }
 
 }
