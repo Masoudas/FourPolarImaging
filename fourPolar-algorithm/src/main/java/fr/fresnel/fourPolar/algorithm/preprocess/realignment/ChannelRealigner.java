ec16046240e4dcc8f104bbfe62780f2ab1e35614
Masoudas <Masoudas@hotmail.com>
2020-05-27 14:25:54 +0200

    Add a static factory

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/realignment/ChannelRealigner.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/realignment/ChannelRealigner.java
@@ -9,10 +9,8 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
-import fr.fresnel.fourPolar.core.preprocess.BeadSetProcessResult;
 import fr.fresnel.fourPolar.core.preprocess.registration.IChannelRegistrationResult;
 import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationOrder;
-import fr.fresnel.fourPolar.core.util.transform.AffineTransform;
 import fr.fresnel.fourPolar.core.util.transform.AffineTransform2D;
 import net.imglib2.Cursor;
 import net.imglib2.RealRandomAccessible;
@@ -26,35 +24,47 @@ import net.imglib2.view.IntervalView;
 import net.imglib2.view.Views;
 
 /**
- * Realign polarization images using ImgLib2 library.
+ * A concrete implementation of {@link IChannelRealigner.}
  */
-public class ImgLib2Realigner implements IChannelRealigner {
-    private final BeadSetProcessResult _beadSetProcessResult;
+public class ChannelRealigner implements IChannelRealigner {
+    private final IChannelRegistrationResult _channelRegistrationResult;
 
-    public ImgLib2Realigner(BeadSetProcessResult beadSetProcessResult) {
-        this._beadSetProcessResult = beadSetProcessResult;
+    /**
+     * Create an instance for the desired channel, based on its registration result.
+     * 
+     * @return
+     */
+    public static IChannelRealigner create(final IChannelRegistrationResult channelRegistrationResult) {
+        return new ChannelRealigner(channelRegistrationResult);
+    }
+
+    /**
+     * Realign polarization images using ImgLib2 library.
+     */
+    private ChannelRealigner(final IChannelRegistrationResult channelRegistrationResult) {
+        this._channelRegistrationResult = channelRegistrationResult;
     }
 
     @Override
-    public void realign(IPolarizationImageSet imageSet) {
-        int channel = imageSet.channel();
-        IChannelRegistrationResult registrationResult = this._beadSetProcessResult.getRegistrationResult(channel);
+    public void realign(final IPolarizationImageSet imageSet) {
+        final Image<UINT16> pol45 = imageSet.getPolarizationImage(Polarization.pol45).getImage();
+        this._realignPolarization(pol45,
+                this._channelRegistrationResult.getAffineTransform(RegistrationOrder.Pol45_to_Pol0));
 
-        Image<UINT16> pol45 = imageSet.getPolarizationImage(Polarization.pol45).getImage();
-        this._realignPolarization(pol45, registrationResult.getAffineTransform(RegistrationOrder.Pol45_to_Pol0));
+        final Image<UINT16> pol90 = imageSet.getPolarizationImage(Polarization.pol90).getImage();
+        this._realignPolarization(pol90,
+                this._channelRegistrationResult.getAffineTransform(RegistrationOrder.Pol90_to_Pol0));
 
-        Image<UINT16> pol90 = imageSet.getPolarizationImage(Polarization.pol90).getImage();
-        this._realignPolarization(pol90, registrationResult.getAffineTransform(RegistrationOrder.Pol90_to_Pol0));
-
-        Image<UINT16> pol135 = imageSet.getPolarizationImage(Polarization.pol135).getImage();
-        this._realignPolarization(pol135, registrationResult.getAffineTransform(RegistrationOrder.Pol135_to_Pol0));
+        final Image<UINT16> pol135 = imageSet.getPolarizationImage(Polarization.pol135).getImage();
+        this._realignPolarization(pol135,
+                this._channelRegistrationResult.getAffineTransform(RegistrationOrder.Pol135_to_Pol0));
     }
 
-    private void _realignPolarization(Image<UINT16> image, AffineTransform2D transform) {
+    private void _realignPolarization(final Image<UINT16> image, final AffineTransform2D transform) {
         Img<UnsignedShortType> imageAsImgLib2 = null;
         try {
             imageAsImgLib2 = ImageToImgLib2Converter.getImg(image, UINT16.zero());
-        } catch (ConverterToImgLib2NotFound e) {
+        } catch (final ConverterToImgLib2NotFound e) {
             // TODO Get rid of this!
         }
         final AffineGet expandedTransform = _expandTransfromToAffineOfImageSize(image.getMetadata(), transform);
@@ -67,9 +77,18 @@ public class ImgLib2Realigner implements IChannelRealigner {
         // apply the original bounding box to the transformed image
         final IntervalView<UnsignedShortType> bounded = Views.interval(sheared, imageAsImgLib2);
 
-        Cursor<UnsignedShortType> transformedCursor = bounded.cursor();
-        for (IPixelCursor<UINT16> cursor = image.getCursor(); cursor.hasNext();) {
-            IPixel<UINT16> pixel = cursor.next();
+        _setImageToTransformedPixels(image, bounded);
+    }
+
+    /**
+     * Using the cursor of ImgLib2, set the content of the original image to the
+     * transformed image.
+     */
+    private void _setImageToTransformedPixels(final Image<UINT16> image,
+            final IntervalView<UnsignedShortType> bounded) {
+        final Cursor<UnsignedShortType> transformedCursor = bounded.cursor();
+        for (final IPixelCursor<UINT16> cursor = image.getCursor(); cursor.hasNext();) {
+            final IPixel<UINT16> pixel = cursor.next();
             pixel.value().set(transformedCursor.get().get());
             cursor.setPixel(pixel);
         }
@@ -79,10 +98,10 @@ public class ImgLib2Realigner implements IChannelRealigner {
      * In order to transform each plane, we need to define an affine transform that
      * has the same length as the image, but only applies to the plane.
      */
-    private AffineGet _expandTransfromToAffineOfImageSize(IMetadata metadata, AffineTransform2D transform) {
-        int numDimensions = metadata.getDim().length;
+    private AffineGet _expandTransfromToAffineOfImageSize(final IMetadata metadata, final AffineTransform2D transform) {
+        final int numDimensions = metadata.getDim().length;
 
-        net.imglib2.realtransform.AffineTransform expandedTransform = new net.imglib2.realtransform.AffineTransform(
+        final net.imglib2.realtransform.AffineTransform expandedTransform = new net.imglib2.realtransform.AffineTransform(
                 numDimensions);
         expandedTransform.set(transform.get(0, 0), 0, 0);
         expandedTransform.set(transform.get(0, 1), 0, 1);
