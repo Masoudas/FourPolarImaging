4edab93aecc4b308ff522a9634f3ee20135d9e4c
Masoudas <Masoudas@hotmail.com>
2020-05-28 16:12:36 +0200

    Write with metadata

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/Float32SCIFIOTiffImageWriter.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/scifio/Float32SCIFIOTiffImageWriter.java
@@ -2,68 +2,97 @@ package fr.fresnel.fourPolar.io.image.generic.tiff.scifio;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Objects;
 
 import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.ConverterToImgLib2NotFound;
-import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImageToImgLib2Converter;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
+import fr.fresnel.fourPolar.io.image.generic.ImageWriter;
+import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.metadata.SCIFIOTiffMetadataConverter;
+import io.scif.FormatException;
+import io.scif.ImageMetadata;
+import io.scif.config.SCIFIOConfig;
+import io.scif.formats.TIFFFormat.Metadata;
+import io.scif.formats.TIFFFormat.Writer;
+import io.scif.img.ImgSaver;
+import io.scif.util.FormatTools;
 
 /**
  * Class for writing grayscale tiffs to disk.
  * 
  */
-public class Float32SCIFIOTiffImageWriter extends GrayScaleSCIFIOTiffWriter<Float32> {
+public class Float32SCIFIOTiffImageWriter implements ImageWriter<Float32> {
     final private Float32 _pixelType = Float32.zero();
+    final private SCIFIOConfig _config;
+    final private Writer<Metadata> _writer;
+    final private ImgSaver _imgSaver;
+
+    public Float32SCIFIOTiffImageWriter() {
+        this._config = _setSCFIOConfig();
+        this._imgSaver = new ImgSaver();
+        this._writer = new Writer<>();
+        this._writer.setContext(this._imgSaver.context());
+    }
 
     @Override
     public void write(File path, Image<Float32> image) throws IOException {
-        if (path.exists()) {
-            path.delete();
-        }
+        Objects.requireNonNull(path, "path should not be null");
+        Objects.requireNonNull(image, "image should not be null");
 
+        SCIFIOUtils.checkExtension(path.getName());
+        SCIFIOUtils.deleteFileIfExists(path);
+
+        Metadata scifioMetadata = this._createSCIFIOMetadata();
+        SCIFIOTiffMetadataConverter.convertTo(image.getMetadata(), scifioMetadata);
+        
         try {
-            this._saver.saveImg(path.getAbsolutePath(), ImageToImgLib2Converter.getImg(image, this._pixelType),
-                    this._config);
+            this._writer.setMetadata(scifioMetadata);
+            this._writer.setDest(path.getAbsolutePath(), this._config);
+
+            this._imgSaver.saveImg(this._writer, ImageToImgLib2Converter.getImg(image, this._pixelType));
         } catch (ConverterToImgLib2NotFound e) {
-            // This exception potentially should not be caught, if we use the same Image 
-            // type, and especially if converter has been implemented.
+        } catch (FormatException e) {
+            // This exception is not caught, because we've used tiff formater.
         }
 
     }
- 
+
     @Override
-    public void write(File path, IMetadata metadata, Image<Float32> tiff) throws IOException {
-        /**
-         * We are probably going to need to define a Dataplane object for each plane of
-         * the image to be written, then populate it with img object plane, and finally
-         * write every plane with the writer object. It appears that if I want to write
-         * omero metadata, I need to manually iterate over the image.
-         * 
-         * It will be interesting to see how the saveImg method of ImgSaver populates
-         * the metadata too.
-         * 
-         * If it comes down to simply adding comments, this link is helpful
-         * https://github.com/ome/bioformats/blob/develop/components/formats-gpl/utils/EditTiffComment.java,
-         * where the TiffParser gets us the comments and TiffSaver has a method for
-         * overriding comments.
-         * 
-         * This link is also interesting:
-         * https://github.com/ome/bioformats/blob/develop/components/formats-gpl/utils/EditImageName.java
-         */
-        if (path.exists()) {
-            path.delete();
-        }
-
-        // try {
-        //     // _writer.setMetadata(metadata);
-        //     // _writer.setDest(destination.getAbsolutePath(), this._config);
-
-        //     // this._saver.saveImg(_writer, img, _config);
-        // } catch (FormatException e) {
-
-        // }
-
-
+    public void close() throws IOException {
+        this._imgSaver.context().dispose();
     }
+
+    /**
+     * Sets the configuration for how the image is opened.
+     * 
+     * @return
+     */
+    private SCIFIOConfig _setSCFIOConfig() {
+        // For the time being, we use the very basic config.
+        SCIFIOConfig config = new SCIFIOConfig();
+        return config;
+    }
+
+    /**
+     * Create a base metadata, and let axis and axis length be set later.
+     */
+    private Metadata _createSCIFIOMetadata() {
+        Metadata metadata = new Metadata();
+
+        metadata.createImageMetadata(1);
+
+        ImageMetadata imageMetadata = metadata.get(0);
+        imageMetadata.setFalseColor(true);
+        imageMetadata.setPixelType(FormatTools.FLOAT);
+        imageMetadata.setPlanarAxisCount(2);
+        imageMetadata.setLittleEndian(false);
+        imageMetadata.setIndexed(false);
+        imageMetadata.setInterleavedAxisCount(0);
+        imageMetadata.setThumbnail(false);
+        imageMetadata.setOrderCertain(true);
+        imageMetadata.setMetadataComplete(true);
+        return metadata;
+    }
+
 }
