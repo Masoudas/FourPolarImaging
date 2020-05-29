17fca3a4035dea79c77295b067bc21f121bf82c6
Masoudas <Masoudas@hotmail.com>
2020-05-29 11:18:46 +0200

    Define access interface

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/visualization/figures/stickFigure/gauge3D/IWholeSampleStick3DPainterBuilder.java
@@ -0,0 +1,20 @@
+package fr.fresnel.fourPolar.algorithm.visualization.figures.stickFigure.gauge3D;
+
+import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
+import fr.fresnel.fourPolar.core.image.soi.ISoIImage;
+import fr.fresnel.fourPolar.core.util.image.colorMap.ColorMap;
+
+/**
+ * A simple private interface for accessing the parameters of the builder.
+ */
+abstract public class IWholeSampleStick3DPainterBuilder {
+    abstract ColorMap getColorMap();
+
+    abstract int getSticklength();
+
+    abstract IOrientationImage getOrientationImage();
+
+    abstract ISoIImage getSoIImage();
+
+    abstract int getStickThickness();
+}
