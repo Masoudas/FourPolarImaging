53fe81f9d78c4f79516f26a4066451cf76ad72db
Masoudas <Masoudas@hotmail.com>
2020-05-29 16:04:57 +0200

    Use ImageJ for writing RGB16

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/TiffImageWriterFactory.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/tiff/TiffImageWriterFactory.java
@@ -1,15 +1,15 @@
 package fr.fresnel.fourPolar.io.image.generic.tiff;
 
 import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2Image;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.NoWriterFoundForImage;
 import fr.fresnel.fourPolar.io.image.generic.ImageWriter;
+import fr.fresnel.fourPolar.io.image.generic.tiff.ImageJ1.ImageJ1RGB16TiffWriter;
 import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.SCIFIOFloat32TiffWriter;
-import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.SCIFIORGB16TiffWriter;
 import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.SCIFIOUINT16TiffWriter;
- 
+
 /**
  * A factory to get a proper writer for the given implementation of
  * {@code Image}.
@@ -19,27 +19,22 @@ public class TiffImageWriterFactory {
      * A factory to get a proper writer for the given implementation of
      * {@code Image}. The writers can be used to write different instances of image.
      * 
-     * @param <T>
-     * @param image
-     * @return
      */
     @SuppressWarnings("unchecked")
-    public static <T extends PixelType> ImageWriter<T> getWriter(Image<T> image, T pixelType) throws NoWriterFoundForImage{
+    public static <T extends PixelType> ImageWriter<T> getWriter(Image<T> image, T pixelType)
+            throws NoWriterFoundForImage {
         ImageWriter<T> writer;
 
-        if (image instanceof ImgLib2Image && pixelType.getType() == PixelTypes.UINT_16){
-            writer = (ImageWriter<T>)new SCIFIOUINT16TiffWriter();
-        }
-        else if (image instanceof ImgLib2Image && pixelType.getType() == PixelTypes.FLOAT_32){
-            writer = (ImageWriter<T>)new SCIFIOFloat32TiffWriter();
-        }
-        else if (image instanceof ImgLib2Image && pixelType.getType() == PixelTypes.RGB_16){
-            writer = (ImageWriter<T>)new SCIFIORGB16TiffWriter();
-        }
-        else{
+        if (image.getFactory() instanceof ImgLib2ImageFactory && pixelType.getType() == PixelTypes.UINT_16) {
+            writer = (ImageWriter<T>) new SCIFIOUINT16TiffWriter();
+        } else if (image instanceof ImgLib2ImageFactory && pixelType.getType() == PixelTypes.FLOAT_32) {
+            writer = (ImageWriter<T>) new SCIFIOFloat32TiffWriter();
+        } else if (image instanceof ImgLib2ImageFactory && pixelType.getType() == PixelTypes.RGB_16) {
+            writer = (ImageWriter<T>) new ImageJ1RGB16TiffWriter();
+        } else {
             throw new NoWriterFoundForImage();
         }
-        
+
         return writer;
     }
 
