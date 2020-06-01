55bf545f1bd393b70553560b906658f317ef1a08
Masoudas <Masoudas@hotmail.com>
2020-06-01 10:24:53 +0200

    Design class

---
+++ fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/algorithms/preprocess/SampleImageSetPreprocessor.java
@@ -0,0 +1,8 @@
+package fr.fresnel.fourPolar.ui.algorithms.preprocess;
+
+/**
+ * Using this class, we can preprocess the given {@link SampleImageSet}
+ */
+public class SampleImageSetPreprocessor {
+    
+}
