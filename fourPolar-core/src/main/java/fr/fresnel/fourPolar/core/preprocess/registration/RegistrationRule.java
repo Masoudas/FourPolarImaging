cec22bb441a262bf6a26e43c3525716ea63bdc07
Masoudas <Masoudas@hotmail.com>
2020-06-02 10:44:56 +0200

    Add method to return base and to register images

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/preprocess/registration/RegistrationRule.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/preprocess/registration/RegistrationRule.java
@@ -1,9 +1,50 @@
 package fr.fresnel.fourPolar.core.preprocess.registration;
 
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+
 /**
  * Determines the rule for applying registration. The rule is that pol45, pol90
  * and pol135 are all registered to pol0.
  */
 public enum RegistrationRule {
-    Pol45_to_Pol0, Pol90_to_Pol0, Pol135_to_Pol0,
+    Pol45_to_Pol0 {
+        public Polarization getBaseImagePolarization() {
+            return Polarization.pol0;
+        }
+
+        public Polarization getToRegisterImagePolarization() {
+            return Polarization.pol45;
+        }
+    },
+
+    Pol90_to_Pol0 {
+        public Polarization getBaseImagePolarization() {
+            return Polarization.pol0;
+        }
+
+        public Polarization getToRegisterImagePolarization() {
+            return Polarization.pol90;
+        }
+    },
+
+    Pol135_to_Pol0 {
+        public Polarization getBaseImagePolarization() {
+            return Polarization.pol0;
+        }
+
+        public Polarization getToRegisterImagePolarization() {
+            return Polarization.pol135;
+        }
+    };
+
+    /**
+     * Returns the polarization that would be the base of registration for this
+     * rule.
+     */
+    abstract public Polarization getBaseImagePolarization();
+
+    /**
+     * Returns the polarization to be registeredd for this rule.
+     */
+    abstract public Polarization getToRegisterImagePolarization();
 }
