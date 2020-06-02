4364dbb0625c39907d35fa03182300676c7d24be
Masoudas <Masoudas@hotmail.com>
2020-06-02 07:42:37 +0200

    Design interface

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/registration/IRegistrationCompositeFigures.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/registration/IRegistrationCompositeFigures.java
@@ -7,8 +7,21 @@ import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
 /**
  * An interface for accessing the composite image resulting from merging a
  * registered image together with the base image, using to different colors, for
- * each {@link RegstrationOrder}.
+ * each {@link RegstrationOrder}, for a given channel.
  */
-public interface IRegistrationCompositeFigure {
+public interface IRegistrationCompositeFigures {
+    /**
+     * The channel number the composite figures belong to.
+     */
+    public int channel();
+
+    /**
+     * Set the composite image corresponding to the given rule.
+     */
+    public void setCompositeImage(RegistrationRule rule, Image<RGB16> compositeImage);
+
+    /**
+     * Get the composite image corresponding to the given rule.
+     */
     public Image<RGB16> getCompositeImage(RegistrationRule rule);
 }
