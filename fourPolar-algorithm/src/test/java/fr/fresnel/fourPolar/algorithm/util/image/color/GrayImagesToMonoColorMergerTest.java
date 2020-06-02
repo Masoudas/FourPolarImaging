4a1ebb003b39074cf05f172ecc1c1047f9ebc0ff
Masoudas <Masoudas@hotmail.com>
2020-06-02 07:21:01 +0200

    Set and get pixels properly

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/color/GrayImagesToMonoColorMergerTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/util/image/color/GrayImagesToMonoColorMergerTest.java
@@ -7,10 +7,12 @@ import org.junit.jupiter.api.Test;
 import fr.fresnel.fourPolar.algorithm.util.image.color.GrayScaleToColorConverter.Color;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
+import fr.fresnel.fourPolar.core.image.generic.pixel.Pixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.RGB16;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
@@ -18,26 +20,34 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 public class GrayImagesToMonoColorMergerTest {
     @Test
     public void merge_MergeAsRAndG_ReturnsCorrectImage() {
-        IMetadata metadata1 = new Metadata.MetadataBuilder(new long[]{2,1}).bitPerPixel(PixelTypes.UINT_16).build();
-        IMetadata metadata2 = new Metadata.MetadataBuilder(new long[]{2,1}).bitPerPixel(PixelTypes.UINT_16).build();
+        IMetadata metadata1 = new Metadata.MetadataBuilder(new long[] { 2, 1 }).bitPerPixel(PixelTypes.UINT_16).build();
+        IMetadata metadata2 = new Metadata.MetadataBuilder(new long[] { 2, 1 }).bitPerPixel(PixelTypes.UINT_16).build();
 
         Image<UINT16> image1 = new ImgLib2ImageFactory().create(metadata1, UINT16.zero());
         Image<UINT16> image2 = new ImgLib2ImageFactory().create(metadata2, UINT16.zero());
 
-        IPixelCursor<UINT16> pixelCursor1 = image1.getCursor();
-        IPixel<UINT16> pixel1 = pixelCursor1.next();
-        pixel1.value().set(UINT16.MAX_VAL);
-        pixelCursor1.setPixel(pixel1);
-
-        IPixelCursor<UINT16> pixelCursor2 = image2.getCursor();
-        IPixel<UINT16> pixel2 = pixelCursor2.next();
-        pixel2.value().set(UINT16.MAX_VAL);
-        pixelCursor2.setPixel(pixel2);
+        _setPixel(image1, new long[] { 0, 0 }, UINT16.MAX_VAL);
+        _setPixel(image2, new long[] { 0, 0 }, UINT16.MAX_VAL);
 
         Image<RGB16> monochromeImage = GrayImagesToMonoColorMerger.convert(image1, Color.Red, image2, Color.Green);
-        RGB16 color = monochromeImage.getCursor().next().value();
+        RGB16 color = _getPixel(monochromeImage, new long[] { 0, 0 });
 
         assertTrue(color.getB() == 0 && color.getG() == 255 && color.getR() == 255);
     }
-    
+
+    private void _setPixel(Image<UINT16> image, long[] position, int value) {
+        IPixelRandomAccess<UINT16> ra = image.getRandomAccess();
+        ra.setPosition(position);
+        ra.getPixel();
+
+        Pixel<UINT16> pixel = new Pixel<>(new UINT16(value));
+        ra.setPixel(pixel);
+    }
+
+    private RGB16 _getPixel(Image<RGB16> image, long[] position) {
+        IPixelRandomAccess<RGB16> ra = image.getRandomAccess();
+        ra.setPosition(position);
+
+        return ra.getPixel().value();
+    }
 }
