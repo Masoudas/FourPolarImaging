f69153156925e5eb69159e06032821f51710d452
Masoudas <Masoudas@hotmail.com>
2020-06-03 18:05:29 +0200

    Introduce interface

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/IGaugeFigureReader.java
@@ -0,0 +1,36 @@
+package fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure;
+
+import java.io.File;
+import java.io.IOException;
+
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
+
+/**
+ * An interface for reading a {@link IGaugeFigure} from the disk.
+ */
+public interface IGaugeFigureReader {
+    /**
+     * Write the given composites to the folder designated to the processed (bead)
+     * images, as defined by
+     * {@link PathFactoryOfProject#getFolder_ProcessedBeadImages}.
+     * 
+     * @param root4PProject        is the location of the 4Polar folder of the
+     *                             project {@see PathFactoryOfProject}.
+     * @param visualizationSession is the name of the visualization session to which
+     *                             this gauge figure belong.
+     * @param capturedImageFileSet is the captured image set to which this gauge
+     *                             figure belong.
+     * @throws IOException thrown in case of low-level problems.
+     * 
+     * @return the composites for the given channel number.
+     */
+    public IGaugeFigure read(File root4PProject, String visualizationSession, ICapturedImageFileSet capturedImageFileSet);
+
+    /**
+     * Close all resources associated with the reader.
+     * 
+     * @throws IOException
+     */
+    public void close() throws IOException;
+}
---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/IGaugeFigureWriter.java
@@ -0,0 +1,30 @@
+package fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure;
+
+import java.io.File;
+
+import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
+
+public interface IGaugeFigureWriter {
+    /**
+     * Write the given composites to the folder designated to the processed (bead)
+     * images, as defined by
+     * {@link PathFactoryOfProject#getFolder_ProcessedBeadImages}.
+     * 
+     * @param root4PProject        is the location of the 4Polar folder of the
+     *                             project {@see PathFactoryOfProject}.
+     * @param visualizationSession is the name of the visualization session to which
+     *                             this gauge figure belong.
+     * @param capturedImageFileSet is the captured image set to which this gauge
+     *                             figure belong.
+     * @throws IOException thrown in case of low-level problems.
+     * 
+     * @return the composites for the given channel number.
+     */
+    public void write(File root4PProject, String visualizationSession, IGaugeFigure gaugeFigure);
+
+    /**
+     * Close all resources associated with this writer.
+     */
+    public void close();
+
+}
