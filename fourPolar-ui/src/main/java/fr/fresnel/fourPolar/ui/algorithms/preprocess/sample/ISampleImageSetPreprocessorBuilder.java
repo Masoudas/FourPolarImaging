501b2d81fa68c7b846d2d3ddb9a0da8a487214db
Masoudas <Masoudas@hotmail.com>
2020-06-05 15:07:59 +0200

    Design builder interface

---
+++ fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/algorithms/preprocess/sample/ISampleImageSetPreprocessorBuilder.java
@@ -0,0 +1,23 @@
+package fr.fresnel.fourPolar.ui.algorithms.preprocess.sample;
+
+import fr.fresnel.fourPolar.algorithm.preprocess.darkBackground.IChannelDarkBackgroundRemover;
+import fr.fresnel.fourPolar.algorithm.preprocess.realignment.IChannelRealigner;
+import fr.fresnel.fourPolar.algorithm.preprocess.segmentation.ICapturedImageSetSegmenter;
+import fr.fresnel.fourPolar.io.image.captured.ICapturedImageSetReader;
+
+/**
+ * An interface for accessing the builder parameters.
+ */
+abstract class ISampleImageSetPreprocessorBuilder {
+
+    abstract IChannelDarkBackgroundRemover getBackgroundRemovers(int channel);
+
+    abstract ICapturedImageSetReader getCapturedImageSetReader();
+
+    abstract int getNumChannels();
+
+    abstract IChannelRealigner getRealigners(int channel);
+
+    abstract ICapturedImageSetSegmenter getSegmenter();
+
+}
