d7c913de2592d59f68e1f076fe86f90a65fc14f3
Masoudas <Masoudas@hotmail.com>
2020-06-08 16:28:42 +0200

    Add base circle

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2CircleShape.java
@@ -0,0 +1,9 @@
+package fr.fresnel.fourPolar.core.util.shape;
+
+/**
+ * DON'T FORGET TO TRANFORM RADIUS RADIUS AND CENTER. ALSO RESET TO ORIGINAL. CHECK OUT BOXSHAPE!!!
+ * Essentiall, override _transformShape method.
+ */
+public class ImgLib2CircleShape {
+    
+}
