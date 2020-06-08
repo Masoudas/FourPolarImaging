1ade72e39f7e7b17e3da9014b16cc898d3b05288
Masoudas <Masoudas@hotmail.com>
2020-06-08 16:28:35 +0200

    Transfer polygon vertices after transformation

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2Polygon2DShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2Polygon2DShape.java
@@ -1,27 +1,38 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
+import java.util.List;
+
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
-import net.imglib2.roi.RealMaskRealInterval;
+import net.imglib2.RealLocalizable;
+import net.imglib2.roi.geom.real.WritablePolygon2D;
 
 public class ImgLib2Polygon2DShape extends ImgLib2Shape implements IPolygon2DShape {
-    private final long[] _x_vertices;
-    private final long[] _y_vertices;
+    private final WritablePolygon2D _shape;
 
-    protected ImgLib2Polygon2DShape(int shapeDim, RealMaskRealInterval shape, AxisOrder axisOrder, long[] x_vertices,
+    protected ImgLib2Polygon2DShape(int shapeDim, WritablePolygon2D shape, AxisOrder axisOrder, long[] x_vertices,
             long[] y_vertices) {
         super(shapeDim, shape, axisOrder);
-        _x_vertices = x_vertices;
-        _y_vertices = y_vertices;
+        this._shape = shape;
     }
 
     @Override
     public long[] x_vertices() {
-        return _x_vertices.clone();
+        return this._getVerticesOnDimension(0);
     }
 
     @Override
     public long[] y_vertices() {
-        return _y_vertices.clone();
+        return this._getVerticesOnDimension(1);
+    }
+
+    private long[] _getVerticesOnDimension(int dimension) {
+        List<RealLocalizable> vertices = this._shape.vertices();
+
+        long[] x_vertices = new long[this._shapeDim];
+        for (int i = 0; i < this._shapeDim; i++) {
+            x_vertices[i] = (long) vertices.get(0).getDoublePosition(dimension);
+        }
+        return x_vertices;
     }
 
 }
