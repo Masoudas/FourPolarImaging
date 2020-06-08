484f3da3a7190fb5a749eb2d1a8bdc3b69ab0c05
Masoudas <Masoudas@hotmail.com>
2020-06-08 11:44:58 +0200

    Add only a string constructor

--- fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/exceptions/algorithms/preprocess/IOIssueRegistrationSetProcessFailure.java
+++ fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/exceptions/algorithms/preprocess/IOIssueRegistrationSetProcessFailure.java
@@ -1,6 +1,6 @@
 package fr.fresnel.fourPolar.ui.exceptions.algorithms.preprocess;
 
-import java.util.Arrays;
+import fr.fresnel.fourPolar.core.imageSet.acquisition.registration.RegistrationImageSet;
 
 /**
  * Thrown in case of failure while reading a {@link RegistrationImageSet} from
@@ -8,34 +8,16 @@ import java.util.Arrays;
  */
 public class IOIssueRegistrationSetProcessFailure extends RegistrationSetProcessFailure {
     private static final long serialVersionUID = -384382905714233075L;
-    private final int[] _channel;
     private final String _message;
 
-    public static IOIssueRegistrationSetProcessFailure failedReadingChannel(int[] channel) {
-        return new IOIssueRegistrationSetProcessFailure(channel);
-    }
-
-    public static IOIssueRegistrationSetProcessFailure failedWithOtherIssues(String reason) {
-        return new IOIssueRegistrationSetProcessFailure(reason);
-    }
-
-    private IOIssueRegistrationSetProcessFailure(String reason) {
-        _channel = new int[0];
+    public IOIssueRegistrationSetProcessFailure(String reason) {
         _message = reason;
     }
 
-    private IOIssueRegistrationSetProcessFailure(int[] channel) {
-        _channel = channel;
-        _message = "Can't load captured image of channel " + Arrays.toString(channel);
-    }
 
     @Override
     protected String _createMessage() {
         return this._message;
-    }
-
-    public int getChannel() {
-        return this._channel;
     }
 
 }
