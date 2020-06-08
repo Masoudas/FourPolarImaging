b697624cc8f349ee5bf5589402e026bc52a914f3
Masoudas <Masoudas@hotmail.com>
2020-06-08 16:51:45 +0200

    Use box shape to get min and max

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
@@ -1,61 +1,44 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
-import net.imglib2.roi.RealMaskRealInterval;
+import net.imglib2.roi.geom.real.WritableBox;
 
 class ImgLib2BoxShape extends ImgLib2Shape implements IBoxShape {
-    private final long[] _originalMin;
-    private final long[] _originalMax;
+    private final WritableBox _shape;
 
-    private long[] _min;
-    private long[] _max;
-
-    public ImgLib2BoxShape(final int shapeDim, RealMaskRealInterval shape, final AxisOrder axisOrder, long[] min,
+    public ImgLib2BoxShape(final int shapeDim, WritableBox shape, final AxisOrder axisOrder, long[] min,
             long[] max) {
         super(shapeDim, shape, axisOrder);
-        _originalMin = min;
-        _originalMax = max;
+        this._shape = shape;
     }
 
-    @Override
-    protected void _transformShape() {
-        super._transformShape();
-        this._transformMinPoint();
-        this._transformMaxPoint();
-    }
 
-    @Override
-    public void resetToOriginalShape() {
-        super.resetToOriginalShape();
-        this._setMax(this._originalMax);
-        this._setMin(this._originalMin);
-    }
 
     @Override
     public long[] min() {
-        return _min.clone();
+        long[] min = new long[this._shapeDim];
+        for (int i = 0; i < min.length; i++) {
+            min[i]= this._getMinDimension(i);
+        }
+        return min;
     }
 
     @Override
     public long[] max() {
-        return _max.clone();
+        long[] max = new long[this._shapeDim];
+        for (int i = 0; i < max.length; i++) {
+            max[i]= this._getMaxDimension(i);
+        }
+        return max;
     }
 
-    private void _setMin(long[] min) {
-        this._min = min;
+
+    private long _getMinDimension(int d){
+        return (long)(this._shape.center().getDoublePosition(d) - this._shape.sideLength(d));
     }
 
-    private void _setMax(long[] max) {
-        this._max = max;
+    private long _getMaxDimension(int d){
+        return (long)(this._shape.center().getDoublePosition(d) + this._shape.sideLength(d));
     }
 
-    private void _transformMinPoint() {
-        long[] newMin = ImgLib2ShapeUtils.applyAffineToLong(this._appliedAffineTransform, this.min());
-        this._setMin(newMin);
-    }
-
-    private void _transformMaxPoint() {
-        long[] newMax = ImgLib2ShapeUtils.applyAffineToLong(this._appliedAffineTransform, this.max());
-        this._setMax(newMax);
-    }
 }
