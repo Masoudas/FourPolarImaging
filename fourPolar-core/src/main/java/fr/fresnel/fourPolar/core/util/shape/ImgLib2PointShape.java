a865a1d2dc81496e86459828d9c7163532a321f5
Masoudas <Masoudas@hotmail.com>
2020-06-08 16:28:06 +0200

    Transfer point after transfer

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2PointShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2PointShape.java
@@ -1,18 +1,29 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
+import java.util.Arrays;
+
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
-import net.imglib2.roi.RealMaskRealInterval;
+import net.imglib2.roi.geom.real.PointMask;
 
 public class ImgLib2PointShape extends ImgLib2Shape implements IPointShape {
-    private final long[] _point;
-    protected ImgLib2PointShape(int shapeDim, RealMaskRealInterval shape, AxisOrder axisOrder, long[] point) {
+    private final PointMask _shape;
+    protected ImgLib2PointShape(int shapeDim, PointMask shape, AxisOrder axisOrder, long[] point) {
         super(shapeDim, shape, axisOrder);
-        this._point = point;
+        _shape = shape;
     }
 
     @Override
     public long[] point() {
-        return _point.clone();
+        double[]  position = new double[this._shapeDim];
+        this._shape.localize(position);
+
+        return Arrays.stream(position).mapToLong((t)->(long)t).toArray();
+    }
+
+    @Override
+    protected void _transformShape() {
+        super._transformShape();
+
     }
     
 }
