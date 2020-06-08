06c5ae4f4add3b88f796c05154ba48042070af04
Masoudas <Masoudas@hotmail.com>
2020-06-08 16:27:46 +0200

    Transform min max when transfering shape

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
@@ -4,14 +4,31 @@ import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import net.imglib2.roi.RealMaskRealInterval;
 
 class ImgLib2BoxShape extends ImgLib2Shape implements IBoxShape {
-    private final long[] _min;
-    private final long[] _max;
+    private final long[] _originalMin;
+    private final long[] _originalMax;
+
+    private long[] _min;
+    private long[] _max;
 
     public ImgLib2BoxShape(final int shapeDim, RealMaskRealInterval shape, final AxisOrder axisOrder, long[] min,
             long[] max) {
-            super(shapeDim, shape, axisOrder);
-            _min = min;
-            _max = max;
+        super(shapeDim, shape, axisOrder);
+        _originalMin = min;
+        _originalMax = max;
+    }
+
+    @Override
+    protected void _transformShape() {
+        super._transformShape();
+        this._transformMinPoint();
+        this._transformMaxPoint();
+    }
+
+    @Override
+    public void resetToOriginalShape() {
+        super.resetToOriginalShape();
+        this._setMax(this._originalMax);
+        this._setMin(this._originalMin);
     }
 
     @Override
@@ -24,4 +41,21 @@ class ImgLib2BoxShape extends ImgLib2Shape implements IBoxShape {
         return _max.clone();
     }
 
+    private void _setMin(long[] min) {
+        this._min = min;
+    }
+
+    private void _setMax(long[] max) {
+        this._max = max;
+    }
+
+    private void _transformMinPoint() {
+        long[] newMin = ImgLib2ShapeUtils.applyAffineToLong(this._appliedAffineTransform, this.min());
+        this._setMin(newMin);
+    }
+
+    private void _transformMaxPoint() {
+        long[] newMax = ImgLib2ShapeUtils.applyAffineToLong(this._appliedAffineTransform, this.max());
+        this._setMax(newMax);
+    }
 }
