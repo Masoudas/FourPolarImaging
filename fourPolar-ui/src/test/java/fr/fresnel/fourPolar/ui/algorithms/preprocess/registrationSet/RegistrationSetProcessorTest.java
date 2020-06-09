f3dd3a149e56921ef343907792c80fa1c7cc8e00
Masoudas <Masoudas@hotmail.com>
2020-06-09 16:43:42 +0200

    Comment test for two camera image

--- fourPolar-ui/src/test/java/fr/fresnel/fourPolar/ui/algorithms/preprocess/registrationSet/RegistrationSetProcessorTest.java
+++ fourPolar-ui/src/test/java/fr/fresnel/fourPolar/ui/algorithms/preprocess/registrationSet/RegistrationSetProcessorTest.java
@@ -1,13 +1,25 @@
 package fr.fresnel.fourPolar.ui.algorithms.preprocess.registrationSet;
 
+import static org.junit.jupiter.api.Assertions.assertDoesNotThrow;
+import static org.junit.jupiter.api.Assertions.assertThrows;
+
 import java.io.File;
 import java.io.IOException;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.Iterator;
+import java.util.stream.IntStream;
+import java.util.stream.Stream;
+
+import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.algorithm.preprocess.darkBackground.estimator.IChannelDarkBackgroundEstimator;
 import fr.fresnel.fourPolar.algorithm.preprocess.darkBackground.estimator.percentile.PercentileChannelDarkBackgroundEstimator;
+import fr.fresnel.fourPolar.algorithm.preprocess.fov.FoVCalculatorFourCamera;
 import fr.fresnel.fourPolar.algorithm.preprocess.fov.FoVCalculatorOneCamera;
+import fr.fresnel.fourPolar.algorithm.preprocess.fov.FoVCalculatorTwoCamera;
 import fr.fresnel.fourPolar.algorithm.preprocess.registration.IChannelRegistrator;
 import fr.fresnel.fourPolar.algorithm.preprocess.registration.descriptorBased.DescriptorBasedRegistration;
 import fr.fresnel.fourPolar.algorithm.preprocess.segmentation.ICapturedImageSetSegmenter;
@@ -15,13 +27,18 @@ import fr.fresnel.fourPolar.algorithm.preprocess.segmentation.RegistrationCaptur
 import fr.fresnel.fourPolar.algorithm.util.image.color.GrayScaleToColorConverter.Color;
 import fr.fresnel.fourPolar.algorithm.visualization.figures.polarization.IPolarizationImageSetCompositesCreater;
 import fr.fresnel.fourPolar.algorithm.visualization.figures.polarization.RegistrationCompositeFigureCreator;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.imageSet.acquisition.registration.RegistrationImageSet;
+import fr.fresnel.fourPolar.core.imageSet.acquisition.registration.RegistrationImageSet.RegistrationImageType;
 import fr.fresnel.fourPolar.core.imagingSetup.IFourPolarImagingSetup;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.IFieldOfView;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.OneCameraPolarizationConstellation;
+import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.TwoCameraPolarizationConstellation;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.OneCameraPolarizationConstellation.Position;
 import fr.fresnel.fourPolar.core.physics.channel.IChannel;
 import fr.fresnel.fourPolar.core.physics.na.INumericalAperture;
@@ -33,6 +50,7 @@ import fr.fresnel.fourPolar.io.exceptions.image.generic.metadata.MetadataParseEr
 import fr.fresnel.fourPolar.io.image.captured.ICapturedImageSetReader;
 import fr.fresnel.fourPolar.io.image.captured.tiff.TiffCapturedImageSetReader;
 import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.metadata.SCIFIOMetadataReader;
+import fr.fresnel.fourPolar.ui.exceptions.algorithms.preprocess.RegistrationSetProcessFailure;
 
 public class RegistrationSetProcessorTest {
     private static String _root = RegistrationSetProcessor.class.getResource("").getPath();
@@ -66,23 +84,168 @@ public class RegistrationSetProcessorTest {
         return imagingSetup;
     }
 
-    public void process_AllZeroImage_ThrowsRegistrationSetProcessFailure() throws IOException, MetadataParseError {
+    @Test
+    public void process_OneCameraAllZeroImage_ThrowsRegistrationSetProcessFailure()
+            throws IOException, MetadataParseError {
+        Cameras camera = Cameras.One;
+        int[] channel1 = { 1 };
+
+        File registrationImage = new File(_root, "OneCamera/AllZeroImage.tif");
+        ICapturedImageFile[] pol0 = { new RSPDummyCapturedImageFile(channel1, registrationImage) };
+        RSPDummyCapturedImageFileSet fileSet = new RSPDummyCapturedImageFileSet();
+        fileSet.setFileSet(Cameras.getLabels(camera)[0], pol0);
+        fileSet.setCameras(camera);
+
+        RegistrationImageSet registrationImageSet = new RegistrationImageSet(new File(_root),
+                RegistrationImageType.SAMPLE);
+        registrationImageSet.addImageSet(fileSet);
+
         SCIFIOMetadataReader metadataReader = new SCIFIOMetadataReader();
-        IMetadata regImgMetdata = metadataReader.read(new File(_root, "OneCamera/AllZeroImage.tif"));
-
-        IPointShape intersection = new ShapeFactory().point(new long[] { 2, 2 }, AxisOrder.XY);
-
+        IMetadata regImgMetdata = metadataReader.read(registrationImage);
         OneCameraPolarizationConstellation constellation = new OneCameraPolarizationConstellation(Position.TopLeft,
                 Position.TopRight, Position.BottomLeft, Position.BottomRight);
-
+        IPointShape intersection = new ShapeFactory().point(new long[] { 1, 1 }, AxisOrder.XY);
         FoVCalculatorOneCamera fovCalculator = new FoVCalculatorOneCamera(regImgMetdata, intersection, constellation);
 
         IFourPolarImagingSetup imagingSetup = createFPSetup(fovCalculator.calculate(), Cameras.One, 1);
         IRegistrationSetProcessorBuilder builder = this._createDefaultBuilder(imagingSetup);
-     
+
         RegistrationSetProcessor processor = new RegistrationSetProcessor(builder);
 
+        assertThrows(RegistrationSetProcessFailure.class, () -> {
+            processor.process(registrationImageSet);
+        });
     }
+
+    @Test
+    public void process_TwoCameraAllZeroImage_ThrowsRegistrationSetProcessFailure()
+            throws IOException, MetadataParseError {
+        Cameras camera = Cameras.Two;
+        int[] channel1 = { 1 };
+
+        File registrationImage_pol0_90 = new File(_root, "TwoCamera/AllZeroImages/Pol0_90.tif");
+        File registrationImage_pol45_135 = new File(_root, "TwoCamera/AllZeroImages/Pol45_135.tif");
+        ICapturedImageFile[] pol0_90 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol0_90) };
+        ICapturedImageFile[] pol45_135 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol45_135) };
+
+        RSPDummyCapturedImageFileSet fileSet = new RSPDummyCapturedImageFileSet();
+        fileSet.setFileSet(Cameras.getLabels(camera)[0], pol0_90);
+        fileSet.setFileSet(Cameras.getLabels(camera)[1], pol45_135);
+
+        fileSet.setCameras(camera);
+
+        RegistrationImageSet registrationImageSet = new RegistrationImageSet(new File(_root),
+                RegistrationImageType.SAMPLE);
+        registrationImageSet.addImageSet(fileSet);
+
+        SCIFIOMetadataReader metadataReader = new SCIFIOMetadataReader();
+        IMetadata regImgMetdata_pol0_90 = metadataReader.read(registrationImage_pol0_90);
+        IMetadata regImgMetdata_pol45_135 = metadataReader.read(registrationImage_pol45_135);
+        TwoCameraPolarizationConstellation constellation = new TwoCameraPolarizationConstellation(
+                TwoCameraPolarizationConstellation.Position.Left, TwoCameraPolarizationConstellation.Position.Left,
+                TwoCameraPolarizationConstellation.Position.Right, TwoCameraPolarizationConstellation.Position.Right);
+        IPointShape intersection = new ShapeFactory().point(new long[] { 1, 1 }, AxisOrder.XY);
+
+        FoVCalculatorTwoCamera fovCalculator = new FoVCalculatorTwoCamera(regImgMetdata_pol0_90, intersection,
+                regImgMetdata_pol45_135, intersection, constellation);
+
+        IFourPolarImagingSetup imagingSetup = createFPSetup(fovCalculator.calculate(), camera, 1);
+        IRegistrationSetProcessorBuilder builder = this._createDefaultBuilder(imagingSetup);
+
+        RegistrationSetProcessor processor = new RegistrationSetProcessor(builder);
+
+        assertThrows(RegistrationSetProcessFailure.class, () -> {
+            processor.process(registrationImageSet);
+        });
+    }
+
+    @Test
+    public void process_FourCameraAllZeroImage_ThrowsRegistrationSetProcessFailure()
+            throws IOException, MetadataParseError {
+        Cameras camera = Cameras.Four;
+        int[] channel1 = { 1 };
+
+        File registrationImage_pol0 = new File(_root, "FourCamera/AllZeroImages/Pol0.tif");
+        File registrationImage_pol45 = new File(_root, "FourCamera/AllZeroImages/Pol45.tif");
+        File registrationImage_pol90 = new File(_root, "FourCamera/AllZeroImages/Pol90.tif");
+        File registrationImage_pol135 = new File(_root, "FourCamera/AllZeroImages/Pol135.tif");
+
+        ICapturedImageFile[] pol0 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol0) };
+        ICapturedImageFile[] pol45 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol45) };
+        ICapturedImageFile[] pol90 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol90) };
+        ICapturedImageFile[] pol135 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol135) };
+
+        RSPDummyCapturedImageFileSet fileSet = new RSPDummyCapturedImageFileSet();
+        fileSet.setFileSet(Cameras.getLabels(camera)[0], pol0);
+        fileSet.setFileSet(Cameras.getLabels(camera)[1], pol45);
+        fileSet.setFileSet(Cameras.getLabels(camera)[2], pol90);
+        fileSet.setFileSet(Cameras.getLabels(camera)[3], pol135);
+        fileSet.setCameras(camera);
+
+        RegistrationImageSet registrationImageSet = new RegistrationImageSet(new File(_root),
+                RegistrationImageType.SAMPLE);
+        registrationImageSet.addImageSet(fileSet);
+
+        SCIFIOMetadataReader metadataReader = new SCIFIOMetadataReader();
+        IMetadata regImgMetdata_pol0 = metadataReader.read(registrationImage_pol0);
+        IMetadata regImgMetdata_pol45 = metadataReader.read(registrationImage_pol45);
+        IMetadata regImgMetdata_pol90 = metadataReader.read(registrationImage_pol90);
+        IMetadata regImgMetdata_pol135 = metadataReader.read(registrationImage_pol135);
+
+        FoVCalculatorFourCamera fovCalculator = new FoVCalculatorFourCamera(regImgMetdata_pol0, regImgMetdata_pol45,
+                regImgMetdata_pol90, regImgMetdata_pol135);
+
+        IFourPolarImagingSetup imagingSetup = createFPSetup(fovCalculator.calculate(), camera, 1);
+        IRegistrationSetProcessorBuilder builder = this._createDefaultBuilder(imagingSetup);
+
+        RegistrationSetProcessor processor = new RegistrationSetProcessor(builder);
+
+        assertThrows(RegistrationSetProcessFailure.class, () -> {
+            processor.process(registrationImageSet);
+        });
+    }
+
+    // @Test
+    // public void process_TwoCameraProperImageSet_ThrowsRegistrationSetProcessFailure()
+    //         throws IOException, MetadataParseError {
+    //     Cameras camera = Cameras.Two;
+    //     int[] channel1 = { 1 };
+
+    //     File registrationImage_pol0_90 = new File(_root, "TwoCamera/ProperImageSet/Pol0_90.tif");
+    //     File registrationImage_pol45_135 = new File(_root, "TwoCamera/ProperImageSet/Pol45_135.tif");
+    //     ICapturedImageFile[] pol0_90 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol0_90) };
+    //     ICapturedImageFile[] pol45_135 = { new RSPDummyCapturedImageFile(channel1, registrationImage_pol45_135) };
+
+    //     RSPDummyCapturedImageFileSet fileSet = new RSPDummyCapturedImageFileSet();
+    //     fileSet.setFileSet(Cameras.getLabels(camera)[0], pol0_90);
+    //     fileSet.setFileSet(Cameras.getLabels(camera)[1], pol45_135);
+
+    //     fileSet.setCameras(camera);
+
+    //     RegistrationImageSet registrationImageSet = new RegistrationImageSet(new File(_root),
+    //             RegistrationImageType.SAMPLE);
+    //     registrationImageSet.addImageSet(fileSet);
+
+    //     SCIFIOMetadataReader metadataReader = new SCIFIOMetadataReader();
+    //     IMetadata regImgMetdata_pol0_90 = metadataReader.read(registrationImage_pol0_90);
+    //     IMetadata regImgMetdata_pol45_135 = metadataReader.read(registrationImage_pol45_135);
+    //     TwoCameraPolarizationConstellation constellation = new TwoCameraPolarizationConstellation(
+    //             TwoCameraPolarizationConstellation.Position.Left, TwoCameraPolarizationConstellation.Position.Left,
+    //             TwoCameraPolarizationConstellation.Position.Right, TwoCameraPolarizationConstellation.Position.Right);
+    //     IPointShape intersection = new ShapeFactory().point(new long[] { 490, 250 }, AxisOrder.XY);
+
+    //     FoVCalculatorTwoCamera fovCalculator = new FoVCalculatorTwoCamera(regImgMetdata_pol0_90, intersection,
+    //             regImgMetdata_pol45_135, intersection, constellation);
+
+    //     IFourPolarImagingSetup imagingSetup = createFPSetup(fovCalculator.calculate(), camera, 1);
+    //     IRegistrationSetProcessorBuilder builder = this._createDefaultBuilder(imagingSetup);
+
+    //     RegistrationSetProcessor processor = new RegistrationSetProcessor(builder);
+
+    //     assertDoesNotThrow(() -> {
+    //         processor.process(registrationImageSet);
+    //     });
+    // }
 
 }
 
@@ -165,38 +328,32 @@ class RSPDummyBuilder extends IRegistrationSetProcessorBuilder {
 
     @Override
     IPolarizationImageSetCompositesCreater getCompositeImageCreator() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.compositeCreator;
     }
 
     @Override
     IChannelDarkBackgroundEstimator getDarkBackgroundEstimator() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.backGroundEstimator;
     }
 
     @Override
     ICapturedImageSetReader getRegistrationImageReader() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.cSetReader;
     }
 
     @Override
     IChannelRegistrator getRegistrator() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.registrator;
     }
 
     @Override
     ICapturedImageSetSegmenter getSegmenter() {
-        // TODO Auto-generated method stub
-        return null;
+        return this.segmenter;
     }
 
     @Override
     int getNumChannels() {
-        // TODO Auto-generated method stub
-        return 0;
+        return this.numchannels;
     }
 
     public RSPDummyBuilder(IPolarizationImageSetCompositesCreater compositeCreator,
@@ -208,6 +365,86 @@ class RSPDummyBuilder extends IRegistrationSetProcessorBuilder {
         this.registrator = registrator;
         this.segmenter = segmenter;
         this.numchannels = numchannels;
+    }
+
+}
+
+class RSPDummyCapturedImageFileSet implements ICapturedImageFileSet {
+    private Hashtable<String, ICapturedImageFile[]> files = new Hashtable<>();
+    private Cameras _cameras;
+
+    public void setFileSet(String label, ICapturedImageFile[] file) {
+        this.files.put(label, file);
+    }
+
+    public void setCameras(Cameras cameras) {
+        this._cameras = cameras;
+    }
+
+    @Override
+    public ICapturedImageFile[] getFile(String label) {
+        return this.files.get(label);
+    }
+
+    @Override
+    public String getSetName() {
+        return null;
+    }
+
+    @Override
+    public Cameras getnCameras() {
+        return this._cameras;
+    }
+
+    @Override
+    public boolean hasLabel(String label) {
+        return true;
+    }
+
+    @Override
+    public boolean deepEquals(ICapturedImageFileSet fileset) {
+        return false;
+    }
+
+    @Override
+    public Iterator<ICapturedImageFile> getIterator() {
+        Stream<ICapturedImageFile> concatStream = Stream.empty();
+        for (Iterator<ICapturedImageFile[]> iterator = this.files.values().iterator(); iterator.hasNext();) {
+            concatStream = Stream.concat(concatStream, Arrays.stream(iterator.next()));
+        }
+
+        return concatStream.iterator();
+    }
+
+    @Override
+    public int[] getChannels() {
+        IntStream channels = IntStream.empty();
+        for (ICapturedImageFile capturedImageFile : files.get(Cameras.getLabels(_cameras)[0])) {
+            channels = IntStream.concat(channels, Arrays.stream(capturedImageFile.channels()));
+        }
+
+        return channels.toArray();
+    }
+
+}
+
+class RSPDummyCapturedImageFile implements ICapturedImageFile {
+    private int[] _channels;
+    private File _file;
+
+    public RSPDummyCapturedImageFile(int[] channels, File file) {
+        _channels = channels;
+        _file = file;
+    }
+
+    @Override
+    public int[] channels() {
+        return this._channels;
+    }
+
+    @Override
+    public File file() {
+        return this._file;
     }
 
 }
