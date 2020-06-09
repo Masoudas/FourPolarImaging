a68d6e8d0fb4aba8c73233d8bb3a9e50f987442f
Masoudas <Masoudas@hotmail.com>
2020-06-09 08:47:37 +0200

    Add create method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2LogicalShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2LogicalShape.java
@@ -8,8 +8,21 @@ import net.imglib2.roi.RealMaskRealInterval;
  * for example.
  */
 public class ImgLib2LogicalShape extends ImgLib2Shape {
-    public static IShape create(int shapeDim, RealMaskRealInterval shape, AxisOrder axisOrder) {
-        return new ImgLib2LogicalShape(shapeDim, shape, axisOrder);
+    public static IShape create(IShape shape1, IShape shape2) {
+        if (shape1.axisOrder() != shape2.axisOrder()) {
+            throw new IllegalArgumentException("The two shapes are not defined over the same axis.");
+        }
+
+        if (!(shape1 instanceof ImgLib2Shape) || !(shape2 instanceof ImgLib2Shape)) {
+            throw new IllegalArgumentException("The shapes are not instances of ImbLib2Shape.");
+        }
+
+        ImgLib2Shape shape1Ref = (ImgLib2Shape) shape1;
+        ImgLib2Shape shape2Ref = (ImgLib2Shape) shape2;
+
+        RealMaskRealInterval andedShape = shape1Ref.getImgLib2Shape().and(shape2Ref.getImgLib2Shape());
+
+        return new ImgLib2LogicalShape(shape1.shapeDim(), andedShape, shape1.axisOrder());
     }
 
     private ImgLib2LogicalShape(int shapeDim, RealMaskRealInterval shape, AxisOrder axisOrder) {
