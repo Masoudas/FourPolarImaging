c55e88585d256f774e4b63f0ed2c7d9e29183a8f
Masoudas <Masoudas@hotmail.com>
2020-06-10 08:25:22 +0200

    Remove constructor

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2RandomAccessConverter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/imgLib2Model/ImgLib2RandomAccessConverter.java
@@ -1,7 +1,6 @@
 package fr.fresnel.fourPolar.core.image.generic.imgLib2Model;
 
 import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.types.ConverterNotFound;
-import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.types.TypeConverter;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.types.TypeConverterFactory;
@@ -14,38 +13,41 @@ import net.imglib2.type.numeric.integer.UnsignedShortType;
 import net.imglib2.type.numeric.real.FloatType;
 
 /**
- * Converts a random access of ImgLib2 to a {@link IPixelRandomAccess}. 
+ * Converts a random access of ImgLib2 to a {@link IPixelRandomAccess}.
  */
 public class ImgLib2RandomAccessConverter {
-      public static IPixelRandomAccess<RGB16> convertARGBType(RandomAccess<ARGBType> ra) {
-            TypeConverter<RGB16, ARGBType> typeConverter = null;
-            try {
-                  typeConverter = TypeConverterFactory.getConverter(RGB16.zero(), new ARGBType());
-            } catch (ConverterNotFound e) {
-                  // Never caught.
-            }
-            return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
-      }
+    private ImgLib2RandomAccessConverter() {
+        throw new AssertionError();
+    }
 
-      public static IPixelRandomAccess<UINT16> convertUnsignedShortType(RandomAccess<UnsignedShortType> ra) {
-            TypeConverter<UINT16, UnsignedShortType> typeConverter = null;
-            try {
-                  typeConverter = TypeConverterFactory.getConverter(UINT16.zero(), new UnsignedShortType());
-            } catch (ConverterNotFound e) {
-                  // Never caught
-            }
+    public static IPixelRandomAccess<RGB16> convertARGBType(RandomAccess<ARGBType> ra) {
+        TypeConverter<RGB16, ARGBType> typeConverter = null;
+        try {
+            typeConverter = TypeConverterFactory.getConverter(RGB16.zero(), new ARGBType());
+        } catch (ConverterNotFound e) {
+            // Never caught, converter is already implemented.
+        }
+        return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
+    }
 
-            return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
-      }
+    public static IPixelRandomAccess<UINT16> convertUnsignedShortType(RandomAccess<UnsignedShortType> ra) {
+        TypeConverter<UINT16, UnsignedShortType> typeConverter = null;
+        try {
+            typeConverter = TypeConverterFactory.getConverter(UINT16.zero(), new UnsignedShortType());
+        } catch (ConverterNotFound e) {
+            // Never caught, converter is already implemented.
+        }
 
-      public static IPixelRandomAccess<Float32> convertFloat32Type(RandomAccess<FloatType> ra) {
-            TypeConverter<Float32, FloatType> typeConverter = null;
-            try {
-                  typeConverter = TypeConverterFactory.getConverter(Float32.zero(), new FloatType());
-            } catch (ConverterNotFound e) {
-                  // TODO Auto-generated catch block
-                  e.printStackTrace();
-            }
-            return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
-      }
+        return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
+    }
+
+    public static IPixelRandomAccess<Float32> convertFloat32Type(RandomAccess<FloatType> ra) {
+        TypeConverter<Float32, FloatType> typeConverter = null;
+        try {
+            typeConverter = TypeConverterFactory.getConverter(Float32.zero(), new FloatType());
+        } catch (ConverterNotFound e) {
+            // Never caught, converter is already implemented.
+        }
+        return new ImgLib2PixelRandomAccess<>(ra, typeConverter);
+    }
 }
