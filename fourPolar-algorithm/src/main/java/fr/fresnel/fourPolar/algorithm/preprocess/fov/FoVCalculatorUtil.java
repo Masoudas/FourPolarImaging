e02f9e1fa5193108844da7fc4d4140df68992b73
Masoudas <Masoudas@hotmail.com>
2020-06-10 17:29:20 +0200

    Add intersection checker

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/FoVCalculatorUtil.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/FoVCalculatorUtil.java
@@ -1,7 +1,21 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.fov;
 
+import fr.fresnel.fourPolar.core.util.shape.IPointShape;
+
 public class FoVCalculatorUtil {
-    private FoVCalculatorUtil(){
+    private FoVCalculatorUtil() {
         throw new AssertionError();
-    }    
+    }
+
+    /**
+     * Checks that the intersection point for one camera and two camera case is 2D
+     * and if not, throws an exception.
+     * 
+     * @throws IllegalArgumentException in case point is not 2D.
+     */
+    public static void checkIntersectionPointIs2D(IPointShape intersectionPoint) throws IllegalArgumentException {
+        if (intersectionPoint.point().length != 2) {
+            throw new IllegalArgumentException("Intersection point for calculating the FoV must be two dimensional");
+        }
+    }
 }
