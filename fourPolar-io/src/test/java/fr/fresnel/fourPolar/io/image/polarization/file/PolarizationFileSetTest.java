82c7a4c9eb647d9972e6ed9a8b8236e036f63359
Masoudas <Masoudas@hotmail.com>
2020-06-11 10:41:10 +0200

    Refactor test

--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/file/PolarizationFileSetTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/file/PolarizationFileSetTest.java
@@ -3,14 +3,14 @@ package fr.fresnel.fourPolar.io.image.polarization.file;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
-import java.nio.file.Path;
-import java.nio.file.Paths;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.core.PathFactoryOfProject;
-import fr.fresnel.fourPolar.core.image.captured.file.CapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 public class PolarizationFileSetTest {
@@ -18,77 +18,74 @@ public class PolarizationFileSetTest {
     File _root = new File("/root");
     File _rootPol = PathFactoryOfProject.getFolder_PolarizationImages(_root);
 
-    private File formSetParentFolder(String setName) {
-        Path path = Paths.get(_rootPol.getAbsolutePath(), "Channel" + _channel, setName);
-        return path.toFile();
-    }
-
     @Test
-    public void getFile_OneCameraCase_CreatesFilesInCorrectPath(){
-        File pol0_45_90_135 = new File(_root, "pol0_45_90_135.tif");
-        
-        ICapturedImageFileSet fileSet = new CapturedImageFileSet(_channel, pol0_45_90_135);
-        IPolarizationImageFileSet pfileSet = new TiffPolarizationImageFileSet(_root, fileSet);
+    public void getFile_OneCameraCase_CreatesFilesInCorrectPath() {
+        int channel = 1;
+        String setName_alias = "pol0_45_90_135";
 
-        File parentFolder = formSetParentFolder(fileSet.getSetName());
-        File expected_pol0 = new File(parentFolder, "pol0_45_90_135_p0.tif");
-        File expected_pol45 = new File(parentFolder, "pol0_45_90_135_p45.tif");
-        File expected_pol90 = new File(parentFolder, "pol0_45_90_135_p90.tif");
-        File expected_pol135 = new File(parentFolder, "pol0_45_90_135_p135.tif");
+        ICapturedImageFileSet fileSet = new DummyCapturedImageFileSet(setName_alias);
+        IPolarizationImageFileSet pfileSet = new TiffPolarizationImageFileSet(_root, fileSet, channel);
 
-        assertTrue(
-            pfileSet.getFile(Polarization.pol0).getAbsolutePath().contentEquals(expected_pol0.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol45).getAbsolutePath().contentEquals(expected_pol45.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol90).getAbsolutePath().contentEquals(expected_pol90.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol135).getAbsolutePath().contentEquals(expected_pol135.getAbsolutePath())
-        );
+        assertTrue(_checkParentFolderEqual(pfileSet, Polarization.pol0, Polarization.pol45)
+                && _checkParentFolderEqual(pfileSet, Polarization.pol0, Polarization.pol90)
+                && _checkParentFolderEqual(pfileSet, Polarization.pol0, Polarization.pol135));
+
+        assertTrue(_checkExtensionTiff(pfileSet, Polarization.pol0) && _checkExtensionTiff(pfileSet, Polarization.pol45)
+                && _checkExtensionTiff(pfileSet, Polarization.pol90)
+                && _checkExtensionTiff(pfileSet, Polarization.pol135));
     }
 
-    @Test
-    public void getFile_TwoCameraCase_CreatesFilesInCorrectPath(){
-        File pol0_90 = new File(_root, "pol0_90.tif");
-        File pol45_135 = new File(_root, "pol45_135.tif");
-        
-        ICapturedImageFileSet fileSet = new CapturedImageFileSet(_channel, pol0_90, pol45_135);
-        IPolarizationImageFileSet pfileSet = new TiffPolarizationImageFileSet(_root, fileSet);
-
-        File parentFolder = formSetParentFolder(fileSet.getSetName());
-        File expected_pol0 = new File(parentFolder, "pol0_90_p0.tif");
-        File expected_pol45 = new File(parentFolder, "pol45_135_p45.tif");
-        File expected_pol90 = new File(parentFolder, "pol0_90_p90.tif");
-        File expected_pol135 = new File(parentFolder, "pol45_135_p135.tif");
-
-        assertTrue(
-            pfileSet.getFile(Polarization.pol0).getAbsolutePath().contentEquals(expected_pol0.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol45).getAbsolutePath().contentEquals(expected_pol45.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol90).getAbsolutePath().contentEquals(expected_pol90.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol135).getAbsolutePath().contentEquals(expected_pol135.getAbsolutePath())
-        );
+    private boolean _checkParentFolderEqual(IPolarizationImageFileSet pfileSet, Polarization pol1, Polarization pol2) {
+        return pfileSet.getFile(pol1).getParentFile().equals(pfileSet.getFile(pol2).getParentFile());
     }
 
-    @Test
-    public void getFile_FourCameraCase_CreatesFilesInCorrectPath(){
-        File pol0 = new File(_root, "pol0.tif");
-        File pol45 = new File(_root, "pol45.tif");
-        File pol90 = new File(_root, "pol90.tif");
-        File pol135 = new File(_root, "pol135.tif");
-        
-        ICapturedImageFileSet fileSet = new CapturedImageFileSet(_channel, pol0, pol45, pol90, pol135);
-        IPolarizationImageFileSet pfileSet = new TiffPolarizationImageFileSet(_root, fileSet);
-
-        File parentFolder = formSetParentFolder(fileSet.getSetName());
-        File expected_pol0 = new File(parentFolder, "pol0.tif");
-        File expected_pol45 = new File(parentFolder, "pol45.tif");
-        File expected_pol90 = new File(parentFolder, "pol90.tif");
-        File expected_pol135 = new File(parentFolder, "pol135.tif");
-
-        assertTrue(
-            pfileSet.getFile(Polarization.pol0).getAbsolutePath().contentEquals(expected_pol0.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol45).getAbsolutePath().contentEquals(expected_pol45.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol90).getAbsolutePath().contentEquals(expected_pol90.getAbsolutePath()) &&
-            pfileSet.getFile(Polarization.pol135).getAbsolutePath().contentEquals(expected_pol135.getAbsolutePath())
-        );
+    private boolean _checkExtensionTiff(IPolarizationImageFileSet pfileSet, Polarization pol1) {
+        int last = pfileSet.getFile(pol1).getPath().lastIndexOf(".");
+        return pfileSet.getFile(pol1).getPath().substring(last + 1).equals("tif");
     }
 
+}
+
+class DummyCapturedImageFileSet implements ICapturedImageFileSet {
+    String setName;
+
+    public DummyCapturedImageFileSet(String setName) {
+        this.setName = setName;
+    }
+
+    @Override
+    public ICapturedImageFile[] getFile(String label) {
+        return null;
+    }
+
+    @Override
+    public String getSetName() {
+        return this.setName;
+    }
+
+    @Override
+    public Cameras getnCameras() {
+        return null;
+    }
+
+    @Override
+    public boolean hasLabel(String label) {
+        return false;
+    }
+
+    @Override
+    public boolean deepEquals(ICapturedImageFileSet fileset) {
+        return false;
+    }
+
+    @Override
+    public Iterator<ICapturedImageFile> getIterator() {
+        return null;
+    }
+
+    @Override
+    public int[] getChannels() {
+        return null;
+    }
 
 }
