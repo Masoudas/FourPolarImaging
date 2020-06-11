5a0332cad7ca4d030480b2bf56891cd10e41cbaa
Masoudas <Masoudas@hotmail.com>
2020-06-11 11:18:59 +0200

    Refactor tests

--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetReaderTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetReaderTest.java
@@ -5,58 +5,123 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import java.io.File;
 import java.io.IOException;
 import java.util.Arrays;
+import java.util.Iterator;
 
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
-import fr.fresnel.fourPolar.core.image.captured.file.CapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
+import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
-import fr.fresnel.fourPolar.io.exceptions.image.generic.NoReaderFoundForImage;
 import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.SCIFIOUINT16TiffWriter;
 import fr.fresnel.fourPolar.io.image.polarization.file.TiffPolarizationImageFileSet;
 
 public class TiffPolarizationImageSetReaderTest {
     final private static File _root = new File(TiffPolarizationImageSetReaderTest.class.getResource("").getPath(),
-            "TiffPolarizationImageSetReader");
+            "Reader");
 
+    @BeforeAll
+    public static void createPath() {
+        _root.mkdirs();
+    }
+
+    /**
+     * The premise of the test is write and then read an orientation image.
+     * 
+     */
     @Test
     public void read_PolarizationImageSetFromDisk_ReadImagesHaveSameDimensionAsDisk()
-            throws NoReaderFoundForImage, IOException, CannotFormPolarizationImageSet {
-        File capturedImageFile = new File(_root, "testFile.tif");
-        CapturedImageFileSet fileSet = new CapturedImageFileSet(1, capturedImageFile);
-        TiffPolarizationImageFileSet fSet = new TiffPolarizationImageFileSet(_root, fileSet);
+            throws IOException, CannotFormPolarizationImageSet {
+        // Create polarization image set.
+        long[] dim = { 2, 2, 1, 1, 1 };
+        IMetadata metadata = new Metadata.MetadataBuilder(dim).axisOrder(AxisOrder.XYCZT)
+                .bitPerPixel(PixelTypes.FLOAT_32).build();
 
-        long[] dim = { 2, 2 };
-        Image<UINT16> pol0 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol45 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol90 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol135 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
+        Image<UINT16> pol0 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol45 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol90 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol135 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
 
+        // File set
+        int channel = 1;
+        String setNameAlias = "testSet";
+        WriterDummyCapturedImageFileSet fileSet = new WriterDummyCapturedImageFileSet(setNameAlias, channel);
+
+        // Write to presumed destination.
+        TiffPolarizationImageFileSet fSet = new TiffPolarizationImageFileSet(_root, fileSet, channel);
         SCIFIOUINT16TiffWriter writer = new SCIFIOUINT16TiffWriter();
         writer.write(fSet.getFile(Polarization.pol0), pol0);
         writer.write(fSet.getFile(Polarization.pol45), pol45);
         writer.write(fSet.getFile(Polarization.pol90), pol90);
         writer.write(fSet.getFile(Polarization.pol135), pol135);
 
-
         ImageFactory factory = new ImgLib2ImageFactory();
-        IPolarizationImageSetReader reader = new TiffPolarizationImageSetReader(factory);
-        IPolarizationImageSet imageSet = reader.read(_root, fileSet);
+        IPolarizationImageSetReader reader = new TiffPolarizationImageSetReader(factory, channel);
+        IPolarizationImageSet imageSet = reader.read(_root, fileSet, channel);
 
-        assertTrue(       
-            Arrays.equals(
-                imageSet.getPolarizationImage(Polarization.pol0).getImage().getMetadata().getDim(), dim) &&
-            Arrays.equals(
-                imageSet.getPolarizationImage(Polarization.pol45).getImage().getMetadata().getDim(), dim) &&
-            Arrays.equals(
-                imageSet.getPolarizationImage(Polarization.pol90).getImage().getMetadata().getDim(), dim) &&
-            Arrays.equals(
-                imageSet.getPolarizationImage(Polarization.pol135).getImage().getMetadata().getDim(), dim));
+        assertTrue(Arrays.equals(imageSet.getPolarizationImage(Polarization.pol0).getImage().getMetadata().getDim(),
+                dim)
+                && Arrays.equals(imageSet.getPolarizationImage(Polarization.pol45).getImage().getMetadata().getDim(),
+                        dim)
+                && Arrays.equals(imageSet.getPolarizationImage(Polarization.pol90).getImage().getMetadata().getDim(),
+                        dim)
+                && Arrays.equals(imageSet.getPolarizationImage(Polarization.pol135).getImage().getMetadata().getDim(),
+                        dim));
+    }
+
+}
+
+class ReaderDummyCapturedImageFileSet implements ICapturedImageFileSet {
+    String setName;
+
+    public ReaderDummyCapturedImageFileSet(String setName) {
+        this.setName = setName;
+    }
+
+    @Override
+    public ICapturedImageFile[] getFile(String label) {
+        return null;
+    }
+
+    @Override
+    public String getSetName() {
+        return this.setName;
+    }
+
+    @Override
+    public Cameras getnCameras() {
+        return null;
+    }
+
+    @Override
+    public boolean hasLabel(String label) {
+        return false;
+    }
+
+    @Override
+    public boolean deepEquals(ICapturedImageFileSet fileset) {
+        return false;
+    }
+
+    @Override
+    public Iterator<ICapturedImageFile> getIterator() {
+        return null;
+    }
+
+    @Override
+    public int[] getChannels() {
+        return null;
     }
 
 }
--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetWriterTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/polarization/TiffPolarizationImageSetWriterTest.java
@@ -4,48 +4,103 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.core.exceptions.image.polarization.CannotFormPolarizationImageSet;
-import fr.fresnel.fourPolar.core.image.captured.file.CapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
-import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageSet;
+import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
+import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageSetBuilder;
+import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
-import fr.fresnel.fourPolar.io.exceptions.image.generic.NoWriterFoundForImage;
 import fr.fresnel.fourPolar.io.image.polarization.file.TiffPolarizationImageFileSet;
 
 public class TiffPolarizationImageSetWriterTest {
     final private static File _root = new File(TiffPolarizationImageSetReaderTest.class.getResource("").getPath(),
-            "TiffPolarizationImageSetWriter");
+            "Writer");
 
     @Test
     public void write_ImgLib2PolarizationImage_WritesIntoTheTargetFolder()
-            throws CannotFormPolarizationImageSet, IOException, NoWriterFoundForImage {
-        File pol0_45_90_135_File = new File(_root, "testFile.tif");
+            throws CannotFormPolarizationImageSet, IOException {
+        int channel = 1;
+        String setNameAlias = "testSet";
 
-        CapturedImageFileSet fileSet = new CapturedImageFileSet(
-            1, pol0_45_90_135_File);
+        WriterDummyCapturedImageFileSet fileSet = new WriterDummyCapturedImageFileSet(setNameAlias, channel);
 
-        long[] dim = { 2, 2 };
+        // Create polarization image set.
+        long[] dim = { 2, 2, 1, 1, 1 };
+        IMetadata metadata = new Metadata.MetadataBuilder(dim).axisOrder(AxisOrder.XYCZT)
+                .bitPerPixel(PixelTypes.FLOAT_32).build();
 
-        Image<UINT16> pol0 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol45 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol90 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
-        Image<UINT16> pol135 = new ImgLib2ImageFactory().create(dim, UINT16.zero());
+        Image<UINT16> pol0 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol45 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol90 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
+        Image<UINT16> pol135 = new ImgLib2ImageFactory().create(metadata, UINT16.zero());
 
-        PolarizationImageSet imageSet = new PolarizationImageSet(fileSet, pol0, pol45, pol90, pol135);
+        IPolarizationImageSet imageSet = new PolarizationImageSetBuilder(channel).fileSet(fileSet).pol0(pol0)
+                .pol45(pol45).pol90(pol90).pol135(pol135).channel(channel).build();
 
-        IPolarizationImageSetWriter writer = new TiffPolarizationImageSetWriter(imageSet);
-        writer.write(_root, imageSet);    
+        IPolarizationImageSetWriter writer = new TiffPolarizationImageSetWriter();
+        writer.write(_root, imageSet);
 
-        TiffPolarizationImageFileSet fSet = new TiffPolarizationImageFileSet(_root, fileSet);
-        assertTrue(
-            fSet.getFile(Polarization.pol0).exists() &&
-            fSet.getFile(Polarization.pol45).exists() &&
-            fSet.getFile(Polarization.pol90).exists() &&
-            fSet.getFile(Polarization.pol135).exists());
+        TiffPolarizationImageFileSet fSet = new TiffPolarizationImageFileSet(_root, fileSet, channel);
+        assertTrue(fSet.getFile(Polarization.pol0).exists() && fSet.getFile(Polarization.pol45).exists()
+                && fSet.getFile(Polarization.pol90).exists() && fSet.getFile(Polarization.pol135).exists());
     }
 }
+
+class WriterDummyCapturedImageFileSet implements ICapturedImageFileSet {
+    String setName;
+    int channel;
+
+    public WriterDummyCapturedImageFileSet(String setName, int channel) {
+        this.setName = setName;
+        this.channel = channel;
+    }
+
+    @Override
+    public ICapturedImageFile[] getFile(String label) {
+        return null;
+    }
+
+    @Override
+    public String getSetName() {
+        return this.setName;
+    }
+
+    @Override
+    public Cameras getnCameras() {
+        return null;
+    }
+
+    @Override
+    public boolean hasLabel(String label) {
+        return false;
+    }
+
+    @Override
+    public boolean deepEquals(ICapturedImageFileSet fileset) {
+        return false;
+    }
+
+    @Override
+    public Iterator<ICapturedImageFile> getIterator() {
+        return null;
+    }
+
+    @Override
+    public int[] getChannels() {
+        return null;
+    }
+
+}
+
