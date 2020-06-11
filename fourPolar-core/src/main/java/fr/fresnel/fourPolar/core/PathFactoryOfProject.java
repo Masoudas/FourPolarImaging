faa219fdb8247bf2d73ba87b8fcc85083ca262b8
Masoudas <Masoudas@hotmail.com>
2020-06-11 16:57:57 +0200

    Don't cache objects

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/PathFactoryOfProject.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/PathFactoryOfProject.java
@@ -21,34 +21,31 @@ public class PathFactoryOfProject {
      * child of this folder.
      */
     private static String _projectRoot = "4Polar";
-    private static File _projectRootPath = null;
 
     /**
      * The folder that has all the imaging setup data, image information and etc.
      */
     private static String _folder_params = "0_Params";
-    private static File _folder_paramsPath = null;
 
     /**
      * The folder that holds the polarization images of the project.
      */
     private static String _folder_polarizationImages = "2_PolarizationImages";
-    private static File _folder_polarizationImagesPath = null;
 
     /**
      * The folder that holds the orientation images of the project.
      */
     private static String _folder_orientationImages = "3_OrientationImages";
-    private static File _folder_orientationImagesPath = null;
 
     /**
      * The folder that holds the processed registration images of the project.
      */
     private static String _FOLDER_PROCCESSED_REGISTRATION_IMAGES = "1_ProcessedRegistrationImages";
-    private static File _FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH = null;
 
+    /**
+     * The folder that holds the visualization sessions.
+     */
     private static String _FOLDER_VISUALIZATION = "3_Visualization";
-    private static File _FOLDER_VISUALIZATION_PATH = null;
 
     /**
      * Creates and returns the 4Polar folder inside root4PProject.
@@ -57,12 +54,8 @@ public class PathFactoryOfProject {
      * @return
      */
     public static File getFolder_4Polar(File root4PProject) {
-        if (_projectRootPath != null) {
-            return _projectRootPath;
-        }
-
         Path path = Paths.get(root4PProject.getAbsolutePath(), _projectRoot);
-        _projectRootPath = path.toFile();
+        File _projectRootPath = path.toFile();
 
         if (!_projectRootPath.exists()) {
             _projectRootPath.mkdir();
@@ -80,18 +73,14 @@ public class PathFactoryOfProject {
      * @param root4PProject is the location of the 4Polar folder.
      */
     public static File getFolder_Params(File root4PProject) {
-        if (_folder_paramsPath != null) {
-            return _folder_paramsPath;
-        }
-
         Path path = Paths.get(getFolder_4Polar(root4PProject).getAbsolutePath(), _folder_params);
-        _folder_paramsPath = path.toFile();
+        File folder_paramsPath = path.toFile();
 
-        if (!_folder_paramsPath.exists()) {
-            _folder_paramsPath.mkdirs();
+        if (!folder_paramsPath.exists()) {
+            folder_paramsPath.mkdirs();
         }
 
-        return _folder_paramsPath;
+        return folder_paramsPath;
     }
 
     /**
@@ -101,18 +90,14 @@ public class PathFactoryOfProject {
      * @return
      */
     public static File getFolder_PolarizationImages(File root4PProject) {
-        if (_folder_polarizationImagesPath != null) {
-            return _folder_polarizationImagesPath;
-        }
-
-        _folder_polarizationImagesPath = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
+        File folder_polarizationImagesPath = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
                 _folder_polarizationImages);
 
-        if (!_folder_polarizationImagesPath.exists()) {
-            _folder_polarizationImagesPath.mkdirs();
+        if (!folder_polarizationImagesPath.exists()) {
+            folder_polarizationImagesPath.mkdirs();
         }
 
-        return _folder_polarizationImagesPath;
+        return folder_polarizationImagesPath;
     }
 
     /**
@@ -122,17 +107,14 @@ public class PathFactoryOfProject {
      * @return
      */
     public static File getFolder_OrientationImages(File root4PProject) {
-        if (_folder_orientationImagesPath != null) {
-            return _folder_orientationImagesPath;
-        }
-        _folder_orientationImagesPath = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
+        File folder_orientationImagesPath = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
                 _folder_orientationImages);
 
-        if (!_folder_orientationImagesPath.exists()) {
-            _folder_orientationImagesPath.mkdirs();
+        if (!folder_orientationImagesPath.exists()) {
+            folder_orientationImagesPath.mkdirs();
         }
 
-        return _folder_orientationImagesPath;
+        return folder_orientationImagesPath;
     }
 
     /**
@@ -142,39 +124,30 @@ public class PathFactoryOfProject {
      * @return
      */
     public static File getFolder_ProcessedRegistrationImages(File root4PProject) {
-        if (_FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH != null) {
-            return _FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH;
-        }
-
-        _FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
+        File folder_Processed_Registration_Images_Path = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
                 _FOLDER_PROCCESSED_REGISTRATION_IMAGES);
 
-        if (!_FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH.exists()) {
-            _FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH.mkdirs();
+        if (!folder_Processed_Registration_Images_Path.exists()) {
+            folder_Processed_Registration_Images_Path.mkdirs();
         }
 
-        return _FOLDER_PROCCESSED_REGISTRATION_IMAGES_PATH;
+        return folder_Processed_Registration_Images_Path;
     }
 
-        /**
+    /**
      * Returns the folder that would contain the visualization sessions.
      * 
      * @param root4PProject is the location of the 4Polar folder.
      * @return
      */
     public static File getFolder_Visualization(File root4PProject) {
-        if (_FOLDER_VISUALIZATION_PATH != null) {
-            return _FOLDER_VISUALIZATION_PATH;
+        File folder_Visualization_Pth = new File(getFolder_4Polar(root4PProject).getAbsolutePath(), _FOLDER_VISUALIZATION);
+
+        if (!folder_Visualization_Pth.exists()) {
+            folder_Visualization_Pth.mkdirs();
         }
 
-        _FOLDER_VISUALIZATION_PATH = new File(getFolder_4Polar(root4PProject).getAbsolutePath(),
-                _FOLDER_VISUALIZATION);
-
-        if (!_FOLDER_VISUALIZATION_PATH.exists()) {
-            _FOLDER_VISUALIZATION_PATH.mkdirs();
-        }
-
-        return _FOLDER_VISUALIZATION_PATH;
+        return folder_Visualization_Pth;
     }
 
 }
