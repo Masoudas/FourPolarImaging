6c6ddcb1d31e4e76e24cfe2789fa5c9558344947
Masoudas <Masoudas@hotmail.com>
2020-06-12 08:28:21 +0200

    Move check conditions inside the class

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2BoxShape.java
@@ -1,6 +1,8 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
 import java.util.Arrays;
+import java.util.Objects;
+import java.util.stream.IntStream;
 
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import net.imglib2.roi.geom.GeomMasks;
@@ -11,16 +13,45 @@ class ImgLib2BoxShape extends ImgLib2Shape implements IBoxShape {
     private final long[] _max;
 
     public static IBoxShape create(long[] min, long[] max, AxisOrder axisOrder) {
-        int shapeDim = 0;
-        while (shapeDim < min.length && min[shapeDim] < max[shapeDim]) {
-            shapeDim++;
-        }
+        Objects.requireNonNull(min, "min should not be null");
+        Objects.requireNonNull(max, "max should not be null");
+
+        _checkMinAndMaxHaveEqualDimension(min, max);
+        _checkBoxIsAtLeastTwoDimensional(min);
+        _checkMaxIsGreaterThanMin(min, max);
+        _checkMinDimensionEqualsNumAxis(min, axisOrder);
 
         double[] minCopy = Arrays.stream(min).asDoubleStream().toArray();
         double[] maxCopy = Arrays.stream(max).asDoubleStream().toArray();
         WritableBox box = GeomMasks.closedBox(minCopy, maxCopy);
 
         return new ImgLib2BoxShape(box, axisOrder, min, max);
+    }
+
+    private static void _checkMinDimensionEqualsNumAxis(long[] min, AxisOrder axisOrder) {
+        if (axisOrder != AxisOrder.NoOrder && axisOrder.numAxis != min.length) {
+            throw new IllegalArgumentException("Number of axis must correspond to shape min and max");
+        }
+    }
+
+    private static void _checkMaxIsGreaterThanMin(long[] min, long[] max) {
+        if (IntStream.range(0, max.length).anyMatch((i) -> {
+            return min[i] > max[i];
+        })) {
+            throw new IllegalArgumentException("max should be greater than or equal to min");
+        }
+    }
+
+    private static void _checkBoxIsAtLeastTwoDimensional(long[] min) {
+        if (min.length <= 1) {
+            throw new IllegalArgumentException("Number of dimension has to be at least two.");
+        }
+    }
+
+    private static void _checkMinAndMaxHaveEqualDimension(long[] min, long[] max) {
+        if (min.length != max.length) {
+            throw new IllegalArgumentException("min and max should have the same dimension");
+        }
     }
 
     private ImgLib2BoxShape(WritableBox shape, final AxisOrder axisOrder, long[] min, long[] max) {
--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
@@ -20,38 +20,10 @@ public class ShapeFactory {
      *                                  above.
      */
     public IBoxShape closedBox(long[] min, long[] max, AxisOrder axisOrder) {
-        Objects.requireNonNull(min, "min should not be null");
-        Objects.requireNonNull(max, "max should not be null");
-
-        if (min.length != max.length) {
-            throw new IllegalArgumentException("min and max should have the same dimension");
-        }
-
-        if (min.length <= 1) {
-            throw new IllegalArgumentException("Number of dimension has to be at least two.");
-        }
-
-        if (axisOrder != AxisOrder.NoOrder && axisOrder.numAxis != min.length) {
-            throw new IllegalArgumentException("Number of axis must correspond to shape min and max");
-        }
-
-        if (IntStream.range(0, max.length).anyMatch((i) -> {
-            return min[i] > max[i];
-        })) {
-            throw new IllegalArgumentException("max should be greater than or equal to min");
-        }
-
         return ImgLib2BoxShape.create(min, max, axisOrder);
     }
 
     public IPolygon2DShape closedPolygon2D(long[] x, long[] y) {
-        Objects.requireNonNull(x, "x should not be null");
-        Objects.requireNonNull(y, "y should not be null");
-
-        if (x.length != y.length || x.length < 3) {
-            throw new IllegalArgumentException("x and y should have the same dimension and greater than three.");
-        }
-
         return ImgLib2Polygon2DShape.create(x, y);
     }
 
