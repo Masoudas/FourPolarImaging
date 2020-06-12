e67f1a31c91f62d83d7461ffe0c1e27bb0dacc90
Masoudas <Masoudas@hotmail.com>
2020-06-12 08:23:57 +0200

    Check at least three points are given

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2Polygon2DShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2Polygon2DShape.java
@@ -1,6 +1,7 @@
 package fr.fresnel.fourPolar.core.util.shape;
 
 import java.util.Arrays;
+import java.util.Objects;
 
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import net.imglib2.roi.geom.GeomMasks;
@@ -11,12 +12,33 @@ public class ImgLib2Polygon2DShape extends ImgLib2Shape implements IPolygon2DSha
     private final long[] _y_vertices;
 
     public static IPolygon2DShape create(long[] x, long[] y) {
+        Objects.requireNonNull(x, "x should not be null");
+        Objects.requireNonNull(y, "y should not be null");
+
+        _checkXAndYHaveSameDimension(x, y);
+        _checkAtLeastThreeVerticesExist(x, y);
+
         double[] xPoints = Arrays.stream(x).asDoubleStream().toArray();
         double[] yPoints = Arrays.stream(y).asDoubleStream().toArray();
 
         WritablePolygon2D polygon2D = GeomMasks.closedPolygon2D(xPoints, yPoints);
 
         return new ImgLib2Polygon2DShape(polygon2D, x, y);
+    }
+
+    /**
+     * Throws illegalArgumentException if x and y are not equal.
+     */
+    private static void _checkXAndYHaveSameDimension(long[] x, long[] y) {
+        if (x.length != y.length) {
+            throw new IllegalArgumentException("x and y vertice coordinates should have equal number of elements.");
+        }
+    }
+
+    private static void _checkAtLeastThreeVerticesExist(long[] x, long[] y) {
+        if (x.length != 3 || y.length != 3) {
+            throw new IllegalArgumentException("At least three points should be supplied to create a polygon 2d");
+        }
     }
 
     private ImgLib2Polygon2DShape(WritablePolygon2D shape, long[] x_vertices, long[] y_vertices) {
