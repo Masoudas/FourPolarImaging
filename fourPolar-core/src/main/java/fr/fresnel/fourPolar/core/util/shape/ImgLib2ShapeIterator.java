36ee636e782f09bd64f3af794fc519b97feeb2ea
Masoudas <Masoudas@hotmail.com>
2020-06-12 08:52:37 +0200

    Return a new vector for every position

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2ShapeIterator.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ImgLib2ShapeIterator.java
@@ -8,8 +8,8 @@ import net.imglib2.type.logic.BoolType;
  * An iterator to iterator over discrete coordinate points.
  */
 class ImgLib2ShapeIterator implements IShapeIterator {
-    private final long[] _position;
     private final Cursor<Void> _regionCursor;
+    private final int _shapeDim;
 
     /**
      * Form the iterator, corresponding to space properties and shape.
@@ -18,7 +18,7 @@ class ImgLib2ShapeIterator implements IShapeIterator {
      */
     public ImgLib2ShapeIterator(IterableRegion<BoolType> iterableRegion, int shapeDim) {
         this._regionCursor = iterableRegion.cursor();
-        this._position = new long[shapeDim];
+        this._shapeDim = shapeDim;
     }
 
     @Override
@@ -28,10 +28,11 @@ class ImgLib2ShapeIterator implements IShapeIterator {
 
     @Override
     public long[] next() {
+        long[] position = new long[this._shapeDim];
         _regionCursor.next();
-        this._regionCursor.localize(this._position);
+        this._regionCursor.localize(position);
 
-        return this._position;
+        return position;
     }
 
     @Override
