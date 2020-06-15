847827bd398c5808c422f2fbc51582f81271f175
Masoudas <Masoudas@hotmail.com>
2020-06-15 11:30:32 +0200

    Remove compute methods

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/darkBackground/estimator/percentile/PercentileDarkBackgroundUtil.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/darkBackground/estimator/percentile/PercentileDarkBackgroundUtil.java
@@ -1,6 +1,6 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.darkBackground.estimator.percentile;
 
-import java.util.Arrays;
+import java.util.HashMap;
 
 import org.apache.commons.math3.stat.descriptive.rank.Percentile;
 
@@ -9,6 +9,9 @@ import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.metadata.MetadataUtil;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
+import fr.fresnel.fourPolar.core.image.polarization.PolarizationImageUtils;
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
  * Utility methods used for estimating dark background.
@@ -31,39 +34,6 @@ class PercentileDarkBackgroundUtil {
         return imgAsArray;
     }
 
-    public static double computePercentile(double[] values, int quantile) {
-        Percentile percentileCalculator = new Percentile(quantile);
-
-        return percentileCalculator.evaluate(values);
-    }
-
-    public static double computePercentile(double[] values1, double[] values2, int quantile) {
-        Percentile percentileCalculator = new Percentile(quantile);
-
-        double[] concatenated = Arrays.copyOf(values1, values1.length + values2.length);
-        System.arraycopy(values2, 0, concatenated, values1.length, values2.length);
-
-        return percentileCalculator.evaluate(concatenated);
-    }
-
-    public static double computePercentile(double[] values1, double[] values2, double[] values3, double[] values4,
-            int quantile) {
-        Percentile percentileCalculator = new Percentile(quantile);
-
-        double[] concatenated12 = Arrays.copyOf(values1, values1.length + values2.length);
-        System.arraycopy(values2, 0, concatenated12, values1.length, values2.length);
-
-        double[] concatenated34 = Arrays.copyOf(values3, values3.length + values4.length);
-        System.arraycopy(values4, 0, concatenated34, values3.length, values4.length);
-
-        double[] concatenated = Arrays.copyOf(concatenated12, concatenated12.length + concatenated34.length);
-        concatenated12 = null;
-        System.arraycopy(concatenated34, 0, concatenated, values1.length + values2.length, concatenated34.length);
-        concatenated34 = null;
-
-        return percentileCalculator.evaluate(concatenated);
-    }
-
     private static IPixelCursor<UINT16> _getFirstPlaneCursor(Image<UINT16> image) {
         long[] bottomCorner = _getFirstPlaneBottomCorner(image.getMetadata());
         long[] length = _getFirstPlaneLength(image.getMetadata());
@@ -83,5 +53,11 @@ class PercentileDarkBackgroundUtil {
         }
         return planeLen;
     }
+
+    public static double computePercentile(double[] values, int quantile) {
+        Percentile percentileCalculator = new Percentile(quantile);
+        return percentileCalculator.evaluate(values);
+    }
+
 
 }
