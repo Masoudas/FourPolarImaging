e5625b2f3186353e22511cf28b3d45b167fe69e7
Masoudas <Masoudas@hotmail.com>
2020-06-15 14:21:54 +0200

    Create file

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/RegistrationSetProcessResultToYAMLFile.java
@@ -0,0 +1,21 @@
+package fr.fresnel.fourPolar.io.preprocess;
+
+import java.io.File;
+
+import fr.fresnel.fourPolar.core.PathFactoryOfProject;
+
+class RegistrationSetProcessResultToYAMLFile {
+    private static String _FILE_NAME = "Registration_Image_Set_Process_Results.yaml";
+
+    /**
+     * Creates a file that contains the full path to the file that holds the
+     * registration result yaml, including the file itself.
+     */
+    public static File getPathToYaml(File root4PProject) {
+        File rootFolder = PathFactoryOfProject.getFolder_Params(root4PProject);
+
+        return new File(rootFolder, _FILE_NAME);
+
+    }
+
+}
