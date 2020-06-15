ea219cd660b2d177c09d399aa89e38afc0489305
Masoudas <Masoudas@hotmail.com>
2020-06-15 15:15:02 +0200

    Use affine2D adaptor

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/registration/ChannelRegistrationRuleToJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/registration/ChannelRegistrationRuleToJSONAdaptor.java
@@ -4,14 +4,16 @@ import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fr.fresnel.fourPolar.core.preprocess.registration.IChannelRegistrationResult;
 import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
+import fr.fresnel.fourPolar.core.util.transform.Affine2D;
 import fr.fresnel.fourPolar.core.util.transform.AffineTransform;
+import fr.fresnel.fourPolar.io.util.transform.Affine2DToJSONAdaptor;
 
 class ChannelRegistrationRuleToJSONAdaptor {
     @JsonProperty("Rule")
     private String _ruleDescription;
 
     @JsonProperty("Rule Affine Transform")
-    private double[][] _affineTransform;
+    private Affine2DToJSONAdaptor _affineTransformAdaptor;
 
     @JsonProperty("Rule Registration Error")
     private double _registrationError;
@@ -27,8 +29,8 @@ class ChannelRegistrationRuleToJSONAdaptor {
         this._ruleDescription = ruleDescription;
     }
 
-    private void _setAffineTransform(AffineTransform affineTransform) {
-        this._affineTransform = affineTransform.get();
+    private void _setAffineTransform(Affine2D affine2d) {
+        this._affineTransformAdaptor = new Affine2DToJSONAdaptor(affine2d);
     }
 
     private void _setRegistrationError(double registrationError) {
