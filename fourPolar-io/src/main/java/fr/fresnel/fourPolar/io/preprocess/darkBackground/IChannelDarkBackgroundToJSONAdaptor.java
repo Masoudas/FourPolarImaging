4a21778e454c59ec2c53a290e0e715c8f7867bd1
Masoudas <Masoudas@hotmail.com>
2020-06-15 12:43:17 +0200

    Implement dark background adaptor

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/darkBackground/IChannelDarkBackgroundToJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/darkBackground/IChannelDarkBackgroundToJSONAdaptor.java
@@ -1,5 +1,26 @@
 package fr.fresnel.fourPolar.io.preprocess.darkBackground;
 
+import java.util.TreeMap;
+
+import com.fasterxml.jackson.annotation.JsonProperty;
+
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+import fr.fresnel.fourPolar.core.preprocess.darkBackground.IChannelDarkBackground;
+
 public class IChannelDarkBackgroundToJSONAdaptor {
-    
+    @JsonProperty("Dark Background Level")
+    private final TreeMap<Polarization, Double> _darkBackgrounds;
+
+    public IChannelDarkBackgroundToJSONAdaptor(IChannelDarkBackground darkBackground) {
+        this._darkBackgrounds = new TreeMap<>();
+
+        this._setDarkBackgrounds(darkBackground);
+
+    }
+
+    private void _setDarkBackgrounds(IChannelDarkBackground darkBackground) {
+        for (Polarization polarization : Polarization.values()) {
+            this._darkBackgrounds.put(polarization, darkBackground.getBackgroundLevel(polarization));
+        }
+    }
 }
