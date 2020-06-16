540879c4795f68dabe792b95b5304f3dacb2f07a
Masoudas <Masoudas@hotmail.com>
2020-06-16 10:27:05 +0200

    Add method fields

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/RegistrationSetProcessResultJSONSerializer.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/RegistrationSetProcessResultJSONSerializer.java
@@ -9,9 +9,9 @@ import com.fasterxml.jackson.databind.ser.std.StdSerializer;
 
 public class RegistrationSetProcessResultJSONSerializer
         extends StdSerializer<RegistrationSetProcessResultToJSONAdaptor> {
-    /**
-     *
-     */
+    private static final String _REGISTRATION_METHOD_FIELD_NAME = "Registration Method";
+    private static final String _BackgroundEstimation_METHOD_FIELD_NAME = "Background Estimation Method";
+
     private static final long serialVersionUID = 3196084304356664812L;
 
     public RegistrationSetProcessResultJSONSerializer() {
@@ -26,7 +26,10 @@ public class RegistrationSetProcessResultJSONSerializer
     public void serialize(RegistrationSetProcessResultToJSONAdaptor value, JsonGenerator gen,
             SerializerProvider provider) throws IOException {
         gen.writeStartObject();
-        
+
+        gen.writeObjectField(_REGISTRATION_METHOD_FIELD_NAME, value.getRegistrationMethod());
+        gen.writeObjectField(_BackgroundEstimation_METHOD_FIELD_NAME, value.getBackgroundEstimationMethod());
+
         for (Iterator<String> labelItr = value.getChannelLabels(); labelItr.hasNext();) {
             String channelLabel = labelItr.next();
             gen.writeObjectField(channelLabel, value.getChannelResultAdaptor(channelLabel));
