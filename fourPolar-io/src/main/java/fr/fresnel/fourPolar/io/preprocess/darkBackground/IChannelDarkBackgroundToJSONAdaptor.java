95480183153cadd4966bb787a3d75171ee769c96
Masoudas <Masoudas@hotmail.com>
2020-06-16 10:18:19 +0200

    Introduce fields for directly writing background

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/darkBackground/IChannelDarkBackgroundToJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/darkBackground/IChannelDarkBackgroundToJSONAdaptor.java
@@ -1,30 +1,33 @@
 package fr.fresnel.fourPolar.io.preprocess.darkBackground;
 
-import java.util.TreeMap;
-
-import com.fasterxml.jackson.databind.annotation.JsonSerialize;
+import com.fasterxml.jackson.annotation.JsonProperty;
 
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 import fr.fresnel.fourPolar.core.preprocess.darkBackground.IChannelDarkBackground;
 
-@JsonSerialize(using = IChannelDarkBackgroundJSONSerializer.class)
+
 public class IChannelDarkBackgroundToJSONAdaptor {
-    private final TreeMap<Polarization, Double> _darkBackgrounds;
+    @JsonProperty("Polarization 0")
+    private final double _darkBackground_pol0;
+    
+    @JsonProperty("Polarization 45")
+    private final double _darkBackground_pol45;
+    
+    @JsonProperty("Polarization 90")
+    private final double _darkBackground_pol90;
+    
+    @JsonProperty("Polarization 135")
+    private final double _darkBackground_pol135;
+
 
     public IChannelDarkBackgroundToJSONAdaptor(IChannelDarkBackground darkBackground) {
-        this._darkBackgrounds = new TreeMap<>();
-
-        this._setDarkBackgrounds(darkBackground);
-
+        this._darkBackground_pol0 = this._getDarkBackground(darkBackground, Polarization.pol0);
+        this._darkBackground_pol45 = this._getDarkBackground(darkBackground, Polarization.pol45);
+        this._darkBackground_pol90 = this._getDarkBackground(darkBackground, Polarization.pol90);
+        this._darkBackground_pol135 = this._getDarkBackground(darkBackground, Polarization.pol135);
     }
 
-    private void _setDarkBackgrounds(IChannelDarkBackground darkBackground) {
-        for (Polarization polarization : Polarization.values()) {
-            this._darkBackgrounds.put(polarization, darkBackground.getBackgroundLevel(polarization));
-        }
-    }
-
-    public Double getDarkBackgrounds(Polarization polarization) {
-        return _darkBackgrounds.get(polarization);
+    private double _getDarkBackground(IChannelDarkBackground darkBackground, Polarization polarization) {
+        return darkBackground.getBackgroundLevel(polarization);
     }
 }
