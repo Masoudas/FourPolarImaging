c7c8c089e5badcf8d9f1152bb5e8ac8eb21591de
Masoudas <Masoudas@hotmail.com>
2020-06-16 12:40:16 +0200

    Comply to new result interface

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/registration/descriptorBased/DescriptorBasedChannelRegistrationResult.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/registration/descriptorBased/DescriptorBasedChannelRegistrationResult.java
@@ -1,41 +1,19 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.registration.descriptorBased;
 
 import java.util.HashMap;
-import java.util.Optional;
 
 import fr.fresnel.fourPolar.core.preprocess.registration.IChannelRegistrationResult;
 import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
 import fr.fresnel.fourPolar.core.util.transform.Affine2D;
 
 class DescriptorBasedChannelRegistrationResult implements IChannelRegistrationResult {
-    public final static String _NOT_ENOUGH_FP_DESCRIPTION = "Not enough feature points found.";
-    public final static String _NO_TRANSFORMATION_DESCRIPTION = "No (invertible) transformation found between images.";
-
-    private HashMap<RegistrationRule, Boolean> _isSuccessfulRegistration;
     private HashMap<RegistrationRule, Affine2D> _affineTransform;
-    private HashMap<RegistrationRule, String> _description;
     private HashMap<RegistrationRule, Double> _error;
     private int _channelNum = 0;
 
     public DescriptorBasedChannelRegistrationResult() {
-        this._isSuccessfulRegistration = _initializeIsSuccessfulRegistrationToFalse();
         this._affineTransform = new HashMap<>();
         this._error = new HashMap<>();
-        this._description = new HashMap<>();
-    }
-
-    /**
-     * All inital results are set to false, so that if the registration algorithm
-     * fails with unknown exception, is successful will be false.
-     * 
-     * @return
-     */
-    private HashMap<RegistrationRule, Boolean> _initializeIsSuccessfulRegistrationToFalse() {
-        HashMap<RegistrationRule, Boolean> isSuccessful = new HashMap<>();
-        for (RegistrationRule rule : RegistrationRule.values()) {
-            isSuccessful.put(rule, false);
-        }
-        return isSuccessful;
     }
 
     public void setAffineTransform(RegistrationRule order, Affine2D affineTransform) {
@@ -46,21 +24,13 @@ class DescriptorBasedChannelRegistrationResult implements IChannelRegistrationRe
         this._error.put(order, error);
     }
 
-    public void setDescription(RegistrationRule order, String description) {
-        this._description.put(order, description);
-    }
-
-    public void setIsSuccessfulRegistration(RegistrationRule order, boolean isSuccessful) {
-        this._isSuccessfulRegistration.put(order, isSuccessful);
-    }
-
     public void setChannel(int channelNum) {
         this._channelNum = channelNum;
     }
 
     @Override
-    public Optional<Affine2D> getAffineTransform(RegistrationRule order) {
-        return Optional.ofNullable(_affineTransform.get(order));
+    public Affine2D getAffineTransform(RegistrationRule order) {
+        return _affineTransform.get(order);
     }
 
     @Override
@@ -68,15 +38,6 @@ class DescriptorBasedChannelRegistrationResult implements IChannelRegistrationRe
         return this._error.get(order);
     }
 
-    @Override
-    public Optional<String> getFailureDescription(RegistrationRule order) {
-        return Optional.ofNullable(this._description.get(order));
-    }
-
-    @Override
-    public boolean registrationSuccessful(RegistrationRule order) {
-        return this._isSuccessfulRegistration.get(order);
-    }
 
     @Override
     public int channel() {
