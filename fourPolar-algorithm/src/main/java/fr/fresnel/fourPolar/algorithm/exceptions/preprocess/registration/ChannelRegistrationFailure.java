c3402b81af7d1a6b2d68d0973a7680e5ca6e5ecd
Masoudas <Masoudas@hotmail.com>
2020-06-16 12:32:01 +0200

    Add for channel rather than rule

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/exceptions/preprocess/registration/ChannelRegistrationFailure.java
@@ -0,0 +1,58 @@
+package fr.fresnel.fourPolar.algorithm.exceptions.preprocess.registration;
+
+import java.util.HashMap;
+import java.util.Iterator;
+import java.util.Objects;
+
+import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
+
+/**
+ * Thrown in case the registration algorithm fails to register at least one
+ * {@link RegistrationRule} for a channel.
+ * <p>
+ * NOTE: By registration failure we imply failing to get an affine transform.
+ * Note however if registration is for example weak (has large error), we still
+ * say it has been successful.
+ */
+public class ChannelRegistrationFailure extends Exception {
+    public static class Builder {
+        private final HashMap<RegistrationRule, String> _buildFailures = new HashMap<>();
+
+        /**
+         * Register a failure reason.
+         */
+        public Builder addRuleFailure(RegistrationRule rule, String failureCause) {
+            Objects.requireNonNull(rule, "rule cannot be null");
+            Objects.requireNonNull(failureCause, "failureCause cannot be null");
+
+            this._buildFailures.put(rule, failureCause);
+
+            return this;
+        }
+
+        public ChannelRegistrationFailure buildException() {
+            return new ChannelRegistrationFailure(this);
+        }
+    }
+
+    private final HashMap<RegistrationRule, String> _failures;
+
+    private static final long serialVersionUID = 7499711200932393875L;
+
+    /**
+     * Create an exception, by registration a message indicating the failure reason.
+     * 
+     * @param failureReason is the description of why registration failed.
+     */
+    private ChannelRegistrationFailure(Builder builder) {
+        this._failures = builder._buildFailures;
+    }
+
+    public Iterator<RegistrationRule> getFailedRules() {
+        return _failures.keySet().iterator();
+    }
+
+    public String getFailureReason(RegistrationRule rule) {
+        return _failures.get(rule);
+    }
+}
