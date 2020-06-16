38e8d71fd0aa9e4a25ade201ada10f3b206d8bfc
Masoudas <Masoudas@hotmail.com>
2020-06-16 17:23:10 +0200

    Remove unused imorts

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/exceptions/preprocess/registration/ChannelRegistrationFailure.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/exceptions/preprocess/registration/ChannelRegistrationFailure.java
@@ -1,8 +1,6 @@
 package fr.fresnel.fourPolar.algorithm.exceptions.preprocess.registration;
 
 import java.util.HashMap;
-import java.util.Iterator;
-import java.util.Map;
 import java.util.Objects;
 
 import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
