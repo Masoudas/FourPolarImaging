77221513bfd2ad644655c59b65cc5f5f27c5b296
Masoudas <Masoudas@hotmail.com>
2020-06-16 10:27:37 +0200

    Add method names

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/RegistrationSetProcessResultToJSONAdaptor.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/preprocess/RegistrationSetProcessResultToJSONAdaptor.java
@@ -18,19 +18,41 @@ import fr.fresnel.fourPolar.core.preprocess.registration.IChannelRegistrationRes
 @JsonSerialize(using = RegistrationSetProcessResultJSONSerializer.class)
 class RegistrationSetProcessResultToJSONAdaptor {
     private final TreeMap<String, ChannelRegistrationSetProcessResultToJSONAdaptor> _channelAdaptors;
+    private final String _registrationMethod;
+    private final String _backgroundEstimationMethod;
 
     public RegistrationSetProcessResultToJSONAdaptor(IFourPolarImagingSetup imagingSetup,
             RegistrationSetProcessResult result) {
         this._channelAdaptors = this._createChannelsResultJSONAdaptor(result, imagingSetup.getNumChannel());
+        
+        this._backgroundEstimationMethod = _getBackgroundEstimationMethod(result);
+        this._registrationMethod = this._getRegistrationMethod(result);
     }
 
-    public TreeMap<String, ChannelRegistrationSetProcessResultToJSONAdaptor> _createChannelsResultJSONAdaptor(
+    /**
+     * Returns the background estimation method. Given that the method is the same
+     * across all channels, we return the instance for channel 1.
+     */
+    private String _getBackgroundEstimationMethod(RegistrationSetProcessResult result) {
+        return result.getDarkBackground(1).estimationMethod();
+    }
+
+    /**
+     * Returns the registration method. Given that this method is the same across all channels,
+     * we use only the method name for channel 1.
+     */
+    private String _getRegistrationMethod(RegistrationSetProcessResult result) {
+        return result.getRegistrationResult(1).registrationMethod();
+    }
+
+    private TreeMap<String, ChannelRegistrationSetProcessResultToJSONAdaptor> _createChannelsResultJSONAdaptor(
             RegistrationSetProcessResult result, int numChannels) {
         TreeMap<String, ChannelRegistrationSetProcessResultToJSONAdaptor> channelAdaptors = new TreeMap<>();
         for (int channel = 1; channel <= numChannels; channel++) {
+            String channelLabel = this._channelAsString(channel);
+
             IChannelDarkBackground channelDarkBackground = result.getDarkBackground(channel);
             IChannelRegistrationResult registrationResult = result.getRegistrationResult(channel);
-            String channelLabel = this._channelAsString(channel);
 
             channelAdaptors.put(channelLabel,
                     this._createChannelJSONAdaptor(channelDarkBackground, registrationResult));
@@ -52,8 +74,16 @@ class RegistrationSetProcessResultToJSONAdaptor {
         return this._channelAdaptors.keySet().iterator();
     }
 
-    public ChannelRegistrationSetProcessResultToJSONAdaptor getChannelResultAdaptor(String channelLabel){
+    public ChannelRegistrationSetProcessResultToJSONAdaptor getChannelResultAdaptor(String channelLabel) {
         return this._channelAdaptors.get(channelLabel);
+    }
+
+    public String getRegistrationMethod() {
+        return this._registrationMethod;
+    }
+
+    public String getBackgroundEstimationMethod(){
+        return this._backgroundEstimationMethod;
     }
 
 }
