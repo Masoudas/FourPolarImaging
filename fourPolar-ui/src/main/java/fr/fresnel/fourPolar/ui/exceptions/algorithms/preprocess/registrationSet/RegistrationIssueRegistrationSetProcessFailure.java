99852caa0b494e848a4bc2e2ecd7826545122149
Masoudas <Masoudas@hotmail.com>
2020-06-16 15:34:48 +0200

    Get instances of ChannelRegistrationFailure

--- fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/exceptions/algorithms/preprocess/registrationSet/RegistrationIssueRegistrationSetProcessFailure.java
+++ fourPolar-ui/src/main/java/fr/fresnel/fourPolar/ui/exceptions/algorithms/preprocess/registrationSet/RegistrationIssueRegistrationSetProcessFailure.java
@@ -1,10 +1,9 @@
 package fr.fresnel.fourPolar.ui.exceptions.algorithms.preprocess.registrationSet;
 
-import java.util.ArrayList;
-import java.util.EnumMap;
-import java.util.List;
+import java.util.HashMap;
 import java.util.Map;
 
+import fr.fresnel.fourPolar.algorithm.exceptions.preprocess.registration.ChannelRegistrationFailure;
 import fr.fresnel.fourPolar.core.imageSet.acquisition.registration.RegistrationImageSet;
 import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
 
@@ -14,28 +13,19 @@ import fr.fresnel.fourPolar.core.preprocess.registration.RegistrationRule;
  * channel (if any).
  */
 public class RegistrationIssueRegistrationSetProcessFailure extends RegistrationSetProcessFailure {
-    /**
-     *
-     */
     private static final long serialVersionUID = -3746773829399830474L;
-    private final Map<RegistrationRule, ArrayList<Integer>> _failedRegistrations;
+    private final Map<Integer, RegistrationRule[]> _failedRegistrations;
 
     public RegistrationIssueRegistrationSetProcessFailure() {
         _failedRegistrations = _initializeFailedRegistrationMap();
     }
 
-    private Map<RegistrationRule, ArrayList<Integer>> _initializeFailedRegistrationMap() {
-        Map<RegistrationRule, ArrayList<Integer>> failedRegistrations = new EnumMap<>(RegistrationRule.class);
-
-        for (RegistrationRule rule : RegistrationRule.values()) {
-            failedRegistrations.put(rule, new ArrayList<Integer>());
-        }
-
-        return failedRegistrations;
+    private Map<Integer, RegistrationRule[]> _initializeFailedRegistrationMap() {
+        return new HashMap<>();
     }
 
-    public void setRuleFailure(RegistrationRule rule, int channel) {
-        this._failedRegistrations.get(rule).add(channel);
+    public void setRuleFailure(ChannelRegistrationFailure failureException, int channel) {
+        this._failedRegistrations.put(channel, failureException.getFailedRules());
     }
 
     @Override
@@ -43,22 +33,21 @@ public class RegistrationIssueRegistrationSetProcessFailure extends Registration
         return "Registration failed for the following channels: " + this._failedRegistrations.toString();
     }
 
-    public List<Integer> getRuleFailure(RegistrationRule rule) {
-        return this._failedRegistrations.get(rule);
+    public int[] getFailedChannels() {
+        return this._failedRegistrations.keySet().stream().mapToInt((t)->t).toArray();
+    }
+
+    public RegistrationRule[] getChannelFailedRules(int channel) {
+        return this._failedRegistrations.get(channel);
     }
 
     /**
-     * Returns true if at least one rule has failed among all channels.
+     * Returns true if at least one channel has failed.
      * 
      * @return
      */
     public boolean hasFailure() {
-        boolean hasFailure = false;
-        for (int ruleIndex = 0; ruleIndex < RegistrationRule.values().length && !hasFailure; ruleIndex++) {
-            hasFailure = this.getRuleFailure(RegistrationRule.values()[ruleIndex]).size() != 0;
-        }
-
-        return hasFailure;
+        return !this._failedRegistrations.isEmpty();
     }
 
 }
