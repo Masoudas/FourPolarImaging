2aec217611f3550fb80fc5e31ef2af03f94508b5
Masoudas <Masoudas@hotmail.com>
2020-06-17 11:15:16 +0200

    Add dimension method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/IBoxShape.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/IBoxShape.java
@@ -5,6 +5,22 @@ package fr.fresnel.fourPolar.core.util.shape;
  * point.
  */
 public interface IBoxShape extends IShape {
+    /**
+     * Return box length on the given dimension.
+     * 
+     * @param boxShape is the desired box.
+     * @param dim is the dimension from zero.
+     * 
+     * @throws IllegalArgumentException if dimension does not exist.
+     * @return
+     */
+    public static long len(IBoxShape boxShape, int dim) {
+        if (dim < 0 || dim > boxShape.min().length){
+            throw new IllegalArgumentException("The given dimension does not exist");
+        }
+        return boxShape.max()[dim] - boxShape.min()[dim];
+    }
+
     public long[] min();
 
     public long[] max();
