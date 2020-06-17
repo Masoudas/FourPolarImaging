bdba66088ae779a696265b0d8b2486cd9ec938ab
Masoudas <Masoudas@hotmail.com>
2020-06-17 15:12:47 +0200

    Add reassign with resize

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/AxisReassigner.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/AxisReassigner.java
@@ -15,7 +15,7 @@ public class AxisReassigner {
 
     /**
      * Reassigns the axis of an image. Note that if an axis does not exist, it's
-     * appended to the new image with dimension one. 
+     * appended to the new image with dimension one.
      * 
      * @throws IllegalArgumentException if the old or new axis don't start with XY.
      */
@@ -23,36 +23,23 @@ public class AxisReassigner {
         return ReassingerToXYCZT.reassign(image, t);
     }
 
-
-    // /**
-    //  * Breaks down the channels of the given image and returns an array of images,
-    //  * where each element is a channel image. Note that axis order of the new image
-    //  * would be that of
-    //  * 
-    //  */
-    // public static <T extends PixelType> Image<T>[] separateChannels(Image<T> image, T t) {
-    //     if (image.getMetadata().numChannels() == 1) {
-    //         return new Image[] { image };
-    //     }
-
-    //     IMetadata channelImageMetadata = _createChannelImageMetadata(image.getMetadata());
-    //     Image<T>[] channelImages = new Image[image.getMetadata().numChannels()];
-    //     for (int c = 0; c < image.getMetadata().numChannels(); c++) {
-    //         long[] top = channelImageMetadata.getDim();
-    //         long[] bottom = new long[top.length];
-            
-    //         IPixelCursor<T> cursor = image.getCursor(bottom, top);
-    //     }
-
-    // }
-
-    // private static IMetadata _createChannelImageMetadata(IMetadata metadata) {
-    //     int c_axis = metadata.axisOrder().c_axis;
-    //     long[] channelImageDim = metadata.getDim();
-    //     channelImageDim[c_axis] = 1;
-
-    //     return new Metadata.MetadataBuilder(channelImageDim).axisOrder(metadata.axisOrder())
-    //             .bitPerPixel(metadata.bitPerPixel()).build();
-    // }
+    /**
+     * Reassigns the axis of an image using its cursor, and resize the new image as
+     * specified. Note that if an axis does not exist, it's appended to the new
+     * image. Moreover, all extra dimensions will have zero intensity.
+     * 
+     * @param image     is the image interface to be reassigned.
+     * @param t         is the image pixel type.
+     * @param newImgDim is the new image dimension (should be greater than equal
+     *                  original image, and five dimensional)
+     * 
+     * @throws IllegalArgumentException if the old or new axis don't start with XY,
+     *                                  or if new dimension is not 5 dimensional, or
+     *                                  new dim is not greater than equal old
+     *                                  dimension.
+     */
+    public static <T extends PixelType> Image<T> reassignAndResize(Image<T> image, T t, long[] newImgDim) {
+        return ReassingerToXYCZT.reassignAndResize(image, t, newImgDim);
+    }
 
 }
--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/ReassingerToXYCZT.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/util/image/axis/ReassingerToXYCZT.java
@@ -1,6 +1,7 @@
 package fr.fresnel.fourPolar.algorithm.util.image.axis;
 
 import java.util.Objects;
+import java.util.stream.IntStream;
 
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
@@ -23,21 +24,48 @@ class ReassingerToXYCZT {
      * @param image is the image interface to be reassigned.
      * @param t     is the image pixel type.
      * 
-     * @throws IllegalArgumentException if the old or new axis don't start with XY,
-     *                                  or if dimension of cursor and metadata don't
-     *                                  match.
+     * @throws IllegalArgumentException if the old or new axis don't start with XY.
      */
     public static <T extends PixelType> Image<T> reassign(Image<T> image, T t) {
         Objects.requireNonNull(image, "image can't be null");
         Objects.requireNonNull(t, "t can't be null");
 
-        if (!image.getMetadata().axisOrder().name().substring(0, 2).equals("XY")) {
-            throw new IllegalArgumentException("Axis does not start with XY");
-        }
+        _checkAxisOrderStartsWithXY(image);
+        Image<T> reassignedImage = _createXYCZTImage(image, t);
+        _copyImagePixelsToXYCZTImage(image, reassignedImage);
 
-        IMetadata metadata_xyczt = _createMetadata(image.getMetadata());
-        Image<T> reassignedImage = image.getFactory().create(metadata_xyczt, t);
+        return reassignedImage;
+    }
 
+    /**
+     * Reassigns the axis of an image using its cursor. Note that if an axis does
+     * not exist, it's appended to the new image. Moreover, all extra dimensions
+     * will have zero intensity.
+     * 
+     * @param image     is the image interface to be reassigned.
+     * @param t         is the image pixel type.
+     * @param newImgDim is the new image dimension (should be greater than equal
+     *                  original image, and five dimensional)
+     * 
+     * @throws IllegalArgumentException if the old or new axis don't start with XY,
+     *                                  or if new dimension is not 5 dimensional, or
+     *                                  new dim is not greater than equal old
+     *                                  dimension.
+     */
+    public static <T extends PixelType> Image<T> reassignAndResize(Image<T> image, T t, long[] newImgDim) {
+        Objects.requireNonNull(image, "image can't be null");
+        Objects.requireNonNull(t, "t can't be null");
+        _checkAxisOrderStartsWithXY(image);
+        _checkNewDimIs5D(newImgDim);
+        _checkNewDimIsGreaterThanEqualOldDim(image.getMetadata().getDim(), newImgDim);
+
+        Image<T> reassignedImage = _createXYCZTImage(image, t);
+        _copyImagePixelsToXYCZTImage(image, reassignedImage);
+
+        return reassignedImage;
+    }
+
+    private static <T extends PixelType> void _copyImagePixelsToXYCZTImage(Image<T> image, Image<T> reassignedImage) {
         AxisOrder oldAxisOrder = image.getMetadata().axisOrder();
         IPixelRandomAccess<T> ra = reassignedImage.getRandomAccess();
         for (IPixelCursor<T> cursor = image.getCursor(); cursor.hasNext();) {
@@ -47,8 +75,42 @@ class ReassingerToXYCZT {
             ra.setPosition(position_xyczt);
             ra.setPixel(pixel);
         }
+    }
 
-        return reassignedImage;
+    private static <T extends PixelType> Image<T> _createXYCZTImage(Image<T> image, T t) {
+        IMetadata metadata_xyczt = _createXYCZTMetadata(image.getMetadata());
+        return image.getFactory().create(metadata_xyczt, t);
+    }
+
+    private static <T extends PixelType> Image<T> _createXYCZTImage(Image<T> image, T t, long[] newDim) {
+        IMetadata oldImageMetadata = image.getMetadata();
+        IMetadata metadata_xyczt_resized = new Metadata.MetadataBuilder(newDim).axisOrder(AxisOrder.XYCZT)
+                .bitPerPixel(oldImageMetadata.bitPerPixel()).build();
+        return image.getFactory().create(metadata_xyczt_resized, t);
+    }
+
+    private static <T extends PixelType> void _checkAxisOrderStartsWithXY(Image<T> image) {
+        if (!image.getMetadata().axisOrder().name().substring(0, 2).equals("XY")) {
+            throw new IllegalArgumentException("Axis does not start with XY");
+        }
+    }
+
+    private static void _checkNewDimIs5D(long[] newDim) {
+        if (newDim.length != 5) {
+            throw new IllegalArgumentException("New image dimension for resizing must 5 dimensional.");
+        }
+    }
+
+    private static void _checkNewDimIsGreaterThanEqualOldDim(long[] oldDim, long[] newDim) {
+        if (oldDim.length > newDim.length) {
+            throw new IllegalArgumentException(
+                    "New image dimension for resizing must be greater than equal old dimension.");
+        }
+
+        if (IntStream.range(0, oldDim.length).anyMatch((i) -> oldDim[i] < newDim[i])) {
+            throw new IllegalArgumentException(
+                    "New image dimension for resizing must be greater than equal old dimension.");
+        }
     }
 
     /**
@@ -58,7 +120,7 @@ class ReassingerToXYCZT {
      * @param metadata
      * @return
      */
-    private static IMetadata _createMetadata(IMetadata metadata) {
+    private static IMetadata _createXYCZTMetadata(IMetadata metadata) {
         int z_axis = metadata.axisOrder().z_axis;
         int c_axis = metadata.axisOrder().c_axis;
         int t_axis = metadata.axisOrder().t_axis;
