5ef34cb5d61e1816e2e76cf5de7b6a2a06e5f9af
Masoudas <Masoudas@hotmail.com>
2020-06-17 14:04:34 +0200

    Add checker method for metadata and box

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/FoVCalculatorUtil.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/FoVCalculatorUtil.java
@@ -1,8 +1,11 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.fov;
 
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
+import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
 import fr.fresnel.fourPolar.core.util.shape.IPointShape;
 
-public class FoVCalculatorUtil {
+class FoVCalculatorUtil {
     private FoVCalculatorUtil() {
         throw new AssertionError();
     }
@@ -17,5 +20,34 @@ public class FoVCalculatorUtil {
         if (intersectionPoint.point().length != 2) {
             throw new IllegalArgumentException("Intersection point for calculating the FoV must be two dimensional");
         }
+    }
+
+    /**
+     * Checks wether this 2D box is inside the image.
+     */
+    public static void checkFoVBoxIsInImageBoundary(IMetadata imageMetadata, IBoxShape fovBox, Polarization pol) {
+        _checkFoVBoxIs2D(fovBox, pol);
+        _checkFoVBoxMinIsPositive(fovBox, pol);
+
+        long[] fovMax = fovBox.max(); 
+        long[] imgDim = imageMetadata.getDim();
+        
+        if (fovMax[0] > imgDim[0] || fovMax[1] > imgDim[1]) {
+            throw new IllegalArgumentException("Field of View Box is outside image boundary.");
+        }
+    }
+
+    private static void _checkFoVBoxIs2D(IBoxShape fovBox, Polarization pol) {
+        if (fovBox.shapeDim() != 2) {
+            throw new IllegalArgumentException("fov region for " + pol + " must be a 2D box.");
+        }
+    }
+
+    private static void _checkFoVBoxMinIsPositive(IBoxShape fovBox, Polarization pol) {
+        long[] fovMin = fovBox.min();
+        if (fovMin[0] < 0 || fovMin[1] < 0) {
+            throw new IllegalArgumentException("FoV box should be non-negative");
+        }
+
     }
 }
