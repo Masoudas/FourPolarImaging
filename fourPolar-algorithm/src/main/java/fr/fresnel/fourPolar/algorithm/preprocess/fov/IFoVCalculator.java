107f00f6cf195d4cdf1f4607696fdb3b7db62f01
Masoudas <Masoudas@hotmail.com>
2020-06-18 17:22:09 +0200

    Add min and max points

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/IFoVCalculator.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/fov/IFoVCalculator.java
@@ -1,26 +1,48 @@
 package fr.fresnel.fourPolar.algorithm.preprocess.fov;
 
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.fov.IFieldOfView;
-import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
+import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
  * An interface for calculating the {@link IFieldOfView} of a particular
  * {@link Camera},
  */
 public interface IFoVCalculator {
+    /**
+     * Set the x and y coordinate of minimum point (starting point) of this
+     * polarization.
+     * 
+     * @param x   is the x coordiante.
+     * @param y   is the y coordinate.
+     * @param pol is the polarization.
+     * 
+     * @throws IllegalArgumentException if the x and y points are negative or are
+     *                                  outside the boundary of this polarization
+     *                                  dimension.
+     */
+    public void setMin(long x, long y, Polarization pol);
 
     /**
-     * Calculate fov using four {@link IBoxShape} defined on each polarization. For
-     * this end, the minimum box dimension is calculated among boxes (so that all
-     * {@link IPolarizationImage} have the same dimension, and then fov for each
-     * polarization is returned from box minimum plus the minimum length of all
-     * boxes.
+     * Set the x and y coordinate of maximum point (end point) of this polarization.
      * 
-     * @param pol0_fov   is the field of view of pol0.
-     * @param pol45_fov  is the field of view of pol45.
-     * @param pol90_fov  is the field of view of pol90.
-     * @param pol135_fov is the field of view of pol135.
-     * @return the field of view.
+     * @param x   is the x coordiante.
+     * @param y   is the y coordinate.
+     * @param pol is the polarization.
+     * 
+     * @throws IllegalArgumentException if the x and y points are negative or are
+     *                                  outside the boundary of this polarization
+     *                                  dimension.
      */
-    public IFieldOfView calculate(IBoxShape pol0_fov, IBoxShape pol45_fov, IBoxShape pol90_fov, IBoxShape pol135_fov);
+    public void setMax(long x, long y, Polarization pol);
+
+    /**
+     * Calculate fov using the minimum and maximum points for each polarization.
+     * 
+     * @return the field of view.
+     * 
+     * @throws IllegalStateException if the top and bottom point is not provided for
+     *                               all polarizations.
+     */
+    public IFieldOfView calculate();
+
 }
