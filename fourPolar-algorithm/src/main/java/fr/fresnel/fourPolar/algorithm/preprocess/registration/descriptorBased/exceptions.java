5331edcc44d5dc4b15e9104364b910ffec8471e0
Masoudas <Masoudas@hotmail.com>
2020-06-19 12:46:40 +0200

    Reminder of exceptions

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/preprocess/registration/descriptorBased/exceptions.java
@@ -0,0 +1,5 @@
+package fr.fresnel.fourPolar.algorithm.preprocess.registration.descriptorBased;
+
+public class exceptions {
+    mpicbg.pointdescriptor.exception.NoSuitablePointsException: At least 1 nearest neighbor is required for matching descriptors.
+}
