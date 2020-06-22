be48f06344467da35df3544545fcc675306dd54e
sbrasselet <sophie.brasselet@gmail.com>
2020-06-22 15:58:52 +0200

    Implement full test for angle and radian

--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageReaderTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageReaderTest.java
@@ -10,22 +10,33 @@ import java.util.Iterator;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 
+import fr.fresnel.fourPolar.core.exceptions.image.generic.imgLib2Model.ConverterToImgLib2NotFound;
 import fr.fresnel.fourPolar.core.exceptions.image.orientation.CannotFormOrientationImage;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImageToImgLib2Converter;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
+import fr.fresnel.fourPolar.core.image.orientation.IOrientationImageRandomAccess;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
+import fr.fresnel.fourPolar.core.physics.dipole.IOrientationVector;
 import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
+import fr.fresnel.fourPolar.core.physics.dipole.OrientationVector;
 import fr.fresnel.fourPolar.io.image.generic.tiff.scifio.SCIFIOFloat32TiffWriter;
 import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageFileSet;
+import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageInDegreeFileSet;
+import io.scif.img.ImgIOException;
+import io.scif.img.ImgSaver;
+import net.imglib2.exception.IncompatibleTypeException;
 
 public class TiffOrientationImageReaderTest {
     final private static File _root = new File(TiffOrientationImageReaderTest.class.getResource("").getPath(),
@@ -39,15 +50,72 @@ public class TiffOrientationImageReaderTest {
     /**
      * The premise of the test is write and then read an orientation image.
      * 
+     * @throws ConverterToImgLib2NotFound
+     * @throws IncompatibleTypeException
+     * @throws ImgIOException
+     * 
      */
     @Test
-    public void read_PolarizationImageSetFromDisk_ReadImagesHaveSameDimensionAsDisk()
-            throws IOException, CannotFormOrientationImage {
+    public void read_PolarizationImageSetFromDisk_ReadImagesHaveSameDimensionAsDisk() throws IOException,
+            CannotFormOrientationImage, ImgIOException, IncompatibleTypeException, ConverterToImgLib2NotFound {
         // Create an alias for a captured set.
         String setName = "TestCapturedSet";
         int channel = 1;
         ReaderDummyCapturedImageFileSet fileSet = new ReaderDummyCapturedImageFileSet(setName);
         TiffOrientationImageFileSet fSet = new TiffOrientationImageFileSet(_root, fileSet, channel);
+
+        // Create orientation image to be written.
+        long[] dim = { 2, 1, 1, 1, 1 };
+        IMetadata metadata = new Metadata.MetadataBuilder(dim).axisOrder(AxisOrder.XYCZT)
+                .bitPerPixel(PixelTypes.FLOAT_32).build();
+
+        Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+        Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+        Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+
+        long[] pos0 = new long[] { 0, 0, 0, 0, 0 };
+        long[] pos1 = new long[] { 1, 0, 0, 0, 0 };
+
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float) OrientationVector.MAX_Rho);
+
+        _setPixel(delta, pos0, (float) Math.PI / 4);
+        _setPixel(delta, pos1, (float) (3 * Math.PI / 4));
+
+        _setPixel(eta, pos0, 0);
+        _setPixel(eta, pos1, (float) OrientationVector.MAX_Eta);
+
+        // Write the orientation images manually.
+        ImgSaver saver = new ImgSaver();
+        saver.saveImg(fSet.getFile(OrientationAngle.rho).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(rho, Float32.zero()));
+        saver.saveImg(fSet.getFile(OrientationAngle.delta).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(delta, Float32.zero()));
+        saver.saveImg(fSet.getFile(OrientationAngle.eta).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(eta, Float32.zero()));
+
+        ImageFactory factory = new ImgLib2ImageFactory();
+        IOrientationImageReader reader = new TiffOrientationImageReader(factory, channel);
+        IOrientationImage imageSet = reader.read(_root, fileSet, channel);
+
+        assertTrue(Arrays.equals(imageSet.getAngleImage(OrientationAngle.rho).getImage().getMetadata().getDim(), dim)
+                && Arrays.equals(imageSet.getAngleImage(OrientationAngle.delta).getImage().getMetadata().getDim(), dim)
+                && Arrays.equals(imageSet.getAngleImage(OrientationAngle.eta).getImage().getMetadata().getDim(), dim));
+
+        assertTrue(_isOrientationVectorEqualTo(imageSet, pos0, new OrientationVector(0, Math.PI / 4, 0)));
+        assertTrue(_isOrientationVectorEqualTo(imageSet, pos1,
+                new OrientationVector(OrientationVector.MAX_Rho, 3 * Math.PI / 4, OrientationVector.MAX_Eta)));
+
+    }
+
+    @Test
+    public void readFromDegrees_ManuallyMadeAngleImagesInDegrees_ReadImagesHaveSameDimensionAsDisk()
+            throws ImgIOException, IncompatibleTypeException, ConverterToImgLib2NotFound, IOException,
+            CannotFormOrientationImage {
+        String setName = "TestCapturedSet";
+        int channel = 1;
+        ReaderDummyCapturedImageFileSet fileSet = new ReaderDummyCapturedImageFileSet(setName);
+        TiffOrientationImageInDegreeFileSet fSet = new TiffOrientationImageInDegreeFileSet(_root, fileSet, channel);
 
         // Create orientation image to be written.
         long[] dim = { 2, 2, 1, 1, 1 };
@@ -58,19 +126,57 @@ public class TiffOrientationImageReaderTest {
         Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
         Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
 
-        // Write the orientation images manually.
-        SCIFIOFloat32TiffWriter writer = new SCIFIOFloat32TiffWriter();
-        writer.write(fSet.getFile(OrientationAngle.rho), rho);
-        writer.write(fSet.getFile(OrientationAngle.delta), delta);
-        writer.write(fSet.getFile(OrientationAngle.eta), eta);
+        long[] pos0 = new long[] { 0, 0, 0, 0, 0 };
+        long[] pos1 = new long[] { 1, 0, 0, 0, 0 };
 
-        ImageFactory factory = new ImgLib2ImageFactory();
-        IOrientationImageReader reader = new TiffOrientationImageReader(factory, channel);
-        IOrientationImage imageSet = reader.read(_root, fileSet, channel);
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, 180);
 
-        assertTrue(Arrays.equals(imageSet.getAngleImage(OrientationAngle.rho).getImage().getMetadata().getDim(), dim)
-                && Arrays.equals(imageSet.getAngleImage(OrientationAngle.delta).getImage().getMetadata().getDim(), dim)
-                && Arrays.equals(imageSet.getAngleImage(OrientationAngle.eta).getImage().getMetadata().getDim(), dim));
+        _setPixel(delta, pos0, 45);
+        _setPixel(delta, pos1, 135);
+
+        _setPixel(eta, pos0, 0);
+        _setPixel(eta, pos1, 90);
+
+        ImgSaver saver = new ImgSaver();
+        saver.saveImg(fSet.getFile(OrientationAngle.rho).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(rho, Float32.zero()));
+        saver.saveImg(fSet.getFile(OrientationAngle.delta).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(delta, Float32.zero()));
+        saver.saveImg(fSet.getFile(OrientationAngle.eta).getAbsolutePath(),
+                ImageToImgLib2Converter.getImg(eta, Float32.zero()));
+
+        TiffOrientationImageReader reader = new TiffOrientationImageReader(new ImgLib2ImageFactory(), 1);
+
+        IOrientationImage orientationImage = reader.readFromDegrees(_root, fileSet, 1);
+
+        assertTrue(_isOrientationVectorEqualTo(orientationImage, pos0, new OrientationVector(0, Math.PI / 4, 0)));
+        assertTrue(_isOrientationVectorEqualTo(orientationImage, pos1,
+                new OrientationVector(OrientationVector.MAX_Rho, 3 * Math.PI / 4, OrientationVector.MAX_Eta)));
+
+    }
+
+    private void _setPixel(Image<Float32> image, long[] position, float value) {
+        IPixelRandomAccess<Float32> ra = image.getRandomAccess();
+
+        ra.setPosition(position);
+        IPixel<Float32> pixel = ra.getPixel();
+
+        pixel.value().set(value);
+        ra.setPixel(pixel);
+    }
+
+    private boolean _isOrientationVectorEqualTo(IOrientationImage image, long[] position, IOrientationVector vector) {
+        IOrientationImageRandomAccess ra = image.getRandomAccess();
+        ra.setPosition(position);
+
+        IOrientationVector diskVector = ra.getOrientation();
+
+        // Check the value is close enough
+        return Math.abs(diskVector.getAngle(OrientationAngle.rho) - vector.getAngle(OrientationAngle.rho)) < 1e-6
+                && Math.abs(
+                        diskVector.getAngle(OrientationAngle.delta) - vector.getAngle(OrientationAngle.delta)) < 1e-6
+                && Math.abs(diskVector.getAngle(OrientationAngle.eta) - vector.getAngle(OrientationAngle.eta)) < 1e-6;
     }
 
 }
--- fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageWriterTest.java
+++ fourPolar-io/src/test/java/fr/fresnel/fourPolar/io/image/orientation/TiffOrientationImageWriterTest.java
@@ -12,17 +12,28 @@ import fr.fresnel.fourPolar.core.exceptions.image.orientation.CannotFormOrientat
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.Float32;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
+import fr.fresnel.fourPolar.core.image.orientation.IOrientationImageRandomAccess;
 import fr.fresnel.fourPolar.core.image.orientation.OrientationImageFactory;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
+import fr.fresnel.fourPolar.core.physics.dipole.IOrientationVector;
 import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
+import fr.fresnel.fourPolar.core.physics.dipole.OrientationVector;
 import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageFileSet;
+import fr.fresnel.fourPolar.io.image.orientation.file.TiffOrientationImageInDegreeFileSet;
+import io.scif.img.ImgOpener;
+import net.imglib2.RandomAccess;
+import net.imglib2.img.Img;
+import net.imglib2.img.array.ArrayImgFactory;
+import net.imglib2.type.numeric.real.FloatType;
 
 public class TiffOrientationImageWriterTest {
     final private static File _root = new File(TiffOrientationImageWriterTest.class.getResource("").getPath(),
@@ -37,13 +48,25 @@ public class TiffOrientationImageWriterTest {
         WriterDummyCapturedImageFileSet fileSet = new WriterDummyCapturedImageFileSet(setNameAlias);
 
         // Create orientation image to be written.
-        long[] dim = { 2, 2, 1, 1, 1 };
+        long[] dim = { 2, 1, 1, 1, 1 };
         IMetadata metadata = new Metadata.MetadataBuilder(dim).axisOrder(AxisOrder.XYCZT)
                 .bitPerPixel(PixelTypes.FLOAT_32).build();
 
         Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
         Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
         Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+
+        long[] pos0 = new long[] { 0, 0, 0, 0, 0 };
+        long[] pos1 = new long[] { 1, 0, 0, 0, 0 };
+
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float) OrientationVector.MAX_Rho);
+
+        _setPixel(delta, pos0, (float) Math.PI / 4);
+        _setPixel(delta, pos1, (float) (3 * Math.PI / 4));
+
+        _setPixel(eta, pos0, 0);
+        _setPixel(eta, pos1, (float) OrientationVector.MAX_Eta);
 
         IOrientationImage imageSet = OrientationImageFactory.create(fileSet, channel, rho, delta, eta);
 
@@ -53,6 +76,93 @@ public class TiffOrientationImageWriterTest {
         TiffOrientationImageFileSet fSet = new TiffOrientationImageFileSet(_root, fileSet, channel);
         assertTrue(fSet.getFile(OrientationAngle.rho).exists() && fSet.getFile(OrientationAngle.delta).exists()
                 && fSet.getFile(OrientationAngle.eta).exists());
+
+        ImgOpener opener = new ImgOpener();
+        Img<FloatType> diskRho = opener.openImgs(fSet.getFile(OrientationAngle.rho).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+        Img<FloatType> diskDelta = opener.openImgs(fSet.getFile(OrientationAngle.delta).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+        Img<FloatType> diskEta = opener.openImgs(fSet.getFile(OrientationAngle.eta).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+
+        assertTrue(_isOrientationVectorEqualTo(diskRho, diskDelta, diskEta, pos0, 0, (float) Math.PI / 4, 0));
+        assertTrue(_isOrientationVectorEqualTo(diskRho, diskDelta, diskEta, pos1, (float) OrientationVector.MAX_Rho,
+                (float) (3 * Math.PI / 4), (float) OrientationVector.MAX_Eta));
+    }
+
+    @Test
+    public void writeInDegrees_ManuallyMadeAngleImagesInDegrees_ReadImagesHaveSameDimensionAsDisk()
+            throws CannotFormOrientationImage, IOException {
+        int channel = 1;
+        String setNameAlias = "testSet";
+
+        WriterDummyCapturedImageFileSet fileSet = new WriterDummyCapturedImageFileSet(setNameAlias);
+
+        // Create orientation image to be written.
+        long[] dim = { 2, 1, 1, 1, 1 };
+        IMetadata metadata = new Metadata.MetadataBuilder(dim).axisOrder(AxisOrder.XYCZT)
+                .bitPerPixel(PixelTypes.FLOAT_32).build();
+
+        Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+        Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+        Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+
+        long[] pos0 = new long[] { 0, 0, 0, 0, 0 };
+        long[] pos1 = new long[] { 1, 0, 0, 0, 0 };
+
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float) OrientationVector.MAX_Rho);
+
+        _setPixel(delta, pos0, (float) Math.PI / 4);
+        _setPixel(delta, pos1, (float) (3 * Math.PI / 4));
+
+        _setPixel(eta, pos0, 0);
+        _setPixel(eta, pos1, (float) OrientationVector.MAX_Eta);
+
+        IOrientationImage imageSet = OrientationImageFactory.create(fileSet, channel, rho, delta, eta);
+
+        IOrientationImageWriter writer = new TiffOrientationImageWriter();
+        writer.writeInDegrees(_root, imageSet);
+
+        TiffOrientationImageInDegreeFileSet fSet = new TiffOrientationImageInDegreeFileSet(_root, fileSet, channel);
+        assertTrue(fSet.getFile(OrientationAngle.rho).exists() && fSet.getFile(OrientationAngle.delta).exists()
+                && fSet.getFile(OrientationAngle.eta).exists());
+
+        ImgOpener opener = new ImgOpener();
+        Img<FloatType> diskRho = opener.openImgs(fSet.getFile(OrientationAngle.rho).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+        Img<FloatType> diskDelta = opener.openImgs(fSet.getFile(OrientationAngle.delta).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+        Img<FloatType> diskEta = opener.openImgs(fSet.getFile(OrientationAngle.eta).getAbsolutePath(),
+                new ArrayImgFactory<FloatType>(new FloatType())).get(0);
+
+        assertTrue(_isOrientationVectorEqualTo(diskRho, diskDelta, diskEta, pos0, 0, 45, 0));
+        assertTrue(_isOrientationVectorEqualTo(diskRho, diskDelta, diskEta, pos1, 180, 135, 90));
+    }
+
+    private void _setPixel(Image<Float32> image, long[] position, float value) {
+        IPixelRandomAccess<Float32> ra = image.getRandomAccess();
+
+        ra.setPosition(position);
+        IPixel<Float32> pixel = ra.getPixel();
+
+        pixel.value().set(value);
+        ra.setPixel(pixel);
+    }
+
+    private boolean _isOrientationVectorEqualTo(Img<FloatType> diskRho, Img<FloatType> diskDelta,
+            Img<FloatType> diskEta, long[] position, float rho, float delta, float eta) {
+        RandomAccess<FloatType> ra_Rho = diskRho.randomAccess();
+        RandomAccess<FloatType> ra_Delta = diskDelta.randomAccess();
+        RandomAccess<FloatType> ra_Eta = diskEta.randomAccess();
+
+        ra_Rho.setPosition(position);
+        ra_Delta.setPosition(position);
+        ra_Eta.setPosition(position);
+
+        // Check the value is close enough
+        return Math.abs(ra_Rho.get().get() - rho) < 1e-6 && Math.abs(ra_Delta.get().get() - delta) < 1e-6
+                && Math.abs(ra_Eta.get().get() - eta) < 1e-6;
     }
 
 }
