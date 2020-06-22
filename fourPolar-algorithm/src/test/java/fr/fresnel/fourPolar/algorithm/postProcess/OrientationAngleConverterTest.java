de2802bc2d5bcbf70829cc7b1ddff54963697432
sbrasselet <sophie.brasselet@gmail.com>
2020-06-22 13:49:19 +0200

    Implement test

--- fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/postProcess/OrientationAngleConverterTest.java
+++ fourPolar-algorithm/src/test/java/fr/fresnel/fourPolar/algorithm/postProcess/OrientationAngleConverterTest.java
@@ -2,7 +2,6 @@ package fr.fresnel.fourPolar.algorithm.postProcess;
 
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-import java.util.EnumSet;
 import java.util.Iterator;
 
 import org.junit.jupiter.api.Test;
@@ -12,7 +11,7 @@ import fr.fresnel.fourPolar.core.exceptions.image.orientation.CannotFormOrientat
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
-import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.imgLib2Model.ImgLib2ImageFactory;
@@ -23,10 +22,12 @@ import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
 import fr.fresnel.fourPolar.core.image.orientation.OrientationImageFactory;
 import fr.fresnel.fourPolar.core.imagingSetup.imageFormation.Cameras;
 import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
+import fr.fresnel.fourPolar.core.physics.dipole.OrientationVector;
 
 public class OrientationAngleConverterTest {
     @Test
-    public void convertToDegree_OrientationImageWithTwoData_ReturnsCorrectDegrees() throws CannotFormOrientationImage {
+    public void convertToDegree_OrientationImageWithCriticalPointData_ReturnsCorrectDegrees()
+            throws CannotFormOrientationImage {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 1, 1, 1, 1 }).axisOrder(AxisOrder.XYCZT)
                 .build();
 
@@ -34,66 +35,108 @@ public class OrientationAngleConverterTest {
         Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
         Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
 
-        _setRadianAngles(rho.getCursor());
-        _setRadianAngles(delta.getCursor());
-        _setRadianAngles(eta.getCursor());
+        long[] pos0 = new long[]{0,0,0,0,0};
+        long[] pos1 = new long[]{1,0,0,0,0};
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float)OrientationVector.MAX_Rho);
+
+        _setPixel(delta, pos0, 0);
+        _setPixel(delta, pos1, (float)OrientationVector.MAX_Delta);
+        
+        _setPixel(eta, pos0, 0);
+        _setPixel(eta, pos1, (float)OrientationVector.MAX_Eta);
 
         IOrientationImage orientationImage = OrientationImageFactory.create(new DummySet(), 1, rho, delta, eta);
 
-        OrientationAngleConverter.convertToDegree(orientationImage);
+        Image<Float32> degreeImage_rho = OrientationAngleConverter.convertToDegree(orientationImage, OrientationAngle.rho);
+        assertTrue(_isPixelEqualToValue(degreeImage_rho, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(degreeImage_rho, pos1, (float)Math.toDegrees(OrientationVector.MAX_Rho)));
 
-        for (OrientationAngle orientationAngle : EnumSet.allOf(OrientationAngle.class)) {
-            IPixelCursor<Float32> angleCursor = orientationImage.getAngleImage(orientationAngle).getImage().getCursor();
+        Image<Float32> degreeImage_delta = OrientationAngleConverter.convertToDegree(orientationImage, OrientationAngle.delta);
+        assertTrue(_isPixelEqualToValue(degreeImage_delta, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(degreeImage_delta, pos1, (float)Math.toDegrees(OrientationVector.MAX_Delta)));
 
-            assertTrue(Math.abs(angleCursor.next().value().get() - 45) < 0.01);
-            assertTrue(Math.abs(angleCursor.next().value().get() - 90) < 0.01);
-        }
+        Image<Float32> degreeImage_eta = OrientationAngleConverter.convertToDegree(orientationImage, OrientationAngle.eta);
+        assertTrue(_isPixelEqualToValue(degreeImage_eta, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(degreeImage_eta, pos1, (float)Math.toDegrees(OrientationVector.MAX_Eta)));
+        
     }
 
     @Test
-    public void convertToRadian_OrientationImageWithTwoData_ReturnsCorrectDegrees() throws CannotFormOrientationImage {
+    public void convertToRadian_RhoImageWithCritialPointData_ReturnsCorrectDegrees() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 1, 1, 1, 1 }).axisOrder(AxisOrder.XYCZT)
                 .build();
 
         Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
-        Image<Float32> delta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
-        Image<Float32> eta = new ImgLib2ImageFactory().create(metadata, Float32.zero());
 
-        _setDegreeAngles(rho.getCursor());
-        _setDegreeAngles(delta.getCursor());
-        _setDegreeAngles(eta.getCursor());
+        long[] pos0 = new long[]{0,0,0,0,0};
+        long[] pos1 = new long[]{1,0,0,0,0};
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float)180);
 
-        IOrientationImage orientationImage = OrientationImageFactory.create(new DummySet(), 1, rho, delta, eta);
+        OrientationAngleConverter.convertToRadian(rho);
 
-        OrientationAngleConverter.convertToRadian(orientationImage);
+        assertTrue(_isPixelEqualToValue(rho, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(rho, pos1, (float)OrientationVector.MAX_Rho));
 
-        for (OrientationAngle orientationAngle : EnumSet.allOf(OrientationAngle.class)) {
-            IPixelCursor<Float32> angleCursor = orientationImage.getAngleImage(orientationAngle).getImage().getCursor();
-
-            assertTrue(Math.abs(angleCursor.next().value().get() - (float) Math.PI / 4) < 0.01);
-            assertTrue(Math.abs(angleCursor.next().value().get() - (float) Math.PI / 2) < 0.01);
-        }
     }
 
-    private void _setDegreeAngles(IPixelCursor<Float32> cursor) {
-        IPixel<Float32> angle1 = cursor.next();
-        angle1.value().set(45);
-        cursor.setPixel(angle1);
+    @Test
+    public void convertToRadian_DeltaImageWithCritialPointData_ReturnsCorrectDegrees() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 1, 1, 1, 1 }).axisOrder(AxisOrder.XYCZT)
+                .build();
 
-        IPixel<Float32> angle2 = cursor.next();
-        angle2.value().set(90);
-        cursor.setPixel(angle2);
+        Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+
+        long[] pos0 = new long[]{0,0,0,0,0};
+        long[] pos1 = new long[]{1,0,0,0,0};
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float)180);
+
+        OrientationAngleConverter.convertToRadian(rho);
+
+        assertTrue(_isPixelEqualToValue(rho, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(rho, pos1, (float)OrientationVector.MAX_Delta));
+
     }
 
-    private void _setRadianAngles(IPixelCursor<Float32> cursor) {
-        IPixel<Float32> angle1 = cursor.next();
-        angle1.value().set(((float) Math.PI / 4));
-        cursor.setPixel(angle1);
+    @Test
+    public void convertToRadian_EtaImageWithCritialPointData_ReturnsCorrectDegrees() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 1, 1, 1, 1 }).axisOrder(AxisOrder.XYCZT)
+                .build();
 
-        IPixel<Float32> angle2 = cursor.next();
-        angle2.value().set(((float) Math.PI / 2));
-        cursor.setPixel(angle2);
+        Image<Float32> rho = new ImgLib2ImageFactory().create(metadata, Float32.zero());
+
+        long[] pos0 = new long[]{0,0,0,0,0};
+        long[] pos1 = new long[]{1,0,0,0,0};
+        _setPixel(rho, pos0, 0);
+        _setPixel(rho, pos1, (float)90);
+
+        OrientationAngleConverter.convertToRadian(rho);
+
+        assertTrue(_isPixelEqualToValue(rho, pos0, (float)0));
+        assertTrue(_isPixelEqualToValue(rho, pos1, (float)OrientationVector.MAX_Eta));
+
     }
+
+    private void _setPixel(Image<Float32> image, long[] position, float value) {
+        IPixelRandomAccess<Float32> ra = image.getRandomAccess();
+
+        ra.setPosition(position);
+        IPixel<Float32> pixel = ra.getPixel();
+
+        pixel.value().set(value);
+        ra.setPixel(pixel);        
+    }
+
+    private boolean _isPixelEqualToValue(Image<Float32> image, long[] position, float value) {
+        IPixelRandomAccess<Float32> ra = image.getRandomAccess();
+        ra.setPosition(position);
+
+        // Check the value is close enough
+        return Math.abs(ra.getPixel().value().get() - value) < 1e-10;
+    }
+
 
 }
 
