db88b0005d64a9b93cc9d63606327568c467e614
Masoudas <Masoudas@hotmail.com>
2020-06-24 10:08:05 +0200

    Remove channel Setter. Refactor

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/soi/SoIImage.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/soi/SoIImage.java
@@ -11,38 +11,36 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImage;
 import fr.fresnel.fourPolar.core.image.polarization.IPolarizationImageSet;
+import fr.fresnel.fourPolar.core.physics.channel.ChannelUtils;
 import fr.fresnel.fourPolar.core.physics.polarization.Polarization;
 
 /**
  * A concrete implementation of {@link ISoIImage}.
  */
 public class SoIImage implements ISoIImage {
-    final private Image<UINT16> _image;
-    final private ICapturedImageFileSet _fileSet;
-
-    private int _channel = -1;
-
     /**
      * Create an SoI image that corresponds (in size) to a particular
-     * {@link IPolarizationImage}
+     * {@link IPolarizationImage}. The image is created with the same
+     * {@link ImageFactory} as the polarization image.
      * 
      * @param polarizationImageSet is a polarization image set.
-     * 
-     * @param factory              is the desired image factory.
-     * 
      */
     public static ISoIImage create(IPolarizationImageSet polarizationImageSet) {
+        Image<UINT16> soiImage = _createSoIImageFromPolarizationImage(polarizationImageSet);
+        return new SoIImage(polarizationImageSet.getFileSet(), soiImage, polarizationImageSet.channel());
+    }
+
+    /**
+     * Use one of the polarization images (here pol0), and create an image that has
+     * the same size as this image to be used for SoI.
+     */
+    private static Image<UINT16> _createSoIImageFromPolarizationImage(IPolarizationImageSet polarizationImageSet) {
         Image<UINT16> pol0 = polarizationImageSet.getPolarizationImage(Polarization.pol0).getImage();
 
-        IMetadata soiMetadata = new Metadata.MetadataBuilder(pol0.getMetadata().getDim()).axisOrder(AxisOrder.XYCZT)
-                .bitPerPixel(PixelTypes.UINT_16).build();
+        IMetadata soiMetadata = new Metadata.MetadataBuilder(pol0.getMetadata().getDim())
+                .axisOrder(ISoIImage.AXIS_ORDER).bitPerPixel(PixelTypes.UINT_16).build();
 
-        Image<UINT16> image = pol0.getFactory().create(soiMetadata, UINT16.zero());
-
-        SoIImage soiImage = new SoIImage(polarizationImageSet.getFileSet(), image);
-        soiImage.setChannel(polarizationImageSet.channel());
-
-        return soiImage;
+        return pol0.getFactory().create(soiMetadata, UINT16.zero());
     }
 
     /**
@@ -60,11 +58,13 @@ public class SoIImage implements ISoIImage {
             throw new IllegalArgumentException("Channel must be greater than zero.");
         }
 
-        SoIImage soiImage = new SoIImage(fileSet, image);
-        soiImage.setChannel(channel);
-
-        return soiImage;
+        return new SoIImage(fileSet, image, channel);
     }
+
+    final private Image<UINT16> _image;
+    final private ICapturedImageFileSet _fileSet;
+
+    final private int _channel;
 
     /**
      * This constructor is used for an already generated SoI image.
@@ -72,17 +72,26 @@ public class SoIImage implements ISoIImage {
      * @param fileSet
      * @param image
      */
-    private SoIImage(ICapturedImageFileSet fileSet, Image<UINT16> image) {
-        if (image.getMetadata().axisOrder() != AxisOrder.XYCZT) {
-            throw new IllegalArgumentException("image must be XYCZT.");
-        }
-
-        if (image.getMetadata().numChannels() != 1) {
-            throw new IllegalArgumentException("image must jave only one channel.");
-        }
+    private SoIImage(ICapturedImageFileSet fileSet, Image<UINT16> image, int channel) {
+        _checkAxisOrder(image);
+        _checkImageIsSingleChannel(image);
+        ChannelUtils.checkChannel(channel, fileSet.getChannels().length);
 
         this._fileSet = fileSet;
         this._image = image;
+        this._channel = channel;
+    }
+
+    private void _checkImageIsSingleChannel(Image<UINT16> image) {
+        if (image.getMetadata().numChannels() != 1) {
+            throw new IllegalArgumentException("image must jave only one channel.");
+        }
+    }
+
+    private void _checkAxisOrder(Image<UINT16> image) {
+        if (image.getMetadata().axisOrder() != AxisOrder.XYCZT) {
+            throw new IllegalArgumentException("image must be XYCZT.");
+        }
     }
 
     @Override
@@ -93,14 +102,6 @@ public class SoIImage implements ISoIImage {
     @Override
     public ICapturedImageFileSet getFileSet() {
         return this._fileSet;
-    }
-
-    private void setChannel(int channel) {
-        if (this._channel > 0) {
-            throw new IllegalAccessError("Can't reset channel");
-        }
-
-        this._channel = channel;
     }
 
     @Override
