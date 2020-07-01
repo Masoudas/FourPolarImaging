c539caf0c62f1e32308bf17bcb580a4ea21cc720
Masoudas <Masoudas@hotmail.com>
2020-07-01 16:58:40 +0200

    Add class

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/image/pixel/PixelTypeUtil.java
@@ -0,0 +1,22 @@
+package fr.fresnel.fourPolar.core.util.image.pixel;
+
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
+
+/**
+ * A set of utility methods for the {@PixelType}
+ */
+public class PixelTypeUtil {
+    private PixelTypeUtil(){
+        throw new AssertionError();
+    }
+
+    /**
+     * @return the pixel type of the image
+     */
+    public static <T extends PixelType> PixelType getPixelType(Image<T> image){
+        PixelTypes type = image.getCursor().next().value().getType();
+        return PixelTypes.create(type);
+    }
+}
