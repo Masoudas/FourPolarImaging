b9d86165231a077b75b8d8668563a7421e633667
Masoudas <Masoudas@hotmail.com>
2020-07-03 18:03:11 +0200

    Implement uint16 model

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTImageModel/UINT16BufferedImage.java
@@ -0,0 +1,31 @@
+package fr.fresnel.fourPolar.core.image.generic.AWTImageModel;
+
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
+import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.AWTImageModel.type.BufferedImageTypes;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+
+public class UINT16BufferedImage extends AWTBufferedImage<UINT16> {
+
+    protected UINT16BufferedImage(IMetadata metadata, ImageFactory factory) {
+        super(metadata, BufferedImageTypes.TYPE_USHORT_GRAY, factory);
+    }
+
+    @Override
+    public IPixelCursor<UINT16> getCursor() {
+        throw new UnsupportedOperationException("No cursor is defined for buffered image implementation.");
+    }
+
+    @Override
+    public IPixelCursor<UINT16> getCursor(long[] bottomCorner, long[] len) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("No interval cursor is defined for buffered image implementation.");
+    }
+
+    @Override
+    public IPixelRandomAccess<UINT16> getRandomAccess() {
+        return null;
+    }
+
+}
