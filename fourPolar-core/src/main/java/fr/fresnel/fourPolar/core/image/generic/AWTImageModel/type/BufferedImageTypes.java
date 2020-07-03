89e1b3dca2da94f8c46609382e6ebdc663b7e3cd
Masoudas <Masoudas@hotmail.com>
2020-07-03 18:03:24 +0200

    Implement types

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTImageModel/type/BufferedImageTypes.java
@@ -0,0 +1,18 @@
+package fr.fresnel.fourPolar.core.image.generic.AWTImageModel.type;
+
+import java.awt.image.BufferedImage;
+
+/**
+ * Represents the pixel types of {@link BufferedImage}. To each type is assigned
+ * an integer which wraps the pixel types of buffered image.
+ */
+public enum BufferedImageTypes {
+    TYPE_USHORT_GRAY {
+        @Override
+        public int getBufferedType() {
+            return BufferedImage.TYPE_USHORT_GRAY;
+        }
+    };
+
+    abstract public int getBufferedType();
+}
