f1747ce665420c24ce3b01bee0d31a29c15d2cb1
Masoudas <Masoudas@hotmail.com>
2020-07-03 09:48:07 +0200

    Set only message is io issue

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataParseError.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataParseError.java
@@ -6,18 +6,7 @@ package fr.fresnel.fourPolar.io.exceptions.image.generic.metadata;
 public class MetadataParseError extends Exception {
     private static final long serialVersionUID = 5374378579477003124L;
 
-    /**
-     * This error message is printed in case an axis in metadata does not correspond
-     * to x, y, z, time or channel.
-     */
-    public static final String UNDEFINED_AXIS = "Undefined axis label.";
-
-    /**
-     * This error message is printed in case all axis in the image are defined, but
-     * no proper axis order can be deduced from {@link AxisOrder} (including
-     * NoOrder).
-     */
-    public static final String UNDEFINED_AXIS_ORDER = "Axis order can't be set.";
+    public static final String IO_ISSUES = "Metadata can't be read due to low-level IO Issues.";
 
     public MetadataParseError(String message) {
         super(message);
