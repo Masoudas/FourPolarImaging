f6a53c00f97a418d2f8b9ffc1a60ea2bb53428d7
Masoudas <Masoudas@hotmail.com>
2020-07-03 10:52:49 +0200

    Check for undefined axis order instead of exception

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/captured/tiff/checker/TiffCapturedImageChecker.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/captured/tiff/checker/TiffCapturedImageChecker.java
@@ -1,13 +1,11 @@
 package fr.fresnel.fourPolar.io.image.captured.tiff.checker;
 
-import java.io.IOException;
-
-import fr.fresnel.fourPolar.core.exceptions.image.generic.axis.UnsupportedAxisOrder;
 import fr.fresnel.fourPolar.core.exceptions.imageSet.acquisition.IncompatibleCapturedImage;
 import fr.fresnel.fourPolar.core.image.captured.checker.ICapturedImageChecker;
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFile;
 import fr.fresnel.fourPolar.core.image.captured.file.RejectedCapturedImage;
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.io.exceptions.image.generic.metadata.MetadataParseError;
 import fr.fresnel.fourPolar.io.image.generic.IMetadataReader;
 
@@ -20,11 +18,16 @@ public class TiffCapturedImageChecker implements ICapturedImageChecker {
      * List of all the conditions that are checked together with conditions.
      */
     public final static String NOT_EXIST = "The file does not exist or cannot be accessed.";
+    public final static String NOT_ACCESSIBLE = "The file does cannot be accessed.";
     public final static String NOT_16_BIT = "Bit depth != 16. The image should not be used.";
     public final static String NOT_TIFF = "Not a tiff (tif) file.";
     public final static String CONTENT_CORRUPT = "File IO issue or Corrupt tiff content.";
     public final static String UNDEFINED_AXIS = "At least one axis is undefined or axis order is incompatible (e.g CXY)";
-    public final static String WRONG_NUM_CHANNEL = "The number of image channels (wavelengths) don't match imaging wavelengths.";
+    /**
+     * Message shown in case the given captured image does not have as many channels
+     * as specified when defining the captured image.
+     */
+    public final static String WRONG_NUM_CHANNEL = "The number of channels does not correspond.";
     final private IMetadataReader _metaDataReader;
 
     public TiffCapturedImageChecker(IMetadataReader reader) {
@@ -39,31 +42,35 @@ public class TiffCapturedImageChecker implements ICapturedImageChecker {
      *                                   to the file that violates the condition
      *                                   together with its cause is in the
      *                                   exception.
-     * @throws UnsupportedAxisOrder
-     * @throws IOException
      */
     @Override
     public void check(ICapturedImageFile image) throws IncompatibleCapturedImage {
         _imageExistsAndReadable(image);
         _extensionIsTifOrTiff(image);
 
-        IMetadata metadata = _axisOrderIsWellDefined(image);
+        IMetadata metadata = _readMetadata(image);
 
+        _isAxisOrderWellDefined(metadata, image);
         _bitDepthAbove16(metadata, image);
         _NumImageChannelsCorrespond(metadata, image);
     }
 
-    private IMetadata _axisOrderIsWellDefined(ICapturedImageFile image) throws IncompatibleCapturedImage {
+    private IMetadata _readMetadata(ICapturedImageFile image) throws IncompatibleCapturedImage {
         IMetadata metadata = null;
         try {
             metadata = this._metaDataReader.read(image.file());
         } catch (MetadataParseError e) {
-            throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), UNDEFINED_AXIS));
-        } catch (IOException e) {
             throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), CONTENT_CORRUPT));
         }
 
         return metadata;
+    }
+
+    private void _isAxisOrderWellDefined(IMetadata metadata, ICapturedImageFile image)
+            throws IncompatibleCapturedImage {
+        if (metadata.axisOrder() == AxisOrder.NoOrder) {
+            throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), UNDEFINED_AXIS));
+        }
     }
 
     private void _imageExistsAndReadable(ICapturedImageFile image) throws IncompatibleCapturedImage {
@@ -72,7 +79,7 @@ public class TiffCapturedImageChecker implements ICapturedImageChecker {
                 throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), NOT_EXIST));
             }
         } catch (SecurityException e) {
-            throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), NOT_EXIST));
+            throw new IncompatibleCapturedImage(new RejectedCapturedImage(image.file(), NOT_ACCESSIBLE));
         }
     }
 
