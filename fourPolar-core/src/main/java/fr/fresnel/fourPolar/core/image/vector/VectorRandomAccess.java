92a99b38887c9dffa8470de087223e57cf5b75c6
Masoudas <Masoudas@hotmail.com>
2020-07-06 17:21:07 +0200

    Design interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/VectorRandomAccess.java
@@ -0,0 +1,21 @@
+package fr.fresnel.fourPolar.core.image.vector;
+
+import java.util.RandomAccess;
+
+/**
+ * An interface for randomly accessing the underlying {@link Vector} of the
+ * {@link VectorImage}.
+ */
+public interface VectorRandomAccess extends RandomAccess {
+    /**
+     * Sets the position for accessing the underlying vector. It's the
+     * responsability of the caller to ensure that the given position is valid.
+     */
+    public void setPosition(double[] position);
+
+    /**
+     * Sets the vector of the given position.
+     */
+    public void setVector(Vector vector);
+
+}
