113f34d731760f39246ddb78f04c98b4f01994b0
Masoudas <Masoudas@hotmail.com>
2020-07-06 14:17:54 +0200

    Implement base test

---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/UINT16BufferedImageTest.java
@@ -0,0 +1,37 @@
+package fr.fresnel.fourPolar.core.image.generic.AWTModel;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import java.awt.image.BufferedImage;
+
+import org.junit.jupiter.api.Test;
+
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
+
+public class UINT16BufferedImageTest {
+    @Test
+    public void init_UINT16ImageType_Creates_TYPE_USHORT_GRAY_BufferedImageForEachPlane() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1 }).axisOrder(AxisOrder.XY)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        UINT16BufferedImage image = new UINT16BufferedImage(metadata, new U16BIDummyImageFactory(), UINT16.zero());
+        assertTrue(image.getImagePlane(1).getImage().getType() == BufferedImage.TYPE_USHORT_GRAY);
+    }
+
+}
+
+class U16BIDummyImageFactory implements ImageFactory {
+
+    @Override
+    public <T extends PixelType> Image<T> create(IMetadata metadata, T pixelType) throws IllegalArgumentException {
+        throw new UnsupportedOperationException("Not defined");
+    }
+
+}
