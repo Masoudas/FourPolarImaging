3908457de8b3e4853b2facffac4260d2fe10062f
Masoudas <Masoudas@hotmail.com>
2020-07-06 13:30:09 +0200

    Add image dimension to test methods

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/AWTBufferedImageTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/AWTBufferedImageTest.java
@@ -1,5 +1,6 @@
 package fr.fresnel.fourPolar.core.image.generic.AWTModel;
 
+import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import org.junit.jupiter.api.Test;
@@ -22,7 +23,7 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
  */
 public class AWTBufferedImageTest {
     @Test
-    public void getPlaneNumber_XYImage_Return1ForPosition00() {
+    public void getPlaneNumber_11XYImage_Return1ForPosition00() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1 }).axisOrder(AxisOrder.XY)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
@@ -34,7 +35,7 @@ public class AWTBufferedImageTest {
     }
 
     @Test
-    public void getPlaneNumber_XYZImage_Return3ForPosition002() {
+    public void getPlaneNumber_113XYZImage_Return3ForPosition002() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
@@ -46,7 +47,7 @@ public class AWTBufferedImageTest {
     }
 
     @Test
-    public void getPlaneNumber_XYZTImage_Return3ForPosition0020() {
+    public void getPlaneNumber_2233XYZTImage_Return3ForPosition0020() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 3, 3 }).axisOrder(AxisOrder.XYZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
@@ -58,7 +59,7 @@ public class AWTBufferedImageTest {
     }
 
     @Test
-    public void getPlaneNumber_XYZTImage_Return0ForPosition0022() {
+    public void getPlaneNumber_2233XYZTImage_Return0ForPosition0022() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 3, 3 }).axisOrder(AxisOrder.XYZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
@@ -70,7 +71,7 @@ public class AWTBufferedImageTest {
     }
 
     @Test
-    public void getPlaneNumber_XYZCTImage_Return2ForPosition00001() {
+    public void getPlaneNumber_11113XYZCTImage_Return2ForPosition00001() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 1, 1, 3 }).axisOrder(AxisOrder.XYCZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
@@ -79,6 +80,56 @@ public class AWTBufferedImageTest {
         int planeNo = image.getPlaneNumber(new long[] { 0, 0, 0, 0, 2 });
         
         assertTrue(planeNo == 3);
+    }
+
+    @Test
+    public void getImagePlane_11XYImage_ReturnSinglePlane1() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1 }).axisOrder(AxisOrder.XY)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+
+        assertTrue(image.getImagePlane(1).planeIndex() == 1);
+        assertThrows(IllegalArgumentException.class, ()->image.getImagePlane(2));
+
+    }
+
+    @Test
+    public void getImagePlane_113XYZImage_ReturnAllThreePlanes() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+
+        for (int planeIndex = 1; planeIndex <= 3; planeIndex++) {
+            assertTrue(image.getImagePlane(planeIndex).planeIndex() == planeIndex);    
+        }
+    }
+
+    @Test
+    public void getImagePlane_113XYZImage_ThrowsIllegalArgumentExceptionForPlane10() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+
+        assertThrows(IllegalArgumentException.class, ()->image.getImagePlane(10).planeIndex());
+        
+
+    }
+
+    @Test
+    public void getImagePlane_1133XYZTImage_ReturnAllNinePlanes() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3, 3 }).axisOrder(AxisOrder.XYZT)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+
+        for (int planeIndex = 1; planeIndex <= 9; planeIndex++) {
+            assertTrue(image.getImagePlane(planeIndex).planeIndex() == planeIndex);    
+        }
+        
+
     }
 
 }
