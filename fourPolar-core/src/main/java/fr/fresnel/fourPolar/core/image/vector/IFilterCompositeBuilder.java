d903d07c76d44115e5eb87d1fe2458276d0ce27c
Masoudas <Masoudas@hotmail.com>
2020-07-08 11:05:27 +0200

    Add builder interface

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/IFilterCompositeBuilder.java
@@ -0,0 +1,34 @@
+package fr.fresnel.fourPolar.core.image.vector;
+
+import java.util.List;
+
+import fr.fresnel.fourPolar.core.image.vector.filter.Filter;
+
+/**
+ * A private interface for accessing the parameters of the
+ */
+abstract class IFilterCompositeBuilder {
+    /**
+     * @return -1 if no start, else the positive value.
+     */
+    abstract int xStart();
+
+    /**
+     * @return -1 if no start, else the positive value.
+     */
+    abstract int yStart();
+
+    /**
+     * @return -1 if no percent, else the positive value.
+     */
+    abstract int widthPercent();
+
+    /**
+     * @return -1 if no percent, else the positive value.
+     */
+    abstract int heightPercent();
+
+    abstract String id();
+
+    abstract List<Filter> filters();
+}
