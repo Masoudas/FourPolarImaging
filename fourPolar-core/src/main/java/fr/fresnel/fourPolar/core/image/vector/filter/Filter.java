23329921050299c9d6fe04d363bd7b33d7c8f53f
Masoudas <Masoudas@hotmail.com>
2020-07-08 10:43:40 +0200

    Add methods and enum

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/filter/Filter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/filter/Filter.java
@@ -1,7 +1,45 @@
 package fr.fresnel.fourPolar.core.image.vector.filter;
 
+import java.util.Optional;
+
 /**
- * An interface for defining SVG filters.
+ * An interface for defining SVG filters. Specific implementations of the filter
+ * have their own methods for defining the attributes of the filter.
  */
 public interface Filter {
+    /**
+     * Indicates a list of most famous in tags for filter.
+     */
+    public enum IN {
+        SOURCE_GRAPHIC {
+            public String asString() {
+                return "SourceGraphic";
+            }
+        },
+        BACKGROUND_IMAGE {
+            public String asString() {
+                return "BackgroundImage";
+            }
+        };
+
+        abstract public String asString();
+    }
+
+    /**
+     * @return the first source this filter will work on. This source is always non
+     *         empty.
+     */
+    public String in();
+
+    /**
+     * @return the optional second source this filter would work on. This source can
+     *         be empty.
+     */
+    public Optional<String> in2();
+
+    /**
+     * @return an optional indicating the result of the filter. The result can be
+     *         empty.
+     */
+    public Optional<String> resultName();
 }
