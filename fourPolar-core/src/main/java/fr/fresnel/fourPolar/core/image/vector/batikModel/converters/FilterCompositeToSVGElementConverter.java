b53c7fca1ce7c25b82e3bf208f74a74b8251b129
Masoudas <Masoudas@hotmail.com>
2020-07-08 17:59:17 +0200

    Design basic converter

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/FilterCompositeToSVGElementConverter.java
@@ -0,0 +1,14 @@
+package fr.fresnel.fourPolar.core.image.vector.batikModel.converters;
+
+import org.w3c.dom.svg.SVGDocument;
+
+/**
+ * Using this class, we can convert a {@link FilterComposite} to an SVG element
+ * that would be written in the root node of the SVG.
+ */
+class FilterCompositeToSVGElementConverter {
+    public static void convert(SVGDocument svgDocument) {
+        
+    }
+    
+}
