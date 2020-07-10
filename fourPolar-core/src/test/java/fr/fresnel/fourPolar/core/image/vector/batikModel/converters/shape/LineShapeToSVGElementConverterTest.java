fb6659f4ce18f44eab6a30b465ebee1c6d1a25e0
Masoudas <Masoudas@hotmail.com>
2020-07-10 17:50:54 +0200

    Implement test

---
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/shape/LineShapeToSVGElementConverterTest.java
@@ -0,0 +1,48 @@
+package fr.fresnel.fourPolar.core.image.vector.batikModel.converters.shape;
+
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
+import org.apache.batik.anim.dom.SVGDOMImplementation;
+import org.junit.jupiter.api.Test;
+import org.w3c.dom.Element;
+import org.w3c.dom.NodeList;
+import org.w3c.dom.svg.SVGDocument;
+
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.util.shape.ILineShape;
+import fr.fresnel.fourPolar.core.util.shape.ShapeFactory;
+
+public class LineShapeToSVGElementConverterTest {
+    @Test
+    public void convert_lineFrom00to11_createsLineElementWithX1Y1X2Y2Attrs() {
+        ILineShape lineShape = new ShapeFactory().line2DShape(new long[] { 1, 1 }, Math.PI / 4, 2, 1, AxisOrder.XY);
+
+        SVGDocument svgDocument = (SVGDocument) SVGDOMImplementation.getDOMImplementation()
+                .createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
+
+        Element line = LineShapeToSVGElementConverter.convert(svgDocument, SVGDOMImplementation.SVG_NAMESPACE_URI, lineShape);
+
+        assertTrue(line.getTagName().equals("line"));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "x1").equals(String.valueOf(0)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "y1").equals(String.valueOf(0)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "x2").equals(String.valueOf(1)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "y2").equals(String.valueOf(1)));
+    }
+
+    @Test
+    public void convert_lineFrom0000to1100_createsLineElementWithX1Y1X2Y2Attrs() {
+        ILineShape lineShape = new ShapeFactory().line2DShape(new long[] { 1, 1, 0, 0 }, Math.PI / 4, 2, 1, AxisOrder.XYCT);
+
+        SVGDocument svgDocument = (SVGDocument) SVGDOMImplementation.getDOMImplementation()
+                .createDocument(SVGDOMImplementation.SVG_NAMESPACE_URI, "svg", null);
+
+        Element line = LineShapeToSVGElementConverter.convert(svgDocument, SVGDOMImplementation.SVG_NAMESPACE_URI, lineShape);
+
+        assertTrue(line.getTagName().equals("line"));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "x1").equals(String.valueOf(0)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "y1").equals(String.valueOf(0)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "x2").equals(String.valueOf(1)));
+        assertTrue(line.getAttributeNS(SVGDOMImplementation.SVG_NAMESPACE_URI, "y2").equals(String.valueOf(1)));
+    }
+
+}
