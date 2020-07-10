ca69cbfb67ca8a9df245e246ebe35dcdfd26f42b
Masoudas <Masoudas@hotmail.com>
2020-07-10 17:49:23 +0200

    Change documentation

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/shape/ShapeFactory.java
@@ -42,20 +42,21 @@ public class ShapeFactory {
     }
 
     /**
-     * Create a line with the given slope, length, thickness from the position. Note
-     * that even though the line is created in the xy plane, it can be merged in
-     * higher dimensions, meaning the position and axis order don't necessarily have
-     * to be 2 dimensional and XY.
+     * Create a line with the given slope, length, thickness from the position. The
+     * length of line on each side of position is approximately len/2. Note that
+     * even though the line is created in the xy plane, it can be merged in higher
+     * dimensions, meaning the position and axis order don't necessarily have to be
+     * 2 dimensional and XY.
      * 
-     * @param slopeAngle is the slope of the dipole in particular direction.
-     * @param position   is the pixel position of the dipole, as [x, y, z, ...]
+     * @param slopeAngle is the slope of the line in radian.
+     * @param position   is the position of the line.
      * @param length     is the desired length of the line.
      * @param thickness  is the desired thickness of the line.
      * @param axisOrder  is the axis order associated with position.
      * 
      */
     public ILineShape line2DShape(long[] position, double slopeAngle, long length, int thickness, AxisOrder axisOrder) {
-        double[] positionAsDouble = Arrays.stream(position).mapToDouble(t->t).toArray();
+        double[] positionAsDouble = Arrays.stream(position).mapToDouble(t -> t).toArray();
         return ImageJ1LineShape.create(positionAsDouble, slopeAngle, length, thickness, axisOrder);
     }
 
