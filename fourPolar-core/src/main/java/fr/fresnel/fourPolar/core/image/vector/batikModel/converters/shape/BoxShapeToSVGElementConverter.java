264fe5e26201914c50f93929ef8985830f762910
Masoudas <Masoudas@hotmail.com>
2020-07-10 16:22:38 +0200

    Implement shape converters

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/shape/BoxShapeToSVGElementConverter.java
@@ -0,0 +1,46 @@
+package fr.fresnel.fourPolar.core.image.vector.batikModel.converters.shape;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
+
+import fr.fresnel.fourPolar.core.util.shape.IBoxShape;
+
+/**
+ * Converts a box shape to a "rect" svg element.
+ */
+class BoxShapeToSVGElementConverter {
+    private final static String _ELEMENT_TAG = "rect";
+
+    private final static String _X_ATTR = "x";
+    private final static String _Y_ATTR = "y";
+    private final static String _WIDTH_ATTR = "width";
+    private final static String _HEIGHT_ATTR = "height";
+
+    private BoxShapeToSVGElementConverter() {
+        throw new AssertionError();
+    }
+
+    /**
+     * Converts a box shape to a {@link Rectangle}. Only the first two coordinates
+     * are used to create the rectangle element.
+     * 
+     * @param svgDocument  is the source svg document
+     * @param namespaceURI is the name space of the document
+     * @param boxShape     is the box shape.
+     * 
+     * @return an svg element that is a rectangle.
+     */
+    public static Element convert(SVGDocument svgDocument, String namespaceURI, IBoxShape boxShape) {
+        Element rectangleElement = svgDocument.createElementNS(namespaceURI, _ELEMENT_TAG);
+
+        long[] rectangleMin = boxShape.min();
+        long[] rectangleMax = boxShape.max();
+
+        rectangleElement.setAttributeNS(namespaceURI, _X_ATTR, String.valueOf(rectangleMin[0]));
+        rectangleElement.setAttributeNS(namespaceURI, _Y_ATTR, String.valueOf(rectangleMin[1]));
+        rectangleElement.setAttributeNS(namespaceURI, _WIDTH_ATTR, String.valueOf(rectangleMax[0] - rectangleMin[0]));
+        rectangleElement.setAttributeNS(namespaceURI, _HEIGHT_ATTR, String.valueOf(rectangleMax[1] - rectangleMin[1]));
+
+        return rectangleElement;
+    }
+}
---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/shape/LineShapeToSVGElementConverter.java
@@ -0,0 +1,39 @@
+package fr.fresnel.fourPolar.core.image.vector.batikModel.converters.shape;
+
+import org.w3c.dom.Element;
+import org.w3c.dom.svg.SVGDocument;
+
+import fr.fresnel.fourPolar.core.util.shape.ILineShape;
+
+public class LineShapeToSVGElementConverter {
+    private final static String _ELEMENT_TAG = "line";
+
+    private final static String _X1_ATTR = "x1";
+    private final static String _Y1_ATTR = "y1";
+    private final static String _X2_ATTR = "x2";
+    private final static String _Y2_ATTR = "y2";
+    
+    /**
+     * Converts a line shape to a {@link Line2D}. Only the first two coordinates *
+     * are used to create the rectangle element.
+     * 
+     * @param svgDocument  is the source svg document.
+     * @param namespaceURI is the name space of the document.
+     * @param lineShape    is the line shape.
+     * @return an svg element that is a line.
+     */
+    public static Element convert(SVGDocument svgDocument, String namespaceURI, ILineShape lineShape) {
+        Element lineElement = svgDocument.createElementNS(namespaceURI, _ELEMENT_TAG);
+
+        long[] start = lineShape.lineStart();
+        long[] end = lineShape.lineEnd();
+
+        lineElement.setAttributeNS(namespaceURI, _X1_ATTR, String.valueOf(start[0]));
+        lineElement.setAttributeNS(namespaceURI, _Y1_ATTR, String.valueOf(start[1]));
+        lineElement.setAttributeNS(namespaceURI, _X2_ATTR, String.valueOf(end[0]));
+        lineElement.setAttributeNS(namespaceURI, _Y2_ATTR, String.valueOf(end[1]));
+
+        return lineElement;
+
+    }
+}
