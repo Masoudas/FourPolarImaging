32f0498b5f83b33b337c23d2d70cb99d07c504b5
Masoudas <Masoudas@hotmail.com>
2020-07-13 16:57:36 +0200

    Remove unused imports

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/ImagePlane.java
@@ -0,0 +1,8 @@
+package fr.fresnel.fourPolar.core.image;
+
+public interface ImagePlane<T> {
+    /**
+     * @return the plane index associated with this image.
+     */
+    public int planeIndex();
+}
