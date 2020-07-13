01338200d40ae01fe2d0eeefbe81f83acf742882
Masoudas <Masoudas@hotmail.com>
2020-07-13 15:30:35 +0200

    Make all methods static

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/VectorToSVGElementConverter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/vector/batikModel/converters/VectorToSVGElementConverter.java
@@ -7,7 +7,6 @@ import fr.fresnel.fourPolar.core.image.generic.pixel.types.ARGB8;
 import fr.fresnel.fourPolar.core.image.vector.Vector;
 import fr.fresnel.fourPolar.core.image.vector.batikModel.converters.animation.AnimationToSVGElementConverter;
 import fr.fresnel.fourPolar.core.image.vector.batikModel.converters.shape.ShapeToSVGElementConverter;
-import fr.fresnel.fourPolar.core.image.vector.filter.FilterComposite;
 import fr.fresnel.fourPolar.core.util.shape.IShape;
 
 /**
@@ -18,6 +17,11 @@ import fr.fresnel.fourPolar.core.util.shape.IShape;
  * method directly asks for the svg document.
  */
 class VectorToSVGElementConverter {
+    private static final String _FILL_ATTR = "fill";
+    private static final String _STROKE_ATTR = "stroke";
+    private static final String _OPACITY_ATTR = "opacity";
+    private static final String _FILTER_ATTR = "filter";
+
     private VectorToSVGElementConverter() {
         throw new AssertionError();
     }
@@ -32,10 +36,10 @@ class VectorToSVGElementConverter {
      * @param namespaceURI is the name space inside the document to which the
      *                     element equivalent of vector must be added.
      * 
-     * @throws IllegalArgumentException if no converter is for an element of this
-     *                                  vector.
+     * @throws IllegalArgumentException if at least one component of the vector
+     *                                  can't be converted to an svg element.
      */
-    public void convert(Vector vector, SVGDocument svgDocument, String namespaceURI) {
+    public static void convert(Vector vector, SVGDocument svgDocument, String namespaceURI) {
         Element vectorElement = _createElementBasedOnVectorShape(vector.shape(), svgDocument, namespaceURI);
 
         _setVectorAttributes(vectorElement, vector);
@@ -43,7 +47,7 @@ class VectorToSVGElementConverter {
         _appendVectorElementToDocumentElement(svgDocument, vectorElement);
     }
 
-    private void _appendVectorElementToDocumentElement(SVGDocument svgDocument, Element vectorElement) {
+    private static void _appendVectorElementToDocumentElement(SVGDocument svgDocument, Element vectorElement) {
         svgDocument.getDocumentElement().appendChild(vectorElement);
     }
 
@@ -51,18 +55,17 @@ class VectorToSVGElementConverter {
      * Adds all the attributes related to the vector element, such as fill, stroke
      * width and so forth.
      */
-    private void _setVectorAttributes(Element vectorElement, Vector vector) {
+    private static void _setVectorAttributes(Element vectorElement, Vector vector) {
         _setElementFillColor(vectorElement, vector);
         _setElementOpacity(vectorElement, vector);
         _setElementStrokeColor(vectorElement, vector);
-
-        vector.filter().ifPresent((filter) -> _setElementFilter(vectorElement, filter));
+        _setElementFilter(vectorElement, vector);
     }
 
     /**
      * Set the vector animation, if such an animation exists.
      */
-    private void _setVectorAnimation(Vector vector, Element vectorElement, SVGDocument svgDocument) {
+    private static void _setVectorAnimation(Vector vector, Element vectorElement, SVGDocument svgDocument) {
         vector.animation()
                 .ifPresent(anim -> AnimationToSVGElementConverter.addToVectorElement(anim, vectorElement, svgDocument));
     }
@@ -71,7 +74,7 @@ class VectorToSVGElementConverter {
      * Create the element based on the shape interface that is at the backend of
      * this vector using the shape converter.
      */
-    private Element _createElementBasedOnVectorShape(IShape vectorShape, SVGDocument svgDocument, String namespaceURI) {
+    private static Element _createElementBasedOnVectorShape(IShape vectorShape, SVGDocument svgDocument, String namespaceURI) {
         return ShapeToSVGElementConverter.convert(vectorShape, svgDocument, namespaceURI);
     }
 
@@ -82,9 +85,11 @@ class VectorToSVGElementConverter {
      * @param namespaceURI  is the document name space.
      * @param vector        is the vector to be converted.
      */
-    private void _setElementFillColor(Element vectorElement, Vector vector) {
-        String fillColorAsString = _getRGBColorAsString(vector.fill());
-        vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), "fill", fillColorAsString);
+    private static void _setElementFillColor(Element vectorElement, Vector vector) {
+        if (vector.fill().isPresent()) {
+            String fillColorAsString = _getRGBColorAsString(vector.fill().get());
+            vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), _FILL_ATTR, fillColorAsString);
+        }
     }
 
     /**
@@ -93,9 +98,11 @@ class VectorToSVGElementConverter {
      * @param vectorElement is the SVG document element.
      * @param vector        is the vector to be converted.
      */
-    private void _setElementStrokeColor(Element vectorElement, Vector vector) {
-        String colorAsString = _getRGBColorAsString(vector.color());
-        vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), "stroke", colorAsString);
+    private static void _setElementStrokeColor(Element vectorElement, Vector vector) {
+        if (vector.color().isPresent()) {
+            String colorAsString = _getRGBColorAsString(vector.color().get());
+            vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), _STROKE_ATTR, colorAsString);
+        }
     }
 
     /**
@@ -104,9 +111,11 @@ class VectorToSVGElementConverter {
      * @param vectorElement is the SVG document element.
      * @param vector        is the vector to be converted.
      */
-    private void _setElementOpacity(Element vectorElement, Vector vector) {
-        String opacityAsString = _getOpacityAsString(vector.color());
-        vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), "opacity", opacityAsString);
+    private static void _setElementOpacity(Element vectorElement, Vector vector) {
+        if (vector.color().isPresent()) {
+            String opacityAsString = _getOpacityAsString(vector.color().get());
+            vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), _OPACITY_ATTR, opacityAsString);
+        }
     }
 
     /**
@@ -115,16 +124,18 @@ class VectorToSVGElementConverter {
      * @param vectorElement is the SVG document element.
      * @param vector        is the vector to be converted.
      */
-    private void _setElementFilter(Element vectorElement, FilterComposite filterComposite) {
-        String filterId = filterComposite.id();
-        vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), "filter", "url(#" + filterId + ")");
+    private static void _setElementFilter(Element vectorElement, Vector vector) {
+        if (vector.fill().isPresent()) {
+            String filterId = vector.filter().get().id();
+            vectorElement.setAttributeNS(vectorElement.getNamespaceURI(), _FILTER_ATTR, "url(#" + filterId + ")");
+        }
     }
 
     /**
      * @return the string representation of {@link ARGB8} color part suitable for
      *         SVG color attribute
      */
-    private String _getRGBColorAsString(ARGB8 color) {
+    private static String _getRGBColorAsString(ARGB8 color) {
         return "rgb(" + String.valueOf(color.getR()) + "," + String.valueOf(color.getG()) + ","
                 + String.valueOf(color.getB()) + ")";
     }
@@ -133,7 +144,7 @@ class VectorToSVGElementConverter {
      * @return the alpha value of {@link ARGB8} suitable for SVG opacity attribute.
      *         The returned string has a value between zero and one.
      */
-    private String _getOpacityAsString(ARGB8 color) {
+    private static String _getOpacityAsString(ARGB8 color) {
         return String.valueOf(color.getAlpha() / 255f);
     }
 }
