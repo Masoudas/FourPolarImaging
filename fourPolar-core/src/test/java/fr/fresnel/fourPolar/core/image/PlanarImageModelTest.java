11036165439ae778662b697a74d428af2f90407c
Masoudas <Masoudas@hotmail.com>
2020-07-14 10:26:49 +0200

    Move test to planar image model

--- fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/PlanarImageModelTest.java
+++ fourPolar-core/src/test/java/fr/fresnel/fourPolar/core/image/PlanarImageModelTest.java
@@ -1,4 +1,4 @@
-package fr.fresnel.fourPolar.core.image.generic.AWTModel;
+package fr.fresnel.fourPolar.core.image;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
 import static org.junit.jupiter.api.Assertions.assertTrue;
@@ -6,78 +6,73 @@ import static org.junit.jupiter.api.Assertions.assertTrue;
 import org.junit.jupiter.api.Test;
 
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
-import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
-import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
-import fr.fresnel.fourPolar.core.image.generic.Image;
-import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
 import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
-import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 
 /**
  * Dedicated to testing the non-abstract methods of the class. Note that all
  * methods of this are independent of {@link PixelType} and AWT buffered image
  * types.
  */
-public class AWTBufferedImageTest {
+public class PlanarImageModelTest {
     @Test
-    public void getPlaneNumber_11XYImage_Return1ForPosition00() {
+    public void getPlaneIndex_11XYImage_Return1ForPosition00() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1 }).axisOrder(AxisOrder.XY)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
-        int planeNo = image.getPlaneNumber(new long[] { 0, 0 });
+        int planeNo = image.getPlaneIndex(new long[] { 0, 0 });
 
         assertTrue(planeNo == 1);
     }
 
     @Test
-    public void getPlaneNumber_113XYZImage_Return3ForPosition002() {
+    public void getPlaneIndex_113XYZImage_Return3ForPosition002() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
-        int planeNo = image.getPlaneNumber(new long[] { 0, 0, 2 });
-
-        assertTrue(planeNo == 3);
-    }
-
-    @Test
-    public void getPlaneNumber_2233XYZTImage_Return3ForPosition0020() {
-        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 3, 3 }).axisOrder(AxisOrder.XYZT)
-                .bitPerPixel(PixelTypes.UINT_16).build();
-
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
-
-        int planeNo = image.getPlaneNumber(new long[] { 0, 0, 2, 0 });
+        int planeNo = image.getPlaneIndex(new long[] { 0, 0, 2 });
 
         assertTrue(planeNo == 3);
     }
 
     @Test
-    public void getPlaneNumber_2233XYZTImage_Return0ForPosition0022() {
+    public void getPlaneIndex_2233XYZTImage_Return3ForPosition0020() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 3, 3 }).axisOrder(AxisOrder.XYZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
-        int planeNo = image.getPlaneNumber(new long[] { 0, 0, 2, 2 });
+        int planeNo = image.getPlaneIndex(new long[] { 0, 0, 2, 0 });
+
+        assertTrue(planeNo == 3);
+    }
+
+    @Test
+    public void getPlaneIndex_2233XYZTImage_Return0ForPosition0022() {
+        IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 2, 2, 3, 3 }).axisOrder(AxisOrder.XYZT)
+                .bitPerPixel(PixelTypes.UINT_16).build();
+
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
+
+        int planeNo = image.getPlaneIndex(new long[] { 0, 0, 2, 2 });
 
         assertTrue(planeNo == 9);
     }
 
     @Test
-    public void getPlaneNumber_11113XYZCTImage_Return2ForPosition00001() {
+    public void getPlaneIndex_11113XYZCTImage_Return2ForPosition00001() {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 1, 1, 3 }).axisOrder(AxisOrder.XYCZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
-        int planeNo = image.getPlaneNumber(new long[] { 0, 0, 0, 0, 2 });
+        int planeNo = image.getPlaneIndex(new long[] { 0, 0, 0, 0, 2 });
         
         assertTrue(planeNo == 3);
     }
@@ -87,7 +82,7 @@ public class AWTBufferedImageTest {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1 }).axisOrder(AxisOrder.XY)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
         assertTrue(image.getImagePlane(1).planeIndex() == 1);
         assertThrows(IllegalArgumentException.class, ()->image.getImagePlane(2));
@@ -99,7 +94,7 @@ public class AWTBufferedImageTest {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
         for (int planeIndex = 1; planeIndex <= 3; planeIndex++) {
             assertTrue(image.getImagePlane(planeIndex).planeIndex() == planeIndex);    
@@ -111,7 +106,7 @@ public class AWTBufferedImageTest {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3 }).axisOrder(AxisOrder.XYZ)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
         assertThrows(IllegalArgumentException.class, ()->image.getImagePlane(10).planeIndex());
         
@@ -123,7 +118,7 @@ public class AWTBufferedImageTest {
         IMetadata metadata = new Metadata.MetadataBuilder(new long[] { 1, 1, 3, 3 }).axisOrder(AxisOrder.XYZT)
                 .bitPerPixel(PixelTypes.UINT_16).build();
 
-        DummyAWTBufferedImage<?> image = new DummyAWTBufferedImage<>(metadata, UINT16.zero());
+        DummyPlanarImage image = new DummyPlanarImage(metadata);
 
         for (int planeIndex = 1; planeIndex <= 9; planeIndex++) {
             assertTrue(image.getImagePlane(planeIndex).planeIndex() == planeIndex);    
@@ -134,33 +129,21 @@ public class AWTBufferedImageTest {
 
 }
 
-class DummyAWTBufferedImage<T extends PixelType> extends AWTBufferedImage<T> {
-    DummyAWTBufferedImage(IMetadata metadata, T type) {
-        super(metadata, new DummyImageFactory(), type);
-    }
+class DummyPlaneModel{
 
-    @Override
-    public IPixelCursor<T> getCursor() {
-        throw new UnsupportedOperationException("Not defined");
-    }
+}
 
+class DummyPlaneSupplier implements ImagePlaneSupplier<DummyPlaneModel>{
     @Override
-    public IPixelCursor<T> getCursor(long[] bottomCorner, long[] len) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not defined");
-    }
-
-    @Override
-    public IPixelRandomAccess<T> getRandomAccess() {
-        throw new UnsupportedOperationException("Not defined");
+    public DummyPlaneModel get() {
+        return new DummyPlaneModel();
     }
 
 }
 
-class DummyImageFactory implements ImageFactory {
-
-    @Override
-    public <T extends PixelType> Image<T> create(IMetadata metadata, T pixelType) throws IllegalArgumentException {
-        throw new UnsupportedOperationException("Not defined");
+class DummyPlanarImage extends PlanarImageModel<DummyPlaneModel> {
+    public DummyPlanarImage(IMetadata metadata) {
+        super(metadata, new DummyPlaneSupplier());
     }
-
 }
+
