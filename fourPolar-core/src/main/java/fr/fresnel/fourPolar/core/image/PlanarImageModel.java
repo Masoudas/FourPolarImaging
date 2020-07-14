a07bfcc9f6abf67c8cbe5e208aaf7bd5a595acdb
Masoudas <Masoudas@hotmail.com>
2020-07-14 10:08:06 +0200

    Implements planeIndex  method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/PlanarImageModel.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/PlanarImageModel.java
@@ -1,7 +1,6 @@
 package fr.fresnel.fourPolar.core.image;
 
 import java.util.Objects;
-import java.util.function.Supplier;
 import java.util.stream.IntStream;
 
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
@@ -14,21 +13,30 @@ import fr.fresnel.fourPolar.core.util.image.metadata.MetadataUtil;
  * @param <T> is the backend for image each plane.
  */
 public abstract class PlanarImageModel<T> implements ImagePlaneAccesser<T> {
+    private static final int _FIRST_PLANE_INDEX = 1;
     private final long[] _nPlanesPerDim;
-
     private final int _totalNumPlanes;
+    private final long[] _imageDim;
 
     protected final ImagePlane<T>[] _planes;
 
-    private final long[] _imageDim;
-
-    protected PlanarImageModel(Supplier<T> supplier, IMetadata metadata) {
+    /**
+     * Instantiates the planar model using the metadata of the image together with a
+     * supplier for image planes. The plane supplier interface would be responsible
+     * for creating instances of planes of type T.
+     * 
+     * @param metadata      is the metadata of the image.
+     * @param planeSupplier is the supplier for creating new instances of plane.
+     */
+    protected PlanarImageModel(IMetadata metadata, ImagePlaneSupplier<T> planeSupplier) {
         Objects.requireNonNull(metadata, "Metadata can't be null");
-        Objects.requireNonNull(supplier, "supplier can't be null");
+        Objects.requireNonNull(planeSupplier, "supplier can't be null");
 
         _totalNumPlanes = _getNumPlanesFromMetadata(metadata);
         _imageDim = metadata.getDim();
         _nPlanesPerDim = _nPlanesPerDim();
+
+        _planes = _createImageArray(metadata, planeSupplier);
     }
 
     private int _getNumPlanesFromMetadata(IMetadata metadata) {
@@ -36,18 +44,21 @@ public abstract class PlanarImageModel<T> implements ImagePlaneAccesser<T> {
     }
 
     /**
-     * Create a buffered image for each plane of the given dimension. Assume that
-     * image is 2 * 2 * 3 * 2. Then an array of size 3 * 2 is created, where each
-     * buffered image would be 2 * 2 in size.
+     * Create an instance of plane (or T) for each plane.
      * 
-     * @param dim is the dimension of the image.
+     * Example: Assume that image the is 2 * 2 * 3 * 2. Then an array of size 3 * 2
+     * is created.
+     * 
+     * @param metadata      is the metadata of the image.
+     * @param planeSupplier is the supplier of the image planes.
      * @return an array of buffered image plane that belong to each plane.
      */
-    private T[] _createBuffreredImageArray(IMetadata metadata, Supplier<T> planeSupplier) {
+    @SuppressWarnings("unchecked")
+    private ImagePlane<T>[] _createImageArray(IMetadata metadata, ImagePlaneSupplier<T> planeSupplier) {
         ImagePlane<T>[] _images = new ImagePlane[_totalNumPlanes];
 
         IntStream.range(0, _images.length).forEach(
-                planeIndex -> _images[planeIndex] = planeSupplier.get());
+                planeIndex -> _images[planeIndex] = new DefaultImagePlane<T>(planeSupplier.get(), planeIndex + 1));
         return _images;
     }
 
@@ -74,4 +85,34 @@ public abstract class PlanarImageModel<T> implements ImagePlaneAccesser<T> {
         return nPlanesPerDim;
     }
 
+    /**
+     * Returns the image plane. Calculates plane number using
+     * {@link #getPlaneNumber()}.
+     */
+    @Override
+    public ImagePlane<T> getImagePlane(int planeNumber) {
+        if (planeNumber < 1 || planeNumber > _totalNumPlanes) {
+            throw new IllegalArgumentException(
+                    "plane number should be greater than zero and less than total number of planes.");
+        }
+        return _planes[planeNumber - 1];
+
+    }
+
+    @Override
+    public int getPlaneIndex(long[] position) {
+        if (position.length != _imageDim.length) {
+            throw new IllegalArgumentException("position does not have same dimension as image.");
+        }
+
+        if (this._totalNumPlanes == 1) {
+            return 1;
+        }
+
+        int offSet = (int) position[2];
+        return offSet
+                + (int) IntStream.range(2, position.length).mapToLong(i -> position[i] * _nPlanesPerDim[i - 1]).sum()
+                + _FIRST_PLANE_INDEX;
+
+    }
 }
