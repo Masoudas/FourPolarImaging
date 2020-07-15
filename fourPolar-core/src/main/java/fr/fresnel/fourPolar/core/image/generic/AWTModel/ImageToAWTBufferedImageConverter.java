5e8372cb901af84525c0cd1e395bdeda5471e799
Masoudas <Masoudas@hotmail.com>
2020-07-15 17:35:05 +0200

    Implement converter of plane

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/ImageToAWTBufferedImageConverter.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/ImageToAWTBufferedImageConverter.java
@@ -3,16 +3,24 @@ package fr.fresnel.fourPolar.core.image.generic.AWTModel;
 import java.util.Objects;
 
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.core.image.generic.IPixelCursor;
+import fr.fresnel.fourPolar.core.image.generic.IPixelRandomAccess;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
+import fr.fresnel.fourPolar.core.image.generic.pixel.IPixel;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelType;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.UINT16;
 import fr.fresnel.fourPolar.core.util.image.ImageUtil;
+import fr.fresnel.fourPolar.core.util.image.metadata.MetadataUtil;
 
 /**
  * Provides static methods for Converting the given {@link Image} implementation
  * to {@link AWTBufferedImage}.
  */
 public class ImageToAWTBufferedImageConverter {
+    private static final AWTBufferedImageFactory _BUFFERED_IMAGE_FACTORY = new AWTBufferedImageFactory();
+
     private ImageToAWTBufferedImageConverter() {
         throw new AssertionError();
     }
@@ -37,6 +45,36 @@ public class ImageToAWTBufferedImageConverter {
             _copyImageToBuffuredImage(image, uint16BufferedImage);
             return (AWTBufferedImage<UINT16>) uint16BufferedImage;
         }
+
+    }
+
+    /**
+     * Converts the demanded plane to and awt buffered image.
+     * 
+     * @param image      is the image.
+     * @param pixelType  is the pixel type of the image.
+     * @param planeIndex is the plane index we wish to convert.
+     * 
+     * @return an awt buffered image implementation of {@link Image}.
+     * @throws IndexOutOfBounds if the plane index is less than one or greater than
+     *                          number of planes.
+     */
+    public static AWTBufferedImage<UINT16> convertPlane(Image<UINT16> image, UINT16 pixelType, int planeIndex) {
+        long[] planeDim = MetadataUtil.getPlaneDim(image.getMetadata());
+        AxisOrder planeAxisOrder = AxisOrder.planeAxisOrder(image.getMetadata().axisOrder());
+
+        IMetadata metadata = new Metadata.MetadataBuilder(planeDim).axisOrder(planeAxisOrder)
+                .bitPerPixel(pixelType.getType()).build();
+        Image<UINT16> bufferedImg = _BUFFERED_IMAGE_FACTORY.create(metadata, pixelType);
+
+        IPixelRandomAccess<UINT16> bufferedImgRA = bufferedImg.getRandomAccess();
+        for (IPixelCursor<UINT16> planeCursor = ImageUtil.getPlaneCursor(image, planeIndex); planeCursor.hasNext();) {
+            IPixel<UINT16> pixel = planeCursor.next();
+            bufferedImgRA.setPosition(planeCursor.localize());
+            bufferedImgRA.setPixel(pixel);
+        }
+
+        return (AWTBufferedImage<UINT16>)bufferedImg;
 
     }
 
