f2b135b194e367db65b74ceb350e89ab21b887f5
Masoudas <Masoudas@hotmail.com>
2020-07-16 12:17:20 +0200

    Add createFromPlanes method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/AWTBufferedImageFactory.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/image/generic/AWTModel/AWTBufferedImageFactory.java
@@ -1,5 +1,7 @@
 package fr.fresnel.fourPolar.core.image.generic.AWTModel;
 
+import java.awt.image.BufferedImage;
+
 import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
 import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
@@ -18,6 +20,32 @@ public class AWTBufferedImageFactory implements ImageFactory {
         switch (pixelType.getType()) {
             case UINT_16:
                 return (Image<T>) new UINT16BufferedImage(metadata, this, UINT16.zero());
+
+            default:
+                throw new IllegalArgumentException("No implementation was found for the requested type.");
+        }
+    }
+
+    /**
+     * Construct the image directly by providing the planes. Planes are indexed in
+     * the order they're provided.
+     * 
+     * @param <T>       is the pixel type
+     * @param metadata  is the metadata of the image.
+     * @param pixelType is the pixel type of the image.
+     * @param planes    are the planes.
+     * @return an image instance with buffered image as backend.
+     * 
+     * @throws IllegalArgumentException if a plane does not have the same dimension
+     *                                  as given by the metadata, or if number of
+     *                                  planes in metadata does not match the number
+     *                                  of planes,
+     */
+    @SuppressWarnings("unchecked")
+    public <T extends PixelType> Image<T> createFromPlanes(IMetadata metadata, T pixelType, BufferedImage[] planes) {
+        switch (pixelType.getType()) {
+            case UINT_16:
+                return (Image<T>) new UINT16BufferedImage(metadata, this, UINT16.zero(), planes);
 
             default:
                 throw new IllegalArgumentException("No implementation was found for the requested type.");
