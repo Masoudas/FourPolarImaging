4cbaa176da39dff55beee45d9ddc224a53b922d7
sbrasselet <sophie.brasselet@gmail.com>
2020-07-17 18:01:26 +0200

    Change parameter name

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataIOIssues.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataIOIssues.java
@@ -6,7 +6,8 @@ package fr.fresnel.fourPolar.io.exceptions.image.generic.metadata;
 public class MetadataIOIssues extends Exception {
     private static final long serialVersionUID = 5374378579477003124L;
 
-    public static final String IO_ISSUES = "Metadata can't be read due to low-level IO Issues.";
+    public static final String READ_ISSUES = "Metadata can't be read due to low-level IO Issues.";
+    public static final String Write_ISSUES = "Metadata can't be written due to low-level IO Issues.";
 
     public MetadataIOIssues(String message) {
         super(message);
