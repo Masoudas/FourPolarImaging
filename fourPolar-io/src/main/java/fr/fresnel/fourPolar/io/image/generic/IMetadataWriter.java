5b732aec7af2b2a6d6fbcaf217cdc2df831458d7
sbrasselet <sophie.brasselet@gmail.com>
2020-07-17 16:17:55 +0200

    Introduce writer interface

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/generic/IMetadataWriter.java
@@ -0,0 +1,36 @@
+package fr.fresnel.fourPolar.io.image.generic;
+
+import java.io.File;
+import java.io.IOException;
+
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
+import fr.fresnel.fourPolar.io.exceptions.image.generic.metadata.MetadataParseError;
+
+/**
+ * An interface for writing {@link IMetadata}. This interface is intended for
+ * writing metadata as a separate entity from the image this metadata is
+ * associated with, as most image writer methods have internal methods for
+ * writing their metadata.
+ */
+public interface IMetadataWriter {
+    /**
+     * Write the given metadata to the following root, and under the given name.
+     * 
+     * @param metadata is the metadata to be written to disk.
+     * @param root     is the root folder of where the image should be written (must
+     *                 not contain file name).
+     * @param name     is the name underwhich metadata is written (must not contain
+     *                 file extension).
+     * 
+     * @throws MetadataParseError in case the metadata can't be written to disk.
+     */
+    public void write(IMetadata metadata, File root, String name) throws MetadataParseError;
+
+    /**
+     * Closes all resources associated with this writer.
+     * 
+     * @throws IOException in case of io issues when closing resources.
+     */
+    public void close() throws IOException;
+
+}
