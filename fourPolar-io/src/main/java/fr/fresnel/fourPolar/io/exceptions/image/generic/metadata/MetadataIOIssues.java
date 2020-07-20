059168161a4a19f4f727011c2a87a8989a612c54
Masoudas <Masoudas@hotmail.com>
2020-07-20 10:21:09 +0200

    Add new message

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataIOIssues.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/exceptions/image/generic/metadata/MetadataIOIssues.java
@@ -7,6 +7,7 @@ public class MetadataIOIssues extends Exception {
     private static final long serialVersionUID = 5374378579477003124L;
 
     public static final String READ_ISSUES = "Metadata can't be read due to low-level IO Issues.";
+    public static final String INCOMPLETE_METADATA = "Metadata instance has missing fileds.";
     public static final String Write_ISSUES = "Metadata can't be written due to low-level IO Issues.";
 
     public MetadataIOIssues(String message) {
