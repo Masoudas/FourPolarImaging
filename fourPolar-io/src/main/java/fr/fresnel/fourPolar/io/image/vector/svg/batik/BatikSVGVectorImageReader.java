17cfcd4ac07adc3124a4b92885a53077d8006135
Masoudas <Masoudas@hotmail.com>
2020-07-20 09:59:38 +0200

    Implement interface methods

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/vector/svg/batik/BatikSVGVectorImageReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/image/vector/svg/batik/BatikSVGVectorImageReader.java
@@ -1,10 +1,26 @@
 package fr.fresnel.fourPolar.io.image.vector.svg.batik;
 
+import java.io.File;
+
+import fr.fresnel.fourPolar.core.image.vector.VectorImage;
+import fr.fresnel.fourPolar.io.exceptions.image.vector.VectorImageIOIssues;
 import fr.fresnel.fourPolar.io.image.vector.VectorImageReader;
 
 /**
  * 
  */
 public class BatikSVGVectorImageReader implements VectorImageReader {
+
+    @Override
+    public VectorImage read(File root, String imageName) throws VectorImageIOIssues {
+        // TODO Auto-generated method stub
+        return null;
+    }
+
+    @Override
+    public void close() throws VectorImageIOIssues {
+        // TODO Auto-generated method stub
+
+    }
 
 }
