241e0c46c268532845c1bbbeaf3eec76e169d00a
Masoudas <Masoudas@hotmail.com>
2020-07-20 17:11:11 +0200

    Add util class

---
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/vectorImage/VectorImageUtil.java
@@ -0,0 +1,27 @@
+package fr.fresnel.fourPolar.core.util.vectorImage;
+
+import fr.fresnel.fourPolar.core.image.PlanarImageModel;
+import fr.fresnel.fourPolar.core.image.vector.VectorImage;
+
+/**
+ * Provides a set of static utility methods for {@link VectorImage}.
+ */
+public class VectorImageUtil {
+    private VectorImageUtil(){
+        throw new AssertionError();
+    }
+
+    /**
+     * Returns true if the given vector image implements {@link ImagePlaneAccessor} and that its backend planes
+     * are of type T.
+     * @param <T> is the expected backend plane type of the image.
+     * @param vectorImage is the vector image.
+     * @param t is the class of T.
+     */
+    public static <T> boolean hasBackEndPlanes(VectorImage vectorImage, Class<T> t){
+        boolean implementsPlaneAccessor = vectorImage instanceof PlanarImageModel<?>;
+        boolean hasTAsBackend = ((PlanarImageModel<?>)vectorImage).getImagePlane(1).getClass() == t;
+        
+        return implementsPlaneAccessor && hasTAsBackend;
+    }
+}
