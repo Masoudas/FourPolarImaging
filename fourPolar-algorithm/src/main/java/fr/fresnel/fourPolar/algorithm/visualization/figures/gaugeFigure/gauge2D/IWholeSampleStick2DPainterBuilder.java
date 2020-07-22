eeba87030868a43f350565faa1317704677b1c2c
Masoudas <Masoudas@hotmail.com>
2020-07-22 16:06:11 +0200

    Add separate build method for each stick type

--- fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/visualization/figures/gaugeFigure/gauge2D/IWholeSampleStick2DPainterBuilder.java
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/visualization/figures/gaugeFigure/gauge2D/IWholeSampleStick2DPainterBuilder.java
@@ -3,8 +3,9 @@ package fr.fresnel.fourPolar.algorithm.visualization.figures.gaugeFigure.gauge2D
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.color.ColorBlender;
 import fr.fresnel.fourPolar.core.image.orientation.IOrientationImage;
 import fr.fresnel.fourPolar.core.image.soi.ISoIImage;
+import fr.fresnel.fourPolar.core.physics.dipole.OrientationAngle;
 import fr.fresnel.fourPolar.core.util.image.colorMap.ColorMap;
-import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.guage.AngleGaugeType;
+import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
 
 /**
  * A simple private interface for accessing the parameters of the builder.
@@ -20,8 +21,11 @@ abstract class IWholeSampleStick2DPainterBuilder {
 
     abstract int getStickThickness();
 
-    abstract AngleGaugeType getAngleGaugeType();
+    abstract IGaugeFigure getGauageFigure();
 
     abstract ColorBlender getColorBlender();
 
+    abstract OrientationAngle getSlopeAngle();
+
+    abstract OrientationAngle getColorAngle();
 }
