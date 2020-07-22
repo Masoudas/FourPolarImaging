c72f1509f1d3dc266316a634b22c59aa3dcc0062
Masoudas <Masoudas@hotmail.com>
2020-07-22 16:08:11 +0200

    Add create method

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/GaugeFigure.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/visualization/figures/gaugeFigure/GaugeFigure.java
@@ -1,13 +1,24 @@
 package fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure;
 
+import java.util.Arrays;
+
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.image.generic.IMetadata;
 import fr.fresnel.fourPolar.core.image.generic.Image;
+import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
+import fr.fresnel.fourPolar.core.image.generic.axis.AxisOrder;
+import fr.fresnel.fourPolar.core.image.generic.metadata.Metadata;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.ARGB8;
+import fr.fresnel.fourPolar.core.image.generic.pixel.types.PixelTypes;
+import fr.fresnel.fourPolar.core.image.soi.ISoIImage;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.guage.AngleGaugeType;
 
 /**
  * A concrete implementation of the {@link IGaugeFigure} that uses {@link Image}
  * as its backend.
+ * <p>
+ * Note that regardless of the form of gauge figure, the underlying image is
+ * always equivalent to {@link ISoIImage#AXIS_ORDER}.
  */
 public class GaugeFigure implements IGaugeFigure {
     private final Image<ARGB8> _image;
@@ -17,18 +28,128 @@ public class GaugeFigure implements IGaugeFigure {
     private final int _channel;
 
     /**
-     * Create an stick figure using the interface of colored {@link ISoIImage}.
+     * Creates an empty 2D gauge figure, that can be used for representing
+     * orientation of a dipole stick.
      * 
-     * @param type    is the type of the figure.
-     * @param image   is the colored SoI image.
-     * @param fileSet is the captured file set this stick image corresponds to.
+     * @param figDim  is the dimension of the figure (both x and y).
+     * @param channel is the channel number of this figure.
+     * @param fileSet is the fileSet of the image this figure represents.
+     * @param factory is the factory by which this figure is created.
+     * 
+     * @return an empty 2D gauge figure.
      */
-    public GaugeFigure(GaugeFigureLocalization figureType, AngleGaugeType type, Image<ARGB8> image,
+    public static GaugeFigure singleDipoleDelta2DStick(long figDim, int channel, ICapturedImageFileSet fileSet,
+            ImageFactory factory) {
+        long[] fig_dim = new long[AXIS_ORDER.numAxis];
+        Arrays.setAll(fig_dim, (i) -> 1);
+        fig_dim[0] = figDim;
+        fig_dim[1] = figDim;
+          
+        IMetadata fig_metadata = new Metadata.MetadataBuilder(fig_dim).axisOrder(AXIS_ORDER)
+                .bitPerPixel(PixelTypes.ARGB_8).build();
+        Image<ARGB8> figure = factory.create(fig_metadata, ARGB8.zero());
+
+        return new GaugeFigure(GaugeFigureLocalization.SINGLE_DIPOLE, AngleGaugeType.Delta2D, figure, fileSet, channel);
+    }
+
+    /**
+     * Creates a figure instance for representing {@link AngleGaugeType#Rho2D}. Note
+     * that the same factory as the soi image is used for creating this gauge
+     * figure.
+     * 
+     * @param soiImage is the soi image for which we want to create this gauge
+     *                 figure.
+     * @return an empty gauge figure.
+     */
+    public static GaugeFigure wholeSampleRho2DStick(ISoIImage soiImage) {
+        return _createWholeSample2DStickFigure(soiImage, AngleGaugeType.Rho2D);
+    }
+
+    /**
+     * Creates a figure instance for representing {@link AngleGaugeType#Delta2D}.
+     * Note that the same factory as the soi image is used for creating this gauge
+     * figure.
+     * 
+     * @param soiImage is the soi image for which we want to create this gauge
+     *                 figure.
+     * @return an empty gauge figure.
+     */
+    public static GaugeFigure wholeSampleDelta2DStick(ISoIImage soiImage) {
+        return _createWholeSample2DStickFigure(soiImage, AngleGaugeType.Delta2D);
+    }
+
+    /**
+     * Creates a figure instance for representing {@link AngleGaugeType#Eta2D}. Note
+     * that the same factory as the soi image is used for creating this gauge
+     * figure.
+     * 
+     * @param soiImage is the soi image for which we want to create this gauge
+     *                 figure.
+     * @return an empty gauge figure.
+     */
+    public static GaugeFigure wholeSampleEta2DStick(ISoIImage soiImage) {
+        return _createWholeSample2DStickFigure(soiImage, AngleGaugeType.Eta2D);
+    }
+
+    /**
+     * Creates a figure instance for representing a {@link AngleGaugeType#Stick3D}.
+     * To accommodate the sticks in the z direction, the generated image is
+     * interleaved in the z direction by a factor. Hence if factor is 2, the gauge
+     * figure would have to z-planes for each z-plane of soi.
+     * <p>
+     * Note that the same factory as the soi image is used for creating this gauge
+     * figure.
+     * 
+     * @param soiImage           is the soi image for which we want to create this
+     *                           gauge figure.
+     * @param z_interleaveFactor is ratio of z-planes to be placed for each soi
+     *                           z-plane.
+     * @return an empty gauge figure.
+     */
+    public static GaugeFigure wholeSample3DStick(ISoIImage soiImage, int z_interleaveFactor) {
+        if (z_interleaveFactor < 1) {
+            throw new IllegalArgumentException("z-interleave factor can't be less than one.");
+        }
+
+        IMetadata soi_metadata = soiImage.getImage().getMetadata();
+        long[] img_size = soi_metadata.getDim();
+        img_size[AXIS_ORDER.z_axis] *= z_interleaveFactor;
+
+        IMetadata fig_metadata = new Metadata.MetadataBuilder(img_size).axisOrder(AXIS_ORDER)
+                .bitPerPixel(PixelTypes.ARGB_8).build();
+        Image<ARGB8> figure = soiImage.getImage().getFactory().create(fig_metadata, ARGB8.zero());
+
+        return new GaugeFigure(GaugeFigureLocalization.WHOLE_SAMPLE, AngleGaugeType.Stick3D, figure,
+                soiImage.getFileSet(), soiImage.channel());
+    }
+
+    /**
+     * Create a gauge figure of same size as soi with the given gauge.
+     */
+    private static GaugeFigure _createWholeSample2DStickFigure(ISoIImage soiImage, AngleGaugeType gaugeType) {
+        IMetadata fig_metadata = new Metadata.MetadataBuilder(soiImage.getImage().getMetadata().getDim())
+                .axisOrder(AXIS_ORDER).bitPerPixel(PixelTypes.ARGB_8).build();
+        Image<ARGB8> figure = soiImage.getImage().getFactory().create(fig_metadata, ARGB8.zero());
+
+        return new GaugeFigure(GaugeFigureLocalization.WHOLE_SAMPLE, gaugeType, figure, soiImage.getFileSet(),
+                soiImage.channel());
+    }
+
+    /**
+     * Create an stick figure using the provided color image.
+     * 
+     * @param localization   is the localization level of this figure
+     * @param angleGaugeType is the type of gauge.
+     * @param image          is the image instance.
+     * @param fileSet        is the file set this figure is associated with.
+     * @param channel        is the channel this figure is associated with.
+     */
+    public GaugeFigure(GaugeFigureLocalization localization, AngleGaugeType angleGaugeType, Image<ARGB8> image,
             ICapturedImageFileSet fileSet, int channel) {
         this._image = image;
-        this._type = type;
+        this._type = angleGaugeType;
         this._fileSet = fileSet;
-        this._figureType = figureType;
+        this._figureType = localization;
         this._channel = channel;
     }
 
