dfaf0129c433c18604c2f91faf075c88a695c1f5
Masoudas <Masoudas@hotmail.com>
2020-07-23 18:00:31 +0200

    Design base builder

---
+++ fourPolar-algorithm/src/main/java/fr/fresnel/fourPolar/algorithm/visualization/figures/gaugeFigure/gauge2D/vectorModel/VectorWholeSampleStick2DPainterBuilder.java
@@ -0,0 +1,8 @@
+package fr.fresnel.fourPolar.algorithm.visualization.figures.gaugeFigure.gauge2D.vectorModel;
+
+/**
+ * !!!Note that color blending happens inside the builder!!!
+ */
+public class VectorWholeSampleStick2DPainterBuilder {
+    
+}
