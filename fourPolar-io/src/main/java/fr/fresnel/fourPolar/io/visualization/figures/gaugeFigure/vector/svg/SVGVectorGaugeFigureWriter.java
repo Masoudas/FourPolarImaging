bfd2f4348835c7ca915a691136638fd7bef176a5
Masoudas <Masoudas@hotmail.com>
2020-07-27 10:03:48 +0200

    Design class

---
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/vector/svg/SVGVectorGaugeFigureWriter.java
@@ -0,0 +1,11 @@
+package fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure.vector.svg;
+
+import fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure.IGaugeFigureWriter;
+
+/**
+ * A concrete implementation of {@IGaugeFigureWriter} that writes a vector gauge figure
+ * to the root
+ */
+public class SVGVectorGaugeFigureWriter implements IGaugeFigureWriter {
+    
+}
