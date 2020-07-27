ddd4d1fa2c09a79a86aa0b5917cac0c1fae2ef62
Masoudas <Masoudas@hotmail.com>
2020-07-27 09:12:29 +0200

    Remove hasBackEndPlanes

--- fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/vectorImage/VectorImageUtil.java
+++ fourPolar-core/src/main/java/fr/fresnel/fourPolar/core/util/vectorImage/VectorImageUtil.java
@@ -11,26 +11,4 @@ public class VectorImageUtil {
         throw new AssertionError();
     }
 
-    /**
-     * Returns true if the given vector image implements {@link ImagePlaneAccessor} and that its backend planes
-     * are of type T. Checks backend plane types by trying to cast to type T.
-     * @param <T> is the expected backend plane type of the image.
-     * @param vectorImage is the vector image.
-     * @param t is the class of T.
-     */
-    @SuppressWarnings("unchecked")
-    public static <T> boolean hasBackEndPlanes(VectorImage vectorImage, Class<T> t){
-        if (!(vectorImage instanceof ImagePlaneAccessor<?>)){
-            return false;
-        }
-
-        try{
-            T t_prime = (T)((ImagePlaneAccessor<?>)vectorImage).getImagePlane(1).getPlane();
-            return true;
-        } catch (ClassCastException e){
-            
-        }
-        
-        return false;
-    }
 }
