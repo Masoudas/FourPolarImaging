c269fd5fc43f5ad38d97c484d353ff32dddef999
Masoudas <Masoudas@hotmail.com>
2020-07-27 11:09:35 +0200

    Add set methods

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/IGaugeFigureReader.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/IGaugeFigureReader.java
@@ -4,13 +4,30 @@ import java.io.File;
 import java.io.IOException;
 
 import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
+import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.GaugeFigureLocalization;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
+import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.guage.AngleGaugeType;
 import fr.fresnel.fourPolar.io.exceptions.visualization.gaugeFigure.GaugeFigureIOException;
 
 /**
- * An interface for reading a {@link IGaugeFigure} from the disk.
+ * An interface for reading a {@link IGaugeFigure} from the disk as written by
+ * {@link IGaugeFigureWriter}.
  */
 public interface IGaugeFigureReader {
+    /**
+     * Set the localization level of the figures to be read.
+     * 
+     * @param localization is the localization of the gauge figure to be read.
+     */
+    public void setLocalization(GaugeFigureLocalization localization);
+
+    /**
+     * Set the angle gauge type of the figures to be read.
+     * 
+     * @param gaugeType is the gauge type of the figure to be read.
+     */
+    public void setAngleGaugeType(AngleGaugeType gaugeType);
+
     /**
      * Write the given composites to the folder designated to the processed (bead)
      * images, as defined by
