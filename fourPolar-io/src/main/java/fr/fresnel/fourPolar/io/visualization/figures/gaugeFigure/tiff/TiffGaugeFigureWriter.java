6b44babb3b64c78a7ba6606ce3948e3a0f8f6933
Masoudas <Masoudas@hotmail.com>
2020-07-27 11:21:37 +0200

    Comply to new exception

--- fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/tiff/TiffGaugeFigureWriter.java
+++ fourPolar-io/src/main/java/fr/fresnel/fourPolar/io/visualization/figures/gaugeFigure/tiff/TiffGaugeFigureWriter.java
@@ -3,15 +3,19 @@ package fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure.tiff;
 import java.io.File;
 import java.io.IOException;
 
+import fr.fresnel.fourPolar.core.image.captured.file.ICapturedImageFileSet;
 import fr.fresnel.fourPolar.core.image.generic.ImageFactory;
 import fr.fresnel.fourPolar.core.image.generic.pixel.types.ARGB8;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.GaugeFigure;
 import fr.fresnel.fourPolar.core.visualization.figures.gaugeFigure.IGaugeFigure;
+import fr.fresnel.fourPolar.io.exceptions.visualization.gaugeFigure.GaugeFigureIOException;
 import fr.fresnel.fourPolar.io.image.generic.ImageWriter;
 import fr.fresnel.fourPolar.io.image.generic.tiff.TiffImageWriterFactory;
 import fr.fresnel.fourPolar.io.visualization.figures.gaugeFigure.IGaugeFigureWriter;
 
 public class TiffGaugeFigureWriter implements IGaugeFigureWriter {
+    private static final String _LOW_LEVEL_IO_MESSAGE = "Can't write the gauge figure due to low-level IO issues";
+
     private ImageWriter<ARGB8> _writer;
 
     /**
@@ -25,18 +29,27 @@ public class TiffGaugeFigureWriter implements IGaugeFigureWriter {
     }
 
     @Override
-    public void write(File root4PProject, String visualizationSession, IGaugeFigure gaugeFigure) throws IOException {
+    public void write(File root4PProject, String visualizationSession, IGaugeFigure gaugeFigure)
+            throws GaugeFigureIOException {
         if (!(gaugeFigure instanceof GaugeFigure)) {
             throw new IllegalArgumentException(
                     "The given gauge figure is not a pixel figure, and can't be saved with this writer.");
         }
-
         this._createWriter(gaugeFigure);
 
         File pathToFigure = this._getPathToFigure(root4PProject, visualizationSession, gaugeFigure);
         this._createParentFolder(pathToFigure);
 
-        this._writer.write(pathToFigure, ((GaugeFigure) gaugeFigure).getImage());
+        try {
+            _writeGaugeFigure(pathToFigure, (GaugeFigure) gaugeFigure);
+        } catch (IOException e) {
+            throw _createGaugeFigureIOException(visualizationSession, gaugeFigure, _LOW_LEVEL_IO_MESSAGE);
+        }
+
+    }
+
+    private void _writeGaugeFigure(File pathToFigure, GaugeFigure gaugeFigure) throws IOException {
+        this._writer.write(pathToFigure, gaugeFigure.getImage());
     }
 
     private File _getPathToFigure(File root4PProject, String visualizationSession, IGaugeFigure gaugeFigure) {
@@ -65,6 +78,11 @@ public class TiffGaugeFigureWriter implements IGaugeFigureWriter {
             _writer = TiffImageWriterFactory.getWriter(factoryType, ARGB8.zero());
 
         }
+    }
+
+    private GaugeFigureIOException _createGaugeFigureIOException(String visualizationSession, IGaugeFigure gaugeFigure,
+            String message) {
+        return new GaugeFigureIOException(message, gaugeFigure, visualizationSession);
     }
 
 }
